959e93968095971916a76ffab1c2e38ef4f309c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:51 +0000

    Add package gdsfactory/6.116.0/gdsfactory-6.116.0-py3-none-any.whl

---
+++ packages/gdsfactory/gdsfactory-6.116.0-py3-none-any.whl/gdsfactory/simulation/__init__.py
@@ -0,0 +1,25 @@
+"""gdsfactory interface to simulations."""
+
+from __future__ import annotations
+
+from gdsfactory.simulation import plot
+from gdsfactory.simulation.get_effective_indices import get_effective_indices
+from gdsfactory.simulation.get_sparameters_path import (
+    get_sparameters_data_lumerical,
+    get_sparameters_data_meep,
+    get_sparameters_data_tidy3d,
+    get_sparameters_path_lumerical,
+    get_sparameters_path_meep,
+    get_sparameters_path_tidy3d,
+)
+
+__all__ = [
+    "plot",
+    "get_sparameters_path_meep",
+    "get_sparameters_path_lumerical",
+    "get_sparameters_path_tidy3d",
+    "get_sparameters_data_meep",
+    "get_sparameters_data_lumerical",
+    "get_sparameters_data_tidy3d",
+    "get_effective_indices",
+]
