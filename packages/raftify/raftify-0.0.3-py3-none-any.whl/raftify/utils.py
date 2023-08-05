202c8cdbe11b2dcea336131faa115ab947eefb62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:57 +0000

    Add package raftify/0.0.3/raftify-0.0.3-py3-none-any.whl

---
+++ packages/raftify/raftify-0.0.3-py3-none-any.whl/raftify/utils.py
@@ -0,0 +1,63 @@
+import pickle
+import threading
+from typing import Union
+
+
+class PickleSerializer:
+    def encode(self):
+        return pickle.dumps(self)
+
+    @classmethod
+    def decode(cls, data: bytes):
+        return cls(*pickle.loads(data))
+
+
+class SocketAddr:
+    def __init__(self, host: str, port: str):
+        self.host = host
+        self.port = port
+
+    def __repr__(self) -> str:
+        return f"{self.host}:{self.port}"
+
+    @staticmethod
+    def from_str(s: str):
+        return SocketAddr(*s.split(":"))
+
+
+class AtomicInteger:
+    def __init__(self, value: int = 0):
+        self.__value = value
+        self.__lock = threading.Lock()
+
+    def __repr__(self) -> str:
+        return str(self.__value)
+
+    def __hash__(self) -> int:
+        return self.__value
+
+    def __eq__(self, other: Union["AtomicInteger", int]) -> bool:
+        if isinstance(other, AtomicInteger):
+            return self.__value == other.__value
+        elif isinstance(other, int):
+            return self.__value == other
+        return False
+
+    def increase(self, value: int = 1) -> "AtomicInteger":
+        with self.__lock:
+            self.__value += value
+        return self
+
+    def decrease(self, value: int = 1) -> "AtomicInteger":
+        with self.__lock:
+            self.__value -= value
+        return self
+
+    def set(self, value: int) -> "AtomicInteger":
+        with self.__lock:
+            self.__value = value
+        return self
+
+    @property
+    def value(self) -> int:
+        return self.__value
