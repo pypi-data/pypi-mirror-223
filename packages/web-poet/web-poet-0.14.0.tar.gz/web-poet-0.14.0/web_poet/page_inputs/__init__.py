42a27a48ce4022aa32b2ad593e8643f385b10a40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:18 +0000

    Add package web-poet/0.14.0/web-poet-0.14.0.tar.gz

---
+++ packages/web-poet/web-poet-0.14.0.tar.gz/web-poet-0.14.0/web_poet/page_inputs/__init__.py
@@ -0,0 +1,12 @@
+from .browser import BrowserHtml, BrowserResponse
+from .client import HttpClient
+from .http import (
+    HttpRequest,
+    HttpRequestBody,
+    HttpRequestHeaders,
+    HttpResponse,
+    HttpResponseBody,
+    HttpResponseHeaders,
+)
+from .page_params import PageParams
+from .url import RequestUrl, ResponseUrl
