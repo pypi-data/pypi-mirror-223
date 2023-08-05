d5d30441d28f05a470f91daf8e604f5723134780
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:51 +0000

    Add package hubble-exchange/0.5.0/hubble_exchange-0.5.0.tar.gz

---
+++ packages/hubble-exchange/hubble_exchange-0.5.0.tar.gz/hubble_exchange-0.5.0/hubble_exchange/constants.py
@@ -0,0 +1,8 @@
+from eth_typing import Address
+
+OrderBookContractAddress = Address("0x0300000000000000000000000000000000000000")
+ClearingHouseContractAddress = Address("0x0300000000000000000000000000000000000002")
+
+CHAIN_ID = 321123
+MAX_GAS_LIMIT = 7_000_000  # 7 million
+GAS_PER_ORDER = 400_000  # 200k
