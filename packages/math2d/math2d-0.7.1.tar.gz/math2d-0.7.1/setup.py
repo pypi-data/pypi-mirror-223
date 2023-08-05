d8f4f00c6d6b660c21ba3730ea32672a9137f926
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:01 +0000

    Add package math2d/0.7.1/math2d-0.7.1.tar.gz

---
+++ packages/math2d/math2d-0.7.1.tar.gz/math2d-0.7.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup (
+    name='math2d',
+    version='0.7.1',
+    description='2D mathematics package for Python.',
+    author='Morten Lind',
+    author_email='morten@lind.fairuse.org',
+    url='https://codeberg.org/moli/pymath2d',
+    packages=['math2d', 'math2d.geometry'],
+    provides=['math2d'],
+    install_requires=['numpy'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering',
+    ],
+    license='GNU Lesser General Public License v3 (LGPLv3)',
+    data_files=[('share/doc/pymath2d', ['COPYING.LESSER'])]
+)
