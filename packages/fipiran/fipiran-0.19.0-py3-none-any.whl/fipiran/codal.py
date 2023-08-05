edf33fc04846d493b0812231f0bbf4503e6269b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:53 +0000

    Add package fipiran/0.19.0/fipiran-0.19.0-py3-none-any.whl

---
+++ packages/fipiran/fipiran-0.19.0-py3-none-any.whl/fipiran/codal.py
@@ -0,0 +1,21 @@
+from functools import partial as _partial
+from re import compile as _rc
+
+from . import _DataFrame, _fipiran, _read_html
+
+_parenthesis_to_negative = _partial(_rc(r'\((\d+)\)').sub, r'-\1')
+
+
+async def financial_ratios() -> _DataFrame:
+    text = await _fipiran('Codal/Ratio')
+    return _read_html(text)[0]
+
+
+async def profit_growth() -> _DataFrame:
+    text = await _fipiran('Codal/RoshdPos')
+    return _read_html(_parenthesis_to_negative(text))[0]
+
+
+async def profit_decline() -> _DataFrame:
+    text = await _fipiran('Codal/RoshdNeg')
+    return _read_html(_parenthesis_to_negative(text))[0]
