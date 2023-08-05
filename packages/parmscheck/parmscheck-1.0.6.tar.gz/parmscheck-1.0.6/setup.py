5e822395870d7430e79a37558a635a8018718170
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:55 +0000

    Add package parmscheck/1.0.6/parmscheck-1.0.6.tar.gz

---
+++ packages/parmscheck/parmscheck-1.0.6.tar.gz/parmscheck-1.0.6/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+setup(
+    name='parmscheck',
+    version='1.0.6',
+    author='Lfr',
+    author_email='1733535832@qq.com',
+    description='Decorator for type checking arguments ',
+    packages=['parmscheck'],
+    url='https://github.com/violetDelia/parmscheck',
+    install_requires=[
+    ],
+)
