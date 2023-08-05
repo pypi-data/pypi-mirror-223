4ce2ab40c288f88867ed7f1c4f62ff95737918f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:49 +0000

    Add package py-keycloak-utils/1.0.3/py-keycloak-utils-1.0.3.tar.gz

---
+++ packages/py-keycloak-utils/py-keycloak-utils-1.0.3.tar.gz/py-keycloak-utils-1.0.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+VERSION = "1.0.3"
+DESCRIPTION = "Utilities with Keycloak"
+
+# Setting up
+setup(
+    name="py-keycloak-utils",
+    version=VERSION,
+    author="KE",
+    author_email="",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        "flask>=2.0.0,<=2.2.3",
+        "python-keycloak==3.3.0"
+    ],
+    keywords=["python", "flask", "PyJWT", "decorator", "token", "keycloak"],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.11",
+        "Operating System :: Unix",
+        "Operating System :: MacOS",
+    ],
+)
