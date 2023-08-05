227c95ddfb483e696a8b84652bdd2b649a259854
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:52 +0000

    Add package boostnano/0.1/boostnano-0.1.tar.gz

---
+++ packages/boostnano/boostnano-0.1.tar.gz/boostnano-0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages
+from distutils.core import setup, Extension
+import numpy as np
+
+ext_modules = [ Extension('boostnano.hmm', sources = ['boostnano/hmm.cpp'],language='c++')]
+exec(open('boostnano/_version.py').read()) #readount the __version__ variable
+setup(
+	name = 'boostnano',
+	version = __version__,
+	include_dirs = [np.get_include()],
+	ext_modules = ext_modules,
+	packages=find_packages(),
+    package_data={
+        'boostnano': ['boostnano/model/*'],  # Include all .txt files under the 'data' directory
+    },
+    include_package_data=True,
+)
