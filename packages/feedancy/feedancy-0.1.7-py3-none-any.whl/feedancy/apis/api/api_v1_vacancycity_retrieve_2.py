ce4b7704dd6c5371708dbb8e674903032e00d7e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:03 +0000

    Add package feedancy/0.1.7/feedancy-0.1.7-py3-none-any.whl

---
+++ packages/feedancy/feedancy-0.1.7-py3-none-any.whl/feedancy/apis/api/api_v1_vacancycity_retrieve_2.py
@@ -0,0 +1,53 @@
+from __future__ import annotations
+
+import datetime
+import pydantic
+import typing
+
+from pydantic import BaseModel
+
+from feedancy.lib.base import BaseApi
+from feedancy.lib.request import ApiRequest
+from feedancy.lib import json
+class VacancyCity(BaseModel):
+    city: int 
+    id: int 
+    vacancy: int 
+
+def make_request(self: BaseApi,
+
+
+    id: str,
+
+) -> VacancyCity:
+    
+
+    
+    body = None
+    
+
+    m = ApiRequest(
+        method="GET",
+        path="/api/v1/vacancycity/{id}/".format(
+            
+                id=id,
+            
+        ),
+        content_type=None,
+        body=body,
+        headers=self._only_provided({
+        }),
+        query_params=self._only_provided({
+        }),
+        cookies=self._only_provided({
+        }),
+    )
+    return self.make_request({
+    
+        "200": {
+            
+                "application/json": VacancyCity,
+            
+        },
+    
+    }, m)
