305f7133a4f9d16f38803f413a0c13403df2824b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:51 +0000

    Add package news-toolkit-api/0.0.31/news_toolkit_api-0.0.31.tar.gz

---
+++ packages/news-toolkit-api/news_toolkit_api-0.0.31.tar.gz/news_toolkit_api-0.0.31/news_toolkit_api/api/v1/response/article.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+
+from news_toolkit_api.news_blocks import (
+    ArticleIntroductionBlock,
+    BannerAdContent,
+    TextLeadParagraphBlock,
+)
+
+ContentType = list[ArticleIntroductionBlock | TextLeadParagraphBlock | BannerAdContent]
+
+
+@dataclass(frozen=True)
+class ArticleResponse:
+    title: str
+    content: ContentType
+    url: str
+    is_premium: bool
+    is_preview: bool
+    cursor: str
+    next_cursor: str
---
+++ packages/news-toolkit-api/news_toolkit_api-0.0.31.tar.gz/news_toolkit_api-0.0.31/news_toolkit_api/api/v1/response/feed.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass
+from datetime import datetime
+
+from news_toolkit_api.news_blocks import (
+    BlockType,
+    NavigateToArticleAction,
+    SectionHeaderBlock,
+)
+
+
+@dataclass(frozen=True)
+class FeedArticleResponse:
+    id: str
+    title: str
+    category: str
+    image_url: str
+    author: str | None
+    published_at: datetime | None
+    is_premium: bool
+    type: BlockType
+    action: NavigateToArticleAction
+
+
+FeedType = list[SectionHeaderBlock | FeedArticleResponse]
+
+
+@dataclass(frozen=True)
+class FeedResponse:
+    feed: FeedType
+    cursor: str
+    next_cursor: str | None
