e26e82ae7b72d856b8ccd7ba69b7e6916d51a28f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:08 +0000

    Add package ploomber/0.22.6/ploomber-0.22.6-py3-none-any.whl

---
+++ packages/ploomber/ploomber-0.22.6-py3-none-any.whl/ploomber/cli/__init__.py
@@ -0,0 +1,25 @@
+from ploomber.cli import (
+    cloud,
+    build,
+    plot,
+    task,
+    report,
+    interact,
+    status,
+    examples,
+    install,
+    nb,
+)
+
+__all__ = [
+    "cloud",
+    "task",
+    "plot",
+    "build",
+    "report",
+    "interact",
+    "status",
+    "examples",
+    "install",
+    "nb",
+]
