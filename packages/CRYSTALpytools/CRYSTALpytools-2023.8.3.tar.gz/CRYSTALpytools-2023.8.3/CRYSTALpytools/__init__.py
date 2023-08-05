07226ddf71b994c216747706673372442c4f0108
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:23 +0000

    Add package CRYSTALpytools/2023.8.3/CRYSTALpytools-2023.8.3.tar.gz

---
+++ packages/CRYSTALpytools/CRYSTALpytools-2023.8.3.tar.gz/CRYSTALpytools-2023.8.3/CRYSTALpytools/__init__.py
@@ -0,0 +1,16 @@
+from CRYSTALpytools import adsorb
+from CRYSTALpytools import base
+from CRYSTALpytools import calculate
+from CRYSTALpytools import convert
+from CRYSTALpytools import execute
+from CRYSTALpytools import crystal_io
+from CRYSTALpytools import plot
+from CRYSTALpytools import thermodynamics
+from CRYSTALpytools import units
+from CRYSTALpytools import utils
+
+from importlib.metadata import version
+
+__author__ = "CRYSTALpytools Development Team"
+__email__ = "crystalcodetools@gmail.com"
+__version__ = version('CRYSTALpytools')
