1a03e10e91c3d78dfb011d0f7d2cb7cc7e5d9a0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:39 +0000

    Add package sqlup/0.0.2/sqlup-0.0.2.tar.gz

---
+++ packages/sqlup/sqlup-0.0.2.tar.gz/sqlup-0.0.2/sqls/sqls.py
@@ -0,0 +1,9 @@
+﻿from sqlalchemy import create_engine
+import mysql.connector
+
+def sqls(host,user,password,db,dfs,tablename):
+    db_connection_str =f'mysql+pymysql://{user}:{password}@{host}/{db}' #접속할 db설정
+    db_connection = create_engine(db_connection_str)
+    conn = db_connection.connect()
+    dfs.to_sql(name=tablename,con=db_connection, if_exists='append',index=False)
+    
