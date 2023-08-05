aba7a05f9f37b5ecb61491510b1699c88a2bf92c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:12 +0000

    Add package install-pybci/1.0.2/install_pybci-1.0.2-py3-none-any.whl

---
+++ packages/install-pybci/install_pybci-1.0.2-py3-none-any.whl/pybci/Configuration/FeatureSettings.py
@@ -0,0 +1,15 @@
+class GeneralFeatureChoices:
+    psdBand = True
+    appr_entropy = False
+    perm_entropy = False
+    spec_entropy = False
+    svd_entropy = False
+    samp_entropy = False
+    rms = True
+    meanPSD = True
+    medianPSD = True
+    variance = True
+    meanAbs = True
+    waveformLength = False
+    zeroCross = False
+    slopeSignChange = False
