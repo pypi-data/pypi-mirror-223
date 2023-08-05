501298003f11fc67fde9f6181bd4a62319c1c980
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:17 +0000

    Add package utz/0.4.1/utz-0.4.1.tar.gz

---
+++ packages/utz/utz-0.4.1.tar.gz/utz-0.4.1/setup.py
@@ -0,0 +1,38 @@
+from utz.setup import setup
+
+extras_require = {
+    'dt': [
+        'click',
+        'python-dateutil',
+        'pytz',
+    ],
+    'git': [
+        'click',
+        'GitPython',
+        'PyGithub',
+    ],
+    'pd': [
+        'numpy',
+        'pandas',
+    ],
+    'pdf': [
+        'reportlab',
+        'pyPDF2',
+    ],
+    'setup': [
+        'setuptools',
+    ],
+    'test': [
+        'pytest',
+        'pytest-mock',
+    ],
+}
+extras_require['all'] = sum(extras_require.values(), [ 'pyyaml', ])
+
+setup(
+    name="utz",
+    version="0.4.1",
+    install_requires=open('requirements.txt', 'r').read(),
+    extras_require=extras_require,
+    url="https://github.com/runsascoded/utz",
+)
