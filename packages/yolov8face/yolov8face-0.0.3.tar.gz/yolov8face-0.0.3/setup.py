c6812ee2ca607392242c827064e657b326b9937d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:34 +0000

    Add package yolov8face/0.0.3/yolov8face-0.0.3.tar.gz

---
+++ packages/yolov8face/yolov8face-0.0.3.tar.gz/yolov8face-0.0.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+import os
+
+# Read the contents of your README file
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='yolov8face',
+    version='0.0.3',
+    description='A wrapper to YOLOv8 face detector.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[
+        'ultralytics'
+    ],
+)
