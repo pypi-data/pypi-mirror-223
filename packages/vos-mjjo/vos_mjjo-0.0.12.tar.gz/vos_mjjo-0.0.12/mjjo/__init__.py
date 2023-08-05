4b5a795b3cc52328228a92f0a3ffe4e5ac59fe21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:28 +0000

    Add package vos-mjjo/0.0.12/vos_mjjo-0.0.12.tar.gz

---
+++ packages/vos-mjjo/vos_mjjo-0.0.12.tar.gz/vos_mjjo-0.0.12/mjjo/__init__.py
@@ -0,0 +1,16 @@
+version = '0.0.12' 
+author = 'mjjo'
+description = 'This library is designed to correct dates in building permit records'
+license = 'MIT'
+
+def __version__():
+    return version
+
+def __author__():
+    return author
+
+def __description__():
+    return description
+
+def __license__():
+    return license
