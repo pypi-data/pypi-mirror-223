21825fa93ea09e311280ac1e0e28772bb5e5b62b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:11 +0000

    Add package django-compat-patcher/0.12/django-compat-patcher-0.12.tar.gz

---
+++ packages/django-compat-patcher/django-compat-patcher-0.12.tar.gz/django-compat-patcher-0.12/src/django_compat_patcher/fixers/__init__.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import, print_function, unicode_literals
+
+from . import (
+    django1_6,
+    django1_7,
+    django1_8,
+    django1_9,
+    django1_10,
+    django1_11,
+    django2_0,
+    django2_1,
+    django2_2,
+    django3_0,
+    django3_1,
+    django3_2,
+    django4_0,
+    django4_1,
+)
