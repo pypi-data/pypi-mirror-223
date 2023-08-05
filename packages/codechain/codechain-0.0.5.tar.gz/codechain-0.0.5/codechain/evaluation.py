952cd6bf8c831517b926dc406a920f0b53031fe2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:20 +0000

    Add package codechain/0.0.5/codechain-0.0.5.tar.gz

---
+++ packages/codechain/codechain-0.0.5.tar.gz/codechain-0.0.5/codechain/evaluation.py
@@ -0,0 +1,28 @@
+from typing import Optional
+
+from langsmith.evaluation import RunEvaluator, EvaluationResult
+from langsmith.schemas import Run, Example
+class HumanEvalEvaluator(RunEvaluator):
+    """ This is a wrapper around the HumanEval evaluator. """
+
+    def __init__(self):
+
+        from human_eval.data import read_problems
+        
+        self.problems = read_problems()
+
+    def evaluate_run(self, run: Run, example: Optional[Example] = None) -> EvaluationResult:
+
+        from human_eval.execution import check_correctness
+
+        print("Evaluating " + run.inputs["task_id"])
+        problem = self.problems[run.inputs["task_id"]]
+        solution = run.outputs["output"]
+
+        # The HumanEval evaluator, which runs the Python code against unit tests
+        result = check_correctness(problem, solution, 5)
+
+        return EvaluationResult(
+            key = "Correctness",
+            score = bool(result["passed"])
+            )
