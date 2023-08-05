dc3459a2ea20f94d9c6fd993e739706aabe0114e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:20 +0000

    Add package CrazyDG/0.0.7/CrazyDG-0.0.7-py3-none-any.whl

---
+++ packages/CrazyDG/CrazyDG-0.0.7-py3-none-any.whl/CrazyDG/guidance/utils/_goto.py
@@ -0,0 +1,56 @@
+from ...crazy import CrazyDragon
+
+from .smoother import smooth_command
+
+from .constants import Kp, Kd, g
+
+from numpy import array, zeros
+
+from time import sleep
+
+
+
+def goto( cf: CrazyDragon, destination, T, dt=0.1 ):
+
+    cur     = zeros(3)
+    des     = zeros(3)
+    des_cmd = zeros(3)
+    acc_cmd = zeros(3)
+    P_pos   = zeros(3)
+    D_pos   = zeros(3)
+    care_g  = array([0,0,g])
+
+    print( 'goto =>', destination )
+
+    n = int( T / dt )
+    t = 0
+
+    command = cf.command
+
+    cur[:] = cf.pos
+    pos    = cf.pos
+    vel    = cf.vel
+
+    des[:] = destination
+
+    cf.destination[:] = des
+
+    for _ in range( n ):
+
+        des_cmd[:] = smooth_command( 
+            des, cur, t, int( T / 2 )
+        )
+
+        P_pos[:] = des_cmd - pos
+        D_pos[:] = vel
+
+        acc_cmd[:] = 0
+        acc_cmd[:] += P_pos * Kp
+        acc_cmd[:] -= D_pos * Kd
+        acc_cmd[:] += care_g
+
+        command[:] = acc_cmd
+
+        t += dt
+
+        sleep( dt )
---
+++ packages/CrazyDG/CrazyDG-0.0.7-py3-none-any.whl/CrazyDG/navigation/__init__.py
@@ -0,0 +1,56 @@
+from ..crazy import CrazyDragon
+
+from threading import Thread
+
+from .imu       import IMU
+from .qualisys  import Qualisys
+
+from time import sleep
+
+
+
+class Navigation( Thread ):
+
+    def __init__( self, cf: CrazyDragon, config ):
+
+        super().__init__()
+
+        self.daemon = True
+
+        self.cf = cf
+
+        self.imu = IMU( cf )
+        self.qtm = Qualisys( config['body_name'] )
+
+        self.navigate = True
+
+
+    @classmethod
+    def _on_pose( cls, cf: CrazyDragon, data: list ):
+        
+        cf.pos[:] = data[0:3]
+        cf.att[:] = data[3:6]
+
+        cf.extpos.send_extpos( data[0], data[1], data[2] )
+
+    def run( self ):
+
+        cf = self.cf
+
+        imu = self.imu
+        qtm = self.qtm
+
+        imu.start_get_acc()
+        imu.start_get_vel()
+
+        qtm.on_pose = lambda pose: __class__._on_pose( cf, pose )
+
+        while self.navigate:
+
+            sleep( 0.1 )
+
+    def join( self ):
+
+        self.navigate = False
+
+        super().join()
