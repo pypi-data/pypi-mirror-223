340d12b4e2614ea00d732fbf973ca2a221ca941f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:43 +0000

    Add package Nabilkz/1.0/Nabilkz-1.0.tar.gz

---
+++ packages/Nabilkz/Nabilkz-1.0.tar.gz/Nabilkz-1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Nabilkz',
+    version='1.0',
+    description='An AI package',
+    author='Nabil Kzez',
+    author_email='nn2510220@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'customtkinter',
+        'requests',
+    ],
+)
