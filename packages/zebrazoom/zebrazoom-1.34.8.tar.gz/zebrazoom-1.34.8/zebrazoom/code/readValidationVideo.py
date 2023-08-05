9b8735b7c77c5fede6f0bb8cee7c2560bfe34922
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:29 +0000

    Add package zebrazoom/1.34.8/zebrazoom-1.34.8.tar.gz

---
+++ packages/zebrazoom/zebrazoom-1.34.8.tar.gz/zebrazoom-1.34.8/zebrazoom/code/readValidationVideo.py
@@ -0,0 +1,323 @@
+import collections
+import math
+import os
+import random
+import sys
+
+import cv2
+import json
+import numpy as np
+
+from PyQt5.QtCore import Qt, QSize, QTimer
+from PyQt5.QtWidgets import QApplication, QButtonGroup, QCheckBox, QFileDialog, QHBoxLayout, QMessageBox, QLabel, QRadioButton, QSlider, QSpinBox, QStyleOptionSlider, QVBoxLayout
+
+import zebrazoom.code.paths as paths
+import zebrazoom.code.util as util
+import zebrazoom.videoFormatConversion.zzVideoReading as zzVideoReading
+from zebrazoom.code.createValidationVideo import calculateInfoFrameForFrame, drawInfoFrame, improveContrast
+from zebrazoom.code.getHyperparameters import getHyperparametersSimple
+from zebrazoom.code.preprocessImage import preprocessImage
+
+
+def getFramesCallback(videoPath, folderName, numWell, numAnimal, zoom, start, framesToShow=0, ZZoutputLocation='', supstruct=None, config=None):
+  s1  = "ZZoutput"
+  s2  = folderName
+  s3b = "results_"
+  s4  = folderName
+  s5  = ".avi"
+  s5b = ".txt"
+
+  if len(ZZoutputLocation):
+    initialPath = ZZoutputLocation
+  else:
+    initialPath = paths.getDefaultZZoutputFolder()
+
+  if config is None:
+    with open(os.path.join(initialPath, os.path.join(s2, 'configUsed.json'))) as f:
+      config = json.load(f)
+  hyperparameters = getHyperparametersSimple(config)
+
+  if os.path.splitext(folderName)[1] != '.h5':
+    folderPath = os.path.join(initialPath, folderName)
+    resultFile = next((f for f in os.listdir(folderPath) if os.path.isfile(os.path.join(folderPath, f)) and os.path.splitext(f)[1] == '.txt' and f.startswith('results_')), None)
+    if resultFile is None:
+      return
+    resultsPath = os.path.join(initialPath, folderName, resultFile)
+    if supstruct is None:
+      with open(resultsPath) as f:
+        supstruct = json.load(f)
+  else:
+    resultsPath = os.path.join(initialPath, folderName)
+  
+  if "pathToOriginalVideo" in supstruct:
+    videoPath = supstruct["pathToOriginalVideo"]
+    if not os.path.exists(videoPath):
+      app = QApplication.instance()
+      if QMessageBox.critical(app.window, "Video not found", "Cannot display the validation video because the video used to run the tracking can no longer be found. Would you like to update the video path stored in the results file?",
+                              buttons=QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No, defaultButton=QMessageBox.StandardButton.Yes) != QMessageBox.StandardButton.Yes:
+        return None
+      videoName, _ = QFileDialog.getOpenFileName(app.window, 'Select video', os.path.expanduser("~"))
+      if not videoName:
+        return
+      videoPath = supstruct["pathToOriginalVideo"] = videoName
+      if os.path.splitext(resultsPath)[1] != '.h5':
+        with open(resultsPath, 'w') as f:
+          json.dump(supstruct, f)
+      else:
+        import h5py
+        with h5py.File(resultsPath, 'a') as results:
+          results.attrs['pathToOriginalVideo'] = videoName
+  else:
+    if hyperparameters["copyOriginalVideoToOutputFolderForValidation"] and os.path.exists(os.path.join(initialPath, os.path.join(s1, os.path.join(s2, 'originalVideoWithoutAnyTrackingDisplayed_pleaseUseTheGUIToVisualizeTrackingPoints.avi')))):
+      # The "exist" check above is only to insure compatibility with videos tracked prior to this update
+      if os.path.splitext(resultsPath)[1] != '.h5':
+        videoPath = os.path.join(initialPath, os.path.join(s2, 'originalVideoWithoutAnyTrackingDisplayed_pleaseUseTheGUIToVisualizeTrackingPoints.avi'))
+      else:
+        videoPath = f'{os.path.splitext(resultsPath)[0]}_originalVideoWithoutAnyTrackingDisplayed_pleaseUseTheGUIToVisualizeTrackingPoints.avi'
+    else:
+      if os.path.splitext(resultsPath)[1] != '.h5':
+        videoPath = os.path.join(initialPath, os.path.join(s2, s4 + s5))
+      else:
+        videoPath = f'{os.path.splitext(resultsPath)[0]}.avi'
+
+  if not(os.path.exists(videoPath)) and os.path.splitext(resultsPath)[1] != '.h5':
+    mypath = os.path.join(initialPath, s2)
+    onlyfiles = [f for f in os.listdir(mypath) if os.path.isfile(os.path.join(mypath, f))]
+    resultFile = ''
+    for fileName in onlyfiles:
+      if '.avi' in fileName:
+        resultFile = fileName
+    videoPath = os.path.join(initialPath, os.path.join(s2, resultFile))
+
+  if not os.path.isfile(videoPath):
+    app = QApplication.instance()
+    QMessageBox.critical(app.window, "Video not found", "Cannot display the validation video because it could not be found.")
+    return
+
+  cap = zzVideoReading.VideoCapture(videoPath)
+
+  nx    = int(cap.get(3))
+  ny    = int(cap.get(4))
+  max_l = int(cap.get(7))
+  if max_l == 1:
+    return None
+  frameRange = (supstruct["firstFrame"], supstruct["lastFrame"] - 1) if hyperparameters["copyOriginalVideoToOutputFolderForValidation"] or "pathToOriginalVideo" in supstruct else (0, max_l -1)
+
+  if not("firstFrame" in supstruct):
+    supstruct["firstFrame"] = 1
+    print("supstruct['firstFrame'] not found")
+
+  infoWells = []
+
+  HeadX = np.zeros(max_l + supstruct["firstFrame"])
+  HeadY = np.zeros(max_l + supstruct["firstFrame"])
+
+  if ((numWell != -1) and (zoom)):
+    lastEnd = 0
+    lastXpos = supstruct["wellPoissMouv"][numWell][numAnimal][0]["HeadX"][0]
+    lastYpos = supstruct["wellPoissMouv"][numWell][numAnimal][0]["HeadY"][0]
+    for k in range(0,len(supstruct["wellPoissMouv"][numWell][numAnimal])):
+      beg = supstruct["wellPoissMouv"][numWell][numAnimal][k]["BoutStart"]
+      end = supstruct["wellPoissMouv"][numWell][numAnimal][k]["BoutEnd"]
+      for l in range(lastEnd, beg):
+        HeadX[l] = lastXpos
+        HeadY[l] = lastYpos
+      for l in range(beg, end):
+        HeadX[l]  = supstruct["wellPoissMouv"][numWell][numAnimal][k]["HeadX"][l-beg]
+        HeadY[l]  = supstruct["wellPoissMouv"][numWell][numAnimal][k]["HeadY"][l-beg]
+      lastEnd = end
+      lastXpos = supstruct["wellPoissMouv"][numWell][numAnimal][k]["HeadX"][end-1-beg]
+      lastYpos = supstruct["wellPoissMouv"][numWell][numAnimal][k]["HeadY"][end-1-beg]
+
+    for l in range(lastEnd, max_l + supstruct["firstFrame"]):
+      HeadX[l] = lastXpos
+      HeadY[l] = lastYpos
+
+  # /* Getting the info about well positions */
+  analyzeAllWellsAtTheSameTime = 0
+  if (analyzeAllWellsAtTheSameTime == 0):
+    for i in range(0, len(supstruct["wellPositions"])):
+      x = 0
+      y = 0
+      lengthX = 0
+      lengthY = 0
+      rectangleWellArea = 1
+      if (rectangleWellArea == 0): # circular wells
+        x = supstruct["wellPositions"][i]["topLeftX"]
+        y = supstruct["wellPositions"][i]["topLeftY"]
+        r = supstruct["wellPositions"][i]["diameter"]
+        lengthX = 300 # wellOutputVideoDiameter;
+        lengthY = 300 # wellOutputVideoDiameter;
+      else:
+        x = supstruct["wellPositions"][i]["topLeftX"]
+        y = supstruct["wellPositions"][i]["topLeftY"]
+        lengthX = supstruct["wellPositions"][i]["lengthX"]
+        lengthY = supstruct["wellPositions"][i]["lengthY"]
+      if (x < 0):
+        x = 0
+      if (y < 0):
+        y = 0
+      infoWells.append([x, y, lengthX, lengthY])
+  else:
+    infoWells.append([0, 0, nx, ny])
+  
+  boutMap = None
+  if hyperparameters["copyOriginalVideoToOutputFolderForValidation"] or "pathToOriginalVideo" in supstruct:
+    boutMap = collections.defaultdict(list)
+    for wellIdx, well in enumerate(supstruct["wellPoissMouv"]):
+      for animalIdx, animal in enumerate(well):
+        for boutIdx, bout in enumerate(animal):
+          for frame in range(bout['BoutStart'], bout['BoutEnd'] + 1):
+            boutMap[frame].append((wellIdx, animalIdx, boutIdx))
+    colorModifTab = [{"red": random.randrange(255), "green": random.randrange(255), "blue": random.randrange(255)} for i in range(1, hyperparameters["nbAnimalsPerWell"])]
+    colorModifTab.insert(0, {"red": 0, "green": 0, "blue": 0})
+
+  x = 0
+  y = 0
+  lengthX = 0
+  lengthY = 0
+  if (numWell != -1):
+    x = infoWells[numWell][0]
+    y = infoWells[numWell][1]
+    lengthX = infoWells[numWell][2]
+    lengthY = infoWells[numWell][3]
+  else:
+    lengthX = nx
+    lengthY = ny
+
+  l = start - supstruct["firstFrame"] + 1 if start > 0 else 0
+  if hyperparameters["copyOriginalVideoToOutputFolderForValidation"] or "pathToOriginalVideo" in supstruct:
+    l += supstruct["firstFrame"]
+
+  xOriginal = x
+  yOriginal = y
+
+  def getFrame(frameSlider, timer=None, trackingPointsGroup=None, stopTimer=True):
+    nonlocal x
+    nonlocal y
+    nonlocal lengthX
+    nonlocal lengthY
+
+    l = frameSlider.value()
+    if timer is not None and timer.isActive() and (l == frameSlider.maximum() or stopTimer):
+      timer.stop()
+
+    cap.set(1, l)
+    ret, img = cap.read()
+
+    if hyperparameters["imagePreProcessMethod"]:
+      img = preprocessImage(img, hyperparameters)
+    if hyperparameters["outputValidationVideoContrastImprovement"]:
+      img = improveContrast(img, hyperparameters)
+
+    if boutMap is not None and l in boutMap and trackingPointsGroup is not None and trackingPointsGroup.checkedId():
+      hyperparameters["plotOnlyOneTailPointForVisu"] = trackingPointsGroup.checkedId() == 1
+      infoFrame = [info for args in boutMap[l] for info in calculateInfoFrameForFrame(supstruct, hyperparameters, *args, l, colorModifTab)]
+      drawInfoFrame(img, infoFrame, colorModifTab, hyperparameters)
+
+    if numWell != -1 and zoom:
+      length = 250
+      xmin = int(HeadX[l + supstruct["firstFrame"] - 1] - length/2)
+      xmax = int(HeadX[l + supstruct["firstFrame"] - 1] + length/2)
+      ymin = int(HeadY[l + supstruct["firstFrame"] - 1] - length/2)
+      ymax = int(HeadY[l + supstruct["firstFrame"] - 1] + length/2)
+
+      x = max(xmin + xOriginal, 0)
+      y = max(ymin + yOriginal, 0)
+      lengthX = xmax - xmin
+      lengthY = ymax - ymin
+
+      if y + lengthY >= len(img):
+        lengthY = len(img) - y - 1
+      if x + lengthX >= len(img[0]):
+        lengthX = len(img[0]) - x - 1
+
+    if (numWell != -1):
+      img = img[y:y+lengthY, x:x+lengthX]
+
+    frameNumber = l if hyperparameters["copyOriginalVideoToOutputFolderForValidation"] or "pathToOriginalVideo" in supstruct else (l + supstruct["firstFrame"])
+    if lengthX > 100 and lengthY > 100:
+      font = cv2.FONT_HERSHEY_SIMPLEX
+      cv2.putText(img,str(frameNumber),(int(lengthX-110), int(lengthY-30)),font,1,(0,255,0))
+    else:
+      blank_image = np.zeros((len(img)+30, len(img[0]), 3), np.uint8)
+      blank_image[0:len(img), 0:len(img[0])] = img
+      img = blank_image
+      font = cv2.FONT_HERSHEY_SIMPLEX
+      cv2.putText(img, str(frameNumber), (int(0), int(lengthY+25)), font, 1, (0,255,0))
+
+    return img
+
+  wellShape = None if config.get("noWellDetection", False) or (hyperparameters["headEmbeded"] and not hyperparameters["oneWellManuallyChosenTopLeft"]) else 'rectangle' if config.get("wellsAreRectangles", False) or len(config.get("oneWellManuallyChosenTopLeft", '')) or int(config.get("multipleROIsDefinedDuringExecution", 0)) or config.get("groupOfMultipleSameSizeAndShapeEquallySpacedWells", False) else 'circle'
+  return getFrame, frameRange, l, boutMap is not None, supstruct['wellPositions'], wellShape, hyperparameters
+
+
+def readValidationVideo(videoPath, folderName, numWell, numAnimal, zoom, start, framesToShow=0, ZZoutputLocation='', supstruct=None, config=None):
+  frameInfo = getFramesCallback(videoPath, folderName, numWell, numAnimal, zoom, start, framesToShow=framesToShow, ZZoutputLocation=ZZoutputLocation, supstruct=supstruct, config=config)
+  if frameInfo is None:
+    return
+  getFrame, frameRange, frame, toggleTrackingPoints, _, _, hyperparameters = frameInfo
+  layout = QVBoxLayout()
+
+  video = QLabel()
+  layout.addWidget(video, stretch=1)
+
+  frameSlider = QSlider(Qt.Orientation.Horizontal)
+  frameSlider.setFocusPolicy(Qt.FocusPolicy.StrongFocus)
+  frameSlider.setPageStep(50)
+  frameSlider.setRange(*frameRange)
+  frameSlider.setValue(frame)
+  frameSlider.valueChanged.connect(lambda: util.setPixmapFromCv(getFrame(frameSlider, timer, btnGroup, stopTimer), video))
+  layout.addWidget(frameSlider)
+  shortcutsLabel = QLabel("Left Arrow, Right Arrow, Page Up, Page Down, Home and End keys can be used to navigate through the video.")
+  shortcutsLabel.setWordWrap(True)
+  layout.addWidget(shortcutsLabel)
+  btnGroup = None
+  if toggleTrackingPoints:
+    trackingPointsLayout = QHBoxLayout()
+    plotTrackingPointsLabel = QLabel("Tracking points:")
+    trackingPointsLayout.addWidget(plotTrackingPointsLabel)
+    btnGroup = QButtonGroup()
+    allPointsRadioButton = QRadioButton('All')
+    allPointsRadioButton.setChecked(True)
+    trackingPointsLayout.addWidget(allPointsRadioButton)
+    btnGroup.addButton(allPointsRadioButton, id=2)
+    minimalPointsRadioButton = QRadioButton('Minimal')
+    minimalPointsRadioButton.setChecked(bool(hyperparameters['plotOnlyOneTailPointForVisu']))
+    trackingPointsLayout.addWidget(minimalPointsRadioButton)
+    btnGroup.addButton(minimalPointsRadioButton, id=1)
+    noPointsRadioButton = QRadioButton('None')
+    trackingPointsLayout.addWidget(noPointsRadioButton)
+    btnGroup.addButton(noPointsRadioButton, id=0)
+    btnGroup.idToggled.connect(lambda: timer.isActive() or util.setPixmapFromCv(getFrame(frameSlider, timer, btnGroup, stopTimer), video))
+    trackingPointsLayout.addWidget(QLabel('Size:'))
+    sizeSpinbox = QSpinBox()
+    sizeSpinbox.setMinimum(1)
+    sizeSpinbox.setValue(hyperparameters["trackingPointSizeDisplay"])
+    sizeSpinbox.valueChanged.connect(lambda value: hyperparameters.update({"trackingPointSizeDisplay": value}))
+    sizeSpinbox.valueChanged.connect(lambda: timer.isActive() or util.setPixmapFromCv(getFrame(frameSlider, timer, btnGroup, stopTimer), video))
+    trackingPointsLayout.addWidget(sizeSpinbox)
+    contrastCheckbox = QCheckBox('Contrast')
+    contrastCheckbox.setChecked(bool(hyperparameters['outputValidationVideoContrastImprovement']))
+    contrastCheckbox.toggled.connect(lambda checked: hyperparameters.update({"outputValidationVideoContrastImprovement": int(checked)}))
+    contrastCheckbox.toggled.connect(lambda: timer.isActive() or util.setPixmapFromCv(getFrame(frameSlider, timer, btnGroup, stopTimer), video))
+    trackingPointsLayout.addWidget(contrastCheckbox)
+    trackingPointsLayout.addStretch(1)
+    layout.addLayout(trackingPointsLayout)
+
+  stopTimer = True
+  timer = QTimer()
+  timer.setInterval(1)
+
+  def nextFrame():
+    nonlocal stopTimer
+    stopTimer = False
+    frameSlider.setValue(frameSlider.value() + 1)
+    stopTimer = True
+  timer.timeout.connect(nextFrame)
+
+  startFrame = getFrame(frameSlider, timer, btnGroup, stopTimer)
+  timer.start()
+  util.showDialog(layout, title="Video", labelInfo=(startFrame, video))
+  timer.stop()
+  del getFrame
---
+++ packages/zebrazoom/zebrazoom-1.34.8.tar.gz/zebrazoom-1.34.8/zebrazoom/code/tracking/customTrackingImplementations/fastFishTracking/backgroundSubtractionOnlyOnROIs.py
@@ -0,0 +1,104 @@
+from zebrazoom.code.tracking.customTrackingImplementations.fastFishTracking.detectMovementWithRawVideoInsideTracking import detectMovementWithRawVideoInsideTracking
+from zebrazoom.code.tracking.customTrackingImplementations.fastFishTracking.trackTail import trackTail
+# import zebrazoom.code.tracking
+import numpy as np
+import time
+import math
+import cv2
+
+def backgroundSubtractionOnlyOnROIs(self, frame, k):
+
+  # Color to grey scale transformation
+  frame = frame[:,:,0]
+  
+  # Bout detection
+  if self._hyperparameters["detectMovementWithRawVideoInsideTracking"]:
+    detectMovementWithRawVideoInsideTracking(self, k, frame)
+  
+  # Going through each well/arena/tank and applying tracking method on it
+  t1 = time.time()
+  for wellNumber in self._listOfWellsOnWhichToRunTheTracking:
+    
+    if self._hyperparameters["detectMovementWithRawVideoInsideTracking"] == 0 or k <= 2 or np.sum([self._auDessusPerAnimalIdList[wellNumber][i][k] for i in range(0, self._hyperparameters["nbAnimalsPerWell"])]):
+      
+      for animalId in range(0, self._hyperparameters["nbAnimalsPerWell"]):
+        
+        if self._hyperparameters["detectMovementWithRawVideoInsideTracking"] == 0 or self._auDessusPerAnimalIdList[wellNumber][animalId][k] or k <= 2:
+          
+          # Retrieving ROI coordinates and selecting ROI
+          roiXStart = self._wellPositions[wellNumber]['topLeftX'] + int(self._trackingDataPerWell[wellNumber][animalId][k-1][0][0] - self._hyperparameters["backgroundSubtractionOnROIhalfDiameter"])
+          roiYStart = self._wellPositions[wellNumber]['topLeftY'] + int(self._trackingDataPerWell[wellNumber][animalId][k-1][0][1] - self._hyperparameters["backgroundSubtractionOnROIhalfDiameter"])
+          roiXEnd   = roiXStart + 2 * self._hyperparameters["backgroundSubtractionOnROIhalfDiameter"]
+          roiYEnd   = roiYStart + 2 * self._hyperparameters["backgroundSubtractionOnROIhalfDiameter"]
+          if roiXStart < 0:
+            roiXStart = 0
+            roiXEnd   = 2 * self._hyperparameters["backgroundSubtractionOnROIhalfDiameter"]
+          if roiYStart < 0:
+            roiYStart = 0
+            roiYEnd   = 2 * self._hyperparameters["backgroundSubtractionOnROIhalfDiameter"]
+          if roiXEnd >= len(frame[0]):
+            roiXEnd   = len(frame[0]) - 1
+            roiXStart = len(frame[0]) - 2 * self._hyperparameters["backgroundSubtractionOnROIhalfDiameter"]
+          if roiYEnd >= len(frame):
+            roiYEnd   = len(frame) - 1
+            roiYStart = len(frame) - 2 * self._hyperparameters["backgroundSubtractionOnROIhalfDiameter"]
+          frameROI = frame[roiYStart:roiYEnd, roiXStart:roiXEnd].copy()
+          
+          # Subtracting background of image
+          backgroundROI = self._background[roiYStart:roiYEnd, roiXStart:roiXEnd]
+          frameROI = 255 - np.where(backgroundROI >= frameROI, backgroundROI - frameROI, 0).astype(np.uint8)
+
+          # Applying gaussian filter
+          paramGaussianBlur = self._hyperparameters["paramGaussianBlur"]
+          frameROI = cv2.GaussianBlur(frameROI, (paramGaussianBlur, paramGaussianBlur), 0)
+          
+          # Head position tracking
+          (minVal, maxVal, headPosition, maxLoc) = cv2.minMaxLoc(frameROI)
+          if minVal >= self._hyperparameters["minimumHeadPixelValue"]:
+            self._trackingDataPerWell[wellNumber][animalId][k] = self._trackingDataPerWell[wellNumber][animalId][k-1]
+          else:
+            if self._hyperparameters["trackTail"]:
+              # Tail tracking
+              a, self._lastFirstTheta[wellNumber] = trackTail(self, frameROI, headPosition, self._hyperparameters, wellNumber, k, self._lastFirstTheta[wellNumber])
+              a[0, :, 0] += roiXStart - self._wellPositions[wellNumber]['topLeftX']
+              a[0, :, 1] += roiYStart - self._wellPositions[wellNumber]['topLeftY']
+              self._trackingDataPerWell[wellNumber][animalId][k][:len(a[0])] = a
+            else:
+              self._trackingDataPerWell[wellNumber][animalId][k][0][0] = headPosition[0] + roiXStart - self._wellPositions[wellNumber]['topLeftX']
+              self._trackingDataPerWell[wellNumber][animalId][k][0][1] = headPosition[1] + roiYStart - self._wellPositions[wellNumber]['topLeftY']
+        else:
+          if k > 0:
+            self._trackingDataPerWell[wellNumber][animalId][k] = self._trackingDataPerWell[wellNumber][animalId][k-1]
+        
+        # 'Removing' animal just tracked
+        if self._hyperparameters["nbAnimalsPerWell"] > 1:
+          if "largerPixelRemoval" in self._hyperparameters and self._hyperparameters["largerPixelRemoval"]:
+            frame = cv2.circle(frame.copy(), (int(self._trackingDataPerWell[wellNumber][animalId][k][0][0]), int(self._trackingDataPerWell[wellNumber][animalId][k][0][1])), int(self._hyperparameters["maxDepth"]/3), (255, 255, 255), -1)
+            for pointOnTail in range(1, len(self._trackingDataPerWell[wellNumber][animalId][k])):
+              start_point = (int(self._trackingDataPerWell[wellNumber][animalId][k][pointOnTail-1][0]), int(self._trackingDataPerWell[wellNumber][animalId][k][pointOnTail-1][1]))
+              end_point   = (int(self._trackingDataPerWell[wellNumber][animalId][k][pointOnTail][0]), int(self._trackingDataPerWell[wellNumber][animalId][k][pointOnTail][1]))
+              if pointOnTail == 1:
+                end_point   = (int(3 * self._trackingDataPerWell[wellNumber][animalId][k][pointOnTail-1][0] - 2 * self._trackingDataPerWell[wellNumber][animalId][k][pointOnTail][0]), int(3 * self._trackingDataPerWell[wellNumber][animalId][k][pointOnTail-1][1] - 2 * self._trackingDataPerWell[wellNumber][animalId][k][pointOnTail][1]))
+              if pointOnTail == len(self._trackingDataPerWell[wellNumber][animalId][k]) - 1:
+                end_point   = (int(2 * self._trackingDataPerWell[wellNumber][animalId][k][pointOnTail][0] - self._trackingDataPerWell[wellNumber][animalId][k][pointOnTail-1][0]), int(2 * self._trackingDataPerWell[wellNumber][animalId][k][pointOnTail][1] - self._trackingDataPerWell[wellNumber][animalId][k][pointOnTail-1][1]))
+              cv2.line(frame, start_point, end_point, (255, 255, 255), int(self._hyperparameters["maxDepth"]/3))
+          else:
+            frame = cv2.circle(frame.copy(), (int(self._wellPositions[wellNumber]['topLeftX'] + self._trackingDataPerWell[wellNumber][animalId][k][0][0]), int(self._wellPositions[wellNumber]['topLeftY'] + self._trackingDataPerWell[wellNumber][animalId][k][0][1])), int(self._hyperparameters["maxDepth"]/4), (255, 255, 255), -1)
+            for pointOnTail in range(1, len(self._trackingDataPerWell[wellNumber][animalId][k])):
+              start_point = (int(self._wellPositions[wellNumber]['topLeftX'] + self._trackingDataPerWell[wellNumber][animalId][k][pointOnTail-1][0]), int(self._wellPositions[wellNumber]['topLeftY'] + self._trackingDataPerWell[wellNumber][animalId][k][pointOnTail-1][1]))
+              end_point   = (int(self._wellPositions[wellNumber]['topLeftX'] + self._trackingDataPerWell[wellNumber][animalId][k][pointOnTail][0]), int(self._wellPositions[wellNumber]['topLeftY'] + self._trackingDataPerWell[wellNumber][animalId][k][pointOnTail][1]))
+              cv2.line(frame, start_point, end_point, (255, 255, 255), int(self._hyperparameters["maxDepth"]/5))
+        
+      # Id invertion if necessary
+      if self._hyperparameters["nbAnimalsPerWell"] > 1:
+        # NEED TO IMPROVE THIS IN THE FUTURE!!!
+        for animalId in range(0, self._hyperparameters["nbAnimalsPerWell"]-1):
+          dist_animalIdCurFrame_animal0PrevFrame = math.sqrt((self._trackingDataPerWell[wellNumber][animalId][k][0][0] - self._trackingDataPerWell[wellNumber][0][k-1][0][0])**2     +     (self._trackingDataPerWell[wellNumber][animalId][k][0][1] - self._trackingDataPerWell[wellNumber][0][k-1][0][1])**2)
+          dist_animalIdCurFrame_animal1PrevFrame = math.sqrt((self._trackingDataPerWell[wellNumber][animalId][k][0][0] - self._trackingDataPerWell[wellNumber][1][k-1][0][0])**2     +     (self._trackingDataPerWell[wellNumber][animalId][k][0][1] - self._trackingDataPerWell[wellNumber][1][k-1][0][1])**2)
+          if dist_animalIdCurFrame_animal1PrevFrame < dist_animalIdCurFrame_animal0PrevFrame:
+            temp = self._trackingDataPerWell[wellNumber][0][k].copy()
+            self._trackingDataPerWell[wellNumber][0][k] = self._trackingDataPerWell[wellNumber][1][k]
+            self._trackingDataPerWell[wellNumber][1][k] = temp
+    else:  
+      for animalId in range(0, self._hyperparameters["nbAnimalsPerWell"]):
+        self._trackingDataPerWell[wellNumber][animalId][k] = self._trackingDataPerWell[wellNumber][animalId][k-1]
