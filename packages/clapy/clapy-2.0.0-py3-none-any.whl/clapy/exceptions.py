02ecd93b935d66d3fc9067d9446ac2b8009422ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:27 +0000

    Add package clapy/2.0.0/clapy-2.0.0-py3-none-any.whl

---
+++ packages/clapy/clapy-2.0.0-py3-none-any.whl/clapy/exceptions.py
@@ -0,0 +1,8 @@
+class DuplicateServiceError(Exception):
+    def __init__(self, message: str):
+        super().__init__(message)
+
+
+class PipeConfigurationError(Exception):
+    def __init__(self, message: str):
+        super().__init__(message)
