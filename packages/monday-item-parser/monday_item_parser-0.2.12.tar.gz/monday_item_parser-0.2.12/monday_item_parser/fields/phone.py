268a0cc68470fc4c95cc51598bfac4ec6682989d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:27 +0000

    Add package monday-item-parser/0.2.12/monday_item_parser-0.2.12.tar.gz

---
+++ packages/monday-item-parser/monday_item_parser-0.2.12.tar.gz/monday_item_parser-0.2.12/monday_item_parser/fields/phone.py
@@ -0,0 +1,60 @@
+from dataclasses import dataclass
+from typing import Dict
+
+from .field import Field
+from .helpers import get_available_countries
+
+
+@dataclass
+class Phone:
+    phone: str = None
+
+    def __init__(self, **kwargs):
+        self.phone = kwargs.pop("phone", None)
+        self.country_code = kwargs.pop("country_code", None)
+
+    @property
+    def country_code(self) -> str:
+        return self._country_code
+
+    @country_code.setter
+    def country_code(self, country_code: str):
+        if not country_code:
+            self._country_code = None
+            return
+
+        countries = get_available_countries()
+        if country_code not in countries:
+            raise ValueError(
+                "Invalid country id, choose from ({})".format(", ".join(countries))
+            )
+
+        self._country_code = country_code
+
+    def __str__(self) -> str:
+        country_code = "({}) ".format(self.country_code) if self.country_code else ""
+        return "{}{}".format(country_code, self.phone)
+
+
+class PhoneField(Field):
+    __monday_field_type__ = "phone"
+
+    def __init__(self, *args, **kwargs):
+        self.value: Phone = Phone(*args, **kwargs)
+
+    def to_monday_dict(self):
+        return (
+            {
+                "phone": self.value.phone,
+                "countryShortName": self.value.country_code,
+            }
+            if self.value
+            else {}
+        )
+
+    def from_monday_dict(self, data: Dict[str, str]):
+        self.value.phone = data["phone"] if data else None
+        self.value.country_code = data["countryShortName"] if data else None
+
+    def search_representation(self) -> str:
+        return str(self.value.phone)
