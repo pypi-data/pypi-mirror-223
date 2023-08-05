4094d53aadb0a3a6110fcc77dcf2d1fcfbcdb08c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:06 +0000

    Add package UIron/0.0.15/UIron-0.0.15.tar.gz

---
+++ packages/UIron/UIron-0.0.15.tar.gz/UIron-0.0.15/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+VERSION = "0.0.15"
+DESCRIPTION = "Package for User Interface"
+LONG_DESCRIPTION = "Package for User Interface"
+
+setup(
+    name="UIron",
+    version=VERSION,
+    author="Armando Chaparro",
+    author_email="pylejandria@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['ttkbootstrap'],
+    keywords=['python'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    include_package_data=True,
+    package_data={'': ['data/*']}
+)
