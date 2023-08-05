6370e23cdb5d4892ab126314c1ee98c8e4520d77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:47 +0000

    Add package alertmanagermeshtastic/2023.8.3.1/alertmanagermeshtastic-2023.8.3.1-py3-none-any.whl

---
+++ packages/alertmanagermeshtastic/alertmanagermeshtastic-2023.8.3.1-py3-none-any.whl/alertmanagermeshtastic/signals.py
@@ -0,0 +1,15 @@
+"""
+alertmanagermeshtastic.signals
+~~~~~~~~~~~~~~~~~~~
+
+Signals
+
+:Copyright: 2007-2022 Jochen Kupperschmidt
+:Copyright: 2023 Alexander Volz
+:License: MIT, see LICENSE for details.
+"""
+
+from blinker import Signal
+
+
+message_received = Signal()
