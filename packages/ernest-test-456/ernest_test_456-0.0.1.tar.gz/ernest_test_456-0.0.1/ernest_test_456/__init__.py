036cabc645e5826a364b82f0d046b2e36655fa82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:58 +0000

    Add package ernest-test-456/0.0.1/ernest_test_456-0.0.1.tar.gz

---
+++ packages/ernest-test-456/ernest_test_456-0.0.1.tar.gz/ernest_test_456-0.0.1/ernest_test_456/__init__.py
@@ -0,0 +1,2 @@
+# flake8: noqa F401
+from ernest_test_456 import print_hello
---
+++ packages/ernest-test-456/ernest_test_456-0.0.1.tar.gz/ernest_test_456-0.0.1/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+
+setuptools.setup(
+    name="ernest_test_456",
+    version="0.0.1",
+    author="Ernest",
+    description="A small example package",
+    packages=["ernest_test_456"],
+)
