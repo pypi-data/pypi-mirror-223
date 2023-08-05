72b0bab4a82b54f625e04c97a35ce41fc6a0e815
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:06 +0000

    Add package ytmonster/2.95/ytmonster-2.95.tar.gz

---
+++ packages/ytmonster/ytmonster-2.95.tar.gz/ytmonster-2.95/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ytmonster',
+    version='2.95',
+    description='Библиотека для YTmonster.ru или Clifl.com',
+    long_description='''
+Библиотека YTmonster позволяет обращятся к сайтам YTmonster.ru или Clifl.com и делают всю рутину за вас.
+Я хочу что бы API сайта YTmonster было удобно и понятно каждому!
+
+''',
+    author='GODIMONGO',
+    author_email='dbgdhdg13@gmail.com',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
