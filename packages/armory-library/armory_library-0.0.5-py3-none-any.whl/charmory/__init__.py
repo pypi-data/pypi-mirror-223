1ff136f73e0a53636c8aa9104eb97fea9ad51dbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:27 +0000

    Add package armory-library/0.0.5/armory_library-0.0.5-py3-none-any.whl

---
+++ packages/armory-library/armory_library-0.0.5-py3-none-any.whl/charmory/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2023-present matt wartell <matt.wartell@twosixtech.com>
+#
+# SPDX-License-Identifier: MIT
---
+++ packages/armory-library/armory_library-0.0.5-py3-none-any.whl/charmory/main.py
@@ -0,0 +1,10 @@
+from charmory.__about__ import __version__ as version
+
+
+def life_the_universe_and_everything():
+    return 42
+
+
+def main():
+    print(f"charmory version {version}")
+    print("this is a holding space for the future release of armory-library")
