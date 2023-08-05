a4f838d538d5c2df764d44a33763c261ae945809
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:39 +0000

    Add package ouija/0.2.0/ouija-0.2.0-py3-none-any.whl

---
+++ packages/ouija/ouija-0.2.0-py3-none-any.whl/ouija/packet.py
@@ -0,0 +1,72 @@
+from enum import IntEnum
+from typing import Optional
+
+import pbjson
+from cryptography.fernet import Fernet
+
+
+class Phase(IntEnum):
+    OPEN = 1
+    DATA = 2
+    CLOSE = 3
+
+
+TOKENS = {
+    'phase': 'pe',
+    'ack': 'ak',
+    'token': 'tn',
+    'host': 'ht',
+    'port': 'pt',
+    'seq': 'sq',
+    'data': 'da',
+    'drain': 'dn',
+}
+
+
+class Packet:
+    phase: Phase
+    ack: bool
+    token: Optional[str]
+    host: Optional[str]
+    port: Optional[int]
+    seq: Optional[int]
+    data: Optional[bytes]
+    drain: Optional[bool]
+
+    def __init__(
+            self, 
+            *, 
+            phase: Phase,
+            ack: bool,
+            token: Optional[str] = None,
+            host: Optional[str] = None,
+            port: Optional[int] = None,
+            seq: Optional[int] = None,
+            data: Optional[bytes] = None,
+            drain: Optional[bool] = None,
+    ) -> None:
+        self.phase = phase
+        self.ack = ack
+        self.token = token
+        self.host = host
+        self.port = port
+        self.seq = seq
+        self.data = data
+        self.drain = drain
+
+    @staticmethod
+    async def packet(*, data: bytes, fernet: Fernet) -> 'Packet':
+        json = pbjson.loads(fernet.decrypt(data))
+        return Packet(
+            phase=Phase(json.get(TOKENS['phase'])),
+            ack=json.get(TOKENS['ack']),
+            token=json.get(TOKENS['token'], None),
+            host=json.get(TOKENS['host'], None),
+            port=json.get(TOKENS['port'], None),
+            seq=json.get(TOKENS['seq'], None),
+            data=json.get(TOKENS['data'], None),
+            drain=json.get(TOKENS['drain'], None),
+        )
+
+    async def binary(self, *, fernet: Fernet) -> bytes:
+        return fernet.encrypt(pbjson.dumps({TOKENS[k]: v for k, v in self.__dict__.items() if v is not None}))
---
+++ packages/ouija/ouija-0.2.0-py3-none-any.whl/ouija/relay.py
@@ -0,0 +1,96 @@
+import asyncio
+import logging
+
+from .telemetry import Telemetry
+from .tuning import Tuning
+from .ouija import Ouija
+from .packet import Packet
+
+
+logging.basicConfig(
+    format='%(asctime)s,%(msecs)03d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
+    datefmt='%Y-%m-%d:%H:%M:%S',
+    level=logging.ERROR,
+)
+logger = logging.getLogger(__name__)
+
+
+class Relay(Ouija, asyncio.DatagramProtocol):
+    transport: asyncio.DatagramTransport
+    proxy_host: str
+    proxy_port: int
+
+    def __init__(
+            self, 
+            *,
+            telemetry: Telemetry,
+            tuning: Tuning,
+            reader: asyncio.StreamReader, 
+            writer: asyncio.StreamWriter,
+            proxy_host: str,
+            proxy_port: int,
+            remote_host: str,
+            remote_port: int,
+    ) -> None:
+        self.telemetry = telemetry
+        self.tuning = tuning
+        self.reader = reader
+        self.writer = writer
+        self.proxy_host = proxy_host
+        self.proxy_port = proxy_port
+        self.remote_host = remote_host
+        self.remote_port = remote_port
+        self.opened = asyncio.Event()
+        self.sent_buf = dict()
+        self.sent_seq = 0
+        self.read_closed = asyncio.Event()
+        self.recv_buf = dict()
+        self.recv_seq = 0
+        self.write_closed = asyncio.Event()
+
+    def connection_made(self, transport) -> None:
+        self.transport = transport
+
+    async def _datagram_received_async(self, *, data, addr) -> None:
+        self.telemetry.packets_received += 1
+        self.telemetry.bytes_received += len(data)
+        try:
+            packet = await Packet.packet(data=data, fernet=self.tuning.fernet)
+        except Exception as e:
+            logger.error(e)
+            self.telemetry.decoding_errors += 1
+            return
+        await self.process(packet=packet)
+
+    def datagram_received(self, data, addr) -> None:
+        asyncio.create_task(self._datagram_received_async(data=data, addr=addr))
+
+    def error_received(self, exc) -> None:
+        logger.error(exc)
+
+    def connection_lost(self, exc) -> None:
+        asyncio.create_task(self.close())
+
+    async def sendto(self, *, data: bytes) -> None:
+        self.transport.sendto(data)
+
+    async def on_open(self, *, packet: Packet) -> bool:
+        if not packet.ack or self.opened.is_set():
+            return False
+
+        await self.write(data=b'HTTP/1.1 200 Connection Established\r\n\r\n', drain=True)
+        self.opened.set()
+        return True
+
+    async def on_serve(self) -> bool:
+        loop = asyncio.get_event_loop()
+        await loop.create_datagram_endpoint(lambda: self, remote_addr=(self.proxy_host, self.proxy_port))
+
+        if not await self.send_open():
+            return False
+
+        return True
+
+    async def on_close(self) -> None:
+        if not self.transport.is_closing():
+            self.transport.close()
