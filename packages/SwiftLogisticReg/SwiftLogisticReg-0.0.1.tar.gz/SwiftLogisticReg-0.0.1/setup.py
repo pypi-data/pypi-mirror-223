6765ffb8bc9c51a1045b5cbaaec3e37976540dad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:18 +0000

    Add package SwiftLogisticReg/0.0.1/SwiftLogisticReg-0.0.1.tar.gz

---
+++ packages/SwiftLogisticReg/SwiftLogisticReg-0.0.1.tar.gz/SwiftLogisticReg-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+from pathlib import Path
+
+
+
+setup(
+    name="SwiftLogisticReg",
+    version="0.0.1",
+    author="Nechba Mohammed, Mouhajir Mohamed, and Sedjari Yassine",
+    description="Efficient logistic regression in Python with GPU support",
+    long_description_content_type="text/markdown",  # Corrected the content type to "text/markdown"
+    url="https://github.com/NechbaMohammed/SwiftLogisticReg",
+    author_email="mohammednechba@gmail.com",
+    packages=['SwiftLogisticReg'],
+)
