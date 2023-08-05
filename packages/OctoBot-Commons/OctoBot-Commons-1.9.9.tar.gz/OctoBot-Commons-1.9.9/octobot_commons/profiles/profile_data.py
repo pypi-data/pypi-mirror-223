c3f937701db0e016674dbe7e940e4c557d982833
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:12 +0000

    Add package OctoBot-Commons/1.9.9/OctoBot-Commons-1.9.9.tar.gz

---
+++ packages/OctoBot-Commons/OctoBot-Commons-1.9.9.tar.gz/OctoBot-Commons-1.9.9/octobot_commons/profiles/profile_data.py
@@ -0,0 +1,234 @@
+# pylint: disable=C0103,R0902
+#  Drakkar-Software OctoBot-Commons
+#  Copyright (c) Drakkar-Software, All rights reserved.
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 3.0 of the License, or (at your option) any later version.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library.
+import dataclasses
+import typing
+
+import octobot_commons.profiles.profile as profile_import
+import octobot_commons.minimizable_dataclass as minimizable_dataclass
+import octobot_commons.enums as enums
+import octobot_commons.constants as constants
+
+
+@dataclasses.dataclass
+class ProfileDetailsData:
+    name: str
+    description: str = ""
+    id: typing.Union[str, None] = None
+    origin_url: typing.Union[str, None] = None
+    avatar: typing.Union[str, None] = None
+    complexity: int = enums.ProfileComplexity.MEDIUM.value
+    risk: int = enums.ProfileRisk.MODERATE.value
+    type: str = enums.ProfileType.LIVE.value
+    imported: bool = False
+    read_only: bool = False
+    bot_id: typing.Union[str, None] = None
+
+
+@dataclasses.dataclass
+class CryptoCurrencyData:
+    trading_pairs: list[str]
+    name: typing.Union[str, None] = None
+    enabled: bool = True
+
+
+@dataclasses.dataclass
+class ExchangeData:
+    name: str
+    type: str = constants.DEFAULT_EXCHANGE_TYPE
+    enabled: bool = True
+    config_name: typing.Union[str, None] = None
+
+
+@dataclasses.dataclass
+class TraderData:
+    enabled: bool = True
+    load_trade_history: bool = True
+
+
+@dataclasses.dataclass
+class TraderSimulatorData:
+    enabled: bool = False
+    starting_portfolio: dict[str, float] = dataclasses.field(default_factory=dict)
+    maker_fees: float = 0.1
+    taker_fees: float = 0.1
+
+
+@dataclasses.dataclass
+class TradingData:
+    reference_market: str
+    risk: float = 1.0
+
+
+@dataclasses.dataclass
+class TentaclesData:
+    name: str
+    config: dict = None
+    enabled: bool = True
+
+
+@dataclasses.dataclass
+class ProfileData(minimizable_dataclass.MinimizableDataclass):
+    profile_details: ProfileDetailsData
+    crypto_currencies: list[CryptoCurrencyData]
+    exchanges: list[ExchangeData]
+    trading: TradingData
+    trader: TraderData = TraderData()
+    trader_simulator: TraderSimulatorData = TraderSimulatorData()
+    tentacles: list[TentaclesData] = None
+
+    # pylint: disable=E1134
+    def __post_init__(self):
+        if isinstance(self.profile_details, dict):
+            self.profile_details = ProfileDetailsData(**self.profile_details)
+        if self.crypto_currencies and isinstance(self.crypto_currencies[0], dict):
+            self.crypto_currencies = [
+                CryptoCurrencyData(**crypto_currency)
+                for crypto_currency in self.crypto_currencies
+            ]
+        if self.exchanges and isinstance(self.exchanges[0], dict):
+            self.exchanges = [ExchangeData(**exchange) for exchange in self.exchanges]
+        if isinstance(self.trader, dict):
+            self.trader = TraderData(**self.trader)
+        if isinstance(self.trader_simulator, dict):
+            self.trader_simulator = TraderSimulatorData(**self.trader_simulator)
+        if isinstance(self.trading, dict):
+            self.trading = TradingData(**self.trading)
+        if self.tentacles and isinstance(self.tentacles[0], dict):
+            self.tentacles = (
+                [TentaclesData(**tentacle) for tentacle in self.tentacles]
+                if self.tentacles
+                else []
+            )
+
+    @classmethod
+    def from_profile(cls, profile: profile_import.Profile):
+        """
+        Creates a cls instance from the given profile
+        """
+        profile_dict = profile.as_dict()
+        content = profile_dict[constants.PROFILE_CONFIG]
+        return cls.from_dict(
+            {
+                "profile_details": profile_dict[constants.CONFIG_PROFILE],
+                "crypto_currencies": [
+                    {
+                        "trading_pairs": details.get(constants.CONFIG_CRYPTO_PAIRS, []),
+                        "name": currency,
+                        "enabled": details.get(constants.CONFIG_ENABLED_OPTION, True),
+                    }
+                    for currency, details in content[
+                        constants.CONFIG_CRYPTO_CURRENCIES
+                    ].items()
+                ],
+                "exchanges": [
+                    {
+                        "name": exchange,
+                        "type": details.get(
+                            constants.CONFIG_EXCHANGE_TYPE,
+                            constants.DEFAULT_EXCHANGE_TYPE,
+                        ),
+                        "enabled": details.get(constants.CONFIG_ENABLED_OPTION, True),
+                    }
+                    for exchange, details in content[constants.CONFIG_EXCHANGES].items()
+                ],
+                "trader": {
+                    "enabled": content[constants.CONFIG_TRADER][
+                        constants.CONFIG_ENABLED_OPTION
+                    ],
+                    "load_trade_history": content[constants.CONFIG_TRADER].get(
+                        constants.CONFIG_LOAD_TRADE_HISTORY, True
+                    ),
+                },
+                "trader_simulator": {
+                    "enabled": content[constants.CONFIG_SIMULATOR][
+                        constants.CONFIG_ENABLED_OPTION
+                    ],
+                    "starting_portfolio": content[constants.CONFIG_SIMULATOR][
+                        constants.CONFIG_STARTING_PORTFOLIO
+                    ],
+                    "maker_fees": content[constants.CONFIG_SIMULATOR][
+                        constants.CONFIG_SIMULATOR_FEES
+                    ].get(constants.CONFIG_SIMULATOR_FEES_MAKER, 0.0),
+                    "taker_fees": content[constants.CONFIG_SIMULATOR][
+                        constants.CONFIG_SIMULATOR_FEES
+                    ].get(constants.CONFIG_SIMULATOR_FEES_TAKER, 0.0),
+                },
+                "trading": {
+                    "reference_market": content[constants.CONFIG_TRADING][
+                        constants.CONFIG_TRADER_REFERENCE_MARKET
+                    ],
+                    "risk": content[constants.CONFIG_TRADING][
+                        constants.CONFIG_TRADER_RISK
+                    ],
+                },
+                "tentacles": [],
+            }
+        )
+
+    def to_profile(self, to_create_profile_path: str) -> profile_import.Profile:
+        """
+        Returns a new Profile from self
+        """
+        profile = profile_import.Profile(to_create_profile_path)
+        profile.from_dict(self._to_profile_dict())
+        return profile
+
+    def set_tentacles_config(self, config_by_tentacle: dict):
+        """
+        Update self.tentacles from the given config_by_tentacle
+        """
+        self.tentacles = [
+            TentaclesData(name=tentacle, config=config)
+            for tentacle, config in config_by_tentacle.items()
+        ]
+
+    def _to_profile_dict(self) -> dict:
+        return {
+            constants.PROFILE_CONFIG: {
+                constants.CONFIG_CRYPTO_CURRENCIES: {
+                    crypto_currency.name: {
+                        constants.CONFIG_CRYPTO_PAIRS: crypto_currency.trading_pairs,
+                        constants.CONFIG_ENABLED_OPTION: crypto_currency.enabled,
+                    }
+                    for crypto_currency in self.crypto_currencies
+                },
+                constants.CONFIG_EXCHANGES: {
+                    exchange.name: {
+                        constants.CONFIG_EXCHANGE_TYPE: exchange.type,
+                        constants.CONFIG_ENABLED_OPTION: exchange.enabled,
+                    }
+                    for exchange in self.exchanges
+                },
+                constants.CONFIG_TRADER: {
+                    constants.CONFIG_ENABLED_OPTION: self.trader.enabled,
+                    constants.CONFIG_LOAD_TRADE_HISTORY: self.trader.load_trade_history,
+                },
+                constants.CONFIG_SIMULATOR: {
+                    constants.CONFIG_ENABLED_OPTION: self.trader_simulator.enabled,
+                    constants.CONFIG_STARTING_PORTFOLIO: self.trader_simulator.starting_portfolio,
+                    constants.CONFIG_SIMULATOR_FEES: {
+                        constants.CONFIG_SIMULATOR_FEES_MAKER: self.trader_simulator.maker_fees,
+                        constants.CONFIG_SIMULATOR_FEES_TAKER: self.trader_simulator.taker_fees,
+                    },
+                },
+                constants.CONFIG_TRADING: {
+                    constants.CONFIG_TRADER_REFERENCE_MARKET: self.trading.reference_market,
+                    constants.CONFIG_TRADER_RISK: self.trading.risk,
+                },
+            },
+            constants.CONFIG_PROFILE: dataclasses.asdict(self.profile_details),
+        }
---
+++ packages/OctoBot-Commons/OctoBot-Commons-1.9.9.tar.gz/OctoBot-Commons-1.9.9/tests/profiles/test_profile_data.py
@@ -0,0 +1,217 @@
+#  Drakkar-Software OctoBot-Commons
+#  Copyright (c) Drakkar-Software, All rights reserved.
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 3.0 of the License, or (at your option) any later version.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library.*
+import json
+import pytest
+
+import octobot_commons.profiles as profiles
+import octobot_commons.constants as constants
+
+from tests.profiles import get_profile_path, profile
+
+
+@pytest.fixture
+def profile_data_dict():
+    return {
+        'profile_details': {
+            'name': 'default',
+            'description': 'OctoBot default profile.',
+            'id': 'default',
+            'origin_url': 'https://default.url',
+            'avatar': 'default_profile.png',
+            'complexity': 2,
+            'risk': 2,
+            'type': 'live',
+            'bot_id': 'bot_1',
+            'imported': False,
+            'read_only': False
+        },
+        'crypto_currencies': [
+            {
+                'trading_pairs': ['BTC/USDT'],
+                'name': 'Bitcoin',
+                'enabled': True
+            }
+        ], 'exchanges': [
+            {
+                'name': 'binance',
+                'type': 'spot',
+                'enabled': True,
+                'config_name': 'binance_1'
+            }
+        ], 'trader': {
+            'enabled': False,
+            'load_trade_history': True
+        }, 'trader_simulator': {
+            'enabled': True,
+            'starting_portfolio': {
+                'BTC': 10,
+                'USDT': 1000
+            },
+            'maker_fees': 0.1,
+            'taker_fees': 0.1
+        }, 'trading': {
+            'reference_market': 'BTC',
+            'risk': 0.5
+        }, 'tentacles': [
+            {
+                'name': 'plopEvaluator',
+                'config': {},
+                'enabled': False,
+            },
+            {
+                'name': 'plopEvaluator',
+                'enabled': True,
+                'config': {
+                    'a': True,
+                    'other': {
+                        'l': [1, 2],
+                        'n': None,
+                    }
+                },
+            },
+        ]
+    }
+
+@pytest.fixture
+def min_profile_data_dict():
+    return {
+        'profile_details': {
+            'name': 'min_profile',
+        },
+        'crypto_currencies': [
+            {
+                'trading_pairs': ['BTC/USDT'],
+            }
+        ], 'exchanges': [
+            {
+                'name': 'binance',
+            }
+        ], 'trading': {
+            'reference_market': 'BTC',
+        }, 'tentacles': [
+            {
+                'name': 'plopEvaluator',
+                'config': {},
+                'enabled': False,
+            },
+            {
+                'name': 'plopEvaluator',
+                'enabled': True,
+                'config': {
+                    'a': True,
+                    'other': {
+                        'l': [1, 2],
+                        'n': None,
+                    }
+                },
+            },
+        ]
+    }
+
+
+def test_from_profile(profile):
+    profile_data = profiles.ProfileData.from_profile(profile.read_config())
+    # check one element per attribute to be sure it's all parsed
+    assert profile_data.profile_details.origin_url == "https://default.url"
+    assert profile_data.crypto_currencies[0].trading_pairs == ['BTC/USDT']
+    assert profile_data.exchanges[0].name == "binance"
+    assert profile_data.trader.enabled is False
+    assert profile_data.trader_simulator.enabled is True
+    assert profile_data.trader_simulator.starting_portfolio == {'BTC': 10, 'USDT': 1000}
+    assert profile_data.trading.risk == 0.5
+    assert profile_data.tentacles == []
+
+
+def test_to_profile(profile):
+    profile_data = profiles.ProfileData.from_profile(profile.read_config())
+    created_profile = profile_data.to_profile("plop_path")
+    # force missing values
+    for crypto_data in profile.config[constants.CONFIG_CRYPTO_CURRENCIES].values():
+        crypto_data[constants.CONFIG_ENABLED_OPTION] = crypto_data.get(constants.CONFIG_ENABLED_OPTION, True)
+    for exchange_data in profile.config[constants.CONFIG_EXCHANGES].values():
+        exchange_data[constants.CONFIG_ENABLED_OPTION] = exchange_data.get(constants.CONFIG_ENABLED_OPTION, True)
+        exchange_data[constants.CONFIG_EXCHANGE_TYPE] = exchange_data.get(constants.CONFIG_EXCHANGE_TYPE,
+                                                                          constants.DEFAULT_EXCHANGE_TYPE)
+    # if both parsing and transforming return the same profile as original one, the whole chain works
+    assert profile.as_dict() == created_profile.as_dict()
+
+
+def test_from_dict(profile_data_dict):
+    profile_data = profiles.ProfileData.from_dict(profile_data_dict)
+    # check one element per attribute to be sure it's all parsed
+    assert profile_data.profile_details.origin_url == "https://default.url"
+    assert profile_data.crypto_currencies[0].trading_pairs == ['BTC/USDT']
+    assert profile_data.exchanges[0].name == "binance"
+    assert profile_data.trader.enabled is False
+    assert profile_data.trader_simulator.enabled is True
+    assert profile_data.trader_simulator.starting_portfolio == {'BTC': 10, 'USDT': 1000}
+    assert profile_data.trading.risk == 0.5
+    assert profile_data.tentacles[0].name == "plopEvaluator"
+    assert profile_data.tentacles[1].config["other"]["l"] == [1, 2]
+
+
+def test_from_min_dict(min_profile_data_dict):
+    profile_data = profiles.ProfileData.from_dict(min_profile_data_dict)
+    # check one element per attribute to be sure it's all parsed
+    assert profile_data.profile_details.name == "min_profile"
+    assert profile_data.profile_details.origin_url is None
+    assert profile_data.crypto_currencies[0].trading_pairs == ['BTC/USDT']
+    assert profile_data.crypto_currencies[0].name is None
+    assert profile_data.crypto_currencies[0].enabled is True
+    assert profile_data.exchanges[0].name == "binance"
+    assert profile_data.trader.enabled is True
+    assert profile_data.trader_simulator.enabled is False
+    assert profile_data.trader_simulator.starting_portfolio == {}
+    assert profile_data.trading.risk == 1
+    assert profile_data.tentacles[0].name == "plopEvaluator"
+    assert profile_data.tentacles[1].config["other"]["l"] == [1, 2]
+
+
+def test_from_dict_objects(profile_data_dict):
+    profile_data_objects = profiles.ProfileData.from_dict(profile_data_dict)
+    profile_data = profiles.ProfileData.from_dict({
+        "profile_details": profile_data_objects.profile_details,
+        "crypto_currencies": profile_data_objects.crypto_currencies,
+        "exchanges": profile_data_objects.exchanges,
+        "trader": profile_data_objects.trader,
+        "trader_simulator": profile_data_objects.trader_simulator,
+        "trading": profile_data_objects.trading,
+        "tentacles": profile_data_objects.tentacles,
+    })
+    # check one element per attribute to be sure it's all parsed
+    assert profile_data.profile_details.origin_url == "https://default.url"
+    assert profile_data.crypto_currencies[0].trading_pairs == ['BTC/USDT']
+    assert profile_data.exchanges[0].name == "binance"
+    assert profile_data.trader.enabled is False
+    assert profile_data.trader_simulator.enabled is True
+    assert profile_data.trader_simulator.starting_portfolio == {'BTC': 10, 'USDT': 1000}
+    assert profile_data.trading.risk == 0.5
+    assert profile_data.tentacles[0].name == "plopEvaluator"
+    assert profile_data.tentacles[1].config["other"]["l"] == [1, 2]
+
+
+def test_to_dict(profile_data_dict):
+    profile_data = profiles.ProfileData.from_dict(profile_data_dict)
+    # if both parsing and transforming return the same profile as original one, the whole chain works
+    assert profile_data_dict == profile_data.to_dict()
+
+    dict_without_default_values = profile_data.to_dict(include_default_values=False)
+    assert profile_data_dict != dict_without_default_values
+
+    # ensure no empty elements
+    assert len(dict_without_default_values) == len(profile_data_dict)
+    for values in dict_without_default_values.values():
+        assert len(values)
