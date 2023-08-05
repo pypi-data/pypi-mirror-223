9b6361fd2ba2cdcb3a017459c8ca4136dfbe9632
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:26 +0000

    Add package autora-synthetic-abstract-sympy-equation/0.0.1/autora_synthetic_abstract_sympy_equation-0.0.1.tar.gz

---
+++ packages/autora-synthetic-abstract-sympy-equation/autora_synthetic_abstract_sympy_equation-0.0.1.tar.gz/autora_synthetic_abstract_sympy_equation-0.0.1/tests/test_autora_synthetic_abstract_sympy_equation.py
@@ -0,0 +1,10 @@
+from src.autora.experiment_runner.synthetic.abstract.sympy_equation import (
+    sympy_experiment,
+)
+
+
+def test():
+    assert True
+
+
+# Note: We encourage you to adjust this test and write more tests.
