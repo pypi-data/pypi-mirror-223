b848ab5df27a65c921b3ccd214cd6b0af0ba669c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:08 +0000

    Add package ayugespidertools/3.3.3/ayugespidertools-3.3.3-py3-none-any.whl

---
+++ packages/ayugespidertools/ayugespidertools-3.3.3-py3-none-any.whl/ayugespidertools/scraper/middlewares/netlib/__init__.py
@@ -0,0 +1,9 @@
+from ayugespidertools.scraper.middlewares.netlib.aiohttplib import (
+    AiohttpAsyncDownloaderMiddleware,
+    AiohttpDownloaderMiddleware,
+)
+
+__all__ = [
+    "AiohttpAsyncDownloaderMiddleware",
+    "AiohttpDownloaderMiddleware",
+]
---
+++ packages/ayugespidertools/ayugespidertools-3.3.3-py3-none-any.whl/ayugespidertools/scraper/pipelines/mongo/asynced.py
@@ -0,0 +1,41 @@
+import asyncio
+import urllib.parse
+
+import motor.motor_asyncio
+
+from ayugespidertools.common.mongodbpipe import AsyncioAsynchronous
+from ayugespidertools.common.multiplexing import ReuseOperation
+
+
+class AsyncMongoPipeline:
+    """通过 motor 实现异步写入 MongoDB 的存储管道"""
+
+    def open_spider(self, spider):
+        assert hasattr(
+            spider, "mongodb_conf"
+        ), "未配置 MongoDB 连接信息，请查看 .conf 或 consul 上对应配置信息！"
+
+        _encoded_pwd = urllib.parse.quote_plus(spider.mongodb_conf.password)
+        self.mongo_uri = (
+            f"mongodb://{spider.mongodb_conf.user}:{_encoded_pwd}"
+            f"@{spider.mongodb_conf.host}:{spider.mongodb_conf.port}/"
+            f"?authSource={spider.mongodb_conf.authsource}&authMechanism=SCRAM-SHA-1",
+        )
+        return ReuseOperation.as_deferred(self._open_spider(spider))
+
+    async def _open_spider(self, spider):
+        self.client = motor.motor_asyncio.AsyncIOMotorClient(self.mongo_uri)
+        self.db = self.client[spider.mongodb_conf.database]
+
+    async def close_spider(self, spider):
+        self.client.close()
+
+    async def process_item(self, item, spider):
+        item_dict = ReuseOperation.item_to_dict(item)
+        await asyncio.shield(
+            AsyncioAsynchronous().process_item_template(
+                item_dict=item_dict,
+                db=self.db,
+            )
+        )
+        return item
