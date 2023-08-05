697a72125f999e98d2dd9f59f79e3e742ae8cb68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:29 +0000

    Add package invenio-records-lom/0.11.0/invenio_records_lom-0.11.0-py2.py3-none-any.whl

---
+++ packages/invenio-records-lom/invenio_records_lom-0.11.0-py2.py3-none-any.whl/invenio_records_lom/ui/theme/webpack.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+#
+# Copyright (C) 2021-2023 Graz University of Technology.
+#
+# invenio-records-lom is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for
+# more details.
+
+"""JS/CSS Webpack bundles for theme."""
+
+from invenio_assets.webpack import WebpackThemeBundle
+
+theme = WebpackThemeBundle(
+    __name__,
+    "assets",
+    default="semantic-ui",
+    themes={
+        # theme-name ~> kwargs_for_WebpackBundle
+        "semantic-ui": {
+            "aliases": {
+                "@translations/invenio_records_lom": "translations/invenio_records_lom",
+            },
+            "entry": {
+                "invenio-records-lom-deposit": "./js/invenio_records_lom/deposit/index.js",
+                "invenio-records-lom-landing-page": "./js/invenio_records_lom/landing_page/index.js",
+                "invenio-records-lom-search": "./js/invenio_records_lom/search/index.js",
+                "invenio-records-lom-theme": "./less/invenio_records_lom/theme.less",
+            },
+            "dependencies": {
+                "@babel/runtime": "^7.9.0",
+                "react": "^16.13.0",
+                "react-dom": "^16.13.0",
+            },
+            "devDependencies": {},
+            "peerDependencies": {},
+        },
+    },
+)
