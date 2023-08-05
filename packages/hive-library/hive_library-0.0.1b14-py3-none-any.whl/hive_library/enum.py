fd5145274c57bc5ce086c22495f8be6e8de3d134
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:59 +0000

    Add package hive-library/0.0.1b14/hive_library-0.0.1b14-py3-none-any.whl

---
+++ packages/hive-library/hive_library-0.0.1b14-py3-none-any.whl/hive_library/enum.py
@@ -0,0 +1,89 @@
+# Description
+"""
+Hive enums
+Author: HexWay
+License: MIT
+Copyright 2023, HexWay
+"""
+
+# Import
+from enum import Enum
+
+
+# Authorship information
+__author__ = "HexWay"
+__copyright__ = "Copyright 2023, HexWay"
+__credits__ = [""]
+__license__ = "MIT"
+__version__ = "0.0.1b14"
+__maintainer__ = "HexWay"
+__email__ = "contact@hexway.io"
+__status__ = "Development"
+
+
+class ListEnum(Enum):
+    @classmethod
+    def list(cls):
+        return list(map(lambda c: c.value, cls))
+
+
+class PermissionTypes(str, ListEnum):
+    EDIT = "EDIT"
+    READONLY = "READONLY"
+    OWNER = "OWNER"
+    ADMIN = "ADMIN"
+
+
+class RecordTypes(str, ListEnum):
+    STRING = "string"
+    TEXT_BLOCK = "text_block"
+    NUMBER = "number"
+    TUPLE = "tuple"
+    CODEBLOCK = "codeblock"
+    FILE = "file"
+    LIST = "list"
+    DIRECTORY = "directory"
+    NESTED = "nested"
+
+
+class TaskStates(str, ListEnum):
+    SUCCESS = "SUCCESS"
+    PROGRESS = "PROGRESS"
+    PENDING = "PENDING"
+    FAILURE = "FAILED"
+    CANCELLED = "CANCELLED"
+
+
+class RowTypes(str, ListEnum):
+    CPELIST = "cpelist"
+    HOSTNAME = "hostname"
+    IP = "ip"
+    NOTE = "note"
+    PORT = "port"
+    PRODUCT = "product"
+    SERVICE = "service"
+    STATE = "state"
+    TAG = "tag"
+    VERSION = "version"
+    DUMMY = "dummy"
+
+
+class CriticalityScores(int, ListEnum):
+    LOW = 1
+    MEDIUM = 2
+    HIGH = 3
+
+
+class ProbabilityScores(int, ListEnum):
+    LOW = 1
+    MEDIUM = 2
+    HIGH = 3
+
+
+class IssueStatuses(str, ListEnum):
+    DRAFT = "draft"
+    VERIFIED = "verified"
+    READY = "ready"
+    FIX_CONFIRMED = "fix_confirmed"
+    FIX_UNCONFIRMED = "fix_unconfirmed"
+    RETEST_REQUIRED = "retest_required"
