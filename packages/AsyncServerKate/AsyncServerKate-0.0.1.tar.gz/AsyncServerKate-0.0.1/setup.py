82edf2d515084895c7e004b384e2e6cf637dc492
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:56 +0000

    Add package AsyncServerKate/0.0.1/AsyncServerKate-0.0.1.tar.gz

---
+++ packages/AsyncServerKate/AsyncServerKate-0.0.1.tar.gz/AsyncServerKate-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name="AsyncServerKate",
+      version="0.0.1",
+      description="Chat with encryption",
+      author="Ekaterina Eranova",
+      author_email="kteranova@mail.ru",
+      packages=find_packages(),
+      install_requires=['PyQt5', 'sqlalchemy', 'pycryptodome', 'pycryptodomex']
+      )
