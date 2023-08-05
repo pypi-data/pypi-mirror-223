1241762ada8a25d425607301c2e17fb00c764f58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:21 +0000

    Add package radicalpy/0.6.5/radicalpy-0.6.5.tar.gz

---
+++ packages/radicalpy/radicalpy-0.6.5.tar.gz/radicalpy-0.6.5/radicalpy/classical.py
@@ -0,0 +1,227 @@
+#!/usr/bin/env python
+
+
+from typing import Tuple
+
+import graphviz
+import numpy as np
+import scipy as sp
+
+from . import utils
+
+
+def get_delta_r(mutual_diffusion: float, delta_T: float) -> float:
+    """Mean path between two radicals.
+
+    Args:
+            mutual_diffusion (float): The mutual diffusion coefficient (m^2/s).
+            delta_T (float): The time interval (s).
+
+    Returns:
+            float: The mean path between two radicals (m).
+    """
+    return np.sqrt(6 * mutual_diffusion * delta_T)
+
+
+def latexify(rate_equations: dict):
+    result = []
+    for lhs_data, rhs_data in rate_equations.items():
+        lhs = f"\\frac{{d[{lhs_data}]}}{{dt}} "
+        rhs_list = [f"{edge.label} [{vertex}]" for vertex, edge in rhs_data.items()]
+        rhs = " + ".join(rhs_list)
+        result.append(f"{lhs} = {rhs}")
+    return result
+
+
+def latex_eqlist_to_align(eqs: list):
+    body = " \\\\\n".join(map(lambda t: t.replace("=", "&="), eqs))
+    return f"\\begin{{align*}}\n{body}\n\\end{{align*}}"
+
+
+class Rate:
+    """Rate class.
+
+    Stores the rate value and (LaTeX) label of the rate (expression).
+
+    """
+
+    value: float
+    label: str
+    """LaTeX representation of the rate constant."""
+
+    def __repr__(self):
+        return f"{self.label} = {self.value}"
+
+    def __init__(self, value: float, label: str):  # noqa D102
+        self.value = value
+        self.label = label
+
+    @staticmethod
+    def _get_value_label(v):
+        return (v.value, v.label) if isinstance(v, Rate) else (v, v)
+
+    def __rmul__(self, v):
+        value, label = self._get_value_label(v)
+        return Rate(self.value * value, f"{label} {self.label}")
+
+    def __mul__(self, v):
+        return self.__rmul__(v)
+
+    def __radd__(self, v):
+        value, label = self._get_value_label(v)
+        return Rate(self.value + value, f"{label} + {self.label}")
+
+    def __add__(self, v):
+        value, label = self._get_value_label(v)
+        return Rate(self.value + value, f"{self.label} + {label}")
+
+    def __neg__(self):
+        return Rate(-self.value, f"-({self.label})")
+
+
+class RateEquations:
+    """Results for `kinetics_solver`"""
+
+    def __init__(self, rate_equations: dict, time: np.ndarray, initial_states: dict):
+        self.rate_equations = rate_equations
+        inner_keys = [list(v.keys()) for v in rate_equations.values()]
+        outer_keys = list(rate_equations.keys())
+        all_keys = list(set(sum(inner_keys, outer_keys)))
+        self.indices = {k: i for i, k in enumerate(all_keys)}
+        self._calc_(time, initial_states)
+
+    @property
+    def all_keys(self) -> list:
+        return list(self.indices.keys())
+
+    def are_valid_keys(self, keys: dict) -> bool:
+        return set(keys).issubset(self.all_keys)
+
+    def check_initial_states(self, initial_states):
+        if not self.are_valid_keys(initial_states.keys()):
+            raise ValueError("Unknown keys specified in `initial_states`")
+        if sum(initial_states.values()) != 1:
+            raise ValueError("Initial state values don't sum up to 1")
+
+    def _calc_(self, time: np.ndarray, initial_states: dict):
+        self.check_initial_states(initial_states)
+        tmp = [
+            (v.value, self.indices[i], self.indices[j])
+            for i, d in self.rate_equations.items()
+            for j, v in d.items()
+        ]
+        dt = time[1] - time[0]
+        data, row_ind, col_ind = zip(*tmp)
+        N = len(self.all_keys)
+        propagator = sp.sparse.linalg.expm(
+            sp.sparse.csc_matrix((data, (row_ind, col_ind)), (N, N)) * dt
+        )
+        self.result = np.zeros([len(time), len(self.all_keys)], dtype=float)
+        self.result[0] = [initial_states.get(k, 0) for k in self.all_keys]
+        for t in range(1, len(time)):
+            self.result[t] = propagator @ self.result[t - 1]
+
+    def __getitem__(self, keys: list) -> np.ndarray:
+        ks = [keys] if isinstance(keys, str) else keys
+        return np.sum([self.result[:, self.indices[k]] for k in ks], axis=0)
+
+
+def reaction_scheme(rate_equations: dict):
+    data = [
+        (v1, v2, edge.label)
+        for v1, rhs_data in rate_equations.items()
+        for v2, edge in rhs_data.items()
+    ]
+    G = graphviz.Digraph("G")
+    for v1, v2, edge in data:
+        if not edge.startswith("-"):
+            # TODO: add only if not present already
+            # TODO: check position
+            # TODO: use sympy (how to check negative)
+            G.node(v1, texlbl=f"${v1}$")
+            G.node(v2, texlbl=f"${v2}$")
+            G.edge(v2, v1, edge, texlbl=f"${edge}$")
+
+    path = Path(f"{__file__[:-3]}_graph.tex")
+    texcode = dot2tex.dot2tex(G.source)
+    path.write_text(texcode)
+
+
+def _random_theta_phi():
+    """Random sampling of theta and phi.
+
+    Returns:
+            Theta and phi (radians).
+    """
+    theta = np.pi * np.random.rand()
+    arg = np.random.uniform(-1, 1)
+    phi = 2 * np.sign(arg) * np.arcsin(np.sqrt(np.abs(arg)))
+    return theta, phi
+
+
+def randomwalk_3d(
+    n_steps: int,
+    x_0: float,
+    y_0: float,
+    z_0: float,
+    delta_r: float,
+    r_min: float,
+    r_max: float = 0,
+) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+    """Simulate Monte Carlo random walk.
+
+    The MC random walk is simulated for radicals pairs in both
+    solution (`r_max = 0`) and microreactor (`r_max > 0`)
+    environments.
+
+    Args:
+
+            n_steps (float): The number of simulation steps.
+
+            x_0 (float): The initial position in the x-axis (m).
+
+            y_0 (float): The initial position in the x-axis (m).
+
+            z_0 (float): The initial position in the x-axis (m).
+
+            delta_r (float): The mean path between two radicals (m).
+
+            r_min (float): The distance of closest approach (m).
+
+            r_max (float): The diameter of the microreactor (m). Set
+                to 0 for solution-based, and to a positive value for
+                microreactor-based simulations.
+
+    Returns:
+        Tuple[np.ndarray, np.ndarray, np.ndarray]:
+            - pos: The positions of the moving radical (m).
+            - dist: The mutual distances between the radical pairs (m).
+            - angle: The angles (theta) of the vector trajectories of
+              the moving radical (m).
+
+    """
+    if r_max != 0 and r_min > r_max:
+        raise ValueError("r_min should be less than (or equal to) r_max.")
+    if r_min < 0 or r_max < 0:
+        raise ValueError("r_min and r_max should not be negative.")
+    pos = np.zeros([n_steps, 3])
+    dist = np.zeros(n_steps)
+    angle = np.zeros(n_steps)
+
+    pos[0] = np.array([x_0, y_0, z_0])
+    dist[0] = np.linalg.norm(pos[0])
+    if np.linalg.norm(pos[0]) <= r_min:
+        raise ValueError("Molecule starting distance is needs to be > r_min.")
+
+    for i in range(1, n_steps):
+        theta, phi = _random_theta_phi()
+        new_pos = pos[i - 1] + delta_r * utils.spherical_to_cartesian(theta, phi)
+        d = np.linalg.norm(new_pos)
+        while (r_max > 0 and d >= r_max - r_min) or d <= r_min + r_min:
+            theta, phi = _random_theta_phi()
+            new_pos = pos[i - 1] + delta_r * utils.spherical_to_cartesian(theta, phi)
+            d = np.linalg.norm(new_pos)
+        angle[i] = theta
+        pos[i] = new_pos
+        dist[i] = d
+    return pos, dist, angle
---
+++ packages/radicalpy/radicalpy-0.6.5.tar.gz/radicalpy-0.6.5/radicalpy/utils.py
@@ -0,0 +1,393 @@
+#!/usr/bin/env python
+
+"""Utility functions.
+
+.. todo:: Add module docstring.
+"""
+from typing import Tuple
+
+import numpy as np
+from scipy.fftpack import fft, ifft, ifftshift
+from scipy.optimize import curve_fit
+from sklearn.metrics import r2_score
+
+from .shared import constants as C
+
+
+def Bhalf_fit(
+    B: np.ndarray, MARY: np.ndarray
+) -> Tuple[float, np.ndarray, float, float]:
+    """B_1/2 fit for MARY spectra.
+
+    Args:
+            B (np.ndarray): Magnetic field values (x-axis).
+            MARY (np.ndarray): Magnetic field effect data
+                (y-axis). Use the `MARY` entry in the result of
+                `radicalpy.simulation.HilbertSimulation.MARY`.
+
+    Returns:
+            (float, np.ndarray, float, float):
+            - `Bhalf` (float): The magnetic field strength at half the
+              saturation magnetic field.
+            - `fit_result` (np.ndarray): y-axis from fit.
+            - `fit_error` (float): Standard error for the fit.
+            - `R2` (float): R-squared value for the fit.
+
+    """
+    popt_MARY, pcov_MARY = curve_fit(
+        Lorentzian,
+        B,
+        MARY,
+        p0=[MARY[-1], int(len(B) / 2)],
+        maxfev=1000000,
+    )
+    fit_error = np.sqrt(np.diag(pcov_MARY))
+
+    A_opt_MARY, Bhalf_opt_MARY = popt_MARY
+    fit_result = Lorentzian(B, *popt_MARY)
+    Bhalf = np.abs(Bhalf_opt_MARY)
+
+    y_pred_MARY = Lorentzian(B, *popt_MARY)
+    R2 = r2_score(MARY, y_pred_MARY)
+
+    return Bhalf, fit_result, fit_error, R2
+
+
+def Gauss_to_MHz(Gauss: float) -> float:
+    """Convert Gauss to MHz.
+
+    Args:
+            Gauss (float): The magnetic flux density in Gauss (G).
+
+    Returns:
+            float: The magnetic flux density converted to MHz.
+    """
+    return Gauss / (1e-10 * -C.g_e * C.mu_B / C.h)
+
+
+def Gauss_to_angular_frequency(Gauss: float) -> float:
+    """Convert Gauss to angular frequency.
+
+    Args:
+            Gauss (float): The magnetic flux density in Gauss (G).
+
+    Returns:
+            float: The magnetic flux density converted to angular
+            frequency (rad/s/T).
+    """
+    return Gauss * (C.mu_B / C.hbar * -C.g_e / 1e10)
+
+
+def Gauss_to_mT(Gauss: float) -> float:
+    """Convert Gauss to millitesla.
+
+    Args:
+            Gauss (float): The magnetic flux density in Gauss (G).
+
+    Returns:
+            float: The magnetic flux density converted to millitesla
+            (mT).
+    """
+    return Gauss / 10
+
+
+def Lorentzian(B: np.ndarray, amplitude: float, Bhalf: float) -> np.ndarray:
+    """Lorentzian function for MARY spectra.
+
+    More information in `radicalpy.utils.Bhalf_fit` (where this is
+    used).
+
+    Args:
+            B (np.ndarray): The x-axis magnetic field values.
+            amplitude (float): The amplitude of the saturation field value.
+            Bhalf (float): The magnetic field strength at half the
+                saturation field value.
+
+    Returns:
+            np.ndarray: Lorentzian function for MARY spectrum.
+
+    """
+    return (amplitude / Bhalf**2) - (amplitude / (B**2 + Bhalf**2))
+
+
+def MHz_in_angular_frequency(MHz: float) -> float:
+    """Convert MHz into angular frequency.
+
+    Args:
+            MHz (float): The angular frequency in MHz/T
+
+    Returns:
+            float: The angular frequency converted to rad/s/T.
+    """
+    return MHz * (2 * np.pi)
+
+
+def MHz_to_Gauss(MHz: float) -> float:
+    """Convert Megahertz to Gauss.
+
+    Args:
+            MHz (float): The frequency in Megahertz (MHz).
+
+    Returns:
+            float: Megahertz (MHz) converted to Gauss (G).
+    """
+    return MHz / (1e-10 * -C.g_e * C.mu_B / C.h)
+
+
+def MHz_to_mT(MHz: float) -> float:
+    """Convert Megahertz to milltesla.
+
+    Args:
+            MHz (float): The frequency in Megahertz (MHz).
+
+    Returns:
+            float: Megahertz (MHz) converted to millitesla (mT).
+    """
+    return MHz / (1e-9 * -C.g_e * C.mu_B / C.h)
+
+
+def angular_frequency_in_MHz(ang_freq: float) -> float:
+    """Convert angular frequency into MHz.
+
+    Args:
+            ang_freq (float): The angular frequency in rad/s/T.
+
+    Returns:
+            float: The angular frequency converted to MHz/T.
+    """
+    return ang_freq / (2 * np.pi)
+
+
+def angular_frequency_to_Gauss(ang_freq: float) -> float:
+    """Convert angular frequency to Gauss.
+
+    Args:
+            ang_freq (float): The angular frequency in rad/s/T.
+
+    Returns:
+            float: The angular frequency converted to Gauss (G).
+    """
+    return ang_freq / (C.mu_B / C.hbar * -C.g_e / 1e10)
+
+
+def angular_frequency_to_mT(ang_freq: float) -> float:
+    """Convert angular frequency to millitesla.
+
+    Args:
+            ang_freq (float): The angular frequency in rad/s/T.
+
+    Returns:
+            float: The angular frequency converted to millitesla (mT).
+    """
+    return ang_freq / (C.mu_B / C.hbar * -C.g_e / 1e9)
+
+
+def autocorrelation(data: np.ndarray, factor: int = 1) -> np.ndarray:
+    """Calculate the autocorrelation of a trajectory.
+
+    An FFT-based implementation of the autocorrelation for Monte Carlo
+    or molecular dynamics trajectories (or any other time dependent
+    value).
+
+    Args:
+            data (np.ndarray): The time dependent trajectory.
+            factor (int): Data length reduction factor.
+
+    Returns:
+            np.ndarray: The autocorrelation of the trajectory.
+    """
+    datap = ifftshift((data - np.average(data)) / np.std(data))
+    n = datap.shape[0]
+    datap = np.r_[datap[: n // factor], np.zeros_like(datap), datap[n // factor :]]
+    f = fft(datap)
+    p = np.absolute(f) ** 2
+    pi = ifft(p)
+    result = np.real(pi)[: n // factor] / np.arange(n, 0, -1)[: n // factor]
+    result = np.delete(result, 0)
+    return result
+
+
+def cartesian_to_spherical(
+    x: float | np.ndarray, y: float | np.ndarray, z: float | np.ndarray
+) -> Tuple[float | np.ndarray, float | np.ndarray, float | np.ndarray]:
+    """Convert Cartesian coordinates to spherical coordinates.
+
+    Args:
+            x (float or np.ndarray): Coordinate(s) in the x plane.
+            y (float or np.ndarray): Coordinate(s) in the y plane.
+            z (float or np.ndarray): Coordinate(s) in the z plane.
+
+    Returns:
+            r (float or np.ndarray): The radial distance(s).
+            theta (float or np.ndarray): The polar angle(s).
+            phi (float or np.ndarray): The azimuthal angle(s).
+    """
+    r = np.sqrt(x**2 + y**2 + z**2)
+    theta = np.arccos(z / r)
+    phi = np.arctan2(y, x)
+    return r, theta, phi
+
+
+def mT_to_Gauss(mT: float) -> float:
+    """Convert millitesla to Gauss.
+
+    Args:
+            mT (float): The magnetic flux density in millitesla (mT).
+
+    Returns:
+            float: The magnetic flux density converted to Gauss (G).
+    """
+    return mT * 10
+
+
+def mT_to_MHz(mT: float) -> float:
+    """Convert millitesla to Megahertz.
+
+    Args:
+            mT (float): The magnetic flux density in millitesla (mT).
+
+    Returns:
+            float: The magnetic flux density converted to Megahertz (MHz).
+    """
+    return mT * (1e-9 * -C.g_e * C.mu_B / C.h)
+
+
+def mT_to_angular_frequency(mT: float) -> float:
+    """Convert millitesla to angular frequency.
+
+    Args:
+            mT (float): The magnetic flux density in millitesla (mT).
+
+    Returns:
+            float: The magnetic flux density converted to angular frequency (rad/s/T).
+    """
+    return mT * (C.mu_B / C.hbar * -C.g_e / 1e9)
+
+
+def spectral_density(omega: float, tau_c: float) -> float:
+    """Frequency at which the motion of the particle exists.
+
+    Args:
+            omega (float): The Larmor frequency of the electron.
+            tau_c (float): The rotational correlation time.
+
+    Returns:
+            float: Spectral density frequency.
+    """
+    return tau_c / (1 + omega**2 * tau_c**2)
+
+
+def anisotropy_check(
+    theta: float | np.ndarray, phi: float | np.ndarray
+) -> Tuple[np.ndarray, np.ndarray]:
+    if isinstance(theta, float):
+        theta = np.array([theta])
+    if isinstance(phi, float):
+        phi = np.array([phi])
+    if theta.min() < 0 or np.pi < theta.max():
+        raise ValueError("Value of `theta` needs to be between 0 and pi!")
+    if min(phi) < 0 or 2 * np.pi < max(phi):
+        raise ValueError("Value of `phi` needs to be between 0 and 2*pi!")
+    lt, lp = len(theta), len(phi)
+    if lt > 1 and lp > 1:
+        # theta odd, phi even
+        if lt % 2 == 0:
+            raise ValueError("Number of `len(theta)` needs to be odd!")
+        if lp % 2 == 1:
+            raise ValueError("Number of `len(phi)` needs to be even!")
+    return theta, phi
+
+
+def _check_full_sphere(theta: np.ndarray, phi: np.ndarray) -> Tuple[int, int]:
+    nth, nph = len(theta), len(phi)
+    if not np.all(np.isclose(theta, np.linspace(0, np.pi, nth))):
+        raise ValueError(
+            "Not a full sphere: `theta` should be `linspace(0, np.pi, ntheta)`"
+        )
+    if not np.all(np.isclose(phi, np.linspace(0, 2 * np.pi, nph))):
+        raise ValueError(
+            "Not a full sphere: `phi` should be `linspace(0, np.pi, nphi)`"
+        )
+    return nth, nph
+
+
+def spherical_average(
+    product_yield: np.ndarray, theta: np.ndarray, phi: np.ndarray
+) -> float:
+    """Spherical average of anisotropic product yields.
+
+    Args:
+            product_yield (np.ndarray): The anisotropic product
+                yields.
+            theta (np.ndarray): The angles theta by which the
+                anisotropic product yields were calculated.
+            phi (np.ndarray): The angles phi by which the anisotropic
+                product yields were calculated.
+
+    Returns:
+            float: The spherical average of the anisotropic product
+                yields.
+    """
+    theta, phi = anisotropy_check(theta, phi)
+    nth, nph = _check_full_sphere(theta, phi)
+
+    wt = 4 * np.ones(nth)
+    wt[2:-2:2] = 2
+    wt[0] = wt[-1] = 1
+
+    wp = 4 * np.ones(nph)
+    wp[0:-1:2] = 2
+    sintheta = np.sin(np.linspace(0, np.pi, nth))
+
+    spherical_avg = sum(
+        product_yield[i, j] * sintheta[i] * wt[i] * wp[j]
+        for i in range(nth)
+        for j in range(nph)
+    )
+
+    return spherical_avg * theta[1] * phi[1] / (4 * np.pi) / 9
+
+
+def spherical_to_cartesian(
+    theta: float | np.ndarray, phi: float | np.ndarray
+) -> np.ndarray:
+    """Spherical coordinates to Cartesian coordinates.
+
+    Args:
+            theta (float or np.ndarray): The polar angle(s).
+            phi (float or np.ndarray): The azimuthal angle(s).
+
+    Returns:
+            np.ndarray: The Cartesian coordinates.
+    """
+    return np.array(
+        [
+            np.sin(theta) * np.cos(phi),
+            np.sin(theta) * np.sin(phi),
+            np.cos(theta),
+        ]
+    )
+
+
+def yield_anisotropy(
+    product_yield: np.ndarray, theta: np.ndarray, phi: np.ndarray
+) -> Tuple[float, float]:
+    """Calculate the yield anisotropy.
+
+    Args:
+            product_yield (np.ndarray): The anisotropic product yields.
+            theta (np.ndarray): The angles theta by which the
+                anisotropic product yields were calculated.
+            phi (np.ndarray): The angles phi by which the anisotropic
+                product yields were calculated.
+
+    Returns:
+            (float, float):
+            - delta_phi (float): Maximum yield - minimum yield.
+            - gamma (float): delta_phi / spherical average.
+
+    """
+    delta_phi = product_yield.max() - product_yield.min()
+    yield_av = spherical_average(product_yield, theta, phi)
+    gamma = delta_phi / yield_av
+    return delta_phi, gamma
