a2a5713fc85d9c82f5f1e11dc12bf605136ce3c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:32 +0000

    Add package airbyte-source-zendesk-support/0.10.4/airbyte-source-zendesk-support-0.10.4.tar.gz

---
+++ packages/airbyte-source-zendesk-support/airbyte-source-zendesk-support-0.10.4.tar.gz/airbyte-source-zendesk-support-0.10.4/setup.py
@@ -0,0 +1,20 @@
+#
+# Copyright (c) 2023 Airbyte, Inc., all rights reserved.
+#
+
+
+from setuptools import find_packages, setup
+
+MAIN_REQUIREMENTS = ["airbyte-cdk", "pytz"]
+
+TEST_REQUIREMENTS = ["pytest~=6.1", "pytest-mock~=3.6", "connector-acceptance-test", "requests-mock==1.9.3"]
+
+setup(
+    description="Source implementation for Zendesk Support.",
+    packages=find_packages(),
+    install_requires=MAIN_REQUIREMENTS,
+    package_data={"": ["*.json", "schemas/*.json", "schemas/shared/*.json"]},
+    extras_require={
+        "tests": TEST_REQUIREMENTS,
+    },
+)
