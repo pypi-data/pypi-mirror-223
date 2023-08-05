e3a7f319bff0a5087bff561b98c1869a8f5339e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:21 +0000

    Add package syntaxlight/0.1.0/syntaxlight-0.1.0-py3-none-any.whl

---
+++ packages/syntaxlight/syntaxlight-0.1.0-py3-none-any.whl/syntaxlight/__init__.py
@@ -0,0 +1,5 @@
+from .syntax_parse import parse, get_lexer, parse_file, get_tokens
+from .export import export_css
+from .example import example_display
+from .ast import display_ast
+from .language import is_language_support, clean_language
