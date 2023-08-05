0b97ec5cb8b0da6652e392c31eb5160bf64c20fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:50 +0000

    Add package pyEager/0.1.2/pyEager-0.1.2.tar.gz

---
+++ packages/pyEager/pyEager-0.1.2.tar.gz/pyEager-0.1.2/pyEager/__init__.py
@@ -0,0 +1,2 @@
+from pyEager.parsers import parse_sexdeterrmine_json, parse_damageprofiler_json, parse_nuclear_contamination_json, parse_snp_coverage_json, parse_endorspy_json, parse_eager_tsv, parse_general_stats_table
+from pyEager.wrappers import collect_damageprofiler_results, compile_endogenous_table, compile_damage_table, compile_snp_coverage_table
