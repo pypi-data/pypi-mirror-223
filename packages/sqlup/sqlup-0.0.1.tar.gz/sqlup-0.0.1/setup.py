d0bf91937494f6553fc86c541699a120d62c4334
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:28 +0000

    Add package sqlup/0.0.1/sqlup-0.0.1.tar.gz

---
+++ packages/sqlup/sqlup-0.0.1.tar.gz/sqlup-0.0.1/setup.py
@@ -0,0 +1,23 @@
+﻿# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='sqlup',
+    version='0.0.1',
+    description='mysql_laod',
+    author='sang',
+    author_email='dnl6098@gamil.com',
+    url='https://github.com/wisangyun/MyProject',
+    packages=find_packages(exclude=[]),
+    python_requires='>=3.6',
+    package_data={},
+    zip_safe=False,
+    install_requires=['pandas','sqlalchemy','mysql-connector-python'],
+    classifiers=[
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    ],
+)
---
+++ packages/sqlup/sqlup-0.0.1.tar.gz/sqlup-0.0.1/sqls/sqls.py
@@ -0,0 +1,9 @@
+﻿from sqlalchemy import create_engine
+import mysql.connector
+
+def sqls(host,port,user,password,db,dfs,tablename):
+    db_connection_str =f'mysql+pymysql://{host}:{password}@{port}/{db}' #접속할 db설정
+    db_connection = create_engine(db_connection_str)
+    conn = db_connection.connect()
+    dfs.to_sql(name=tablename,con=db_connection, if_exists='append',index=False)
+    
