6154a06fcb00f3a62f6a62dddaa2465c5c5f0012
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:17 +0000

    Add package oda-wd-client/0.0.3a1/oda_wd_client-0.0.3a1.tar.gz

---
+++ packages/oda-wd-client/oda_wd_client-0.0.3a1.tar.gz/oda_wd_client-0.0.3a1/oda_wd_client/types/financial_management.py
@@ -0,0 +1,27 @@
+from oda_wd_client.service.financial_management.types import (
+    AccountingJournalData,
+    Company,
+    ConversionRate,
+    ConversionRateType,
+    CostCenter,
+    Currency,
+    JournalEntryLineData,
+    JournalSource,
+    LedgerAccount,
+    LedgerType,
+    SpendCategory,
+)
+
+__all__ = [
+    "ConversionRate",
+    "Currency",
+    "Company",
+    "ConversionRateType",
+    "JournalSource",
+    "LedgerType",
+    "SpendCategory",
+    "CostCenter",
+    "LedgerAccount",
+    "JournalEntryLineData",
+    "AccountingJournalData",
+]
---
+++ packages/oda-wd-client/oda_wd_client-0.0.3a1.tar.gz/oda_wd_client-0.0.3a1/oda_wd_client/types/resource_management.py
@@ -0,0 +1,27 @@
+from oda_wd_client.service.resource_management.types import (
+    CostCenter,
+    SpendCategory,
+    Supplier,
+    SupplierInvoice,
+    SupplierInvoiceLine,
+    TaxApplicability,
+    TaxCode,
+    TaxOption,
+    TaxRate,
+    TaxRateOptionsData,
+    TaxRecoverability,
+)
+
+__all__ = [
+    "CostCenter",
+    "SpendCategory",
+    "Supplier",
+    "SupplierInvoice",
+    "SupplierInvoiceLine",
+    "TaxApplicability",
+    "TaxCode",
+    "TaxOption",
+    "TaxRate",
+    "TaxRateOptionsData",
+    "TaxRecoverability",
+]
