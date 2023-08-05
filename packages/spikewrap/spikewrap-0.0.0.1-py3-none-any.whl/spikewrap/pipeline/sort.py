4ec6d42d6f404b7e60f228fbf2333fb4292a8b71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:16 +0000

    Add package spikewrap/0.0.0.1/spikewrap-0.0.0.1-py3-none-any.whl

---
+++ packages/spikewrap/spikewrap-0.0.0.1-py3-none-any.whl/spikewrap/pipeline/sort.py
@@ -0,0 +1,277 @@
+from __future__ import annotations
+
+import copy
+import os
+import shutil
+from pathlib import Path
+from typing import TYPE_CHECKING, Dict, Literal, Optional, Tuple, Union
+
+if TYPE_CHECKING:
+    from ..data_classes.sorting import SortingData
+
+import platform
+
+import spikeinterface.sorters as ss
+
+from ..pipeline.load_data import load_data_for_sorting
+from ..utils import slurm, utils
+
+
+def run_sorting(
+    preprocessed_data_path: Union[Path, str],
+    sorter: str = "kilosort2_5",
+    sorter_options: Optional[Dict] = None,
+    overwrite_existing_sorter_output: bool = False,
+    verbose: bool = True,
+    slurm_batch: bool = False,
+) -> SortingData:
+    """
+    Run a sorter on pre-processed data. Takes a PreprocessingData (pipeline.data_class)
+    object that contains spikeinterface recording objects for the preprocessing
+    pipeline (or path to existing 'preprocessed' output folder.
+
+    Here, save the preprocessed recording to binary file. Then, run sorting
+    on the saved binary. The preprocessed binary and sorting output are
+    saved in a 'derivatives' folder, in the same top-level folder as 'rawdata'.
+    The folder structure will be the same as in 'rawdata'.
+
+    Parameters
+    ----------
+    preprocessed_data_path : Union[Path, str]
+        Path to previously saved 'preprocessed' directory.
+
+    sorter : str
+        Name of the sorter to use (e.g. "kilosort2_5").
+
+    sorter_options : Dict
+        Kwargs to pass to spikeinterface sorter class.
+
+    overwrite_existing_sorter_output : bool
+         If False, an error will be raised if sorting output already
+         exists. If True, existing sorting output will be overwritten.
+
+    verbose : bool
+        If True, messages will be printed to consolve updating on the
+        progress of preprocessing / sorting.
+
+    slurm_batch : bool
+        If True, the pipeline will be run in a SLURM job. Set False
+        if running on an interactive job, or locally.
+
+    """
+    preprocessed_data_path = Path(preprocessed_data_path)
+
+    sorting_data = load_data_for_sorting(preprocessed_data_path)
+    sorting_data.set_sorter_output_paths(sorter)
+
+    if slurm_batch:
+        local_args = copy.deepcopy(locals())
+        slurm.run_sorting_slurm(**local_args)
+        return sorting_data
+
+    sorter_options_dict = validate_inputs(slurm_batch, sorter, sorter_options, verbose)
+
+    # Load preprocessed data from saved preprocess output path.
+    utils.message_user(
+        f"\nLoading binary preprocessed data from {preprocessed_data_path.as_posix()}\n"
+    )
+
+    # This must be run from the folder that has both sorter output AND rawdata
+    os.chdir(sorting_data.base_path)
+
+    singularity_image, docker_image = get_image_run_settings(sorter)
+
+    utils.message_user(f"Starting {sorter} sorting...")
+
+    # TODO: can remove on API change PR #1908
+    # Also, once #1908 is merged and updated, we need to check
+    # that 'delete_intermediate_files' is not passed by the
+    # user in the config file because it is overridden here.
+    if "kilosort" in sorter:
+        sorter_options_dict.update({"delete_tmp_files": False})
+
+    ss.run_sorter(
+        sorter,
+        sorting_data.data["0-preprocessed"],
+        output_folder=sorting_data.sorting_output_path,
+        singularity_image=singularity_image,
+        docker_image=docker_image,
+        remove_existing_folder=overwrite_existing_sorter_output,
+        **sorter_options_dict,
+    )
+
+    move_singularity_image_if_required(sorting_data, singularity_image, sorter)
+
+    return sorting_data
+
+
+def move_singularity_image_if_required(
+    sorting_data: SortingData,
+    singularity_image: Optional[Union[Literal[True], str]],
+    sorter: str,
+) -> None:
+    """
+    On Linux, images are cased to the sorting_data base folder
+    by default by SpikeInterface. To avoid re-downloading
+    images, these are moved to a pre-determined folder (home
+    for local, pre-set on an HPC). This is only required
+    for singularity, as docker-desktop handles all image
+    storage.
+
+    Parameters
+    ----------
+
+    singularity_image: Optional[Union[Literal[True], Path]]
+        Holds either a path to an existing (stored) sorter, or
+        `True`. If `True`, no stored sorter image exists and so
+        we move it. The next time sorting is performed, it will use
+        this stored image.
+
+    sorter : str
+        Name of the sorter.
+    """
+    if singularity_image is True:
+        assert platform.system() != "Windows", "Docker should be used on windows."
+        store_singularity_image(sorting_data.base_path, sorter)
+
+
+def get_image_run_settings(
+    sorter: str,
+) -> Tuple[
+    Optional[Union[Literal[True], str]], Optional[bool]
+]:  # cannot set this to Literal[True], for unknown reason.
+    """
+    Determine how to run the sorting, either locally or in a container
+    if required (e.g. kilosort2_5). On windows, Docker is used,
+    otherwise singularity. Docker images are handled by Docker-desktop,
+    but singularity image storage is handled internally, see
+    `move_singularity_image_if_required()`.
+
+    Parameters
+    ----------
+
+    sorter : str
+        Sorter name.
+    """
+    can_run_locally = ["spykingcircus", "mountainsort5", "tridesclous"]
+
+    if sorter in can_run_locally:
+        singularity_image = docker_image = None
+    else:
+        if platform.system() == "Windows":
+            singularity_image = None
+            docker_image = True
+        else:
+            singularity_image = get_singularity_image(sorter)
+            docker_image = None
+
+    return singularity_image, docker_image
+
+
+def store_singularity_image(base_path: Path, sorter: str) -> None:
+    """
+    When running locally, SpikeInterface will pull the docker image
+    to the current working directly. Move this to home/.swc_ephys
+    so they can be used again in future and are centralised.
+
+    Parameters
+    ----------
+    base_path : Path
+        Base-path on the SortingData object, the path that holds
+        `rawdata` and `derivatives` folders.
+
+    sorter : str
+        Name of the sorter for which to store the image.
+    """
+    path_to_image = base_path / utils.get_sorter_image_name(sorter)
+    shutil.move(path_to_image, utils.get_local_sorter_path(sorter).parent)
+
+
+def get_singularity_image(sorter: str) -> Union[Literal[True], str]:
+    """
+    Get the path to a pre-installed system singularity image. If none
+    can be found, set to True. In this case SpikeInterface will
+    pull the image to the current working directory, and
+    this will be moved after sorting
+    (see store_singularity_image).
+
+    Parameters
+    ----------
+    sorter : str
+        Name of the sorter to get the image for.
+
+    Returns
+    -------
+    singularity_image [ Union[Literal[True], str]
+        If `str`, the path to the singularity image. Otherwise if `True`,
+        this tells SpikeInterface to pull the image.
+    """
+    singularity_image: Union[Literal[True], str]
+
+    if utils.get_hpc_sorter_path(sorter).is_file():
+        singularity_image = str(utils.get_hpc_sorter_path(sorter))
+
+    elif utils.get_local_sorter_path(sorter).is_file():
+        singularity_image = str(utils.get_local_sorter_path(sorter))
+    else:
+        singularity_image = True
+
+    return singularity_image
+
+
+def validate_inputs(
+    slurm_batch: bool, sorter: str, sorter_options: Optional[Dict], verbose: bool
+) -> Dict:
+    """
+    Check that the sorter is valid, singularity is installed and format
+    the dictionary of options to pass to the sorter.
+
+    Parameters
+    ----------
+    slurm_batch : bool
+        Whether the run is a SLURM batch. This must be False, otherwise
+        indicates some recursion has occurred when SLURM run itself
+        calls this function.
+
+    sorter : str
+        Name of the sorter.
+
+    sorter_options : Optional[Dict]
+        Options to pass to the SpikeInterface sorter. If None, no options
+        are passed.
+
+    verbose : bool
+        Whether SpikeInterface sorting is run in 'verbose' mode.
+
+    Returns
+    -------
+    sorter_options_dict : Dict
+        A dictionary of configurations to run the sorter with, these
+        are passed to SpikeInterface sorter.
+    """
+    assert slurm_batch is False, "SLURM run has slurm_batch set True"
+
+    supported_sorters = [
+        "kilosort2",
+        "kilosort2_5",
+        "kilosort3",
+        "mountainsort5",
+        "spykingcircus",
+        "tridesclous",
+    ]
+
+    assert (
+        sorter in supported_sorters
+    ), f"sorter {sorter} is invalid, must be one of: {supported_sorters}"
+
+    assert (
+        utils.check_singularity_install()
+    ), "Singularity must be installed to run sorting."
+
+    sorter_options_dict = {}
+    if sorter_options is not None and sorter in sorter_options:
+        sorter_options_dict = sorter_options[sorter]
+
+    sorter_options_dict.update({"verbose": verbose})
+
+    return sorter_options_dict
---
+++ packages/spikewrap/spikewrap-0.0.0.1-py3-none-any.whl/spikewrap/utils/slurm.py
@@ -0,0 +1,222 @@
+import datetime
+import subprocess
+from pathlib import Path
+from typing import Callable, Dict, Literal, Union
+
+import submitit
+
+from . import utils
+
+
+def run_job(kwargs, command_func: Callable, command_name: str) -> None:
+    """
+    Run a job (e.g. run_full_pipeline, run_sorting) on SLURM.
+
+    Parameters
+    ----------
+    kwargs : Dict
+        Keyword arguments passed to run_full_pipeline.
+
+    command_func : Callable
+        The function to run (e.g. `run_full_pipeline()`.
+
+    command_name : str
+        The name of the command, typically command_func.__name__,
+        formatted for logging.
+    """
+    slurm_opts = kwargs.pop("slurm_batch")
+    executor = get_executor(kwargs)
+
+    job = executor.submit(
+        wrap_function_with_env_setup, command_func, slurm_opts, **kwargs
+    )
+
+    if isinstance(slurm_opts, dict) and "wait" in slurm_opts and slurm_opts["wait"]:
+        job.wait()
+
+    send_user_start_message(command_name, job, kwargs)
+
+
+def run_full_pipeline_slurm(**kwargs) -> None:
+    """
+    Run the entire preprocessing pipeline in a SLURM job.
+
+    This takes the kwargs passed to the original call of the
+    preprocessing function, and feeds them back to the function
+    from within the SLURM job.
+
+    Parameters
+    ----------
+    kwargs : Dict
+        Keyword arguments passed to run_full_pipeline.
+
+    Notes
+    -----
+    The import must occur here to avoid recursive imports.
+    """
+    from ..pipeline.full_pipeline import run_full_pipeline
+
+    run_job(kwargs, run_full_pipeline, "Full pipeline")
+
+
+def run_sorting_slurm(**kwargs) -> None:
+    """
+    Run the sorting pipeline from within a SLURM job.
+
+    See run_full_pipeline_slurm for details, this is identical
+    except it is for run_sorting rather than run_full_pipeline.
+
+    Notes
+    -----
+    The import must occur here to avoid recursive imports.
+    """
+    from ..pipeline.sort import run_sorting
+
+    run_job(kwargs, run_sorting, "Sorting")
+
+
+# Utils --------------------------------------------------------------------------------
+
+
+def get_executor(kwargs: Dict) -> submitit.AutoExecutor:
+    """
+    Return the executor object that defines parameters
+    of the SLURM node to request and the path to
+    logs.
+
+    Parameters
+    ----------
+    kwargs : Dict
+        Keyword arguments passed to the main running function
+        (e.g. run_full_pipeline, run_sorting).
+
+    Returns
+    -------
+    executor : submitit.AutoExecutor
+        submitit executor object defining requested SLURM
+        node parameters.
+    """
+    log_path = make_job_log_output_path(kwargs)
+
+    print(f"\nThe SLURM batch output logs will " f"be saved to {log_path}\n")
+
+    executor = submitit.AutoExecutor(
+        folder=log_path,
+    )
+
+    executor.update_parameters(
+        nodes=1,
+        mem_gb=40,
+        timeout_min=24 * 60,
+        cpus_per_task=8,
+        tasks_per_node=1,
+        gpus_per_node=1,
+        slurm_gres="gpu:1",
+        slurm_partition="gpu",
+    )
+
+    return executor
+
+
+def wrap_function_with_env_setup(
+    function: Callable, slurm_opts: Union[Literal[True], Dict], **kwargs
+) -> None:
+    """
+    Set up the environment from within the SLURM job, prior
+    to running the processing function (e.g. run_full_pipeline).
+
+    This is required to set up the conda environment within the job
+    or the processing function will fail.
+
+    Parameters
+    ----------
+    function : Callable
+        The ephys processing function to run in the SLURM job
+        e.g. run_full_pipeline, run_sorting
+
+    slurm_opts : Union[Literal[True], Dict]
+        A kwarg passed to the processing function (e.g. run_full_pipeline)
+        indicating whether to run in the SLURM job. If True or a Dict,
+        the SLURM job is run. If a dict, the environment setup
+        can be passed in the 'env_name' field.
+
+    kwargs : Dict
+        Keyword arguments passed to the main running function
+        (e.g. run_full_pipeline, run_sorting)
+    """
+    if isinstance(slurm_opts, dict) and "env_name" in slurm_opts:
+        env_name = slurm_opts["env_name"]
+    else:
+        env_name = "swc_ephys"
+
+    print(f"\nrunning {function.__name__} with SLURM....\n")
+
+    subprocess.run(
+        f"module load miniconda; " f"source activate {env_name};" f"module load cuda",
+        executable="/bin/bash",
+        shell=True,
+    )
+
+    function(**kwargs)
+
+
+def make_job_log_output_path(kwargs: Dict) -> Path:
+    """
+    The SLURM job logs are saved to a folder 'slurm_logs' in the
+    base directory in which the processing is being run
+    (i.e. the folder containing rawdata, derivatives). .
+
+    Parameters
+    ----------
+    kwargs : Dict
+        Keyword arguments passed to the main running function
+        (e.g. run_full_pipeline, run_sorting)
+
+    Returns
+    -------
+    log_path : Path
+        The path to the SLURM log output folder for the current job.
+        The logs are saved to a folder with machine datetime.
+    """
+    now = datetime.datetime.now()
+
+    log_subpath = Path("slurm_logs") / f"{now.strftime('%Y-%m-%d_%H-%M-%S')}"
+
+    if "base_path" in kwargs:
+        log_path = kwargs["base_path"] / log_subpath
+    else:
+        log_path = kwargs["data"].base_path / log_subpath
+
+    log_path.mkdir(exist_ok=True, parents=True)
+
+    return log_path
+
+
+def send_user_start_message(
+    processing_function: str, job: submitit.Job, kwargs: Dict
+) -> None:
+    """
+    Convenience function to print important information
+    regarding the SLURM job.
+
+    Parameters
+    ----------
+    processing_function : str
+        The function being run (i.e. run_full_pipeline, run_sorting)
+
+    job : submitit.job
+        submitit.job object holding the SLURM job_id
+
+    kwargs : Dict
+        Keyword arguments passed to the main running function
+        (e.g. run_full_pipeline, run_sorting)
+    """
+    utils.message_user(
+        f"{processing_function} submitted to SLURM with job id {job.job_id}\n"
+        f"with arguments{kwargs}"
+    )
+
+
+def is_slurm_installed():
+    slurm_installed = subprocess.run("sinfo -v", shell=True).returncode == 0
+    return slurm_installed
