9307d0111e3f71760710326c6f589387fcb6eb8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:03 +0000

    Add package pyrfu/2.4.6/pyrfu-2.4.6-py3-none-any.whl

---
+++ packages/pyrfu/pyrfu-2.4.6-py3-none-any.whl/pyrfu/tests/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = "Louis Richard"
+__email__ = "louisr@irfu.se"
+__copyright__ = "Copyright 2020-2023"
+__license__ = "MIT"
+__version__ = "2.4.2"
+__status__ = "Prototype"
