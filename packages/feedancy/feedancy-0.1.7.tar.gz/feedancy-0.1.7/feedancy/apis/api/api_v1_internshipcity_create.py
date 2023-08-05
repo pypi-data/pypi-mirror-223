52d6953fbfcb58f0825ca7b493db399776689456
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:03 +0000

    Add package feedancy/0.1.7/feedancy-0.1.7.tar.gz

---
+++ packages/feedancy/feedancy-0.1.7.tar.gz/feedancy-0.1.7/feedancy/apis/api/api_v1_internshipcity_create.py
@@ -0,0 +1,50 @@
+from __future__ import annotations
+
+import datetime
+import pydantic
+import typing
+
+from pydantic import BaseModel
+
+from feedancy.lib.base import BaseApi
+from feedancy.lib.request import ApiRequest
+from feedancy.lib import json
+class InternshipCity(BaseModel):
+    city: int 
+    internship: int 
+
+def make_request(self: BaseApi,
+
+    __request__: InternshipCity,
+
+
+) -> InternshipCity:
+    
+
+    
+    body = __request__
+    
+
+    m = ApiRequest(
+        method="POST",
+        path="/api/v1/internshipcity/".format(
+            
+        ),
+        content_type="application/json",
+        body=body,
+        headers=self._only_provided({
+        }),
+        query_params=self._only_provided({
+        }),
+        cookies=self._only_provided({
+        }),
+    )
+    return self.make_request({
+    
+        "200": {
+            
+                "application/json": InternshipCity,
+            
+        },
+    
+    }, m)
