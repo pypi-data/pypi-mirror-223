4075ef9b2f180c52621f482bf968f85e5372511c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:45 +0000

    Add package HABZONEpy/1.0.2/HABZONEpy-1.0.2.tar.gz

---
+++ packages/HABZONEpy/HABZONEpy-1.0.2.tar.gz/HABZONEpy-1.0.2/README.md
@@ -0,0 +1,17 @@
+# HABZONEpy
+funcion la cual calcula la zona de habitabilidad.
+
+para ellos se necesitan los siguientes datos de entrada que seran ingresados por el usuario:
+
+    -la temperatura *EN KELVIN* de la estrella 
+    -el radio de la estrella *EN KILOMETROS*.
+
+finalmente el codigo entregara informacion sobre la zona de habitabilidad (los cuales seran un rango de distancias en kilometros) además de un gráfico donde se verá dicha zona *EN UNIDADES ASTRONOMICAS*
+
+### versiones que utilizamos
+
+version de python : 3.10 
+
+version de matplotlib : 3.5.1
+
+version de numpy : 1.21.5
---
+++ packages/HABZONEpy/HABZONEpy-1.0.2.tar.gz/HABZONEpy-1.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup #no es necesario añadirlo como dependencia
+
+with open('README.md', 'r') as fh: #saltarselo si no tienes el README en la carpeta del projecto
+    long_description = fh.read()
+
+setup(
+      name = 'HABZONEpy', # aqui el nombre de la carpeta donde esta tu modulo/archivo/programa
+      version = '1.0.2', # la version correspondiente del programa, VERSION, no commit de github
+      license = 'MIT',
+      description = 'codigo para visualizar y encontrar la zona habitable', # breve descripcion
+      long_description = long_description,
+      long_description_content_type = 'text/markdown',
+      author = 'Ignacio Solís, Joaquin Lopez, Ignacio Cordova',
+      install_requires = ['numpy', 'matplotlib'], #aqui van tus dependecias de tu programa, como numpy, pandas, cosas asi, esto hara que se instalen solos cuando instalen tu modulo si es que no tienen los necesarios
+      url = 'https://github.com/nachowo21/HABZONEpy.git', # url de tu github, es opcional pero queda bien
+      )
