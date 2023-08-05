43ca21082a41530e54695291b0618ae73f97bd23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:50 +0000

    Add package fairscape-cli/0.1.7a1/fairscape_cli-0.1.7a1-py3-none-any.whl

---
+++ packages/fairscape-cli/fairscape_cli-0.1.7a1-py3-none-any.whl/fairscape_cli/models/computation.py
@@ -0,0 +1,28 @@
+from fairscape_cli.models.base import FairscapeBaseModel
+
+from typing import (
+    Optional,
+    List,
+    Union,
+    Dict,
+)
+from pydantic import (
+    Field,
+    AnyUrl
+)
+import re
+from datetime import datetime
+
+
+class Computation(FairscapeBaseModel):
+    metadataType: str = Field(default="https://w3id.org/EVI#Computation")
+    runBy: str
+    dateCreated: str 
+    description: str = Field(min_length=10, max_length=2056)
+    associatedPublication: Optional[str] = Field(default=None)
+    additionalDocumentation: Optional[str] = Field(default=None)
+    command: Optional[Union[List[str], str]] 
+    usedSoftware: Optional[List[str]]
+    calledBy: Optional[str]
+    usedDataset: Optional[Union[List[str], str]]
+    generated: Optional[Union[str,List[str]]]
