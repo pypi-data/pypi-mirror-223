9900d10d565c203584fc97cb5d234ce5c7a48a78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:38 +0000

    Add package yep-detectors/1.0.3/yep_detectors-1.0.3.tar.gz

---
+++ packages/yep-detectors/yep_detectors-1.0.3.tar.gz/yep_detectors-1.0.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="yep_detectors",
+    version="1.0.3",
+    packages=find_packages(),
+    install_requires=[
+    ],
+    data_files=[
+        "decomp.jar"
+    ],
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "yep_detector = yep_detector:main",
+        ],
+    },
+)
