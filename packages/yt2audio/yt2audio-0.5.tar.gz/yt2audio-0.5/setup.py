72ecf432ca7da7e3e370a6364ccbf67f848ce5c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:57 +0000

    Add package yt2audio/0.5/yt2audio-0.5.tar.gz

---
+++ packages/yt2audio/yt2audio-0.5.tar.gz/yt2audio-0.5/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="yt2audio",
+    version="0.5",
+    author="MRX",
+    description="YouTube Audio Downloader",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    author_email = 'trymrx@pm.me',
+    url = 'https://github.com/isuruwa/YT2AUDIO',
+    download_url = 'https://github.com/isuruwa/YT2AUDIO/blob/main/dist/yt2audio-1.0.tar.gz',
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "yt2audio = yt2audio.__main__:main",
+        ],
+    },
+    install_requires=[
+        "yt-dlp",
+        "ffmpeg",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
