c0ef366e9f6189ff6e5285322ecc27655dee987b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:37 +0000

    Add package auth-lib-profcomff/2023.8.6/auth_lib_profcomff-2023.8.6.tar.gz

---
+++ packages/auth-lib-profcomff/auth_lib_profcomff-2023.8.6.tar.gz/auth_lib_profcomff-2023.8.6/setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8") as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name="auth_lib_profcomff",
+    version="2023.08.06",
+    author="Semyon Grigoriev",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/profcomff/auth-lib",
+    packages=find_packages(),
+    install_requires=["requests", "aiohttp", "setuptools"],
+    extras_require={
+        "fastapi": ["fastapi", "starlette", "pydantic", "pydantic_settings"],
+        "testing": ["pytest"],
+    },
+    entry_points={"pytest11": ["pytest_auth_lib = auth_lib.testing"]},
+    classifiers=[
+        "Programming Language :: Python :: 3.11",
+    ],
+)
