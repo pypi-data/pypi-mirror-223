4b1577c68523bfda4dff130745a44a60ed33c2a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:21 +0000

    Add package MeUtils/2023.8.4.12.20.26/MeUtils-2023.8.4.12.20.26-py3-none-any.whl

---
+++ packages/MeUtils/MeUtils-2023.8.4.12.20.26-py3-none-any.whl/meutils/serving/celery/tasks.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : AI.  @by PyCharm
+# @File         : app
+# @Time         : 2023/8/4 11:15
+# @Author       : betterme
+# @WeChat       : meutils
+# @Software     : PyCharm
+# @Description  : https://blog.csdn.net/weixin_43354181/article/details/126753700
+
+
+from meutils.pipe import *
+from celery import Celery, shared_task
+
+app = Celery(
+    'MeutilsCelery',
+    broker=os.getenv('REDIS_URL', 'redis://127.0.0.1:6379'),
+    backend=os.getenv('REDIS_URL', 'redis://127.0.0.1:6379'),
+    # include=[
+    #     'meutils.serving.celery.app'
+    # ]
+)
+
+
+# @app.task(autoretry_for=(Exception,), default_retry_delay=10, retry_kwargs={'max_retries': 5})
+# def proxy_task(method, url, **kwargs):
+#     """request"""
+#     response = requests.request(method, url, **kwargs).json()
+#     return response
+
+
+@shared_task(autoretry_for=(Exception,),  retry_kwargs={'max_retries': 5})
+def proxy_task(method, url, **kwargs):
+    """request"""
+    response = requests.request(method, url, **kwargs).json()
+    return response
