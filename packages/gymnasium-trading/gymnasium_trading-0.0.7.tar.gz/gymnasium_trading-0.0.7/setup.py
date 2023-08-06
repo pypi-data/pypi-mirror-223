fb342ce20cb7ee27c0f06577b684d4ff9d2b677e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:38 +0000

    Add package gymnasium-trading/0.0.7/gymnasium_trading-0.0.7.tar.gz

---
+++ packages/gymnasium-trading/gymnasium_trading-0.0.7.tar.gz/gymnasium_trading-0.0.7/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="gymnasium_trading",
+    version="0.0.7",
+    install_requires=["gymnasium==0.28.1", "pygame==2.5.0"],
+    author='Alejandro Rioja Chocrón',
+    author_email='archocron@gmail.com',
+    url='https://github.com/archocron/gymnasium-trading',
+    packages=['gymnasium_trading'],
+    license='gpl-3.0',
+    description='Gimnasium environment focus on trading strategies',
+    # I explain this later on
+    download_url='https://github.com/archocron/gymnasium-trading/archive/refs/tags/0.0.7.tar.gz',
+
+)
