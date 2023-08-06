188d77204548dd07efe06ccc9654fcf13f978e2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:01 +0000

    Add package zrouter/0.5.11/zrouter-0.5.11.tar.gz

---
+++ packages/zrouter/zrouter-0.5.11.tar.gz/zrouter-0.5.11/src/zrouter/__init__.py
@@ -0,0 +1,33 @@
+from zrouter.router import Router
+from flask import Flask, request 
+from flask.json import JSONEncoder
+import decimal
+
+
+class JsonEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, decimal.Decimal):
+            return float(obj)
+        return JSONEncoder.default(self, obj)
+
+
+class Joker(Flask):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.json_provider_class = JsonEncoder
+        self.json.ensure_ascii = False
+
+
+def get_user_agent():
+    return request.user_agent.string.lower()
+
+
+def get_ip():
+    nodes = request.headers.getlist("X-Forwarded-For")
+    return nodes[0] if nodes else request.remote_addr
+
+def get_rule():
+    return request.url_rule
+
+
+
