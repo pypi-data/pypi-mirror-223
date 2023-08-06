6a01c45c4aa747a1d8364c14982a0bbea03b98d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:58 +0000

    Add package likeinterface/1.0a0/likeinterface-1.0a0-py3-none-any.whl

---
+++ packages/likeinterface/likeinterface-1.0a0-py3-none-any.whl/likeinterface/methods/auth/__init__.py
@@ -0,0 +1,9 @@
+from .get_authorization_information import GetAuthorizationInformationMethod
+from .root_auth import RootAuthMethod
+from .sign_in import SignInMethod
+
+__all__ = (
+    "GetAuthorizationInformationMethod",
+    "RootAuthMethod",
+    "SignInMethod",
+)
