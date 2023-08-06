308f1bc602bae6b900b7724648b80258d46e748f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:42 +0000

    Add package BeyotekTools/0.0.10/BeyotekTools-0.0.10.tar.gz

---
+++ packages/BeyotekTools/BeyotekTools-0.0.10.tar.gz/BeyotekTools-0.0.10/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+# How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+  'Development Status :: 3 - Alpha',
+  'Intended Audience :: Developers',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+
+setup(
+  name='BeyotekTools',
+  version='0.0.10',
+  description='BeyotekTools',
+  long_description=open('README.md').read(),
+  url='http://beyotek.com',
+  author='Beyotek Inc',
+  author_email='chuck@beyotek.com',
+  license='MIT',
+  classifiers=classifiers,
+  keywords='calculator',
+  packages=find_packages(),
+  install_requires=['requests','selenium','bs4','tqdm','pymongo','pytz']
+)
