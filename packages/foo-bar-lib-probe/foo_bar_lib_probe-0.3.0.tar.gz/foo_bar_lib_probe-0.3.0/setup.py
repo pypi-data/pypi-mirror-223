bbd8eb5732ff6d5eeecc0b36521983f636e3c4df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:53 +0000

    Add package foo-bar-lib-probe/0.3.0/foo_bar_lib_probe-0.3.0.tar.gz

---
+++ packages/foo-bar-lib-probe/foo_bar_lib_probe-0.3.0.tar.gz/foo_bar_lib_probe-0.3.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='foo_bar_lib_probe',
+    version='0.3.0',
+    description='A simple Python library for greetings.',
+    long_description='A longer description of A simple Python library for greetings.',
+    author='Imre Tabur',
+    author_email='info@setmy.info',
+    license='MIT',
+    url='https://github.com/setmy-info/setmy.info/tree/develop/incubation/foo_bar_lib_probe',
+    packages=find_packages(),
+    install_requires=[
+        # List requirements here
+    ],
+)
