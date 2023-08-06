701f5b8f7b86d214521632be6eed999b6374ce8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:22 +0000

    Add package nserver/0.4.0/nserver-0.4.0-py3-none-any.whl

---
+++ packages/nserver/nserver-0.4.0-py3-none-any.whl/nserver/_version.py
@@ -0,0 +1,55 @@
+"""Version information for this package."""
+### IMPORTS
+### ============================================================================
+## Standard Library
+
+## Installed
+
+## Application
+
+### CONSTANTS
+### ============================================================================
+## Version Information - DO NOT EDIT
+## -----------------------------------------------------------------------------
+# These variables will be set during the build process. Do not attempt to edit.
+PACKAGE_VERSION = "0.4.0"
+BUILD_VERSION = "0.4.0"
+BUILD_GIT_HASH = "16023e31cef9d194a2b3def96975425cc285305a"
+BUILD_GIT_HASH_SHORT = "16023e3"
+BUILD_GIT_BRANCH = "main"
+BUILD_TIMESTAMP = 1691309381
+BUILD_DATETIME = datetime.datetime.utcfromtimestamp(1691309381)
+
+VERSION_VARS = vars()  # Don't have f-strings until py36
+
+## Version Information Templates
+## -----------------------------------------------------------------------------
+# You can customise the templates used for version information here.
+VERSION_INFO_TEMPLATE_SHORT = "{BUILD_VERSION}"
+VERSION_INFO_TEMPLATE = "{PACKAGE_VERSION} ({BUILD_VERSION})"
+VERSION_INFO_TEMPLATE_LONG = (
+    "{PACKAGE_VERSION} ({BUILD_VERSION}) ({BUILD_GIT_BRANCH}@{BUILD_GIT_HASH_SHORT})"
+)
+VERSION_INFO_TEMPLATE_FULL = (
+    "{PACKAGE_VERSION} ({BUILD_VERSION})\n"
+    "{BUILD_GIT_BRANCH}@{BUILD_GIT_HASH}\n"
+    "Built: {BUILD_DATETIME}"
+)
+
+
+### FUNCTIONS
+### ============================================================================
+def get_version_info_short() -> str:  # pylint: disable=missing-function-docstring
+    return VERSION_INFO_TEMPLATE_SHORT.format(**VERSION_VARS)
+
+
+def get_version_info() -> str:  # pylint: disable=missing-function-docstring
+    return VERSION_INFO_TEMPLATE.format(**VERSION_VARS)
+
+
+def get_version_info_long() -> str:  # pylint: disable=missing-function-docstring
+    return VERSION_INFO_TEMPLATE_LONG.format(**VERSION_VARS)
+
+
+def get_version_info_full() -> str:  # pylint: disable=missing-function-docstring
+    return VERSION_INFO_TEMPLATE_FULL.format(**VERSION_VARS)
