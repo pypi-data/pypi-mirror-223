41d9d00a5d7ccfa0b57fc51aa3ff7a1e367b8f18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:41 +0000

    Add package rebotics-sdk/0.25.4/rebotics_sdk-0.25.4.tar.gz

---
+++ packages/rebotics-sdk/rebotics_sdk-0.25.4.tar.gz/rebotics_sdk-0.25.4/rebotics_sdk/hook/views.py
@@ -0,0 +1,24 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from rebotics_sdk.hook import signals
+from rebotics_sdk.hook.serializers import WebhookDataSerializer
+
+
+class WebhookHandlerViewSet(APIView):
+    permission_classes = []
+
+    def post(self, request, *args, **kwargs):
+        retailer_code = kwargs['retailer_code']
+        serializer = WebhookDataSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        emitter = signals.SignalEmitter(
+            retailer=retailer_code,
+            event=serializer.validated_data['event'],
+            payload=serializer.validated_data['payload'],
+        )
+        emitter.emit()
+
+        return Response(status=status.HTTP_202_ACCEPTED)
