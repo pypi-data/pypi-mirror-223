441dcca2737c96e746e8f064b4aa709d058c0ad5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:50 +0000

    Add package opaw/0.2/opaw-0.2-py3-none-any.whl

---
+++ packages/opaw/opaw-0.2-py3-none-any.whl/opaw/examples/edit.py
@@ -0,0 +1,16 @@
+from opaw.model.edit import EditBot
+from opaw import util
+
+# api key
+util.setup()
+
+# edit
+bot = EditBot()
+prompt = "Hey, tis was my filst car!!"
+instruction = "Fix the spelling mistakes"
+response = bot.create(prompt, instruction=instruction)
+text = response["choices"][0]["text"]
+print("text:", text)
+
+# save history if needed
+util.save_history(bot, "edit-hist.json")
---
+++ packages/opaw/opaw-0.2-py3-none-any.whl/opaw/examples/embedding.py
@@ -0,0 +1,17 @@
+from opaw.model.embedding import EmbeddingBot
+from opaw import util
+
+# api key
+util.setup()
+
+# embedding
+bot = EmbeddingBot()
+prompt = "Cheese is melting on my laptop"
+response = bot.create(prompt)
+embeddings = response["data"][0]["embedding"]
+print("embeddings:", embeddings)
+
+# save history if needed
+util.save_history(bot, "embedding-hist.json")
+
+
