2f00bcef356bd11510966d20feb6328996a6738b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:32 +0000

    Add package distributions-gauussbinom/0.1/distributions_gauussbinom-0.1.tar.gz

---
+++ packages/distributions-gauussbinom/distributions_gauussbinom-0.1.tar.gz/distributions_gauussbinom-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_gauussbinom',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_gauussbinom'],
+      author= 'Sandeep Solanki',
+      author_github = 'https://github.com/manoritesandeep',
+      zip_safe=False)
