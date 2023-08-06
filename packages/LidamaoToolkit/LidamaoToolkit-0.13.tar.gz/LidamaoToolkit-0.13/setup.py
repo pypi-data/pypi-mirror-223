e97b6aca9c407acf4d37d2edc0182e149480b3c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:51 +0000

    Add package LidamaoToolkit/0.13/LidamaoToolkit-0.13.tar.gz

---
+++ packages/LidamaoToolkit/LidamaoToolkit-0.13.tar.gz/LidamaoToolkit-0.13/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+  name='LidamaoToolkit', 
+  version='0.13',
+  author='lidamao',
+  author_email='lee-perfect@qq.com',
+  description='A simple flask utils toolkit',
+  py_modules=["LidamaoToolkit"],
+  install_requires=["Flask==1.0.2"], # 依赖列表
+)
