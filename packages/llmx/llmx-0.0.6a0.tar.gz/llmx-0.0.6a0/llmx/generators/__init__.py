f864347fa536e12cfb2b4a4f2714a85e7c82abfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:23 +0000

    Add package llmx/0.0.6a0/llmx-0.0.6a0.tar.gz

---
+++ packages/llmx/llmx-0.0.6a0.tar.gz/llmx-0.0.6a0/llmx/generators/__init__.py
@@ -0,0 +1,3 @@
+# from .text.textgen import TextGenerator
+from .text.textgen import text_generator
+from .text.base_textgen import TextGenerator
---
+++ packages/llmx/llmx-0.0.6a0.tar.gz/llmx-0.0.6a0/llmx/generators/text/cohere_textgen.py
@@ -0,0 +1,84 @@
+from typing import Union
+from .base_textgen import TextGenerator
+from ...datamodel import TextGenerationConfig, TextGenerationResponse, Message
+from ...utils import cache_request, num_tokens_from_messages
+import os
+import cohere
+from dataclasses import asdict
+
+
+class CohereTextGenerator(TextGenerator):
+    def __init__(
+        self,
+        api_key: str = os.environ.get("COHERE_API_KEY", None),
+        provider: str = "cohere",
+        organization: str = None,
+    ):
+        super().__init__(provider=provider)
+        if api_key is None:
+            raise ValueError(
+                "Cohere API key is not set. Please set the COHERE_API_KEY environment variable."
+            )
+        self.client = cohere.Client(api_key)
+
+    def format_messages(self, messages):
+        prompt = ""
+        for message in messages:
+            if message["role"] == "system":
+                prompt += message["content"] + "\n"
+            else:
+                prompt += message["role"] + ": " + message["content"] + "\n"
+
+        return prompt
+
+    def generate(
+            self, messages: Union[list[dict],
+                                  str],
+            config: TextGenerationConfig = TextGenerationConfig(),
+            **kwargs) -> TextGenerationResponse:
+
+        use_cache = config.use_cache
+        messages = self.format_messages(messages)
+        self.model_name = config.model
+
+        cohere_config = {
+            "model": config.model or "command",
+            "prompt": messages,
+            "max_tokens": config.max_tokens,
+            "temperature": config.temperature,
+            "k": config.top_k,
+            "p": config.top_p,
+            "num_generations": config.n,
+            "stop_sequences": config.stop,
+            "frequency_penalty": config.frequency_penalty,
+            "presence_penalty": config.presence_penalty,
+        }
+
+        cache_key_params = cohere_config | {"messages": messages}
+        if use_cache:
+            response = cache_request(cache=self.cache, params=cache_key_params)
+            if response:
+                return TextGenerationResponse(**response)
+
+        co_response = self.client.generate(**cohere_config)
+
+        response_text = [
+            Message(
+                role="system",
+                content=x.text,
+            )
+            for x in co_response.generations
+        ]
+
+        response = TextGenerationResponse(
+            text=response_text,
+            logprobs=[],  # You may need to extract log probabilities from the response if needed
+            config=cohere_config,
+            usage={},  # You may need to extract usage metrics from the response if needed
+        )
+
+        cache_request(cache=self.cache, params=cache_key_params, values=asdict(response))
+        return response
+
+    def count_tokens(self, text) -> int:
+        return num_tokens_from_messages(text)
