4ec2a011dbc73f7fb3d592fe4d46084a3f13f7dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:09 +0000

    Add package django-error-report-2/0.4.2/django_error_report_2-0.4.2-py3-none-any.whl

---
+++ packages/django-error-report-2/django_error_report_2-0.4.2-py3-none-any.whl/error_report/urls.py
@@ -0,0 +1,13 @@
+"""Urls for django-error-report-2 package."""
+from __future__ import absolute_import, unicode_literals
+
+try:
+    from django.conf.urls import url
+except ImportError:
+    from django.urls import re_path as url
+
+from error_report.views import error_html
+
+urlpatterns = [
+    url(r'^error_html/(?P<error>[0-9]+)/$', error_html, name='error-html-link'),
+]
