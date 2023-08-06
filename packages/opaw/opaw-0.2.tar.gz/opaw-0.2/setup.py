8ec4246df0b8e75836b756005b806d65d1664b36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:51 +0000

    Add package opaw/0.2/opaw-0.2.tar.gz

---
+++ packages/opaw/opaw-0.2.tar.gz/opaw-0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages, find_namespace_packages
+
+setup(
+    name='opaw',
+    packages=find_packages(),
+    version='0.2',
+    license='MIT',
+    description='Unofficial python wrapper of OpenAI API.',
+    author='hiimanget',
+    author_email='hiimanget@gmail.com',
+    url='https://github.com/hiimanget/openai-pw',
+    keywords=['openai', 'python', 'api', 'wrapper'],
+    install_requires=['openai'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"],
+)
