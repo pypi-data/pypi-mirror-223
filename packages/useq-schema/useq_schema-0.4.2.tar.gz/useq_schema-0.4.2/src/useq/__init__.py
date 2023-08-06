586e5770e06ffc36e45976ffdcab405140587261
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:47 +0000

    Add package useq-schema/0.4.2/useq_schema-0.4.2.tar.gz

---
+++ packages/useq-schema/useq_schema-0.4.2.tar.gz/useq_schema-0.4.2/src/useq/__init__.py
@@ -0,0 +1,51 @@
+from useq._actions import AcquireImage, Action, HardwareAutofocus
+from useq._channel import Channel
+from useq._grid import AnyGridPlan, GridFromEdges, GridRelative
+from useq._hardware_autofocus import AnyAutofocusPlan, AutoFocusPlan, AxesBasedAF
+from useq._mda_event import MDAEvent, PropertyTuple
+from useq._mda_sequence import MDASequence
+from useq._position import Position
+from useq._time import (
+    MultiPhaseTimePlan,
+    TDurationLoops,
+    TIntervalDuration,
+    TIntervalLoops,
+)
+from useq._z import (
+    ZAboveBelow,
+    ZAbsolutePositions,
+    ZRangeAround,
+    ZRelativePositions,
+    ZTopBottom,
+)
+
+__all__ = [
+    "AcquireImage",
+    "Action",
+    "AnyAutofocusPlan",
+    "AnyGridPlan",
+    "AutoFocusPlan",
+    "AxesBasedAF",
+    "Channel",
+    "GridFromEdges",
+    "GridRelative",
+    "HardwareAutofocus",
+    "MDAEvent",
+    "MDASequence",
+    "MultiPhaseTimePlan",
+    "Position",
+    "PropertyTuple",
+    "TDurationLoops",
+    "TIntervalDuration",
+    "TIntervalLoops",
+    "ZAboveBelow",
+    "ZAbsolutePositions",
+    "ZRangeAround",
+    "ZRelativePositions",
+    "ZTopBottom",
+]
+
+
+# type ignores because pydantic-compat consumes the kwargs
+MDAEvent.model_rebuild(MDASequence=MDASequence)  # type: ignore  [call-arg]
+Position.model_rebuild(MDASequence=MDASequence)  # type: ignore  [call-arg]
