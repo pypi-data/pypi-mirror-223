176adbe2760d9e35ccc367f38c30a0d7d9c751c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:36 +0000

    Add package ezfsm/0.2.1.2/ezfsm-0.2.1.2.tar.gz

---
+++ packages/ezfsm/ezfsm-0.2.1.2.tar.gz/ezfsm-0.2.1.2/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding:utf-8
+
+from setuptools import find_packages, setup
+
+setup(
+name='ezfsm',
+version='0.2.1.2',
+description='> Easy and micro fsm could used in python3.x and micropython.',
+long_description=open('readme.md', 'r', encoding='UTF-8').read(),
+long_description_content_type = 'text/markdown',
+author="Eagle'sBaby",
+author_email='2229066748@qq.com',
+maintainer="Eagle'sBaby",
+maintainer_email='2229066748@qq.com',
+packages=find_packages(),
+url="https://gitee.com/eagle-s_baby/fsm/blob/master/README.md",
+platforms=["all"],
+license='Apache Licence 2.0',
+classifiers=[
+'Programming Language :: Python',
+'Programming Language :: Python :: 3',
+],
+#install_requires = ["mkr>=0.1.0", 'efr>=0.1.9'],
+keywords = ["fsm", "micropython"],
+python_requires='>=3', 
+)
