ae4589dffc3a613e5aa2b7fd997700d8adea4fdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:39 +0000

    Add package vika/1.3.1/vika-1.3.1.tar.gz

---
+++ packages/vika/vika-1.3.1.tar.gz/vika-1.3.1/vika/types/node.py
@@ -0,0 +1,29 @@
+from enum import Enum
+
+from typing import List, Optional
+from pydantic import BaseModel
+
+
+class NodeTypeEnum(str, Enum):
+    Datasheet = 'Datasheet'
+    Folder = 'Folder'
+    Form = 'Form'
+    Dashboard = 'Dashboard'
+    Mirror = 'Mirror'
+
+
+class NodeListItem(BaseModel):
+    id: str
+    name: str
+    type: NodeTypeEnum
+    icon: str
+    isFav: bool
+
+
+class NodeDetail(NodeListItem):
+    type = NodeTypeEnum.Folder
+    children: Optional[List[NodeListItem]]
+
+class NodeSearchInfo(NodeListItem):
+    permission: int
+    parentId: Optional[str]
