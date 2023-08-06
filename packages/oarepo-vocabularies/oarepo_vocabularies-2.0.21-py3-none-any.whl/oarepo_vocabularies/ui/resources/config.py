e5265030b1e01223a36b25b310d7a30df5f9de13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:49 +0000

    Add package oarepo-vocabularies/2.0.21/oarepo_vocabularies-2.0.21-py3-none-any.whl

---
+++ packages/oarepo-vocabularies/oarepo_vocabularies-2.0.21-py3-none-any.whl/oarepo_vocabularies/ui/resources/config.py
@@ -0,0 +1,50 @@
+import marshmallow as ma
+from flask import current_app
+
+from oarepo_ui.resources.config import RecordsUIResourceConfig
+from oarepo_vocabularies.ui.resources.components import (
+    VocabularyRecordsComponent,
+    DepositVocabularyOptionsComponent,
+)
+
+
+class InvenioVocabulariesUIResourceConfig(RecordsUIResourceConfig):
+    template_folder = "../templates"
+    url_prefix = "/vocabularies/"
+    blueprint_name = "oarepo_vocabularies_ui"
+    ui_serializer_class = (
+        "oarepo_vocabularies.resources.records.ui.VocabularyUIJSONSerializer"
+    )
+    api_service = "vocabularies"
+    layout = "oarepo_vocabularies_ui"
+
+    templates = {
+        "detail": {
+            "layout": "oarepo_vocabularies_ui/detail.html",
+            "blocks": {
+                "record_main_content": "oarepo_vocabularies_ui/main.html",
+                "record_sidebar": "oarepo_vocabularies_ui/sidebar.html",
+                "vocabulary_descendants": "oarepo_vocabularies_ui/descendants.html",
+            },
+        },
+        "search": {"layout": "oarepo_vocabularies_ui/search.html"},
+        "create": {"layout": "oarepo_vocabularies_ui/form.html"},
+        "edit": {"layout": "oarepo_vocabularies_ui/form.html"},
+    }
+
+    routes = {
+        "create": "/<vocabulary_type>/_new",
+        "edit": "/<vocabulary_type>/<pid_value>/edit",
+        "search": "/<vocabulary_type>/",
+        "detail": "/<vocabulary_type>/<pid_value>",
+        "export": "/<vocabulary_type>/<pid_value>/export/<export_format>",
+    }
+
+    components = [VocabularyRecordsComponent, DepositVocabularyOptionsComponent]
+
+    request_vocabulary_type_args = {"vocabulary_type": ma.fields.Str()}
+
+    def vocabulary_props_config(self, vocabulary_type):
+        return current_app.config.get("INVENIO_VOCABULARY_TYPE_METADATA", {}).get(
+            vocabulary_type, {}
+        )
