672869884baaa260847135b66cbf0051d5404e8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:38 +0000

    Add package promptz/0.0.10/promptz-0.0.10.tar.gz

---
+++ packages/promptz/promptz-0.0.10.tar.gz/promptz-0.0.10/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='promptz',
+    version='0.0.10',
+    description='A Python package for interactive prompts',
+    packages=['promptz'],
+    install_requires=requirements,
+    entry_points={
+        "console_scripts": [
+            "pz = promptz.cli:main",
+        ],
+    },
+)
