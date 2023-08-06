cc4fa5aa5a57e62c13e340505d67985ad1daaca5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:59 +0000

    Add package arelle-release/2.12.0/arelle-release-2.12.0.tar.gz

---
+++ packages/arelle-release/arelle-release-2.12.0.tar.gz/arelle-release-2.12.0/arelleCmdLine.py
@@ -0,0 +1,43 @@
+'''
+
+Use this module to start Arelle in command line modes
+
+See COPYRIGHT.md for copyright information.
+'''
+import os
+import sys
+import regex as re
+
+if sys.platform == "darwin" and getattr(sys, 'frozen', False):
+    for i in range(len(sys.path)):  # signed code can't contain python modules
+        sys.path.append(sys.path[i].replace("MacOS", "Resources"))
+
+from arelle.SocketUtils import INTERNET_CONNECTIVITY, OFFLINE, warnSocket
+
+internetConnectivityArgPattern = rf'--({INTERNET_CONNECTIVITY}|{INTERNET_CONNECTIVITY.lower()})'
+internetConnectivityArgRegex = re.compile(internetConnectivityArgPattern)
+internetConnectivityOfflineEqualsRegex = re.compile(f"{internetConnectivityArgPattern}={OFFLINE}")
+prevArg = ''
+for arg in sys.argv[1:]:
+    if (
+            internetConnectivityOfflineEqualsRegex.fullmatch(arg)
+            or (internetConnectivityArgRegex.fullmatch(prevArg) and arg == OFFLINE)
+    ):
+        warnSocket()
+        break
+    prevArg = arg
+
+from arelle.BetaFeatures import BETA_OBJECT_MODEL_FEATURE, enableNewObjectModel
+
+# Model transition must be enabled before any other imports to avoid mixing base classes.
+if f"--{BETA_OBJECT_MODEL_FEATURE}" in sys.argv or f"--{BETA_OBJECT_MODEL_FEATURE.lower()}" in sys.argv:
+    enableNewObjectModel()
+
+from arelle import CntlrCmdLine, CntlrComServer
+
+if '--COMserver' in sys.argv:
+    CntlrComServer.main()
+elif __name__.startswith('_mod_wsgi_') or os.getenv('wsgi.version'):
+    application = CntlrCmdLine.wsgiApplication()
+elif __name__ in ('__main__', 'arelleCmdLine__main__', 'arellecmdline__main__'): #cx_Freeze 5 prepends module name to __main__
+    CntlrCmdLine.main()
