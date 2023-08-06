cc22e7c69db359765e0026ca83a416cbfd787f13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:53 +0000

    Add package TimeanddateScrapper/0.0.3/TimeanddateScrapper-0.0.3-py3-none-any.whl

---
+++ packages/TimeanddateScrapper/TimeanddateScrapper-0.0.3-py3-none-any.whl/TimeanddateScrapper/worldweather.py
@@ -0,0 +1,46 @@
+import os
+import bs4
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+from datetime import datetime
+
+class Weather:
+    
+    def saveToExcel(self):
+        lis = self.worldWideReport()
+        df = pd.DataFrame(data = lis,columns = ['Country','City','Temparature-in-Celcius','Extraction-Datetime'])
+        df.to_csv('weather.csv')
+        print(f"Data saved in {os.getcwd()}\\weather.csv")
+        
+    def worldWideReport(self):
+        try:
+            url= 'https://www.timeanddate.com/weather/?sort=1&low=c'
+            page = requests.get(url)
+            print(page)
+            soup =  BeautifulSoup(page.content,'html.parser')
+            #print(soup.prettify())
+            table = soup.find_all('tr')
+            #print(table)
+            print(type(table))
+            weatherlist = []
+            for i in range(1,len(table)):
+                text = table[i].find_all('td')
+                city_and_country = text[0].get_text()
+                temp = text[len(text)-1].get_text().translate ({ord(c): " " for c in "!@#$%^&*()[]{};:,./<>?\|`~-=_+°C\xa0"}).strip()
+                city_country_split = city_and_country.split(',')
+                if(len(city_country_split) == 2):
+                    city = city_country_split[1].replace('*','').strip()
+                else:
+                    city = city_country_split[2].replace('*','').strip()
+                country = city_country_split[0].replace('*','').strip()
+                #print(temp)
+                weatherlist.append([country,city,temp,str(datetime.now())])
+            return weatherlist
+        
+        except Exception as e:
+            print("Extraction Error: Please try after some time ...")
+                
+        
+        
+        
