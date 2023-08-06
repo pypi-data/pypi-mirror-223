e91f90eb90857de4bbec7afd2613a8ff1d641e69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:08 +0000

    Add package PigBotFramework/4.1.24/PigBotFramework-4.1.24-py2.py3-none-any.whl

---
+++ packages/PigBotFramework/PigBotFramework-4.1.24-py2.py3-none-any.whl/pbf/model/BotPluginsModel.py
@@ -0,0 +1,16 @@
+from . import DictModel
+
+
+class BotPluginsModel(DictModel):
+    db_table = "bot_plugins"
+    map = ['uuid']
+    format_createTable = ["PRIMARY KEY (`id`)"]
+
+    def id(self):
+        return 'int(11) NOT NULL AUTO_INCREMENT'
+
+    def data(self):
+        return 'varchar(1000)'
+
+    def uuid(self):
+        return 'varchar(255) NOT NULL'
