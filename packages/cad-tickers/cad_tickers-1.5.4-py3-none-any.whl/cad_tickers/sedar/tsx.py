80b9e9e79ec45db29b32ed6f6fae489cbf195ad2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:04 +0000

    Add package cad-tickers/1.5.4/cad_tickers-1.5.4-py3-none-any.whl

---
+++ packages/cad-tickers/cad_tickers-1.5.4-py3-none-any.whl/cad_tickers/sedar/tsx.py
@@ -0,0 +1,104 @@
+import requests
+import json
+from cad_tickers.util.utils import get_random_agent
+from datetime import datetime
+from cad_tickers.exchanges.tsx.gql_data import GQL
+from typing import Union
+
+
+def get_ticker_filings(
+    symbol: str,
+    fromDate: str = datetime.today().replace(day=1).strftime("%Y-%m-%d"),
+    toDate: str = datetime.today().strftime("%Y-%m-%d"),
+    limit: int = 100,
+) -> Union[dict, None]:
+    """
+    Parameters:
+        symbol - ticker symbol from tsx, no prefix
+        fromDate - start date to grab documents
+        toDate - end date to grab documents
+        limit - max number of documents to retrieve
+    Returns:
+        dict - :ref:`Quote By Symbol <quote_by_symbol_query>`
+    """
+    payload = GQL.get_company_filings_payload
+    payload["variables"]["symbol"] = symbol
+    payload["variables"]["fromDate"] = fromDate
+    payload["variables"]["toDate"] = toDate
+    payload["variables"]["limit"] = limit
+    url = "https://app-money.tmx.com/graphql"
+    r = requests.post(
+        url,
+        data=json.dumps(payload),
+        headers={
+            "authority": "app-money.tmx.com",
+            "referer": f"https://money.tmx.com/en/quote/{symbol.upper()}",
+            "locale": "en",
+            "Content-Type": "application/json",
+            "User-Agent": get_random_agent(),
+            "Accept": "*/*"
+        },
+    )
+    try:
+        if r.status_code == 403:
+            print(r.text)
+            return {}
+        else:
+            allData = r.json()
+            print(allData)
+            data = allData["data"]
+            return data
+    except KeyError as _e:
+        print(_e, symbol)
+        pass
+
+# TODO rename this later
+def get_news_and_events(
+    symbol: str,
+    page: int = 1,
+    limit: int = 100,
+    locale: str = "en",
+) -> Union[dict, None]:
+    """
+    Parameters:
+        symbol - ticker symbol from tsx, no prefix
+        page - start date to grab documents
+        limit - max number of documents to retrieve
+        locale - language
+    Returns:
+        dict - :ref:`Quote By Symbol <quote_by_symbol_query>`
+    """
+    payload = GQL.get_company_news_events_payload
+    payload["variables"]["symbol"] = symbol
+    payload["variables"]["page"] = page
+    payload["variables"]["limit"] = limit
+    payload["variables"]["locale"] = locale
+    url = "https://app-money.tmx.com/graphql"
+    r = requests.post(
+        url,
+        data=json.dumps(payload),
+        headers={
+            "Content-Type": "application/json",
+            "User-Agent": get_random_agent(),
+            "Accept": "*/*"
+        },
+    )
+    try:
+        # check headings
+        if r.status_code == 403:
+            print(r.text)
+            return {}
+        else:
+            allData = r.json()
+            data = allData["data"]
+            return data
+    except KeyError as _e:
+        return {}
+
+## consider pulling financial information or just downloading it with selenium.
+## could also use yahoo finance instead.
+if __name__ == "__main__":
+    art = get_news_and_events(
+        "T", 1, 108
+    )
+    print(art)
