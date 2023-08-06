24f24b11cb9345128fad4c59ccb8fad67c4bc782
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:58 +0000

    Add package alchemyjsonschema/0.8.0/alchemyjsonschema-0.8.0.tar.gz

---
+++ packages/alchemyjsonschema/alchemyjsonschema-0.8.0.tar.gz/alchemyjsonschema-0.8.0/setup.py
@@ -0,0 +1,54 @@
+# -*- coding:utf-8 -*-
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+try:
+    with open(os.path.join(here, "README.rst")) as f:
+        README = f.read()
+except IOError:
+    README = ""
+
+install_requires = [
+    "sqlalchemy",
+    "jsonschema",
+    "strict-rfc3339",
+    "isodate",  # hmm.
+    "pytz",
+    "magicalimport",
+    "dictknife>=0.7.2",
+]
+
+docs_extras = []
+
+tests_require = ["pytest", "webob"]
+
+testing_extras = tests_require + []
+
+
+setup(
+    name="alchemyjsonschema",
+    version=open("VERSION").read().strip(),
+    description="A package for generating json-schema models from sqlalchemy models.",
+    long_description=README,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: Implementation :: CPython",
+    ],
+    keywords="alchemyjsonschema sqlalchemy jsonschema schema-generation",
+    author="podhmo",
+    author_email="",
+    url="https://github.com/podhmo/alchemyjsonschema",
+    packages=find_packages(exclude=["alchemyjsonschema.tests"]),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    extras_require={"testing": testing_extras, "docs": docs_extras},
+    tests_require=tests_require,
+    license="mit",
+    entry_points="""
+      [console_scripts]
+      alchemyjsonschema = alchemyjsonschema.command.main:main
+      """,
+)
