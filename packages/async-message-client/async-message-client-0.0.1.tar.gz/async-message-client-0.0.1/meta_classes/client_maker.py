c7e250853eb2372d3bfa2e1870a965eed6ed5331
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:20 +0000

    Add package async-message-client/0.0.1/async-message-client-0.0.1.tar.gz

---
+++ packages/async-message-client/async-message-client-0.0.1.tar.gz/async-message-client-0.0.1/meta_classes/client_maker.py
@@ -0,0 +1,30 @@
+import dis
+import inspect
+import logging
+
+logger = logging.getLogger('app.server')
+
+
+class ClientMaker(type):
+    def __init__(self, clsname, bases, clsdict):
+        methods = []
+        for name, _ in inspect.getmembers(self):
+            methods.append(name)
+        for func in clsdict:
+            try:
+                ret = dis.get_instructions(clsdict[func])
+            except TypeError:
+                pass
+            else:
+                for i in ret:
+                    if i.opname == 'LOAD_GLOBAL':
+                        if i.argval not in methods:
+                            methods.append(i.argval)
+        for command in ('accept', 'listen'):
+            if command in methods:
+                raise TypeError('В классе обнаружено использование запрещённого метода')
+        if 'get_message' in methods or 'send_message' in methods:
+            pass
+        else:
+            raise TypeError('Отсутствуют вызовы функций, работающих с сокетами.')
+        super().__init__(clsname, bases, clsdict)
