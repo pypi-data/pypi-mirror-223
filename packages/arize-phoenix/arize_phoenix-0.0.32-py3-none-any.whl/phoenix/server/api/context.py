a9d39aff07630b410318f1a61558211bc8eb881a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:20 +0000

    Add package arize-phoenix/0.0.32/arize_phoenix-0.0.32-py3-none-any.whl

---
+++ packages/arize-phoenix/arize_phoenix-0.0.32-py3-none-any.whl/phoenix/server/api/context.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Optional, Union
+
+from starlette.requests import Request
+from starlette.responses import Response
+from starlette.websockets import WebSocket
+
+from phoenix.core.model_schema import Model
+
+
+@dataclass
+class Context:
+    request: Union[Request, WebSocket]
+    response: Optional[Response]
+    model: Model
+    export_path: Path
+    corpus: Optional[Model] = None
