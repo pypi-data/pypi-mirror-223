586660bbbc0398c3c0e2e6bfc6e5b692d9aa4c48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:53 +0000

    Add package overhave/4.1.3/overhave-4.1.3-py3-none-any.whl

---
+++ packages/overhave/overhave-4.1.3-py3-none-any.whl/overhave/cli/consumers.py
@@ -0,0 +1,20 @@
+import typer
+
+from overhave.base_settings import DataBaseSettings, LoggingSettings
+from overhave.cli.group import overhave
+from overhave.factory import ConsumerFactory
+from overhave.transport import RedisStream
+
+
+def _run_consumer(stream: RedisStream) -> None:
+    DataBaseSettings().setup_engine()
+    LoggingSettings().setup_logging()
+    ConsumerFactory(stream=stream).runner.run()
+
+
+@overhave.command(short_help="Run Overhave Redis consumer")
+def consumer(
+    stream: RedisStream = typer.Option(..., "-s", "--stream", help="Redis stream, which defines application")
+) -> None:
+    """Run Overhave Redis consumer."""
+    _run_consumer(stream)
