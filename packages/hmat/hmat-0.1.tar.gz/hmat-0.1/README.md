06b2026d01a5a40129dac4f8125aaaabefb92724
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:32 +0000

    Add package hmat/0.1/hmat-0.1.tar.gz

---
+++ packages/hmat/hmat-0.1.tar.gz/hmat-0.1/README.md
@@ -0,0 +1,10 @@
+# HmAt
+A Python package to make home automation easy! Code your own structures for automation's and more!
+
+**HmAt: Home Automation**
+## Installation
+To install HmAt normally:
+```sh
+pip install hmat
+```
+
---
+++ packages/hmat/hmat-0.1.tar.gz/hmat-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(name='hmat',
+      version='0.1',
+      description='A primary Python package to automate your home.',
+      long_description='A primary Python package to automate your home.',
+      keywords='hmat home automation automate easypython',
+      url='http://github.com/JustAnEric/HmAt',
+      author='JustAnEric',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[],
+      include_package_data=True,
+      zip_safe=False)
