ce12a2b1447e1554116dfcadb25fc55df3276df0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:33 +0000

    Add package Jakit/0.5/Jakit-0.5.tar.gz

---
+++ packages/Jakit/Jakit-0.5.tar.gz/Jakit-0.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Jakit",
+    version="0.5",
+    packages=find_packages(),
+    author="Idriss Animashaun",
+    author_email="idriss.animashaun@intel.com",
+    description="Just Another Kappa Information Tool",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/iddy-ani/Jakit",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        
+    ],
+    python_requires='>=3.10',
+)
