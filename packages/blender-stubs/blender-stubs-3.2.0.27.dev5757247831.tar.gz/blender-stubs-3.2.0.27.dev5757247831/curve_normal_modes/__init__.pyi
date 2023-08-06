504441a59120d500cb6b757d12f7e7fd817c8efe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:32 +0000

    Add package blender-stubs/3.2.0.27.dev5757247831/blender-stubs-3.2.0.27.dev5757247831.tar.gz

---
+++ packages/blender-stubs/blender-stubs-3.2.0.27.dev5757247831.tar.gz/blender-stubs-3.2.0.27.dev5757247831/curve_normal_modes/__init__.pyi
@@ -0,0 +1,21 @@
+"""
+
+
+Curve Normal Modes
+^^^^^^^^^^^^^^^^^^
+
+:MINIMUM_TWIST:   
+  Minimum Twist.
+
+  Calculate normals with the smallest twist around the curve tangent across the whole curve.
+
+:Z_UP:            
+  Z Up.
+
+  Calculate normals perpendicular to the Z axis and the curve tangent. If a series of points is vertical, the X axis is used.
+
+.. _rna-enum-curve-normal-modes:
+
+"""
+
+import typing
