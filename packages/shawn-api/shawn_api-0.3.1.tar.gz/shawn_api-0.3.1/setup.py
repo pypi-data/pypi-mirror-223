f1f7c41a75e7b30138f5971aa3fd6d4d69444adf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:22 +0000

    Add package shawn-api/0.3.1/shawn_api-0.3.1.tar.gz

---
+++ packages/shawn-api/shawn_api-0.3.1.tar.gz/shawn_api-0.3.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+
+setup(
+    name="shawn_api",
+    version="0.3.1",
+    description="A Python library for shawn test api",
+    author="Shawn-Tam",
+    author_email="shawn@dcoean.ai",
+    packages=["models"],
+    install_requires=[
+        "requests",
+        "aiohttp",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",  # or "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+)
