bf9e30d1a3afab03e707aac5bd54a9f3032d5f23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:28 +0000

    Add package hezar/0.20.1/hezar-0.20.1-py3-none-any.whl

---
+++ packages/hezar/hezar-0.20.1-py3-none-any.whl/hezar/models/language_modeling/distilbert/distilbert_lm.py
@@ -0,0 +1,47 @@
+"""
+A DistilBERT Language Model (HuggingFace Transformers) wrapped by a Hezar Model class
+"""
+from typing import List, Union
+
+from transformers import DistilBertConfig, DistilBertModel
+
+from ....models import Model
+from ....registry import register_model
+from .distilbert_lm_config import DistilBertLMConfig
+
+
+@register_model("distilbert_lm", config_class=DistilBertLMConfig)
+class DistilBertLM(Model):
+    def __init__(self, config, **kwargs):
+        super().__init__(config=config, **kwargs)
+        self.distilbert = DistilBertModel(DistilBertConfig(**self.config))
+
+    def forward(self, inputs, **kwargs):
+        input_ids = inputs.get("token_ids")
+        attention_mask = inputs.get("attention_mask", None)
+        outputs = self.distilbert(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            **kwargs,
+        )
+        return outputs
+
+    def preprocess(self, inputs: Union[str, List[str]], **kwargs):
+        if isinstance(inputs, str):
+            inputs = [inputs]
+        if "text_normalizer" in self.preprocessor:
+            normalizer = self.preprocessor["text_normalizer"]
+            inputs = normalizer(inputs)
+        tokenizer = self.preprocessor["wordpiece_tokenizer"]
+        inputs = tokenizer(inputs, return_tensors="pt", device=self.device)
+        return inputs
+
+    def post_process(self, inputs, **kwargs):
+        hidden_states = inputs.get("hidden_states", None)
+        attentions = inputs.get("attentions", None)
+        outputs = {
+            "last_hidden_state": inputs.get("last_hidden_state"),
+            "hidden_states": hidden_states,
+            "attentions": attentions,
+        }
+        return outputs
---
+++ packages/hezar/hezar-0.20.1-py3-none-any.whl/hezar/models/language_modeling/roberta/roberta_lm.py
@@ -0,0 +1,47 @@
+"""
+A RoBERTa Language Model (HuggingFace Transformers) wrapped by a Hezar Model class
+"""
+from typing import List, Union
+
+from transformers import RobertaConfig, RobertaModel
+
+from ....models import Model
+from ....registry import register_model
+from .roberta_lm_config import RobertaLMConfig
+
+
+@register_model("roberta_lm", config_class=RobertaLMConfig)
+class RobertaLM(Model):
+    def __init__(self, config, **kwargs):
+        super().__init__(config=config, **kwargs)
+        self.roberta = RobertaModel(RobertaConfig(**self.config))
+
+    def forward(self, inputs, **kwargs):
+        input_ids = inputs.get("token_ids")
+        attention_mask = inputs.get("attention_mask", None)
+        outputs = self.roberta(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            **kwargs,
+        )
+        return outputs
+
+    def preprocess(self, inputs: Union[str, List[str]], **kwargs):
+        if isinstance(inputs, str):
+            inputs = [inputs]
+        if "text_normalizer" in self.preprocessor:
+            normalizer = self.preprocessor["text_normalizer"]
+            inputs = normalizer(inputs)
+        tokenizer = self.preprocessor["bpe_tokenizer"]
+        inputs = tokenizer(inputs, return_tensors="pt", device=self.device)
+        return inputs
+
+    def post_process(self, inputs, **kwargs):
+        hidden_states = inputs.get("hidden_states", None)
+        attentions = inputs.get("attentions", None)
+        outputs = {
+            "last_hidden_state": inputs.get("last_hidden_state"),
+            "hidden_states": hidden_states,
+            "attentions": attentions,
+        }
+        return outputs
