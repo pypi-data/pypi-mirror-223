e1e09109af4f87f1b9f5864969664df0133ad89d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:20 +0000

    Add package testflows.github.runners/1.5.230804.1233152/testflows.github.runners-1.5.230804.1233152.tar.gz

---
+++ packages/testflows.github.runners/testflows.github.runners-1.5.230804.1233152.tar.gz/testflows.github.runners-1.5.230804.1233152/testflows/github/runners/logger.py
@@ -0,0 +1,78 @@
+# Copyright 2023 Katteli Inc.
+# TestFlows.com Open-Source Software Testing Framework (http://testflows.com)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import sys
+import logging
+
+logger = logging.getLogger("testflows.github.runners")
+
+
+class LoggerAdapter(logging.LoggerAdapter):
+    def process(self, msg, kwargs):
+        if kwargs.get("extra") is None:
+            kwargs["extra"] = self.extra
+        else:
+            extra = {}
+            for k, v in self.extra.items():
+                extra[k] = kwargs["extra"].get(k, v)
+            kwargs["extra"] = extra
+        return msg, kwargs
+
+
+logger = LoggerAdapter(
+    logger,
+    {
+        "run_id": "",
+        "job_id": "",
+        "server_name": "",
+        "standby_server": "",
+        "recyclable_server": "",
+        "interval": "",
+    },
+)
+
+columns = [
+    "asctime",
+    "levelname",
+    "message",
+    "interval",
+    "run_id",
+    "job_id",
+    "server_name",
+    "threadName",
+    "funcName",
+]
+
+
+def default_config(level=logging.INFO):
+    """Apply default logging configuration."""
+    global logger
+
+    logger.logger.setLevel(level)
+    handler = logging.StreamHandler(sys.stdout)
+    formatter = logging.Formatter(
+        fmt=(
+            "%(asctime)s "
+            "%(levelname)-5s "
+            "%(message)-70s "
+            "%(run_id)-12s "
+            "%(job_id)-12s "
+            "%(server_name)-30s "
+            "%(funcName)-10s "
+            "%(interval)s "
+        ),
+        datefmt="%H%M%S",
+    )
+    handler.setFormatter(formatter)
+    logger.logger.addHandler(handler)
