400c9f5def5b3fc04765d2fbad1045063146dd6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:13 +0000

    Add package update-submodules/0.0.1/update-submodules-0.0.1.tar.gz

---
+++ packages/update-submodules/update-submodules-0.0.1.tar.gz/update-submodules-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="update-submodules",
+    version="0.0.1",
+    install_requires=open("requirements.txt").read(),
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'git-update-meta-branch=update_submodules.meta_branch:main',
+            'git-update-submodules=update_submodules.git:main',
+            'github-update-submodules=update_submodules.github:main',
+        ]
+    }
+)
