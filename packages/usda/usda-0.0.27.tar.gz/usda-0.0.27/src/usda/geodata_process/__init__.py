d16a74809fb7a76cc3a604930f152b233bafb12e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:30 +0000

    Add package usda/0.0.27/usda-0.0.27.tar.gz

---
+++ packages/usda/usda-0.0.27.tar.gz/usda-0.0.27/src/usda/geodata_process/__init__.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 21 15:11:00 2022
+
+@author: Richie Bao-caDesign设计(cadesign.cn)
+"""
+from ._quadrat import pt_coordi_transform
+from ._quadrat import pt_on_quadrat
+from ._quadrat import rec_quadrats_gdf
+from ._quadrat import rec_quadrats_bounded_gdf
+from ._quadrat import pt_coordi_transform
+from ._quadrat import rasters_minimum_bound
+from ._quadrat import rastercells2shp
+
+from ._raster_dataprocess import rio_read_subset
+from ._raster_dataprocess import raster2polygon
+from ._raster_dataprocess import raster_reprojection
+from ._raster_dataprocess import raster_mosaic
+
+from ._raster_stats import zonal_stats_raster
+from ._raster_stats import zonal_stats_raster_batch
+
+from ._rio_tiler import deg2num
+from ._rio_tiler import centroid
+from._rio_tiler import tiled_web_map_show
+from ._rasterize import create_multiband_raster
+
+from ._sample_pts import meshgrid_pts_in_geoBounds
+from ._sample_pts import random_pts_in_geoBounds
+from ._sample_pts import extract_raster_vals_at_pts
+from ._sample_pts import extract_raster_vals_at_pts_batch
+
+from ._shp_dataprocess import shp2gdf
+from ._build_clipped_raster_dataset import xy_size_elevation
+from ._build_clipped_raster_dataset import build_clipped_raster_dataset
+
+from ._data_format_conversion import kml2gdf_folder
+
+from ._overlapping_polygons import IoU_2Polygons
+from ._overlapping_polygons import drop_overlapping_polygons
+from ._overlapping_polygons import planetary_computer_items_filter4download
+
+__all__ = [
+    "pt_coordi_transform",
+    "pt_on_quadrat",
+    "rio_read_subset",
+    "rec_quadrats_gdf",
+    "raster2polygon",
+    "raster_reprojection",
+    "zonal_stats_raster",
+    "deg2num",
+    "centroid",
+    "tiled_web_map_show",
+    "create_multiband_raster",
+    "meshgrid_pts_in_geoBounds",
+    "random_pts_in_geoBounds",
+    "rec_quadrats_bounded_gdf",
+    "zonal_stats_raster_batch",
+    "raster_mosaic",
+    "shp2gdf",
+    "extract_raster_vals_at_pts",
+    "extract_raster_vals_at_pts_batch",
+    "rasters_minimum_bound",
+    "rastercells2shp",
+    "xy_size_elevation",
+    "build_clipped_raster_dataset",
+    "kml2gdf_folder",
+    "drop_overlapping_polygons",
+    "IoU_2Polygons",
+    "planetary_computer_items_filter4download",
+    ]
+
