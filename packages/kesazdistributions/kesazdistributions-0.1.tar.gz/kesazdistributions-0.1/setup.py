f802ac86cf23caf43bfb4575709cc0c2e0baba06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:25 +0000

    Add package kesazdistributions/0.1/kesazdistributions-0.1.tar.gz

---
+++ packages/kesazdistributions/kesazdistributions-0.1.tar.gz/kesazdistributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='kesazdistributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['kesazdistributions'],
+      zip_safe=False)
