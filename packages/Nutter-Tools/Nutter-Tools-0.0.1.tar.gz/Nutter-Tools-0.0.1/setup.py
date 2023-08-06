a3b81c6b3f89addf3efdf6cf195603e770f4851a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:42 +0000

    Add package Nutter-Tools/0.0.1/Nutter-Tools-0.0.1.tar.gz

---
+++ packages/Nutter-Tools/Nutter-Tools-0.0.1.tar.gz/Nutter-Tools-0.0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='Nutter-Tools',
+    version='0.0.1',
+    description='A simple program to find files easily with similar type of extension and to organize them.',
+    author= 'Ram Nikhilesh',
+    url = 'https://github.com/Spidy20/PyMusic_Player',
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    keywords=['music player python', 'music player tkinter', 'music player gui'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    py_modules=['NTools'],
+    package_dir={'':'src'},
+    install_requires = [
+        'OS',
+        'shutil',
+        'time'
+    ]
+)
