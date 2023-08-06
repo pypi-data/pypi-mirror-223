76732690786a83cfb7e40ad713c430c78f746534
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:57 +0000

    Add package Abg/2.3.5/Abg-2.3.5.tar.gz

---
+++ packages/Abg/Abg-2.3.5.tar.gz/Abg-2.3.5/Abg/__init__.py
@@ -0,0 +1,15 @@
+import logging
+from datetime import datetime
+
+__version__ = "2.3.5"
+__copyright__ = (
+    f"Copyright 2023 - {datetime.now().year} Abishnoi69 <github.com/Abishnoi69>"
+)
+
+LOGGER = logging.getLogger("Abg")
+
+from . import inline, patch
+
+LOGGER.info(f"Version: {__version__}\nCopyright: {__copyright__}")
+
+__all__ = ["patch"]
