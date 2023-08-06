ffa7434800c4d6ff2ddab190a84efd31a1baf6ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:22 +0000

    Add package minio-test/1.0/minio_test-1.0.tar.gz

---
+++ packages/minio-test/minio_test-1.0.tar.gz/minio_test-1.0/setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @time:   2023/8/4 10:05
+# @author: Zx
+
+from setuptools import setup, find_packages
+
+setup(
+    name="minio_test",
+    version="1.0",
+    packages=find_packages(),
+    include_package_data=True
+)
