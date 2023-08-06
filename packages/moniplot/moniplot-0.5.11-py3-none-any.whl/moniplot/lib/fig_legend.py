ae16fb881a3884cc45f0eabdf620439d2472c3e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:46 +0000

    Add package moniplot/0.5.11/moniplot-0.5.11-py3-none-any.whl

---
+++ packages/moniplot/moniplot-0.5.11-py3-none-any.whl/moniplot/lib/fig_legend.py
@@ -0,0 +1,35 @@
+#
+# https://github.com/rmvanhees/moniplot.git
+#
+# Copyright (c) 2022 SRON - Netherlands Institute for Space Research
+#
+# License:  GPLv3
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+This module contains `blank_legend_handle`.
+"""
+__all__ = ['blank_legend_handle']
+
+import matplotlib as mpl
+
+
+def blank_legend_handle():
+    """Show only label in a legend entry, no handle.
+
+    See Also
+    --------
+    matplotlib.pyplot.legend : Place a legend on the Axes.
+    """
+    return mpl.patches.Rectangle((0, 0), 0, 0, fill=False,
+                                 edgecolor='none', visible=False)
