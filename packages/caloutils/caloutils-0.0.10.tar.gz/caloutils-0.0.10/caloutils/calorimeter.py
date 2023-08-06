eb126ef01f1cee45eb4b70d1fe824f8613bf3430
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:09 +0000

    Add package caloutils/0.0.10/caloutils-0.0.10.tar.gz

---
+++ packages/caloutils/caloutils-0.0.10.tar.gz/caloutils-0.0.10/caloutils/calorimeter.py
@@ -0,0 +1,52 @@
+from typing import Optional
+
+
+class Calorimeter:
+    def __init__(self) -> None:
+        self._ds2 = (45, 16, 9)
+        self._ds3 = (45, 50, 18)
+        self._dims: Optional[tuple[int, int, int]] = None
+        self._num_r: Optional[int] = None
+        self._num_alpha: Optional[int] = None
+        self._num_z: Optional[int] = None
+        self._caloname: Optional[str] = None
+
+    def _assert_calo_init(self):
+        if self._caloname is None:
+            raise Exception(
+                "Calorimeter is not initalized. Use"
+                " `init_calorimeter(caloname)` before accassing the attributes."
+            )
+
+    @property
+    def num_r(self):
+        self._assert_calo_init()
+        return self._num_r
+
+    @property
+    def alppha(self):
+        self._assert_calo_init()
+        return self._num_alpha
+
+    @property
+    def z(self):
+        self._assert_calo_init()
+        return self._num_z
+
+    @property
+    def dims(self):
+        self._assert_calo_init()
+        return self._dims
+
+    def set_layout_calochallange_ds2(self):
+        self._num_z, self._num_alpha, self._num_r = self._ds2
+        self._dims = self._ds2
+        self._caloname = "cc_ds2"
+
+    def set_layout_calochallange_ds3(self):
+        self._num_z, self._num_alpha, self._num_r = self._ds3
+        self._dims = self._ds3
+        self._caloname = "cc_ds3"
+
+
+calorimeter = Calorimeter()
---
+++ packages/caloutils/caloutils-0.0.10.tar.gz/caloutils-0.0.10/caloutils/variables/calculate_variables.py
@@ -0,0 +1,58 @@
+from torch_geometric.data import Batch
+
+from ..processing.batch_to_Exyz import batch_to_Exyz
+from ..processing.utils import sum_duplicate_hits
+from .analyze_layers import analyze_layers
+from .energy_ratios import cyratio, sphereratio
+from .first_principal_components import first_principal_components
+from .response import response
+
+
+def calc_vars(
+    batch: Batch,
+    sum_multihits: bool = False,
+    vars: list = [
+        "voxel",
+        "sphereratio",
+        "cyratio",
+        "fpc",
+        "showershape",
+        "response",
+    ],
+):
+    """
+    Calculates specified variables for each event in a batch.
+
+    The function supports calculation of "sphereratio", "cyratio", "fpc", "showershape",
+    and "response". Calculated variables are added to the input batch object.
+
+    Parameters
+    ----------
+    batch : Batch
+        A Batch object from the PyTorch Geometric library that contains the point cloud
+        representation of the events.
+    sum_multihits : bool, optional
+        If True, aggregates the energies of hits in the same cell for each event. Defaults to False.
+    vars : list of str, optional
+        List of variables to calculate for the events in the batch. Defaults to all
+        supported variables: ["voxel","sphereratio","cyratio","fpc","showershape","response"].
+
+    Returns
+    -------
+    Batch
+        The input Batch object with calculated variables added.
+    """
+    if sum_multihits:
+        batch = sum_duplicate_hits(batch)
+    batch = batch_to_Exyz(batch)
+    if "sphereratio" in vars:
+        batch["sphereratio"] = sphereratio(batch)
+    if "cyratio" in vars:
+        batch["cyratio"] = cyratio(batch)
+    if "fpc" in vars:
+        batch["fpc"] = first_principal_components(batch)
+    if "showershape" in vars:
+        batch["showershape"] = analyze_layers(batch)
+    if "response" in vars:
+        batch["response"] = response(batch)
+    return batch
