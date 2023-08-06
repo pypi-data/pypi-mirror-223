3e3c3d2bf55888b36ddfefc8837de477eef8354e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:32 +0000

    Add package EightBitTransit/2.0.7/EightBitTransit-2.0.7-py2.py3-none-any.whl

---
+++ packages/EightBitTransit/EightBitTransit-2.0.7-py2.py3-none-any.whl/EightBitTransit/__init__.py
@@ -0,0 +1,10 @@
+#__all__ = ['cTransitingImage','cGridFunctions','misc','inversion']
+
+__author__ = """Daniel Giles"""
+__email__ = 'daniel.k.giles@gmail.com'
+__version__ = '2.0.7'
+
+from .cGridFunctions import *
+from .misc import *
+from .inversion import *
+from .TransitingImageGpu import *
