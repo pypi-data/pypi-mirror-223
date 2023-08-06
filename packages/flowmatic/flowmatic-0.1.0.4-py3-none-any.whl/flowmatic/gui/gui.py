47c87e433093472ba5e03b93a21c906fe2a817f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:47 +0000

    Add package flowmatic/0.1.0.4/flowmatic-0.1.0.4-py3-none-any.whl

---
+++ packages/flowmatic/flowmatic-0.1.0.4-py3-none-any.whl/flowmatic/gui/gui.py
@@ -0,0 +1,37 @@
+from typing import Protocol
+from .screens.screen import Screen
+from ..flows.flow import Flow
+
+
+class GUI(Protocol):
+    title: str
+    start_screen: Screen
+    current_screen: Screen
+    current_flow: Flow
+
+    def __init__(self, title: str, start_screen: Screen):
+        ...
+
+    def start(self):
+        ...
+
+    def setup(self):
+        ...
+
+    def switch_screen(self, screen: Screen):
+        ...
+
+    def start_flow(self, flow: Flow):
+        ...
+
+    def build_screen(self, screen: Screen):
+        ...
+
+    def clear_screen(self):
+        ...
+
+    def update(self):
+        ...
+
+    def quit(self):
+        ...
