960d71e32694725b46564607a13906bed0c901f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:43:50 +0000

    Add package lexikanon/0.5.1/lexikanon-0.5.1.tar.gz

---
+++ packages/lexikanon/lexikanon-0.5.1.tar.gz/lexikanon-0.5.1/src/lexikanon/tokenizers/mecab.py
@@ -0,0 +1,37 @@
+from typing import List, Optional, Tuple
+
+from hyfi.composer import BaseModel
+
+from lexikanon import HyFI
+from lexikanon.tokenizers.base import Tokenizer
+
+logger = HyFI.getLogger(__name__)
+
+
+class MecabTagger(BaseModel):
+    """
+    userdic_path:
+    backend: ekonlpy
+    verbose: false
+    """
+
+    _config_group_: str = "/tokenizers/tagger"
+    _config_name_: str = "mecab"
+
+    userdic_path: Optional[str] = None
+    backend: str = "ekonlpy"
+    verbose: bool = False
+
+    def _parse(self, text: str) -> List[Tuple[str, str]]:
+        from ekonlpy import Mecab
+
+        mecab = Mecab()
+        return mecab.pos(text)
+
+
+class MecabTokenizer(Tokenizer):
+    tagger: MecabTagger = MecabTagger()
+    flatten: bool = True
+
+    def parse(self, text: str) -> List[Tuple[str, str]]:
+        return self.tagger._parse(text)
