ea055bf608d9b6d074a9d897b8dd120b9b8a14b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:54 +0000

    Add package s3kv/0.1.0/s3kv-0.1.0.tar.gz

---
+++ packages/s3kv/s3kv-0.1.0.tar.gz/s3kv-0.1.0/README.md
@@ -0,0 +1,2 @@
+# s3kv
+S3KV is a key value database backed by S3 Object Storage and implemented as a python library.
---
+++ packages/s3kv/s3kv-0.1.0.tar.gz/s3kv-0.1.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='s3kv',
+    version='0.1.0',
+    author='Balaji Bal',
+    author_email='your.email@example.com',
+    description='A Python library for interacting with S3 key-value store',
+    long_description='A Python library for interacting with S3 key-value store',
+    long_description_content_type='text/plain',
+    url='https://github.com/BalLab/s3kv',
+    packages=['s3kv'],
+    install_requires=[
+        'boto3', 'elasticsearch'
+    ],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
