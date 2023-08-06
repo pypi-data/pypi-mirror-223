82296e2615bb46342f043787cd1858647c64c884
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:28 +0000

    Add package liveramp-automation/0.2.3/liveramp_automation-0.2.3.tar.gz

---
+++ packages/liveramp-automation/liveramp_automation-0.2.3.tar.gz/liveramp_automation-0.2.3/liveramp_automation/utils/selenium.py
@@ -0,0 +1,36 @@
+from urllib.parse import urlparse, urlunsplit
+from liveramp_automation.helpers.file import FileHelper
+from liveramp_automation.utils.time import MACROS
+
+
+class SeleniumUtils:
+
+    def __int__(self, driver):
+        self.driver = driver
+
+    def navigate_url(self, scheme=None, host_name=None, path=None, query=None):
+        """ navigate the url
+
+        :param scheme:
+        :param host_name:
+        :param path:
+        :param query:
+        :return:
+        """
+        parsed_uri = urlparse(self.driver.url)
+        self.driver.get(urlunsplit((parsed_uri.scheme if scheme is None else scheme,
+                                    parsed_uri.hostname if host_name is None else host_name,
+                                    parsed_uri.path if path is None else path,
+                                    parsed_uri.query if query is None else query,
+                                    '')))
+
+    def savescreenshot(self, name):
+        """ to save a screenshot to the destination
+
+        :param name:
+        :return:
+        """
+        data_dict = FileHelper.read_init_file(" /", "pytest.ini", "r")
+        file_path = data_dict.get('screenshot', "reports/")
+        name = file_path + "/{}_{}.png".format(MACROS["now"], name)
+        self.driver.save_screenshot(path=name)
