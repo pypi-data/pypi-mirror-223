7942a0adef81229ace7d31ff3045da32c5e368b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:01 +0000

    Add package npc_session/0.1.0/npc_session-0.1.0.tar.gz

---
+++ packages/npc_session/npc_session-0.1.0.tar.gz/npc_session-0.1.0/src/npc_session/utils.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+import itertools
+import operator
+from collections.abc import Sequence
+from typing import TypeVar
+
+T = TypeVar("T")
+
+
+def subslices(seq: Sequence[T]) -> tuple[Sequence[T], ...]:
+    """Return all contiguous non-empty subslices of a sequence.
+    >>> subslices('ABCD')
+    ('A', 'AB', 'ABC', 'ABCD', 'B', 'BC', 'BCD', 'C', 'CD', 'D')
+    >>> subslices([1, 2])
+    ([1], [1, 2], [2])
+    """
+    slices = itertools.starmap(slice, itertools.combinations(range(len(seq) + 1), 2))
+    return tuple(map(operator.getitem, itertools.repeat(seq), slices))
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod(
+        optionflags=(doctest.IGNORE_EXCEPTION_DETAIL | doctest.NORMALIZE_WHITESPACE)
+    )
