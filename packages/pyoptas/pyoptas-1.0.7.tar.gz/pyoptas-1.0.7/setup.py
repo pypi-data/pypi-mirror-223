7df15065d457daa47a56d9bd762451d8f1b3a206
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:15 +0000

    Add package pyoptas/1.0.7/pyoptas-1.0.7.tar.gz

---
+++ packages/pyoptas/pyoptas-1.0.7.tar.gz/pyoptas-1.0.7/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pyoptas",
+    description="An optimization-based task specification library for task and motion planning (TAMP), trajectory optimization, and model predictive control.",
+    version="1.0.7",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cmower/optas",
+    project_urls={
+        "Bug Tracker": "https://github.com/cmower/optas/issues",
+    },
+    author="Christopher E. Mower",
+    author_email="christopher.mower@kcl.ac.uk",
+    license="Apache License, Version 2.0",
+    packages=["optas"],
+    install_requires=[
+        "numpy",
+        "scipy",
+        "casadi",
+        "urdf-parser-py",
+        "osqp",
+        "cvxopt",
+        "xacro",
+        "vtk",
+        "pyyaml",
+    ],
+    extras_require={
+        "example": ["pybullet", "matplotlib"],
+        "test": ["roboticstoolbox-python", "pybullet", "pytest", "matplotlib"],
+    },
+)
