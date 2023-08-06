5b22f8e972a8997eb55fa3f64254c35202f9f3aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:10 +0000

    Add package pypomes-scheduling/0.2.6/pypomes_scheduling-0.2.6.tar.gz

---
+++ packages/pypomes-scheduling/pypomes_scheduling-0.2.6.tar.gz/pypomes_scheduling-0.2.6/src/pypomes_scheduling/__init__.py
@@ -0,0 +1,16 @@
+from .scheduling_pomes import (
+    scheduler_create, scheduler_destroy,
+    scheduler_start, scheduler_stop,
+    scheduler_add_job, scheduler_add_jobs,
+)
+
+__all__ = [
+    # scheduling_pomes
+    "scheduler_create", "scheduler_destroy",
+    "scheduler_start", "scheduler_stop",
+    "scheduler_add_job", "scheduler_add_jobs",
+]
+
+from importlib.metadata import version
+__version__ = version("pypomes_scheduling")
+__version_info__ = tuple(int(i) for i in __version__.split(".") if i.isdigit())
