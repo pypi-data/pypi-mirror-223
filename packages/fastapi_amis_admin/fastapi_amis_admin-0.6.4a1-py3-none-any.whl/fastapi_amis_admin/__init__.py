5e54d3bd885d024704b64ccd94464aabea553ecc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:25 +0000

    Add package fastapi_amis_admin/0.6.4a1/fastapi_amis_admin-0.6.4a1-py3-none-any.whl

---
+++ packages/fastapi_amis_admin/fastapi_amis_admin-0.6.4a1-py3-none-any.whl/fastapi_amis_admin/__init__.py
@@ -0,0 +1,27 @@
+__version__ = "0.6.4a1"
+__url__ = "https://github.com/amisadmin/fastapi_amis_admin"
+
+import gettext
+from pathlib import Path
+
+from . import globals as g
+from .utils.translation import i18n
+
+BASE_DIR = Path(__file__).resolve().parent
+
+i18n.load_translations(
+    {
+        "zh_CN": gettext.translation(
+            domain="messages",
+            localedir=BASE_DIR / "locale",
+            languages=["zh_CN"],
+        ),
+        "de_DE": gettext.translation(
+            domain="messages",
+            localedir=BASE_DIR / "locale",
+            languages=["de_DE"],
+        ),
+    }
+)
+
+__all__ = ["__version__", "__url__", "g", "i18n"]
