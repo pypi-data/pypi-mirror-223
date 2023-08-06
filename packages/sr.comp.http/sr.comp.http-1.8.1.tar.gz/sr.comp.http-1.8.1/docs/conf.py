ecc2f03e8ab50ae5424d9551ecec565f18cc5f66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:18 +0000

    Add package sr.comp.http/1.8.1/sr.comp.http-1.8.1.tar.gz

---
+++ packages/sr.comp.http/sr.comp.http-1.8.1.tar.gz/sr.comp.http-1.8.1/docs/conf.py
@@ -0,0 +1,21 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('..'))
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
+]
+
+source_suffix = '.rst'
+master_doc = 'index'
+
+project = 'sr.comp.http'
+copyright = "2015-2023, SRComp contributors"  # noqa: A001
+
+html_theme = 'alabaster'
+
+intersphinx_mapping = {'http://docs.python.org/': None}
