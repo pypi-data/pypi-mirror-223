33219c3fe8ebbf3609b8f34bcbfb2f6fc936d888
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:16 +0000

    Add package Swagger2Tests/1.0.7/Swagger2Tests-1.0.7.tar.gz

---
+++ packages/Swagger2Tests/Swagger2Tests-1.0.7.tar.gz/Swagger2Tests-1.0.7/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+readmepath = 'README.md'
+setup(
+    name='Swagger2Tests',
+    version='1.0.7',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'Swagger2Tests = Swagger2Tests.loadswagger:main'
+        ]
+    },
+    install_requires=[
+        'argparse','jinja2','pytest','requests','coloredlogs','Faker'
+    ],
+    author='visonforcoding',
+    author_email='visonforcoding@gmail.com',
+    description='swagger',
+    long_description=open(readmepath, encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    url='https://gitee.com/visonforcoding/code-template-tools',
+    package_data={
+        "": ["helpers", "templates","constants"],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
