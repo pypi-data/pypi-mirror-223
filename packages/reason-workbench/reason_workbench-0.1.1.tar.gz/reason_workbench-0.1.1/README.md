31b06e88d614625c7b24abd505e16af1a4c6023e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:32 +0000

    Add package reason-workbench/0.1.1/reason_workbench-0.1.1.tar.gz

---
+++ packages/reason-workbench/reason_workbench-0.1.1.tar.gz/reason_workbench-0.1.1/README.md
@@ -0,0 +1,11 @@
+# Reason Workbench
+
+A Python package for running Reasoning Workbench and generating outputs.
+
+## Installation
+
+You can install the package using pip:
+
+```bash
+pip install reason_workbench
+
---
+++ packages/reason-workbench/reason_workbench-0.1.1.tar.gz/reason_workbench-0.1.1/setup.py
@@ -0,0 +1,21 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="reason_workbench",
+    version="0.1.1",
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "reason_workbench=reason_workbench.main:main",
+        ],
+    },
+    install_requires=[
+        "transformers",
+        "bert-score",
+        "pandas",
+        "matplotlib",
+    ],
+)
+
