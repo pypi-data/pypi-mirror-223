33d09ba597a65abdb365bb4fd2e58d816efe710d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:26 +0000

    Add package pydeezer-asy/2.2.6/pydeezer_asy-2.2.6.tar.gz

---
+++ packages/pydeezer-asy/pydeezer_asy-2.2.6.tar.gz/pydeezer_asy-2.2.6/deezer_asy/__init__.py
@@ -0,0 +1,9 @@
+from . import util
+from . import exceptions
+from . import constants
+
+
+from .DeezerAsy import DeezerAsy
+from .aiodeezer import aioDeezer
+
+name = "PyDeezer_Asy"
---
+++ packages/pydeezer-asy/pydeezer_asy-2.2.6.tar.gz/pydeezer_asy-2.2.6/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='pydeezer_asy',
+    version='2.2.6',
+    description='Asynchronous version of the `py-deezer` module',
+    author='drhspfn',
+    author_email="drhspfn@gmail.com",
+    packages=setuptools.find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/drhspfn/deezer-asy",
+    install_requires=[
+        "aiofiles",
+        "cryptography",
+        "mutagen",
+        "httpx",
+        "httpx[http2]",
+        "yarl",
+        "asyncio"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent"
+    ],
+
+)
