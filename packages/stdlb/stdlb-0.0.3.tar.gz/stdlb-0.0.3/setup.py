d2adc5327bf3067032891dec4b3975850ce2b001
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:39 +0000

    Add package stdlb/0.0.3/stdlb-0.0.3.tar.gz

---
+++ packages/stdlb/stdlb-0.0.3.tar.gz/stdlb-0.0.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="stdlb",
+    version="0.0.3",
+    packages=["stdlb"],
+    license="MIT",
+    author="Ryan Williams",
+    author_email="ryan@runsascoded.com",
+    author_url="https://github.com/ryan-williams",
+    description="Wildcard-import the Python standard library",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+)
