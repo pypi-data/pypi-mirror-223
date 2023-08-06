5d740f5f418dedaad752ee8c650d37fcf4035f66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:50 +0000

    Add package flask-unity/0.0.2/flask_unity-0.0.2.tar.gz

---
+++ packages/flask-unity/flask_unity-0.0.2.tar.gz/flask_unity-0.0.2/setup.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+from setuptools import find_packages
+from flask_unity import (__title__, __version__, __author__, __author_email__, __repository__, __website__)
+
+
+setup(
+  
+  # name of the main package (base folder)
+  name=__title__,
+  version=__version__,
+  description='An extension of flask web framework that erase the complexity of structuring flask project blueprint, packages, and other annoying stuffs',
+  long_description=open('README.md').read() + '\n\n' + open('CHANGELOG').read(),
+  long_description_content_type="text/markdown",
+  python_requires='>=3.6',
+  
+  # The URL of your package's (project) home page e.g. github link
+  url=__website__,
+  repo=__repository__,
+  author=__author__,
+  author_email=__author_email__,
+  License='MIT',
+  classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Environment :: Web Environment',
+    'Intended Audience :: Developers',
+    'Natural Language :: English',
+    'Operating System :: POSIX :: Linux',
+    'License :: OSI Approved :: MIT License',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.8'
+  ],
+  
+  # used when people are searching for a module, keywords separated with a space
+  keywords='flask_unity',
+  include_package_data = True, # include files listed in MANIFEST.in
+  
+  # The list of packages(directories) for your library
+  packages=find_packages(), # OR packages=['flask_unity'] 
+  # If your package is a single module, use this instead of 'packages':
+  # py_modules=[''] # list of files (modules) that are not in any directory (at the root dir)
+  # the libraries it depends on
+  
+  # List of other python modules which this module depends on.  For example RPi.GPIO
+  install_requires = [
+    "click==8.1.3",
+    "Flask==2.2.3",
+    "Flask-Admin==1.6.0",
+    "Flask-Login==0.6.2",
+    "Flask-SQLAlchemy==3.0.3",
+    "Flask-WTF==1.1.1",
+    "greenlet==2.0.2",
+    "importlib-metadata==6.0.0",
+    "itsdangerous==2.1.2",
+    "Jinja2==3.1.2",
+    "MarkupSafe==2.1.2",
+    "SQLAlchemy==1.4.45",
+    "typing-extensions==4.5.0",
+    "Werkzeug==2.2.3",
+    "WTForms==3.0.1",
+    "zipp==3.13.0",
+  ]
+)
