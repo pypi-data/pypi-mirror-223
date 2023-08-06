212a3b83cf12ae7297c76738110f04d50742981d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:04 +0000

    Add package streamlit-bls-connection/0.8/streamlit_bls_connection-0.8.tar.gz

---
+++ packages/streamlit-bls-connection/streamlit_bls_connection-0.8.tar.gz/streamlit_bls_connection-0.8/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='streamlit_bls_connection',
+    version='0.8',
+    license='MIT',  
+    description='A package to fetch Bureau of Labor Statistics data using Streamlit',
+    long_description=long_description,  
+    long_description_content_type='text/markdown',
+    author='Tony Hollaar',
+    author_email='thollaar@gmail.com',
+    url='https://github.com/tonyhollaar/',
+    download_url='https://github.com/tonyhollaar/streamlit_bls_connection/archive/refs/tags/v0.8.tar.gz',
+    packages=find_packages(),
+    keywords=['streamlit', 'api', 'bls'],  # <-- Comma added here
+    install_requires=[
+        'streamlit',
+        'requests',
+        'pandas',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
