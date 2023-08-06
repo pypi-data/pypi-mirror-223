94fb55735fc93fd67ee3a1b2e364c808d89dd033
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:55 +0000

    Add package airflow-looker/1.0.0/airflow-looker-1.0.0.tar.gz

---
+++ packages/airflow-looker/airflow-looker-1.0.0.tar.gz/airflow-looker-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(
+    name='airflow-looker', #package name
+    version='1.0.0',
+    description='Python Package By Predator_97',
+    author='Predator_97',
+    license='MIT',
+    zip_safe=False
+)
