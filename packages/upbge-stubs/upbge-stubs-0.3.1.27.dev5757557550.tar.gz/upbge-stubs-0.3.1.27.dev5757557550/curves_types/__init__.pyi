c3704f05617b99d3db72a7c98410805e2b7b2af0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:14 +0000

    Add package upbge-stubs/0.3.1.27.dev5757557550/upbge-stubs-0.3.1.27.dev5757557550.tar.gz

---
+++ packages/upbge-stubs/upbge-stubs-0.3.1.27.dev5757557550.tar.gz/upbge-stubs-0.3.1.27.dev5757557550/curves_types/__init__.pyi
@@ -0,0 +1,23 @@
+"""
+
+
+Curves Types
+^^^^^^^^^^^^
+
+:CATMULL_ROM:     
+  Catmull Rom.
+
+:POLY:            
+  Poly.
+
+:BEZIER:          
+  Bezier.
+
+:NURBS:           
+  NURBS.
+
+.. _rna-enum-curves-types:
+
+"""
+
+import typing
---
+++ packages/upbge-stubs/upbge-stubs-0.3.1.27.dev5757557550.tar.gz/upbge-stubs-0.3.1.27.dev5757557550/operator_property_tags/__init__.pyi
@@ -0,0 +1,16 @@
+"""
+
+
+Operator Property Tags
+^^^^^^^^^^^^^^^^^^^^^^
+
+:ADVANCED:        
+  Advanced.
+
+  The property is advanced so UI is suggested to hide it.
+
+.. _rna-enum-operator-property-tags:
+
+"""
+
+import typing
