0b11dd30fc6f5cafb6e5afe18f8d972cc1a008cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:43 +0000

    Add package pensando-dss/1.62.2/pensando_dss-1.62.2-py3-none-any.whl

---
+++ packages/pensando-dss/pensando_dss-1.62.2-py3-none-any.whl/pensando_dss/psm/models/workload/__init__.py
@@ -0,0 +1,59 @@
+# flake8: noqa
+
+# import all models into this package
+# if you have many models here with many references from one model to another this may
+# raise a RecursionError
+# to avoid this, import only the models that you directly need like:
+# from from psm.model.pet import Pet
+# or import this package, but before doing it, use:
+# import sys
+# sys.setrecursionlimit(n)
+
+from pensando_dss.psm.model.api_agg_watch_options import ApiAggWatchOptions
+from pensando_dss.psm.model.api_dse_status import ApiDSEStatus
+from pensando_dss.psm.model.api_kind_watch_options import ApiKindWatchOptions
+from pensando_dss.psm.model.api_label import ApiLabel
+from pensando_dss.psm.model.api_list_meta import ApiListMeta
+from pensando_dss.psm.model.api_list_watch_options import ApiListWatchOptions
+from pensando_dss.psm.model.api_object_meta import ApiObjectMeta
+from pensando_dss.psm.model.api_object_ref import ApiObjectRef
+from pensando_dss.psm.model.api_pdt_status import ApiPDTStatus
+from pensando_dss.psm.model.api_propagation_status import ApiPropagationStatus
+from pensando_dss.psm.model.api_status import ApiStatus
+from pensando_dss.psm.model.api_status_result import ApiStatusResult
+from pensando_dss.psm.model.api_timestamp import ApiTimestamp
+from pensando_dss.psm.model.api_type_meta import ApiTypeMeta
+from pensando_dss.psm.model.api_watch_control import ApiWatchControl
+from pensando_dss.psm.model.api_watch_event import ApiWatchEvent
+from pensando_dss.psm.model.api_watch_event_list import ApiWatchEventList
+from pensando_dss.psm.model.cluster_ip_config import ClusterIPConfig
+from pensando_dss.psm.model.googleprotobuf_any import GoogleprotobufAny
+from pensando_dss.psm.model.labels_requirement import LabelsRequirement
+from pensando_dss.psm.model.labels_selector import LabelsSelector
+from pensando_dss.psm.model.workload_auto_msg_endpoint_watch_helper import WorkloadAutoMsgEndpointWatchHelper
+from pensando_dss.psm.model.workload_auto_msg_endpoint_watch_helper_watch_event import WorkloadAutoMsgEndpointWatchHelperWatchEvent
+from pensando_dss.psm.model.workload_auto_msg_workload_group_watch_helper import WorkloadAutoMsgWorkloadGroupWatchHelper
+from pensando_dss.psm.model.workload_auto_msg_workload_group_watch_helper_watch_event import WorkloadAutoMsgWorkloadGroupWatchHelperWatchEvent
+from pensando_dss.psm.model.workload_auto_msg_workload_watch_helper import WorkloadAutoMsgWorkloadWatchHelper
+from pensando_dss.psm.model.workload_auto_msg_workload_watch_helper_watch_event import WorkloadAutoMsgWorkloadWatchHelperWatchEvent
+from pensando_dss.psm.model.workload_endpoint import WorkloadEndpoint
+from pensando_dss.psm.model.workload_endpoint_list import WorkloadEndpointList
+from pensando_dss.psm.model.workload_endpoint_migration_status import WorkloadEndpointMigrationStatus
+from pensando_dss.psm.model.workload_endpoint_prop_status import WorkloadEndpointPropStatus
+from pensando_dss.psm.model.workload_endpoint_spec import WorkloadEndpointSpec
+from pensando_dss.psm.model.workload_endpoint_status import WorkloadEndpointStatus
+from pensando_dss.psm.model.workload_ip_block import WorkloadIPBlock
+from pensando_dss.psm.model.workload_interface_migration_status import WorkloadInterfaceMigrationStatus
+from pensando_dss.psm.model.workload_migration_source import WorkloadMigrationSource
+from pensando_dss.psm.model.workload_propagation_status import WorkloadPropagationStatus
+from pensando_dss.psm.model.workload_workload import WorkloadWorkload
+from pensando_dss.psm.model.workload_workload_group import WorkloadWorkloadGroup
+from pensando_dss.psm.model.workload_workload_group_list import WorkloadWorkloadGroupList
+from pensando_dss.psm.model.workload_workload_group_spec import WorkloadWorkloadGroupSpec
+from pensando_dss.psm.model.workload_workload_group_status import WorkloadWorkloadGroupStatus
+from pensando_dss.psm.model.workload_workload_intf_spec import WorkloadWorkloadIntfSpec
+from pensando_dss.psm.model.workload_workload_intf_status import WorkloadWorkloadIntfStatus
+from pensando_dss.psm.model.workload_workload_list import WorkloadWorkloadList
+from pensando_dss.psm.model.workload_workload_migration_status import WorkloadWorkloadMigrationStatus
+from pensando_dss.psm.model.workload_workload_spec import WorkloadWorkloadSpec
+from pensando_dss.psm.model.workload_workload_status import WorkloadWorkloadStatus
