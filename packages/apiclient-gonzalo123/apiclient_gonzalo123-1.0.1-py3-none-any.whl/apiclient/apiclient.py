43434270f086adb2aaf448aaeff22544dfd267e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:41 +0000

    Add package apiclient-gonzalo123/1.0.1/apiclient_gonzalo123-1.0.1-py3-none-any.whl

---
+++ packages/apiclient-gonzalo123/apiclient_gonzalo123-1.0.1-py3-none-any.whl/apiclient/apiclient.py
@@ -0,0 +1,49 @@
+import requests
+
+
+class ApiClientException(Exception):
+    pass
+
+
+class ApiClient:
+    def __init__(self, token, base):
+        self.token = token
+        self.base = base
+
+    @staticmethod
+    def _zip(columns, rows):
+        return [dict(zip(columns, row)) for row in rows]
+
+    @staticmethod
+    def _get_headers(token):
+        return {
+            'token': token,
+            'x-client': 'apiclient',
+            'Content-Type': 'application/json'
+        }
+
+    def _get_url(self, uri):
+        return f'{self.base}{uri}'
+
+    def _process_response(self, response):
+        if response.ok:
+            data = response.json()
+            if response.headers['x-recordset'] == 'compact':
+                return self._zip(*data)
+            else:
+                return data
+        else:
+            raise ApiClientException(response.json())
+
+    def get(self, uri, params):
+        return self._process_response(requests.get(
+            url=self._get_url(uri),
+            params=params,
+            headers=self._get_headers(self.token)))
+
+    def post(self, uri, params=None, body=None):
+        return self._process_response(requests.post(
+            url=self._get_url(uri),
+            params=params,
+            json=body,
+            headers=self._get_headers(self.token)))
