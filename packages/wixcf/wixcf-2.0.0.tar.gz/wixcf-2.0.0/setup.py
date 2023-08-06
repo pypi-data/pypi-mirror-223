4a13c090e0cdc8d02a95cbe30a4506e10a3dd23f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:50 +0000

    Add package wixcf/2.0.0/wixcf-2.0.0.tar.gz

---
+++ packages/wixcf/wixcf-2.0.0.tar.gz/wixcf-2.0.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name="wixcf",
+    version="2.0.0",
+    author="Aras Tokdemir",
+    author_email="aras.tokdemir@outlook.com",
+    description="Wix Package",
+    packages=["Wix"],
+    install_requires=[
+        "wikipedia",
+        "numpy",
+        "pandas",
+        "cryptocompare",
+        "keras",
+        "tensorflow",
+        "faker",
+    ],
+    entry_points={
+        "console_scripts": [
+            "wix = Wix.main:main"
+        ]
+    },
+)
