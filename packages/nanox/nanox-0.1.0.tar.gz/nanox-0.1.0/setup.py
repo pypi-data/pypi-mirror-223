0f4c5290124cd37ec963011d49e6f86180ebe597
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:06 +0000

    Add package nanox/0.1.0/nanox-0.1.0.tar.gz

---
+++ packages/nanox/nanox-0.1.0.tar.gz/nanox-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['nanox', 'nanox.model']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['torch>=1.10.0,<2.0.0']
+
+setup_kwargs = {
+    'name': 'nanox',
+    'version': '0.1.0',
+    'description': 'A transformer model for creating bio-compatible nanomachines',
+    'long_description': 'None',
+    'author': 'Kye Gomez',
+    'author_email': 'kye@apac.ai',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
