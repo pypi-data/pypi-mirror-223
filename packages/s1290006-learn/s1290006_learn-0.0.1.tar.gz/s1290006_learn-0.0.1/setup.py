f1e1614911b3ffadc7864e3020b5843622ceb504
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:18 +0000

    Add package s1290006-learn/0.0.1/s1290006_learn-0.0.1.tar.gz

---
+++ packages/s1290006-learn/s1290006_learn-0.0.1.tar.gz/s1290006_learn-0.0.1/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="s1290006_learn",
+    version="0.0.1",
+    author="s1290006",
+    author_email="s1290006@u-aizu.ac.jp",
+    description="heat map pm25",
+    long_description="ex14",
+    long_description_content_type="text/markdown",
+    url="https://github.com/s1290006/s1290006_learn",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
