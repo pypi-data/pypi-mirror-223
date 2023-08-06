65b1d2228df98eb0f187cb765eaa40a6e949206f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:43:45 +0000

    Add package ndokta/0.0.2/ndokta-0.0.2.tar.gz

---
+++ packages/ndokta/ndokta-0.0.2.tar.gz/ndokta-0.0.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.2' 
+DESCRIPTION = 'Notre Dame Okta'
+LONG_DESCRIPTION = 'Wrapper calls for Okta API specific to Notre Dame'
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="ndokta", 
+        version=VERSION,
+        author="Ken Marciniak",
+        author_email="kmarcini@nd.edu",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=['requests','urllib3<=1.27','boto3','botocore'], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        
+        keywords=['python', 'okta', 'Notre Dame'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
