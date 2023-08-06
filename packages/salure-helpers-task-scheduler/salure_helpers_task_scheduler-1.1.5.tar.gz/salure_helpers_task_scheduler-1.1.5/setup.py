d85eefc43573777286318d44415cc4b7dd5b5acf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:59 +0000

    Add package salure-helpers-task-scheduler/1.1.5/salure_helpers_task_scheduler-1.1.5.tar.gz

---
+++ packages/salure-helpers-task-scheduler/salure_helpers_task_scheduler-1.1.5.tar.gz/salure_helpers_task_scheduler-1.1.5/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+setup(
+    name='salure_helpers_task_scheduler',
+    version='1.1.5',
+    description='Task Scheduler from Salure',
+    long_description='Task Schedule from Salure',
+    author='D&A Salure',
+    author_email='support@salureconnnect.com',
+    packages=["salure_helpers.task_scheduler"],
+    license='Salure License',
+    install_requires=[
+        'salure-helpers-mysql>=1',
+        'salure-helpers-salureconnect>=1',
+        'salure-helpers-mandrill>=0',
+        'salure-helpers-salure_functions>=0',
+        'salure-helpers-elastic>=0',
+        'pandas>=1,<=1.35',
+        'requests>=2,<=3'
+    ],
+    zip_safe=False,
+)
