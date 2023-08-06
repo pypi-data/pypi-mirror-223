08a390f336eb363e6e300f0638c988149f9e16b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:56 +0000

    Add package pyjt/0.4.5/pyjt-0.4.5.tar.gz

---
+++ packages/pyjt/pyjt-0.4.5.tar.gz/pyjt-0.4.5/pyjt/Inspector.py
@@ -0,0 +1,48 @@
+""" Inspect UI components """
+import logging
+
+log = logging.getLogger(__name__)
+
+
+class Inspector:
+    """ Helper class for inspecting the component tree of a specific component.
+    """
+    @staticmethod
+    def inspect(component):
+        """ Inspects the component tree of a component.
+
+            Args:
+                component (Fixture):
+                    The component to inspect.
+
+            Returns:
+                dict:
+                    A dictionary with information of all sub-components
+                    for this component.
+        """
+        result = {}
+        result['name'] = str(component.getName())
+        result['class'] = str(component.getClass())
+        result['text'] = _getText(component)
+        title = _getTitle(component)
+        if title:
+            result['title'] = title
+
+        result['childs'] = []
+        if not hasattr(component, 'getComponents'):
+            return result
+        for child in component.getComponents():
+            result['childs'].append(Inspector.inspect(child))
+        return result
+
+
+def _getTitle(component):
+    if not hasattr(component, 'getText'):
+        return "N/A"
+    return str(component.getText())
+
+
+def _getText(component):
+    if not hasattr(component, 'getTitle'):
+        return None
+    return str(component.getTitle())
