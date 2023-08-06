17fcaee47965bb9243d9834dafbbbec1ea5910de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:20 +0000

    Add package aleph-alpha-client/3.2.0/aleph-alpha-client-3.2.0.tar.gz

---
+++ packages/aleph-alpha-client/aleph-alpha-client-3.2.0.tar.gz/aleph-alpha-client-3.2.0/tests/test_embed.py
@@ -0,0 +1,145 @@
+import pytest
+from aleph_alpha_client import EmbeddingRequest
+from aleph_alpha_client.aleph_alpha_client import AsyncClient, Client
+from aleph_alpha_client.embedding import (
+    BatchSemanticEmbeddingRequest,
+    SemanticEmbeddingRequest,
+    SemanticRepresentation,
+)
+from aleph_alpha_client.prompt import Prompt
+from tests.common import (
+    sync_client,
+    async_client,
+    model_name,
+)
+
+# AsyncClient
+
+
+@pytest.mark.system_test
+async def test_can_embed_with_async_client(async_client: AsyncClient, model_name: str):
+    request = request = EmbeddingRequest(
+        prompt=Prompt.from_text("abc"), layers=[-1], pooling=["mean"], tokens=True
+    )
+
+    response = await async_client.embed(request, model=model_name)
+    assert response.model_version is not None
+    assert response.embeddings and len(response.embeddings) == len(
+        request.pooling
+    ) * len(request.layers)
+    assert response.tokens is not None
+
+
+@pytest.mark.system_test
+async def test_can_semantic_embed_with_async_client(
+    async_client: AsyncClient, model_name: str
+):
+    request = SemanticEmbeddingRequest(
+        prompt=Prompt.from_text("hello"),
+        representation=SemanticRepresentation.Symmetric,
+        compress_to_size=128,
+    )
+
+    response = await async_client.semantic_embed(request, model=model_name)
+    assert response.model_version is not None
+    assert response.embedding
+    assert len(response.embedding) == 128
+
+
+@pytest.mark.parametrize("num_prompts", [1, 100, 101, 200, 1000])
+@pytest.mark.system_test
+async def test_batch_embed_semantic_with_async_client(
+    async_client: AsyncClient, num_prompts: int
+):
+    request = BatchSemanticEmbeddingRequest(
+        prompts=[Prompt.from_text(str(i)) for i in range(num_prompts)],
+        representation=SemanticRepresentation.Symmetric,
+        compress_to_size=128,
+    )
+
+    result = await async_client.batch_semantic_embed(
+        request=request, model="luminous-base", num_concurrent_requests=10
+    )
+    assert len(result.embeddings) == num_prompts
+
+
+# Client
+
+
+@pytest.mark.system_test
+def test_embed(sync_client: Client, model_name: str):
+    request = EmbeddingRequest(
+        prompt=Prompt.from_text("hello"), layers=[0, -1], pooling=["mean", "max"]
+    )
+
+    result = sync_client.embed(request=request, model=model_name)
+
+    assert result.model_version is not None
+    assert result.embeddings and len(result.embeddings) == len(request.pooling) * len(
+        request.layers
+    )
+    assert result.tokens is None
+
+
+@pytest.mark.system_test
+def test_embedding_of_one_token_aggregates_identically(
+    sync_client: Client, model_name: str
+):
+    request = EmbeddingRequest(
+        prompt=Prompt.from_text(
+            "hello"
+        ),  # it is important for this test that we only embed one single token
+        layers=[0, -1],
+        pooling=["mean", "max"],
+    )
+
+    result = sync_client.embed(request, model=model_name)
+
+    assert (
+        result.embeddings
+        and result.embeddings[("layer_0", "mean")]
+        == result.embeddings[("layer_0", "max")]
+    )
+
+
+@pytest.mark.system_test
+def test_embed_with_tokens(sync_client: Client, model_name: str):
+    request = EmbeddingRequest(
+        prompt=Prompt.from_text("abc"), layers=[-1], pooling=["mean"], tokens=True
+    )
+
+    result = sync_client.embed(request, model=model_name)
+
+    assert result.model_version is not None
+    assert result.embeddings and len(result.embeddings) == len(request.pooling) * len(
+        request.layers
+    )
+    assert result.tokens is not None
+
+
+@pytest.mark.system_test
+def test_embed_semantic(sync_client: Client):
+    request = SemanticEmbeddingRequest(
+        prompt=Prompt.from_text("hello"),
+        representation=SemanticRepresentation.Symmetric,
+        compress_to_size=128,
+    )
+
+    result = sync_client.semantic_embed(request=request, model="luminous-base")
+
+    assert result.model_version is not None
+    assert result.embedding
+    assert len(result.embedding) == 128
+
+
+@pytest.mark.parametrize("num_prompts", [1, 100, 101, 200, 1000])
+@pytest.mark.system_test
+def test_batch_embed_semantic(sync_client: Client, num_prompts: int):
+    request = BatchSemanticEmbeddingRequest(
+        prompts=[Prompt.from_text("hello") for _ in range(num_prompts)],
+        representation=SemanticRepresentation.Symmetric,
+        compress_to_size=128,
+    )
+
+    result = sync_client.batch_semantic_embed(request=request, model="luminous-base")
+    assert len(result.embeddings) == num_prompts
