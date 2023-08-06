204b545fa956f9901944f4729a1ac62fa8152588
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:52 +0000

    Add package saconvert/0.1.2/saconvert-0.1.2.tar.gz

---
+++ packages/saconvert/saconvert-0.1.2.tar.gz/saconvert-0.1.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='saconvert',
+    version='0.1.2',
+    description="saconvert is a versatile Python library focused on conversions, providing a seamless interface to effortlessly convert between weight and length units. With a growing list of upcoming conversions, saconvert simplifies complex transformations, making it a must-have tool for developers and engineers working with measurements. Empower your projects with the efficiency and precision of saconvert, and stay tuned for new exciting conversion features to be added soon!",
+    author='Skanda Acharya',
+    author_email='skanda.acharya@yahoo.com',
+    url='https://github.com/yourusername/conversions',
+    packages=['saconvert'],
+    install_requires=[],
+)
