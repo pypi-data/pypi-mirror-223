d835bc358ccf66777d90cd8f7a7bd90836921d0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:03 +0000

    Add package MyoSuite/1.7.1/MyoSuite-1.7.1.tar.gz

---
+++ packages/MyoSuite/MyoSuite-1.7.1.tar.gz/MyoSuite-1.7.1/myosuite/__init__.py
@@ -0,0 +1,23 @@
+""" =================================================
+# Copyright (c) Facebook, Inc. and its affiliates
+Authors  :: Vikash Kumar (vikashplus@gmail.com), Vittorio Caggiano (caggiano@gmail.com)
+================================================= """
+
+import gym
+
+# Register MyoSuite Envs
+_current_gym_envs = gym.envs.registration.registry.env_specs.keys()
+_current_gym_envs = set(_current_gym_envs)
+
+# Register Myo Suite
+import myosuite.envs.myo # noqa
+import myosuite.envs.myo.myochallenge # noqa
+myo_suite_envs = set(gym.envs.registration.registry.env_specs.keys())-_current_gym_envs
+myo_suite_envs = sorted(myo_suite_envs)
+
+from typing import List
+
+from .version import __version_tuple__
+
+__version__ = ".".join([str(x) for x in __version_tuple__])
+__all__: List[str] = []
