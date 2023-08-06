89980799a6540510fc067abf3ffaf91cecaf806e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:53 +0000

    Add package my-module123/0.1/my_module123-0.1.tar.gz

---
+++ packages/my-module123/my_module123-0.1.tar.gz/my_module123-0.1/setup.py
@@ -0,0 +1,10 @@
+# setup.py
+import setuptools
+
+setuptools.setup(
+    name="my_module123",
+    version="0.1",
+    author="Atharva Akshat",
+    author_email="helloatharvaakshat@gmail.com",
+    description="A small module that greets people",
+)
