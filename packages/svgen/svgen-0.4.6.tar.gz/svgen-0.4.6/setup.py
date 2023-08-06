7f2140b58b2f9cd37546714e110eb8a3a45484d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:59 +0000

    Add package svgen/0.4.6/svgen-0.4.6.tar.gz

---
+++ packages/svgen/svgen-0.4.6.tar.gz/svgen-0.4.6/setup.py
@@ -0,0 +1,40 @@
+# =====================================
+# generator=datazen
+# version=3.1.2
+# hash=224dbecf0fd19b1222e824b1fbe652a8
+# =====================================
+
+"""
+svgen - Package definition for distribution.
+"""
+
+# third-party
+try:
+    from setuptools_wrapper.setup import setup
+except (ImportError, ModuleNotFoundError):
+    from svgen_bootstrap.setup import setup  # type: ignore
+
+# internal
+from svgen import DESCRIPTION, PKG_NAME, VERSION
+
+author_info = {
+    "name": "Vaughn Kottler",
+    "email": "vaughnkottler@gmail.com",
+    "username": "vkottler",
+}
+pkg_info = {
+    "name": PKG_NAME,
+    "slug": PKG_NAME.replace("-", "_"),
+    "version": VERSION,
+    "description": DESCRIPTION,
+    "versions": [
+        "3.8",
+        "3.9",
+        "3.10",
+        "3.11",
+    ],
+}
+setup(
+    pkg_info,
+    author_info,
+)
---
+++ packages/svgen/svgen-0.4.6.tar.gz/svgen-0.4.6/svgen/__init__.py
@@ -0,0 +1,13 @@
+# =====================================
+# generator=datazen
+# version=3.1.2
+# hash=ee0dfa31dfd460de806a41e3ca149b26
+# =====================================
+
+"""
+Useful defaults and other package metadata.
+"""
+
+DESCRIPTION = "A tool for working with scalable vector graphics."
+PKG_NAME = "svgen"
+VERSION = "0.4.6"
