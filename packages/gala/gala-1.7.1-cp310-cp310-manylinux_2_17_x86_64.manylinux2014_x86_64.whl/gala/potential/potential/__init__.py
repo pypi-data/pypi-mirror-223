10a600626982fb20e0431f534c0996c9b7c290d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:56 +0000

    Add package gala/1.7.1/gala-1.7.1-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/gala/gala-1.7.1-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/gala/potential/potential/__init__.py
@@ -0,0 +1,20 @@
+from .core import *
+from .cpotential import *
+from .ccompositepotential import *
+from .builtin import *
+from .io import *
+from .util import *
+
+
+def __getattr__(name):
+    # Needed for MultipolePotential save/load
+    from . import builtin
+
+    if name in globals():
+        return globals()[name]
+
+    elif name.startswith('MultipolePotentialLmax'):
+        return getattr(builtin.core, name)
+
+    else:
+        raise AttributeError("huh")
