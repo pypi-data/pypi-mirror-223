85b42d103e516978c22ab7619ea5a7d52325ae8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:17 +0000

    Add package pyurlx/2.0.0/pyurlx-2.0.0-py3-none-any.whl

---
+++ packages/pyurlx/pyurlx-2.0.0-py3-none-any.whl/pyurlx/tools.py
@@ -0,0 +1,28 @@
+import urllib.request
+
+def shorten(url):
+  page = urllib.request.urlopen(f'https://mcpi.cc/api.php?url={url}')
+  short = str(page.read())
+  if short.find("b\'") != -1:
+    short = short.split("b'")[1]
+  if short.find("\\n") != -1:
+    short = short.split("\\n")[0]
+  return short
+
+def qr(url):
+  page = urllib.request.urlopen(f'https://mcpi.cc/qrapi.php?url={url}')
+  qr = str(page.read())
+  if qr.find("b\'") != -1:
+    qr = qr.split("b'")[1]
+  if qr.find("\\n") != -1:
+    qr = qr.split("\\n")[0]
+  return qr
+
+def cloak(url):
+  page = urllib.request.urlopen(f'https://mcpi.cc/cloakapi.php?url={url}')
+  cloak = str(page.read())
+  if cloak.find("b\'") != -1:
+    cloak = cloak.split("b'")[1]
+  if cloak.find("\\n") != -1:
+    cloak = cloak.split("\\n")[0]
+  return cloak
