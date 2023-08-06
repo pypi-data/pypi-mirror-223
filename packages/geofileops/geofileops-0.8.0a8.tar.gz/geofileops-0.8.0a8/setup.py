50152bae001004678abe2129066b8618f7f59fbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:55 +0000

    Add package geofileops/0.8.0a8/geofileops-0.8.0a8.tar.gz

---
+++ packages/geofileops/geofileops-0.8.0a8.tar.gz/geofileops-0.8.0a8/setup.py
@@ -0,0 +1,40 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("geofileops/version.txt", mode="r") as file:
+    version = file.readline()
+
+setuptools.setup(
+    name="geofileops",
+    version=version,
+    author="Pieter Roggemans",
+    author_email="pieter.roggemans@gmail.com",
+    description="Package to do spatial operations on large geo files.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/geofileops/geofileops",
+    include_package_data=True,
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "cloudpickle",
+        "fiona",
+        "gdal",
+        "geopandas>=0.11,<0.14",
+        "numpy",
+        "pandas",
+        "psutil",
+        "pygeoops>=0.2,<0.3",
+        "pyogrio",
+        "pyproj",
+        "shapely>=2,<2.1",
+        "topojson",
+    ],
+    extras_require={"full": ["simplification"]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
