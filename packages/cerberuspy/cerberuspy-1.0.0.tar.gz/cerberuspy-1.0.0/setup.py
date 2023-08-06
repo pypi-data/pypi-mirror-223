ae827a33b6dd915aeb9e3ba9fe0bded6645f8159
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:07 +0000

    Add package cerberuspy/1.0.0/cerberuspy-1.0.0.tar.gz

---
+++ packages/cerberuspy/cerberuspy-1.0.0.tar.gz/cerberuspy-1.0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+	name='cerberuspy',
+	version='1.0.0',
+	author='iscerberus',
+	author_email='support@iscerberus.ru',
+	description='Клиент для использования API ИС "Цербер".')
