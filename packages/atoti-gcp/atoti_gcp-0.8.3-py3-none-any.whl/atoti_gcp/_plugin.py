e442d834cec2d4a468b2bc7dfdea4e07a8c6aa9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:34 +0000

    Add package atoti-gcp/0.8.3/atoti_gcp-0.8.3-py3-none-any.whl

---
+++ packages/atoti-gcp/atoti_gcp-0.8.3-py3-none-any.whl/atoti_gcp/_plugin.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+from typing import Optional
+
+from atoti_core import Plugin
+
+
+class GcpPlugin(Plugin):
+    @property
+    def jar_path(self) -> Optional[Path]:
+        return Path(__file__).parent / "data" / "atoti-gcp.jar"
+
+    @property
+    def java_package_name(self) -> Optional[str]:
+        return "io.atoti.loading.gcp"
