9a02b575820e0d37218544eacdfe35e978c30d56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:50 +0000

    Add package xapi-python/0.1.6/xapi-python-0.1.6.tar.gz

---
+++ packages/xapi-python/xapi-python-0.1.6.tar.gz/xapi-python-0.1.6/xapi/__init__.py
@@ -0,0 +1,13 @@
+"""
+.. moduleauthor:: Paweł Knioła <pawel.kn@gmail.com>
+"""
+
+name = "xapi"
+__version__ = "0.1.6"
+
+from .xapi import XAPI, connect
+from .enums import TradeCmd, TradeType, TradeStatus, PeriodCode
+from .connection import Connection
+from .socket import Socket
+from .stream import Stream
+from .exceptions import ConnectionClosed, LoginFailed
