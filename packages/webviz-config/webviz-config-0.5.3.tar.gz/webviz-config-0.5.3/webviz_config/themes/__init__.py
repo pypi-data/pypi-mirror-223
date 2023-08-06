340a6b89967561a07296939d06130054d3106f7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:59 +0000

    Add package webviz-config/0.5.3/webviz-config-0.5.3.tar.gz

---
+++ packages/webviz-config/webviz-config-0.5.3.tar.gz/webviz-config-0.5.3/webviz_config/themes/__init__.py
@@ -0,0 +1,17 @@
+from importlib.metadata import entry_points
+
+from .. import WebvizConfigTheme
+from ._default_theme import default_theme
+
+installed_themes = {default_theme.theme_name: default_theme}
+
+__all__ = ["installed_themes"]
+
+for entry_point in entry_points().get("webviz_config_themes", []):
+    theme = entry_point.load()
+
+    globals()[entry_point.name] = theme
+    __all__.append(entry_point.name)
+
+    if isinstance(theme, WebvizConfigTheme):
+        installed_themes[theme.theme_name] = theme
