e733097e1aa2c7f4486ac2502c2924609afcca84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:33 +0000

    Add package pdpp/0.4.7/pdpp-0.4.7.tar.gz

---
+++ packages/pdpp/pdpp-0.4.7.tar.gz/pdpp-0.4.7/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pdpp",
+    version="0.4.7",
+    install_requires=[
+        'Click>=7.0', 
+        'doit>=0.31.1', 
+        'networkx>=2.2', 
+        'graphviz>=0.10.1', 
+        'pydot>=1.4.1', 
+        'questionary>=1.0.2', 
+        'pyyaml>=5.3'
+        ],
+    packages=find_packages(),
+	include_package_data=True,
+    entry_points = """
+        [console_scripts]
+        pdpp=pdpp.main:main
+        """,
+    readme="README.md",
+    
+
+    author="Pierson Browne, Rachel Wood, Tyler Crick, John McLevey",
+    author_email="pbrowne88@gmail.com",
+    description="Command line tool for automation, transparency, and reproducibility in data processing projects",
+    keywords="doit",
+    url="https://github.com/UWNETLAB/pdpp",
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+    ],		
+)
