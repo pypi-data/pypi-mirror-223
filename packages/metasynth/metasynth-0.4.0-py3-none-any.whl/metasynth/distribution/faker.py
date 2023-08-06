9e7e28a18a5e4dd910851ba41f9627ee9b16db45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:19 +0000

    Add package metasynth/0.4.0/metasynth-0.4.0-py3-none-any.whl

---
+++ packages/metasynth/metasynth-0.4.0-py3-none-any.whl/metasynth/distribution/faker.py
@@ -0,0 +1,61 @@
+"""Module containing an interface to the faker package."""
+from typing import Iterable
+
+from faker import Faker
+
+from metasynth.distribution.base import metadist, BaseDistribution
+
+
+@metadist(implements="core.faker", var_type="string")
+class FakerDistribution(BaseDistribution):
+    """Distribution for the faker package.
+
+    This is mainly an interface for the faker package, so that it
+    can be used within the MetaSynth package. It doesn't have any
+    true fitting/statistical inference method, so it has to be manually
+    selected.
+
+    Parameters
+    ----------
+    faker_type: str
+        The provider function in the faker package, e.g. 'city' or 'ipv4', etc.
+    locale: str
+        Locale used for the faker package.
+    """
+
+    def __init__(self, faker_type: str, locale: str = "en_US"):
+        self.faker_type: str = faker_type
+        self.locale: str = locale
+        self.fake: Faker = Faker(locale=locale)
+
+    @classmethod
+    def _fit(cls, values, faker_type: str = "city", locale: str = "en_US"):  \
+            # pylint: disable=arguments-differ
+        """Select the appropriate faker function and locale."""
+        return cls(faker_type, locale)
+
+    def __str__(self):
+        return f"faker.{self.faker_type}.{self.locale}"
+
+    def draw(self):
+        return getattr(self.fake, self.faker_type)()
+
+    def information_criterion(self, values: Iterable) -> float:
+        return 99999
+
+    @classmethod
+    def default_distribution(cls):
+        return cls("city")
+
+    def _param_dict(self):
+        return {
+            "faker_type": self.faker_type,
+            "locale": self.locale
+        }
+
+    @classmethod
+    def _param_schema(cls):
+        return {
+            "faker_type": {"type": "string"},
+            "locale": {"type": "string"},
+        }
---
+++ packages/metasynth/metasynth-0.4.0-py3-none-any.whl/metasynth/distribution/regex/__init__.py
@@ -0,0 +1,6 @@
+"""Package for fitting regexes to strings."""
+
+from metasynth.distribution.regex.base import (RegexDistribution,
+                                               UniqueRegexDistribution)
+
+__all__ = ["RegexDistribution", "UniqueRegexDistribution"]
