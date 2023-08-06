fa3a2cb7b0de9eb2272b53bbb98cfd0f0a8f85e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:47 +0000

    Add package kuto/0.0.33/kuto-0.0.33.tar.gz

---
+++ packages/kuto/kuto-0.0.33.tar.gz/kuto-0.0.33/demo/tests/test_web.py
@@ -0,0 +1,58 @@
+"""
+@Author: kang.yang
+@Date: 2023/5/16 14:37
+"""
+import kuto
+
+from pages.web_page import IndexPage, LoginPage
+
+
+@kuto.feature("web测试demo")
+@kuto.story("PO模式")
+class TestPoDemo(kuto.Case):
+
+    def start(self):
+        self.ip = IndexPage(self.driver)
+        self.lp = LoginPage(self.driver)
+
+    @kuto.title("登录")
+    def test_login(self):
+        self.open()
+        self.ip.loginBtn.click()
+        self.lp.pwdLoginTab.click()
+        self.lp.userInput.input('xxx')
+        self.lp.pwdInput.input('xxx')
+        self.lp.licenseBtn.click()
+        self.lp.loginBtn.click()
+        self.lp.firstCompanyIcon.click()
+        self.lp.assertContent\
+            ("查专利", url='https://www-test.qizhidao.com/')
+        self.screenshot("首页")
+
+
+@kuto.feature("web测试demo")
+@kuto.story("普通模式")
+class TestNormalDemo(kuto.Case):
+
+    @kuto.title("登录")
+    def test_login(self):
+        self.open()
+        self.elem(text='登录/注册').click()
+        self.elem(text='帐号密码登录').click()
+        self.elem(holder='请输入手机号码').input("xxx")
+        self.elem(holder='请输入密码').input("xxx")
+        self.elem(css="span.el-checkbox__inner", index=1).click()
+        self.elem(text='立即登录').click()
+        self.elem(xpath="(//img[@class='right-icon'])[1]").click()
+        self.assertUrl("https://www-test.qizhidao.com/")
+        self.elem(text='查专利').assertVisible()
+        self.screenshot("首页")
+
+
+if __name__ == '__main__':
+    kuto.main(
+        platform="web",
+        browser="chrome",
+        host="https://www-test.qizhidao.com"
+    )
+
