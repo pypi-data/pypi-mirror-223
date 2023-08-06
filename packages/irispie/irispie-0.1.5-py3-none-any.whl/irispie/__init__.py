2f9ebb32b24cac4dedf1b61874d50e9ce4dd375d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:11 +0000

    Add package irispie/0.1.5/irispie-0.1.5-py3-none-any.whl

---
+++ packages/irispie/irispie-0.1.5-py3-none-any.whl/irispie/__init__.py
@@ -0,0 +1,24 @@
+"""
+Iris Pie
+"""
+
+from .dataman import *
+from .dataman import __all__ as dataman_all
+
+from .models import *
+from .models import __all__ as models_all
+
+from .quantities import *
+from .quantities import __all__ as quantities_all
+
+from .equations import *
+from .equations import __all__ as equations_all
+
+__all__ = (
+    []
+    + dataman_all 
+    + models_all 
+    + quantities_all
+    + equations_all
+)
+
