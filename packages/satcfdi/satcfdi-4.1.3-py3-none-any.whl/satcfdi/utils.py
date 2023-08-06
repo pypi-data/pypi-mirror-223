e63db61fcf9b43e61f94ecd8b37891064965dc12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:43:55 +0000

    Add package satcfdi/4.1.3/satcfdi-4.1.3-py3-none-any.whl

---
+++ packages/satcfdi/satcfdi-4.1.3-py3-none-any.whl/satcfdi/utils.py
@@ -0,0 +1,42 @@
+import enum
+from collections.abc import Sequence, Mapping
+
+from lxml import etree
+
+parser = etree.XMLParser(
+    no_network=True,
+    remove_comments=True,
+    remove_blank_text=True,
+    huge_tree=True,
+    collect_ids=False,
+    remove_pis=True,
+    recover=True
+)
+
+
+class ScalarMap(dict):
+    pass
+
+
+def iterate(item):
+    if isinstance(item, str | bytes | ScalarMap):
+        return [item]
+    if isinstance(item, Mapping):
+        return item.values()
+    if isinstance(item, Sequence):
+        return item
+    if item is None:
+        return []
+    return [item]
+
+
+class StrEnum(str, enum.Enum):  # Compatible with Python 3.10
+    def __str__(self):
+        return self.value
+
+    def __format__(self, format_spec):
+        return self.value.__format__(format_spec)
+
+    @classmethod
+    def get(cls, key, default=None):
+        return cls._member_map_.get(key, default)
