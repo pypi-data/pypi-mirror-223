a4f7d3119d75e0e9669db30a12bc2ffe725cc249
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:28 +0000

    Add package Carter-Offline/2.0.16/Carter-Offline-2.0.16.tar.gz

---
+++ packages/Carter-Offline/Carter-Offline-2.0.16.tar.gz/Carter-Offline-2.0.16/CarterOffline/main.py
@@ -0,0 +1,54 @@
+from Janex import *
+from carterpy import Carter
+
+class CarterOffline:
+    def __init__(self, intents_file_path, CarterAPI):
+        self.intents_file_path = intents_file_path
+        self.CarterAPI = CarterAPI
+
+    def SendToCarter(self, input_string, User):
+        original = sentence
+
+        carter = Carter(self.CarterAPI)
+        ResponseOutput = carter.say(sentence, User)
+
+        ResponseOutput = ResponseOutput.output_text
+
+        ResponseOutput = str(ResponseOutput)
+
+        User = str(User)
+
+        intents_file_path, thesaurus_file_path = "intents.json", "thesaurus.json"
+
+        matcher = IntentMatcher(intents_file_path, thesaurus_file_path)
+
+        tag, similarity_percentage = matcher.pattern_compare(sentence)
+
+        tag = tag.get("tag")
+
+        target_class = None
+        for intent_class in intents['intents']:
+            if intent_class.get("tag") == tag:
+                target_class = intent_class
+                print(intent_class.get("tag"))
+
+                ResponseOutput = ResponseOutput.replace(User, "")
+                print(ResponseOutput)
+
+                if target_class:
+                        target_class.setdefault("patterns", []).append(original)
+                        target_class.setdefault("responses", []).append(ResponseOutput)
+                else:
+                        new_class = {
+                                "tag": User,
+                                "patterns": [sentence],
+                                "responses": [ResponseOutput]
+                                }
+                        intents['intents'].append(new_class)
+
+                with open("intents.json", 'w') as json_file:
+                    json.dump(intents, json_file, indent=4, separators=(',', ': '))
+
+        ResponseOutput = matcher.ResponseGenerator(ResponseOutput)
+
+        return ResponseOutput
