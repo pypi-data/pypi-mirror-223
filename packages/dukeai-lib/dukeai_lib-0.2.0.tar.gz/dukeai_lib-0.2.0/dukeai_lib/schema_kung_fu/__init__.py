afda9b612849e161a40ee29080e02d3f20ea4585
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:42 +0000

    Add package dukeai-lib/0.2.0/dukeai_lib-0.2.0.tar.gz

---
+++ packages/dukeai-lib/dukeai_lib-0.2.0.tar.gz/dukeai_lib-0.2.0/dukeai_lib/schema_kung_fu/__init__.py
@@ -0,0 +1,7 @@
+"""
+
+schema kung-fu
+
+"""
+from . import accessorial, bill_of_lading, classification, classification_exceptions, invoice, noa_lor, \
+    parse_classification_page_range, rate_confirmation, rsi_translator, schema_utilities
