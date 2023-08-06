ec46344efe28a4a21a5f44075a0f3aaca4b5b3f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:06 +0000

    Add package evodiff/0.0.8/evodiff-0.0.8.tar.gz

---
+++ packages/evodiff/evodiff-0.0.8.tar.gz/evodiff-0.0.8/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="evodiff", # Replace with your own username
+    version="0.0.8",
+    description="Python package for generation of protein sequences and evolutionary alignments via discrete diffusion models",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/microsoft/evodiff",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8.5',
+    include_package_data=True,
+    # package_dir = {"": "config"},
+    packages=setuptools.find_packages(),
+    package_data={'': ['config/*']},
+    # data_files=[('config', ['config/config38M.json', 'config/config640M.json', 'config/configMSA.json', 'config/configMSA-600M.json'])],
+)
