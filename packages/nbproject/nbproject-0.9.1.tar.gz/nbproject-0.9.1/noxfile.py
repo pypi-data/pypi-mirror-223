3b29035da4f7bfdfba419e70d0f99f9057d5e067
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:43:42 +0000

    Add package nbproject/0.9.1/nbproject-0.9.1.tar.gz

---
+++ packages/nbproject/nbproject-0.9.1.tar.gz/nbproject-0.9.1/noxfile.py
@@ -0,0 +1,19 @@
+import nox
+from laminci import move_built_docs_to_docs_slash_project_slug
+from laminci.nox import build_docs, login_testuser1, run_pre_commit, run_pytest
+
+nox.options.default_backend = "none"
+
+
+@nox.session
+def lint(session: nox.Session) -> None:
+    run_pre_commit(session)
+
+
+@nox.session
+def build(session):
+    login_testuser1(session)
+    session.run(*"pip install .[dev]".split())
+    run_pytest(session)
+    build_docs(session)
+    move_built_docs_to_docs_slash_project_slug()
