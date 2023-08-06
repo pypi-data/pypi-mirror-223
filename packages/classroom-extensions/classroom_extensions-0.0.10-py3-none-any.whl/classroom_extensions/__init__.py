93568f31b94d82824a2bd144b78b6cfdc29f1291
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:09 +0000

    Add package classroom-extensions/0.0.10/classroom_extensions-0.0.10-py3-none-any.whl

---
+++ packages/classroom-extensions/classroom_extensions-0.0.10-py3-none-any.whl/classroom_extensions/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    Set of IPython extensions used for teaching at ETS Montreal.
+"""
+
+__version__ = '0.0.10'
+
+__all__ = [
+    "mariadb",
+    "mariadb_install",
+    "web",
+    "plantuml",
+    "mongodb",
+    "mongodb_install"
+]
