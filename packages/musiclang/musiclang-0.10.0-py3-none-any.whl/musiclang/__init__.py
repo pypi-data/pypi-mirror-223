484848b1b8618f284a165c906a2bce8d190043e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:17 +0000

    Add package musiclang/0.10.0/musiclang-0.10.0-py3-none-any.whl

---
+++ packages/musiclang/musiclang-0.10.0-py3-none-any.whl/musiclang/__init__.py
@@ -0,0 +1,19 @@
+from .write.score import Score
+from .write.note import Note, Silence, Continuation
+from .write.melody import Melody
+from .write.chord import Chord
+from .write.tonality import Tonality
+from .write.element import Element
+from .write.rhythm import Metric, CompositeMetric, ScoreRhythm
+
+from .write import library
+from .analyze import ScoreFormatter
+from .transform.library import VoiceLeading
+from .transform import PartComposer
+
+__all__ = ['Score', 'Note', 'Silence', 'Continuation', 'Melody',
+           'Chord', 'Tonality', 'Element', 'library', 'Metric', 'CompositeMetric', 'ScoreRhythm', 'ScoreFormatter',
+           'VoiceLeading', 'PartComposer'
+           ]
+
+__author__ = 'Florian GARDIN'
