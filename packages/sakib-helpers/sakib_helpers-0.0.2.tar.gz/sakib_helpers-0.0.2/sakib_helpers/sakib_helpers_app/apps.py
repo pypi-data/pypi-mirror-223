b55b9310570b8c0da3975789ef4488a4ea96b004
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:02 +0000

    Add package sakib-helpers/0.0.2/sakib_helpers-0.0.2.tar.gz

---
+++ packages/sakib-helpers/sakib_helpers-0.0.2.tar.gz/sakib_helpers-0.0.2/sakib_helpers/sakib_helpers_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SakibHelpersAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'sakib_helpers.sakib_helpers_app'
