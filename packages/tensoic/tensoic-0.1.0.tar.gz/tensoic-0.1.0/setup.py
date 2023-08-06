b4c1a2ee9ad7f03748343cb6221a220d95bbfef2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:52 +0000

    Add package tensoic/0.1.0/tensoic-0.1.0.tar.gz

---
+++ packages/tensoic/tensoic-0.1.0.tar.gz/tensoic-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tensoic',
+    version='0.1.0',
+    description='A placeholder package for Tensoic',
+    author='Adarsh Shirawalmath',
+    packages=find_packages(),
+    install_requires=[],
+)
