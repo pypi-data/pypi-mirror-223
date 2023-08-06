d346560ba8b1557af2b75a4262ae620f3313bdc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:43:50 +0000

    Add package mereja/0.0.6/mereja-0.0.6-py3-none-any.whl

---
+++ packages/mereja/mereja-0.0.6-py3-none-any.whl/mereja/functions/jobs.py
@@ -0,0 +1,79 @@
+import json
+
+from pyethiojobs import EthioJobs
+from questionary import Choice
+from rich.status import Status
+
+from mereja.ui import JobView
+from mereja.utils import ask, with_live, save_file, get_path, no_ans_or_back
+
+
+@with_live("Searching for {query}...")
+async def search_for_job(query: str, status: Status) -> None:
+    jobs = await EthioJobs().search(query)
+    if not jobs:
+        status.console.print("[bold yellow]No jobs found")
+        return
+    status.console.print(f"[bold green]Found {len(jobs)} jobs")
+    status.stop()
+    ans = await ask(
+        message="Select a job",
+        choice=[Choice(job.title, value=job.job_id) for job in jobs],
+    )
+    no_ans_or_back(ans)
+    job = await EthioJobs().get_job(ans)
+    await JobView(job).run_async()
+
+
+@with_live("Searching for government jobs...")
+async def get_government_jobs(status: Status):
+    jobs = await EthioJobs().get_gov_jobs()
+    if not jobs:
+        status.console.print("[bold yellow]No jobs found")
+        return
+    status.console.print(f"[bold green]Found {len(jobs)} jobs")
+    status.stop()
+    ans = await ask(
+        message="Select a job",
+        choice=[Choice(job.company, value=job.job) for job in jobs],
+    )
+    no_ans_or_back(ans)
+    job = await EthioJobs().get_job(ans)
+    await JobView(job).run_async()
+
+
+@with_live("Searching for latest jobs...")
+async def get_latest_jobs(status: Status):
+    jobs = await EthioJobs().get_latest_jobs()
+    if not jobs:
+        status.console.print("[bold yellow]No jobs found")
+        return
+    status.console.print(f"[bold green]Found {len(jobs)} jobs")
+    status.stop()
+    ans = await ask(
+        message="Select a job",
+        choice=[Choice(job.title, value=job.job_id) for job in jobs],
+    )
+    no_ans_or_back(ans)
+    job = await EthioJobs().get_job(ans)
+    await JobView(job).run_async()
+
+
+@with_live("Searching for latest jobs...")
+async def export_latest_jobs(path: str, status: Status, limit: int):
+    jobs = await EthioJobs().get_latest_jobs()
+    if not jobs:
+        status.console.print("[bold yellow]No jobs found")
+        return
+    status.console.print(f"[bold green]Found {len(jobs)} jobs")
+    jobs = jobs[:limit] if limit else jobs
+    dict_jobs = []
+    path = get_path(path, "latest_jobs")
+    status.update(f"[bold green]Exporting {len(jobs)} jobs to {path}")
+    for job in jobs:
+        jop_de = await job.get_details()
+        dict_jobs.append(jop_de.dict())
+    save_file(path, json.dumps(dict_jobs, indent=4))
+    status.console.print(
+        f"[bold green]Successfully exported {len(dict_jobs)} jobs to {path}"
+    )
---
+++ packages/mereja/mereja-0.0.6-py3-none-any.whl/mereja/functions/market.py
@@ -0,0 +1,95 @@
+import json
+
+from jiji import JiJi
+from questionary import Choice
+from rich.status import Status
+
+from mereja.ui import start_market_ui
+from mereja.utils import ask, with_live, save_file, get_path, no_ans_or_back
+
+
+@with_live("Searching for {query}...")
+async def search_for_product(
+    query: str,
+    status: Status,
+    page: int = 1,
+    limit: int = -1,
+) -> None:
+    jiji = JiJi()
+    product = await jiji.search(query, page)
+    if not product:
+        status.console.print("[bold yellow]No products found")
+        return
+    products = product.products
+    if limit:
+        products = product.products[:limit]
+    status.console.print(f"[bold green]Found {len(product.products)} products")
+    status.stop()
+    ans = await ask(
+        message="Select a product",
+        choice=[
+            Choice(
+                f"{product.title} - [{product.price} ETB] "
+                f"{f' ({product.price_type})' if product.price_type else ' (Fixed)'}",
+                value=product.id,
+            )
+            for product in products
+        ],
+    )
+    no_ans_or_back(ans)
+    product = await jiji.get_product(ans)
+    start_market_ui(product)
+
+
+@with_live("Getting trending products...")
+async def get_trending_products(status: Status, limit: int = -1, page: int = 1) -> None:
+    jiji = JiJi()
+    products = await jiji.get_trending(page)
+    if not products:
+        status.console.print("[bold yellow]No products found")
+        return
+    status.console.print(f"[bold green]Found {len(products)} products")
+    products = products[:limit]
+    status.stop()
+    ans = await ask(
+        message="Select a product",
+        choice=[
+            Choice(
+                f"{product.title} - [{product.price} ETB]"
+                f" {f' ({product.price_type})' if product.price_type else ' (Fixed)'}",
+                value=product.id,
+            )
+            for product in products
+        ],
+    )
+    no_ans_or_back(ans)
+    product = await jiji.get_product(ans)
+    start_market_ui(product)
+
+
+@with_live("Getting trending products...")
+async def export_products(
+    status: Status,
+    limit: int,
+    path: str,
+    query: str = None,
+    page: int = 1,
+):
+    jiji = JiJi()
+    if query:
+        products = await jiji.search(query, page)
+    else:
+        products = await jiji.get_trending(page)
+    if not products:
+        status.console.print("[bold yellow]No products found")
+        return
+    status.console.print(f"[bold green]Found {len(products)} products")
+    products = products[:limit] if limit else products
+    path = get_path(path, "products")
+    status.update(f"Exporting {len(products)} products to {path}")
+    products_dict = []
+    for product in products:
+        product = await product.get_product()
+        products_dict.append(product.dict())
+    save_file(path, json.dumps(products_dict, indent=4))
+    return status.console.print(f"Products saved to {path}")
