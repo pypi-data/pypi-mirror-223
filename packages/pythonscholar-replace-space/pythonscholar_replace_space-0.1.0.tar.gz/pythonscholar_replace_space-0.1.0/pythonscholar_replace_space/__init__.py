8687c332b84c147e36b5b36985a2ae473de90a9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:39 +0000

    Add package pythonscholar-replace-space/0.1.0/pythonscholar_replace_space-0.1.0.tar.gz

---
+++ packages/pythonscholar-replace-space/pythonscholar_replace_space-0.1.0.tar.gz/pythonscholar_replace_space-0.1.0/pythonscholar_replace_space/__init__.py
@@ -0,0 +1,4 @@
+# string_utils/__init__.py
+
+def replace_spaces_with_underscore(input_string):
+    return input_string.replace(' ', '_')
---
+++ packages/pythonscholar-replace-space/pythonscholar_replace_space-0.1.0.tar.gz/pythonscholar_replace_space-0.1.0/setup.py
@@ -0,0 +1,20 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='pythonscholar_replace_space',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    author='Khushal Jethava',
+    description='A Python package to replace spaces with underscores in a string.',
+    url='https://github.com/khushaljethava/pythonscholar_replace_space',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
