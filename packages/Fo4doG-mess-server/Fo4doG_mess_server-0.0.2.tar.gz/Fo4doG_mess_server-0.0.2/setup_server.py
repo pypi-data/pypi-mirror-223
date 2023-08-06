41a498298cbd98a6f6719488df0186bf003429c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:30 +0000

    Add package Fo4doG-mess-server/0.0.2/Fo4doG_mess_server-0.0.2.tar.gz

---
+++ packages/Fo4doG-mess-server/Fo4doG_mess_server-0.0.2.tar.gz/Fo4doG_mess_server-0.0.2/setup_server.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name="Fo4doG_mess_server",
+      version="0.0.2",
+      description="Fo4doG_mess_server",
+      author="Ivan Ivanov",
+      author_email="iv.iv@yandex.ru",
+      packages=find_packages(),
+      install_requires=['PyQt5', 'sqlalchemy', 'pycryptodome', 'pycryptodomex']
+      )
