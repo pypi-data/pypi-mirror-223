2ab343a83ad96e5bc311cf6fe888c2954ac926f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:46 +0000

    Add package pojw/0.0.4/pojw-0.0.4.tar.gz

---
+++ packages/pojw/pojw-0.0.4.tar.gz/pojw-0.0.4/README.md
@@ -0,0 +1,3 @@
+# pojw
+ OpenJigWare 원격 제어 파이썬 라이브러리
+ojw5014@hanmail.net
---
+++ packages/pojw/pojw-0.0.4.tar.gz/pojw-0.0.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+ 
+setup(
+    name                = 'pojw',
+    # Package version
+    version             = '0.0.4',
+    license             = 'MIT',
+    author              = 'Jinwook.On',
+    author_email        = 'ojw5014@hanmail.net',
+    description         = 'OpenJigWare 원격 제어 소스',
+    url                 = 'https://github.com/ojw5014/pojw',
+    packages            = find_packages(),
+    # 해당 패키지를 사용하기 위해 필요한 파이썬 버전을 적습니다.
+    classifiers         = [
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
