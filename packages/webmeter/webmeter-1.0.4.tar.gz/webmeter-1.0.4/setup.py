9fb6c6d0cede8b07d9db4518c049269c86875ca8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:29 +0000

    Add package webmeter/1.0.4/webmeter-1.0.4.tar.gz

---
+++ packages/webmeter/webmeter-1.0.4.tar.gz/webmeter-1.0.4/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+#
+# Licensed under MIT
+#
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    install_requires=['fastapi','uvicorn', 'requests', 'logzero', 'fire','pyfiglet','psutil'],
+    version='1.0.4',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    description="WebMeter - A web api-performance tool based on jmeter.",
+    packages=setuptools.find_namespace_packages(include=["webmeter", "webmeter.*"], ),
+    include_package_data=True
+)
