68c0d1c66e14c28bb8400499bdd4160f6f71c30f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:33 +0000

    Add package roastedbyai/1.0.1/roastedbyai-1.0.1.tar.gz

---
+++ packages/roastedbyai/roastedbyai-1.0.1.tar.gz/roastedbyai-1.0.1/README.md
@@ -0,0 +1,65 @@
+# RoastedByAI
+This is an *unofficial* Python package for https://roastedby.ai to roast and get roasted by AI.
+
+
+---
+
+
+## How to install
+Install the package using one of these pip commands (they both 
+install the same package, the first command just does not always 
+work properly on all devices, the second one should always work)
+
+```shell
+pip install roastedbyai
+```
+Windows:
+```shell
+python -m pip install roastedbyai
+```
+Linux:
+```shell
+python3 -m pip install roastedbyai
+```
+
+
+---
+
+
+## Usage
+
+```python
+from roastedbyai import Conversation
+
+convo = Conversation()
+
+user_input = input("Start by roasting the AI:\n>>> ")
+response1 = convo.send(user_input)
+print(response1)
+
+user_input = input("\nRoast the AI again:\n>>> ")
+response2 = convo.send(user_input)
+print(response2)
+
+convo.kill()
+# This step isn't necessary, the moment you don't use the `convo` object
+# anymore, python will automatically handle it
+
+print("\n---\n")
+for msg in convo.history:
+    print(msg["role"], msg["content"], sep=": ")
+```
+
+
+---
+
+
+## Contributing
+Feel free to open Pull Requests with new features, improvements or fixes.
+
+
+---
+
+
+## Disclaimer/Credits
+I am not affiliated with https://roastedby.ai, all credits for the AI go to them.
---
+++ packages/roastedbyai/roastedbyai-1.0.1.tar.gz/roastedbyai-1.0.1/roastedbyai/roasted.py
@@ -0,0 +1,175 @@
+# MIT License
+#
+# Copyright (c) 2023 jvherck (on GitHub)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import requests
+from typing import List
+
+from .errors import MessageLimitExceeded, CharacterLimitExceeded
+
+
+__all__ = ("Conversation",)
+
+
+# Create a new object of this class for every simultaneous conversation you want to have
+class Conversation:
+    """
+    An instance of this class represents a conversation with the AI.
+    Create a new instance of this class if this conversation reached its message limit.
+    """
+    def __init__(self):
+        """
+        Creates an instance of this class that represents a conversation with the AI.
+        Create a new instance if this conversation reached its message limit.
+        """
+        self.__history: list = [{
+            "role": "assistant",
+            "content": "Hello there. I'm here to roast you."
+        }]
+        self.__alive: bool = True
+        self.__url: str = "https://roastedby.ai/api/generate"
+
+    def __str__(self):
+        """
+        :returns: the content of the last message in `Conversation.history`
+        """
+        return self.__history[len(self.__history)-1]["content"]
+
+    def __len__(self):
+        """
+        :returns: the amount of messages in `Conversation.history`
+        """
+        return len(self.__history)
+
+    @property
+    def aimessages(self) -> list:
+        """
+        Get the list of the AI's messages.
+
+        :returns: list: the list of messages
+        """
+        aimsgs = []
+        for msg in self.__history:
+            if msg["role"] == "assistant":
+                aimsgs.append(msg["content"])
+        return aimsgs
+
+    @property
+    def usermessages(self) -> list:
+        """
+        Get the list of the user's messages.
+
+        :returns: list: the list of messages
+        """
+        aimsgs = []
+        for msg in self.__history:
+            if msg["role"] == "user":
+                aimsgs.append(msg["content"])
+        return aimsgs
+
+    @property
+    def history(self) -> List[dict]:
+        """
+        Return the full history of this Conversation.
+
+        [
+        {"role": "assistant","content": "..."},
+        {"role": "user","content": "..."},
+        ...
+        ]
+
+        :returns: list[dict]: the history list which contains dicts
+        """
+        return self.__history
+
+    @property
+    def alive(self) -> bool:
+        """
+        Whether you are still able to participate in this Conversation.
+        If this returns False, you can no longer use the `Conversation.send` function, but you can still access the message history.
+
+        :returns: bool: True if you can still send messages, else False
+        """
+        return self.__alive
+
+    def send(self, message: str) -> str:
+        """
+        Sends the user input to the AI and the AI returns a roast as output.
+
+        :param message: str: the user input for the AI
+        :type message: str
+        :return: returns a string containing the AI's (roast) response to the user's input
+        """
+        if self.__alive is False:
+            raise MessageLimitExceeded('Message limit exceeded, you can not send any more messages to this Conversation.')
+
+        if len(message) > 250:
+            raise CharacterLimitExceeded('Character limit exceeded: maximum allowed number of characters is 250!')
+
+        # Removing non-allowed characters
+        for char in message:
+            if ord(char) > 65535:
+                message = message.replace(char, '￿')
+
+        _json_body = {
+            "userMessage": {
+                "role": "user",
+                "content": message
+            },
+            "history": self.__history,
+            "style": "default"
+        }
+        _headers = {
+            "Accept": "*/*",
+            "Accept-Encoding": "br",
+            "Accept-Language": "en-GB,en;q=0.9",
+            "Content-Length": str(len(str(_json_body))),
+            "Content-Type": "application/json",
+            "Origin": "https://roastedby.ai",
+            "Referer": "https://roastedby.ai/",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
+        }
+
+        result = requests.post(self.__url, json=_json_body, headers=_headers).json()["content"]
+
+        self.__history.append({
+            "role": "user",
+            "content": message
+        })
+        self.__history.append({
+            "role": "assistant",
+            "content": result
+        })
+
+        if result == 'Too many messages. Thanks for playing!':
+            self.kill()
+        elif result == 'Can you calm down?! You exceeded the rate limit. Please try again later.':
+            self.kill()
+
+        return result
+
+    def kill(self) -> None:
+        """
+        Quit and close the conversation.
+        User and AI messages are still available using `Conversation.usermessages` and `Conversation.aimessages`.
+        The full conversation is available in `Conversation.history`.
+        """
+        self.__alive = False
