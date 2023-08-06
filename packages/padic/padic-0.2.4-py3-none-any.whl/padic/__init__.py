babc7b942497fc98dc89d446221b43402c680e6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:48 +0000

    Add package padic/0.2.4/padic-0.2.4-py3-none-any.whl

---
+++ packages/padic/padic-0.2.4-py3-none-any.whl/padic/__init__.py
@@ -0,0 +1,7 @@
+from padic.padic import Padic
+from padic.padic import log, exp, sin, cos, binomial
+from padic.padic import binomial_coeff
+from padic.padic import find_approx_root
+from padic.padic import hensel
+from padic.padic import series
+from padic.padic import Rational
