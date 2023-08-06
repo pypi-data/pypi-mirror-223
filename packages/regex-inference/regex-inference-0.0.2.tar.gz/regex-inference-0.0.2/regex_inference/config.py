e5e38c07251e339d729064402a3b0c95a635412b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:34 +0000

    Add package regex-inference/0.0.2/regex-inference-0.0.2.tar.gz

---
+++ packages/regex-inference/regex-inference-0.0.2.tar.gz/regex-inference-0.0.2/regex_inference/config.py
@@ -0,0 +1,34 @@
+"""
+Config the Regex Inference Model
+"""
+
+
+class Config:
+    """
+    - 1. equivalence model: kind | label
+        - kind: Turn on DynamicRecord for unifing Records with different fields
+        - label: Turn off DynamicRecord and construct Union of Records with inconsistent fields
+    - 2. enable_uniform_record: True | False (whether try to unify Record values for Record with values of same kind)
+    """
+
+    def __init__(self, unify_records=True, equivalence_mode='kind'):
+        self.init(
+            unify_records=unify_records,
+            equivalence_mode=equivalence_mode)
+
+    def init(self, unify_records=True, equivalence_mode='kind'):
+        self._unify_records = unify_records
+        assert equivalence_mode == 'kind' or equivalence_mode == 'label'
+        self._equivalence_mode = equivalence_mode
+
+    @property
+    def unify_records(self) -> bool:
+        return self._unify_records
+
+    @property
+    def equivalence_mode(self) -> str:
+        return self._equivalence_mode
+
+
+config = Config()
+init = config.init
---
+++ packages/regex-inference/regex-inference-0.0.2.tar.gz/regex-inference-0.0.2/regex_inference/schema/fitter.py
@@ -0,0 +1,31 @@
+from ..config import config
+from .objs import Record, Array, Atomic
+from .inference.reduce import reduce_schema
+
+
+def fit(data):
+    if isinstance(data, dict):
+        schema_content = dict()
+        for key in data:
+            schema_content[key] = fit(
+                data[key]
+            )
+        schema = Record(schema_content)
+        if config.unify_records:
+            schema = try_unify_dict(schema)
+    elif isinstance(data, list):
+        schema = Array(reduce_schema([fit(e) for e in data]))
+    elif data is None:
+        schema = Atomic(None)
+    else:
+        schema = Atomic(type(data))
+    return schema
+
+
+def try_unify_dict(dict_schema):
+    uni_dict = dict_schema.to_uniform_dict()
+    if isinstance(uni_dict._content, Record) or isinstance(
+            uni_dict._content, Array):
+        return uni_dict
+    else:
+        return dict_schema
