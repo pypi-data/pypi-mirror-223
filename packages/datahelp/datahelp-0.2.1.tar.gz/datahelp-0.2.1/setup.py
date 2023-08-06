fe5bf8466f4b32855b1b231d9792b73b7060f726
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:08 +0000

    Add package datahelp/0.2.1/datahelp-0.2.1.tar.gz

---
+++ packages/datahelp/datahelp-0.2.1.tar.gz/datahelp-0.2.1/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import os
+from setuptools import find_packages, setup
+
+directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name="datahelp",
+    version="0.2.1",
+    license="MIT",
+    description="Data science library for data science / data analysis teams",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Meshack Kitonga",
+    author_email="dev.kitonga@gmail.com",
+    url="https://github.com/datahelp",
+    packages=find_packages(),
+    install_requires=[
+        "pandas",
+        "scikit-learn",
+        "seaborn",
+        "numpy",
+        "jupyter",
+        "matplotlib",
+        "nltk",
+        "joblib",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires=">=3.6",
+)
