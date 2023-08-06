35ec534e7451eeb631efcb427819b5bcb36a9019
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:43 +0000

    Add package dbt-rpc/0.4.2/dbt_rpc-0.4.2-py3-none-any.whl

---
+++ packages/dbt-rpc/dbt_rpc-0.4.2-py3-none-any.whl/dbt_rpc/task/deps.py
@@ -0,0 +1,37 @@
+import os
+import shutil
+
+from dbt_rpc.contracts.rpc import (
+    RPCDepsParameters, RemoteDepsResult, RemoteMethodFlags,
+)
+from dbt_rpc.rpc.method import RemoteMethod
+from dbt.task.deps import DepsTask
+
+
+def _clean_deps(config):
+    if os.path.exists(config.packages_install_path):
+        shutil.rmtree(config.packages_install_path)
+    os.makedirs(config.packages_install_path)
+
+
+class RemoteDepsTask(
+    RemoteMethod[RPCDepsParameters, RemoteDepsResult],
+    DepsTask,
+):
+    METHOD_NAME = 'deps'
+
+    def get_flags(self) -> RemoteMethodFlags:
+        return (
+            RemoteMethodFlags.RequiresConfigReloadBefore |
+            RemoteMethodFlags.RequiresManifestReloadAfter
+        )
+
+    def set_args(self, params: RPCDepsParameters):
+        pass
+
+    def handle_request(self) -> RemoteDepsResult:
+        _clean_deps(self.config)
+        self.project = self.config
+        self.cli_vars = self.config.cli_vars
+        self.run()
+        return RemoteDepsResult([])
