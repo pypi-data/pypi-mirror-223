88abe9e9679cd6a4368ccea51986a395157be6db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:05 +0000

    Add package MineLaunch/0.0.1/MineLaunch-0.0.1.tar.gz

---
+++ packages/MineLaunch/MineLaunch-0.0.1.tar.gz/MineLaunch-0.0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="MineLaunch",
+    version="0.0.1",
+    author="lev Kirillov",
+    author_email="kirillovlev63@gmail.com",
+    description="A package to run minecraft forge or vannila",
+    url="https://github.com/levpro555/MineLaunch",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
