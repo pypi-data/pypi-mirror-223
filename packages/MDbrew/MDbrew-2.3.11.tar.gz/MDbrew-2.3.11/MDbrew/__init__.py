813c5d7f2cde40e751ca57619f8e3833cae5421d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:10 +0000

    Add package MDbrew/2.3.11/MDbrew-2.3.11.tar.gz

---
+++ packages/MDbrew/MDbrew-2.3.11.tar.gz/MDbrew-2.3.11/MDbrew/__init__.py
@@ -0,0 +1,8 @@
+from .analysis.msd import MSD
+from .analysis.rdf import RDF
+from .main.brewery import Brewery
+from .spatial import *
+
+__version__ = "2.3.11"
+__name__ = "MDbrew"
+__all__ = ["MSD", "RDF", "Brewery"]
