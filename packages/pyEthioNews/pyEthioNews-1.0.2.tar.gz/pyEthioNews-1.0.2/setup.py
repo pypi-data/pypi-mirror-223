c82920f664a5aa37a4eb4cf870783969b27c03d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:22 +0000

    Add package pyEthioNews/1.0.2/pyEthioNews-1.0.2.tar.gz

---
+++ packages/pyEthioNews/pyEthioNews-1.0.2.tar.gz/pyEthioNews-1.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    with open(fname) as f:
+        return f.read()
+
+
+setup(
+    name="pyEthioNews",
+    version="1.0.2",
+    url="https://github.com/wizkiye/pyEthioNewsApi",
+    license="MIT",
+    author="https://github.com/wizkiye",
+    author_email="wizkiye@gmail.com",
+    description="Ethiopian News Scraper",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "httpx",
+        "bs4",
+    ],
+    packages=find_packages(),
+)
