05c837ec3e27e01f261a636dac72a4e842f1dc11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:49 +0000

    Add package first-packge/0.1.2/first_packge-0.1.2.tar.gz

---
+++ packages/first-packge/first_packge-0.1.2.tar.gz/first_packge-0.1.2/setup.py
@@ -0,0 +1,13 @@
+# my_package/setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='first_packge',
+    version='0.1.2',
+    author='Sid_',
+    author_email='lodiy87986@sportrid.com',
+    description='A simple Python package',
+    packages=find_packages(),
+    install_requires=[],
+)
