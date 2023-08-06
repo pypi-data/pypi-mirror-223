9100226a9f33d27f2b1729c15797ab557728e351
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:39 +0000

    Add package py-worksdone-utils/1.0.6/py_worksdone_utils-1.0.6-py3-none-any.whl

---
+++ packages/py-worksdone-utils/py_worksdone_utils-1.0.6-py3-none-any.whl/pyworksdoneutils/sqlalchemy/flask/_decorators.py
@@ -0,0 +1,30 @@
+import os
+from functools import wraps
+from urllib.parse import urlparse
+
+from flask import request
+from sqlalchemy import text
+from sqlalchemy.orm import sessionmaker
+
+def db_session(local_session: sessionmaker, default_schema: str):
+    """
+    Method to be used as decorator, which accepts instance of KeycloakOpenID
+    :param local_session: sessionmaker
+    :return: Error message | original function
+    """
+    def funct_decorator(org_function):
+        @wraps(org_function)
+        def start_session(*args, **kwargs):
+            with local_session() as session:
+                subdomain = default_schema
+                if len(urlparse(request.url).hostname.split('.')) > 1:
+                    subdomain = f"tenant_{urlparse(request.url).hostname.split('.')[0]}"
+                else:
+                    if request.headers.get("X-Tenant-Id"):
+                        subdomain = f"tenant_{subdomain}"
+                session.execute(text(f"SET search_path TO {subdomain}"))
+                func_response = org_function(session,*args, **kwargs)
+            return func_response
+        return start_session
+
+    return funct_decorator
