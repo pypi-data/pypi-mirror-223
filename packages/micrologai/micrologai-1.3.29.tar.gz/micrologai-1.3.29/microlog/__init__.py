69cf75ea00fea806b564630fc0b96ed924c1b0c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:12 +0000

    Add package micrologai/1.3.29/micrologai-1.3.29.tar.gz

---
+++ packages/micrologai/micrologai-1.3.29.tar.gz/micrologai-1.3.29/microlog/__init__.py
@@ -0,0 +1,17 @@
+#
+# Microlog. Copyright (c) 2023 laffra, dcharbon. All rights reserved.
+#
+
+__version__ = "1.3.29"
+
+import sys
+
+from .api import info, warn, debug, error
+from .api import start, stop
+from .api import heap
+from .api import enabled
+
+try:
+    from . import server
+except:
+    pass # the server is not included in the UI
