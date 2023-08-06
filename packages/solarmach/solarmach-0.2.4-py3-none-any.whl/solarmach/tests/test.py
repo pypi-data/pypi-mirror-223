74dee5f7db9b51c2628db251c432a2a1a94371a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:52 +0000

    Add package solarmach/0.2.4/solarmach-0.2.4-py3-none-any.whl

---
+++ packages/solarmach/solarmach-0.2.4-py3-none-any.whl/solarmach/tests/test.py
@@ -0,0 +1,52 @@
+import os
+
+import astropy
+import astropy.units as u
+import numpy as np
+from solarmach import SolarMACH, print_body_list
+
+
+def test_solarmach_initialize():
+    body_list = ['STEREO-A']
+    vsw_list = [400]
+    date = '2021-10-28 15:15:00'
+    reference_long = 273
+    reference_lat = 9
+
+    sm = SolarMACH(date=date, body_list=body_list, vsw_list=vsw_list, reference_long=reference_long, reference_lat=reference_lat)
+
+    assert sm.date == date
+    assert sm.reference_lat == reference_lat
+    assert sm.reference_long == reference_long
+    assert np.round(sm.max_dist, 3) == 0.958
+
+    assert isinstance(sm.pos_E, astropy.coordinates.sky_coordinate.SkyCoord)
+    assert sm.pos_E.lat.unit == u.deg
+    assert sm.pos_E.lon.unit == u.deg
+    assert sm.pos_E.radius.unit == u.AU
+
+    assert sm.coord_table.shape == (1, 11)
+
+    assert np.round(sm.coord_table['Longitudinal separation between body and reference_long'][0], 1) == -39.9
+    assert np.round(sm.coord_table["Longitudinal separation between body's mangetic footpoint and reference_long"][0], 2) == 19.33
+
+    # verify backwards compatibility: undefined coord_sys is interpreted as 'Carrington'
+    assert sm.coord_sys == 'Carrington'
+
+
+def test_solarmach_plot():
+    body_list = ['STEREO-A']
+    vsw_list = [400]
+    date = '2021-10-28 15:15:00'
+    reference_long = 273
+    reference_lat = 9
+    reference_vsw = 400
+    filename = 'test.png'
+
+    sm = SolarMACH(date=date, body_list=body_list, vsw_list=vsw_list, reference_long=reference_long, reference_lat=reference_lat)
+    sm.plot(plot_spirals=True, plot_sun_body_line=True,
+            reference_vsw=reference_vsw, transparent=False,
+            show_earth_centered_coord=False, numbered_markers=True,
+            outfile=filename)
+
+    assert os.path.exists(os.getcwd()+os.sep+filename)
