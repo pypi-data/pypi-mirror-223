1f40dd6b2c9261c45e10f612b2ddc3a540d946ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:09 +0000

    Add package mkdocs-python-classy/0.1.3/mkdocs_python_classy-0.1.3-py3-none-any.whl

---
+++ packages/mkdocs-python-classy/mkdocs_python_classy-0.1.3-py3-none-any.whl/mkdocs_python_classy/constants.py
@@ -0,0 +1,28 @@
+"""Module to keep all constants."""
+
+TEMPLATE_STRING = """
+## `${name}`
+
+``` py
+${import_statment}
+```
+
+**Ancestors (MRO)**
+
+The Method Resolution Order is described below.
+
+${ancestors}
+
+**Descendant Classes**
+
+${descendants}
+
+**Attributes**
+
+${attributes}
+
+**Methods**
+
+${methods}
+
+"""
