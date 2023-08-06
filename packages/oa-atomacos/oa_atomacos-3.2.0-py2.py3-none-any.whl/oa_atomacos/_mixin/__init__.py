a2fab2250da94bd629f369eb4f86127622313214
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:57 +0000

    Add package oa-atomacos/3.2.0/oa_atomacos-3.2.0-py2.py3-none-any.whl

---
+++ packages/oa-atomacos/oa_atomacos-3.2.0-py2.py3-none-any.whl/oa_atomacos/_mixin/__init__.py
@@ -0,0 +1,4 @@
+# flake8: noqa: F401
+from oa_atomacos._mixin._input import KeyboardMouseMixin
+from oa_atomacos._mixin._search import SearchMethodsMixin
+from oa_atomacos._mixin._wait import WaitForMixin
