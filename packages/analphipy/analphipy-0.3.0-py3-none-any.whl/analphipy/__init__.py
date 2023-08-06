308849fb8d120ad8137f93b07e15ad5bf1daec41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:58 +0000

    Add package analphipy/0.3.0/analphipy-0.3.0-py3-none-any.whl

---
+++ packages/analphipy/analphipy-0.3.0-py3-none-any.whl/analphipy/__init__.py
@@ -0,0 +1,37 @@
+"""
+Top level API :mod:`analphipy`
+==============================
+
+The top level API provides Pair potentials and analysis routines.
+"""
+from . import measures, norofrenkel, potential
+
+# from .potentials_base import PhiAnalytic, PhiBase, PhiCut, PhiGeneric, PhiLFS
+
+# updated versioning scheme
+try:
+    from ._version import __version__
+except Exception:  # pragma: no cover
+    __version__ = "999"
+
+
+__all__ = [
+    # "PhiBaseCuttable",
+    # "PhiBaseGenericCut",
+    "potential",
+    "norofrenkel",
+    "measures",
+    # "PhiAnalytic",
+    # "PhiGeneric",
+    # "LennardJones",
+    # "LennardJonesNM",
+    # "SquareWell",
+    # "HardSphere",
+    # "Yukawa",
+    # "CubicTable",
+    # "factory_phi",
+    # "NoroFrenkelPair",
+    # "measures",
+    # "norofrenkel",
+    "__version__",
+]
