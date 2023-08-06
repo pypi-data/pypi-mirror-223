78ba285eee7252c396a6a3a756228c0cabe95dba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:37 +0000

    Add package py-packman/1.4.10/py_packman-1.4.10-py3-none-any.whl

---
+++ packages/py-packman/py_packman-1.4.10-py3-none-any.whl/packman/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Author: Pranav Khade, Iowa State University
+# Please read the project licence file for the Copyrights.
+
+"""The py-PACKMAN is a collection of subpackages built on the packman.molecule API.
+
+Please check the corresponding packages and tutorials for more information about the package use.
+"""
+from .molecule import *
+from .apps import *
+from .bin import *
+from .anm import *
+from .constants import *
+from .utilities import *
+
+
+#VERSION CHANGE HERE CHANGES IT IN docs AND setup.py
+__version__='1.4.10'
