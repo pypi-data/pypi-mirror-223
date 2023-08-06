9f99feb2ca8e2f4db5dd028d5b9ee1be768263d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:43:42 +0000

    Add package estela/0.2.6/estela-0.2.6.tar.gz

---
+++ packages/estela/estela-0.2.6.tar.gz/estela-0.2.6/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="estela",
+    version="0.2.6",
+    description="Estela Command Line Interface",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "estela = estela_cli.__main__:cli",
+        ],
+    },
+    install_requires=[
+        "requests",
+        "docker",
+        "pyyaml",
+        "click",
+        "tabulate",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Utilities",
+    ],
+)
