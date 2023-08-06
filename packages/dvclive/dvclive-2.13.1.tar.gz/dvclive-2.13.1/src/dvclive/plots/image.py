50308bbe31a02dc9de7788c34ab84230c560459c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:29 +0000

    Add package dvclive/2.13.1/dvclive-2.13.1.tar.gz

---
+++ packages/dvclive/dvclive-2.13.1.tar.gz/dvclive-2.13.1/src/dvclive/plots/image.py
@@ -0,0 +1,36 @@
+from pathlib import Path, PurePath
+
+from .base import Data
+
+
+class Image(Data):
+    suffixes = (".jpg", ".jpeg", ".gif", ".png")
+    subfolder = "images"
+
+    @property
+    def output_path(self) -> Path:
+        _path = self.output_folder / self.name
+        _path.parent.mkdir(exist_ok=True, parents=True)
+        return _path
+
+    @staticmethod
+    def could_log(val: object) -> bool:
+        acceptable = {
+            ("numpy", "ndarray"),
+            ("PIL.Image", "Image"),
+        }
+        for cls in type(val).mro():
+            if (cls.__module__, cls.__name__) in acceptable:
+                return True
+        if isinstance(val, (PurePath, str)):
+            return True
+        return False
+
+    def dump(self, val, **kwargs) -> None:  # noqa: ARG002
+        if val.__class__.__module__ == "numpy":
+            from PIL import Image as ImagePIL
+
+            pil_image = ImagePIL.fromarray(val)
+        else:
+            pil_image = val
+        pil_image.save(self.output_path)
