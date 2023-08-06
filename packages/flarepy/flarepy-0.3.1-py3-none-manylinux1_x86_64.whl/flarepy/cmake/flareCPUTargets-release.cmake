f8759e950aea880fc833dba5999267bb5a5341d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:08 +0000

    Add package flarepy/0.3.1/flarepy-0.3.1-py3-none-manylinux1_x86_64.whl

---
+++ packages/flarepy/flarepy-0.3.1-py3-none-manylinux1_x86_64.whl/flarepy/cmake/flareCPUTargets-release.cmake
@@ -0,0 +1,19 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "flare::flare_cpu" for configuration "Release"
+set_property(TARGET flare::flare_cpu APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(flare::flare_cpu PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/flarepy/libflare_cpu.so"
+  IMPORTED_SONAME_RELEASE "libflare_cpu.so"
+  )
+
+list(APPEND _cmake_import_check_targets flare::flare_cpu )
+list(APPEND _cmake_import_check_files_for_flare::flare_cpu "${_IMPORT_PREFIX}/flarepy/libflare_cpu.so" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
