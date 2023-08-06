fa6838732cc397603c8999f5372015e830bceb4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:59 +0000

    Add package stdlb/0.0.4/stdlb-0.0.4-py3-none-any.whl

---
+++ packages/stdlb/stdlb-0.0.4-py3-none-any.whl/stdlb/__init__.py
@@ -0,0 +1,88 @@
+import base64
+from base64 import *
+
+import configparser
+from configparser import *
+
+import contextlib
+from contextlib import *
+
+import dataclasses
+from dataclasses import *
+
+from datetime import *
+from datetime import datetime as dt
+import datetime
+fromtimestamp = dt.fromtimestamp
+fromisoformat = dt.fromisoformat
+
+import functools
+from functools import *
+from .cached_property import cached_property
+
+from glob import *
+import glob
+
+import hashlib
+from hashlib import *
+
+import io
+from io import *
+BlockingIOError = __builtins__['BlockingIOError']
+
+import itertools
+from itertools import *
+
+import json
+
+import math
+from math import *
+pow = __builtins__['pow']
+
+import os
+from os import *
+open = __builtins__['open']
+from os.path import *
+
+import pathlib
+from pathlib import *
+
+import re
+from re import *
+compile = __builtins__['compile']
+
+from shlex import *
+import shlex
+
+import shutil
+from shutil import *
+join = os.path.join  # seems like a better default than shlex.join
+
+import subprocess
+from subprocess import *
+
+import sys
+from sys import *
+path = os.path
+copyright = __builtins__['copyright']
+
+import tempfile
+from tempfile import *
+
+from time import *
+import time
+
+import traceback
+from traceback import *
+
+import typing
+from typing import *
+
+import urllib
+from urllib import *
+from urllib.parse import urlparse
+
+import uuid
+from uuid import *
+
+import warnings
