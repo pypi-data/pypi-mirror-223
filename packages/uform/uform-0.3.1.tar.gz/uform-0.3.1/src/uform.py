b13080ea7efc45745fc1b58a85665d6eeab567c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:38 +0000

    Add package uform/0.3.1/uform-0.3.1.tar.gz

---
+++ packages/uform/uform-0.3.1.tar.gz/uform-0.3.1/src/uform.py
@@ -0,0 +1,25 @@
+from json import load
+from typing import Optional
+
+import torch
+from huggingface_hub import hf_hub_download
+
+from models import VLM, TritonClient
+
+
+def get_model(model_name: str, token: Optional[str] = None) -> VLM:
+    config_path = hf_hub_download(model_name, "torch_config.json", token=token)
+    state = torch.load(hf_hub_download(model_name, "torch_weight.pt", token=token))
+    tokenizer_path = hf_hub_download(model_name, "tokenizer.json", token=token)
+
+    with open(config_path, "r") as f:
+        model = VLM(load(f), tokenizer_path)
+
+    model.image_encoder.load_state_dict(state["image_encoder"])
+    model.text_encoder.load_state_dict(state["text_encoder"])
+
+    return model.eval()
+
+
+def get_client(url: str) -> TritonClient:
+    return TritonClient(url)
