dfb3e19ee97c257271247e96c963db2e20d8189c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 14:07:22 +0000

    Add package lamindb/0.50.0/lamindb-0.50.0-py3-none-any.whl

---
+++ packages/lamindb/lamindb-0.50.0-py3-none-any.whl/lamindb/dev/__init__.py
@@ -0,0 +1,30 @@
+"""Developer API.
+
+.. autosummary::
+   :toctree: .
+
+   Registry
+   Data
+   QuerySet
+   QueryManager
+   FeatureManager
+   ValidationAware
+   SynonymsAware
+   datasets
+   hashing
+   storage
+   Settings
+   run_context
+   exc.ValidationError
+"""
+
+from lnschema_core.models import Data, Registry, SynonymsAware, ValidationAware
+
+from lamindb._query_manager import QueryManager
+from lamindb._query_set import QuerySet
+from lamindb.dev._feature_manager import FeatureManager
+
+from .._context import run_context
+from . import datasets  # noqa
+from . import _data, exc
+from ._settings import Settings
