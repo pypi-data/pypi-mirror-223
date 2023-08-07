77f50f94eafc8b9d9ae413c2078594c0ea63a8df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 03:11:19 +0000

    Add package sqlconstructor/1.0.18/sqlconstructor-1.0.18-py3-none-any.whl

---
+++ packages/sqlconstructor/sqlconstructor-1.0.18-py3-none-any.whl/sqlconstructor/helpers.py
@@ -0,0 +1,43 @@
+# coding=utf-8
+"""Helpers which are used in multiple modules"""
+
+__author__ = 'https://github.com/akvilary'
+
+import re
+from typing import Tuple
+
+def indent_lines(text: str, ind: int) -> str:
+    """Indent each line of the string"""
+    text = ''.join((' ' * ind) + x + '\n' for x in text.splitlines()).rstrip()
+    return text
+
+
+def upper_keywords(text: str, keywords: Tuple[str]) -> str:
+    """Upper certain keywords in string (case insensitive)"""
+    for keyword in keywords:
+        if keyword.lower() in text.lower():
+            pattern = (
+                r'(--.*?\Z)|(\/\*[\s\S]*?\*\/)|(".*?")|(\'.*?\')|'
+                r'\b' + keyword + r'\b'
+            )
+            text = re.sub(
+                pattern,
+                upper_keyword_if_required,
+                text,
+                flags=re.I,
+            )
+    return text
+
+
+def upper_keyword_if_required(match):
+    """
+    Upper keyword if keyword:
+    1) Located not in inline sql comment
+    2) Located not in multiline sql comment
+    3) Located not between single quotes
+    4) Located not between double quotes
+    """
+    groups = match.groups()
+    if any(groups):
+        return match.group()
+    return match.group().upper()
