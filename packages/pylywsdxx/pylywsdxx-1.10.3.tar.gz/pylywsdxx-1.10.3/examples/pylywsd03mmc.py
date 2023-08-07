abae0822489e803007cbceabd9b5acbe1b97054b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 14:07:32 +0000

    Add package pylywsdxx/1.10.3/pylywsdxx-1.10.3.tar.gz

---
+++ packages/pylywsdxx/pylywsdxx-1.10.3.tar.gz/pylywsdxx-1.10.3/examples/pylywsd03mmc.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import argparse
+
+import pylywsdxx as pyly  # noqa
+
+parser = argparse.ArgumentParser()
+parser.add_argument("mac", help="MAC address of LYWSD02 device", nargs="+")
+args = parser.parse_args()
+
+for mac in args.mac:
+    try:
+        client = pyly.Lywsd03(mac)
+        print(f"Fetching data from {mac}")
+        data = client.data
+        print(f"Temperature: {data.temperature}°C")
+        print(f"Humidity: {data.humidity}%")
+        print(f"Battery: {data.battery}% ({data.voltage} V)")
+        print()
+    except (Exception,) as e:
+        print(f"An exception of type {type(e).__name__} occured")
+        print(e)
