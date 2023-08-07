f8c201238f1d84397a30ab9393a90587aef79db3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 03:11:12 +0000

    Add package tablate/0.0.41/tablate-0.0.41.tar.gz

---
+++ packages/tablate/tablate-0.0.41.tar.gz/tablate-0.0.41/tablate/tests/playpen/prototype_test.py
@@ -0,0 +1,7 @@
+from tablate.api.Future import TablateFuture
+
+tablate_instance = TablateFuture()
+
+tablate_instance.add_text_frame("")
+tablate_instance.add_grid_frame([])
+tablate_instance.add_table_frame([], [])
