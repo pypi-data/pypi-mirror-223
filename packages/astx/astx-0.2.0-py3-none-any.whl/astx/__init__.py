cdb72b2f3dcdfe261282501b9fda9f3332c5d49d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 14:07:32 +0000

    Add package astx/0.2.0/astx-0.2.0-py3-none-any.whl

---
+++ packages/astx/astx-0.2.0-py3-none-any.whl/astx/__init__.py
@@ -0,0 +1,27 @@
+# mypy: disable-error-code="attr-defined"
+"""ASTx."""
+from importlib import metadata as importlib_metadata
+
+from astx import base
+from astx import blocks
+from astx import callables
+from astx import datatypes
+from astx import operators
+from astx import statements
+from astx import mixes
+from astx import symbol_table
+
+
+def get_version() -> str:
+    """Return the program version."""
+    try:
+        return importlib_metadata.version(__name__)
+    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+        return "0.2.0"  # semantic-release
+
+
+version: str = get_version()
+
+__author__ = "Ivan Ogasawara"
+__email__ = "ivan.ogasawara@gmail.com"
+__version__: str = version
