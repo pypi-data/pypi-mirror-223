21621c92fab1d2ca5581f427ac9b6fdc1079d83b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 03:09:21 +0000

    Add package affinder/0.3.2/affinder-0.3.2.tar.gz

---
+++ packages/affinder/affinder-0.3.2.tar.gz/affinder-0.3.2/src/affinder/_tests/test_affinder.py
@@ -0,0 +1,100 @@
+from affinder import start_affinder
+from affinder.affinder import AffineTransformChoices
+from skimage import data, transform
+import numpy as np
+from itertools import product
+import zarr
+import napari
+import pytest
+
+layer0_pts = np.array([[140.38371886,
+                        322.5390704], [181.91866481, 319.65803368],
+                       [176.15659138, 259.1562627],
+                       [140.14363246, 254.59462124]])
+layer1_pts = np.array([[70.94741072,
+                        117.37477536], [95.80911919, 152.00358359],
+                       [143.16475439, 118.55866623],
+                       [131.32584559, 83.33791256]])
+
+# get reference and moving layer types
+im0 = data.camera()
+im1 = transform.rotate(im0[100:, 32:496], 60)
+labels0 = zarr.open('./src/affinder/_tests/labels0.zarr', mode='r')
+labels1 = zarr.open('./src/affinder/_tests/labels1.zarr', mode='r')
+
+
+def make_vector_border(layer_pts):
+    vectors = np.zeros((layer_pts.shape[0], 2, layer_pts.shape[1]))
+    for n in range(layer_pts.shape[0]):
+        vectors[n, 0, :] = layer_pts[n, :]
+        vectors[n, 1, :] = layer_pts[(n+1)
+                                     % layer_pts.shape[0], :] - layer_pts[n, :]
+    return vectors
+
+
+vectors0 = make_vector_border(layer0_pts)
+vectors1 = make_vector_border(layer1_pts)
+
+ref = [
+        napari.layers.Image(im0),
+        napari.layers.Shapes(layer0_pts),
+        napari.layers.Points(layer0_pts),
+        napari.layers.Labels(labels0),
+        napari.layers.Vectors(vectors0),
+        ]
+mov = [
+        napari.layers.Image(im1),
+        napari.layers.Shapes(layer1_pts),
+        napari.layers.Points(layer1_pts),
+        napari.layers.Labels(labels1),
+        napari.layers.Vectors(vectors1),
+        ]
+# TODO add tracks layer types, after multidim affine support added
+
+
+@pytest.mark.parametrize("reference,moving", [p for p in product(ref, mov)])
+def test_layer_types(make_napari_viewer, tmp_path, reference, moving):
+
+    viewer = make_napari_viewer()
+
+    l0 = viewer.add_layer(reference)
+    viewer.layers[-1].name = "layer0"
+    viewer.layers[-1].colormap = "green"
+
+    l1 = viewer.add_layer(moving)
+    viewer.layers[-1].name = "layer1"
+    viewer.layers[-1].colormap = "magenta"
+
+    my_widget_factory = start_affinder()
+    my_widget_factory(
+            viewer=viewer,
+            reference=l0,
+            moving=l1,
+            model=AffineTransformChoices.affine,
+            output=tmp_path / 'my_affine.txt'
+            )
+
+    viewer.layers['layer0_pts'].data = layer0_pts
+    viewer.layers['layer1_pts'].data = layer1_pts
+
+    actual_affine = np.asarray(l1.affine)
+    expected_affine = np.array([[0.48155037, 0.85804854, 5.43577937],
+                                [-0.88088632, 0.49188026, 328.20642821],
+                                [0., 0., 1.]])
+
+    np.testing.assert_allclose(actual_affine, expected_affine)
+
+
+def test_ensure_different_layers(make_napari_viewer):
+    viewer = make_napari_viewer()
+    image0 = np.random.random((50, 50))
+    image1 = np.random.random((30, 30))
+    image2 = np.random.random((40, 40))
+    for image in [image0, image1, image2]:
+        viewer.add_image(image)
+    qtwidget, widget = viewer.window.add_plugin_dock_widget(
+            'affinder', 'Start affinder'
+            )
+    assert widget.reference.value != widget.moving.value
+    widget.reference.value = widget.moving.value
+    assert widget.reference.value != widget.moving.value
---
+++ packages/affinder/affinder-0.3.2.tar.gz/affinder-0.3.2/src/affinder/affinder.py
@@ -0,0 +1,213 @@
+import warnings
+from typing import Optional
+
+from enum import Enum
+import pathlib
+import toolz as tz
+from magicgui import magicgui, magic_factory
+import numpy as np
+from skimage.transform import (
+        AffineTransform,
+        EuclideanTransform,
+        SimilarityTransform,
+        )
+
+
+class AffineTransformChoices(Enum):
+    affine = AffineTransform
+    Euclidean = EuclideanTransform
+    similarity = SimilarityTransform
+
+
+def reset_view(viewer: 'napari.Viewer', layer: 'napari.layers.Layer'):
+    if viewer.dims.ndisplay != 2:
+        return
+    extent = layer.extent.world[:, viewer.dims.displayed]
+    size = extent[1] - extent[0]
+    center = extent[0] + size/2
+    viewer.camera.center = center
+    with warnings.catch_warnings():
+        warnings.simplefilter('ignore')
+        canvas_size = viewer._canvas_size
+    viewer.camera.zoom = np.min(canvas_size) / np.max(size)
+
+
+@tz.curry
+def next_layer_callback(
+        value,  # we ignore the arguments returned with the event -- we will
+        *args,  # instead introspect the layer data and selection state
+        viewer,
+        reference_image_layer,
+        reference_points_layer,
+        moving_image_layer,
+        moving_points_layer,
+        model_class,
+        output,
+        ):
+    pts0, pts1 = reference_points_layer.data, moving_points_layer.data
+    n0, n1 = len(pts0), len(pts1)
+    ndim = pts0.shape[1]
+    if reference_points_layer in viewer.layers.selection:
+        if n0 < ndim + 1:
+            return
+        if n0 == ndim + 1:
+            reset_view(viewer, moving_image_layer)
+        if n0 > n1:
+            viewer.layers.selection.active = moving_points_layer
+            viewer.layers.move(viewer.layers.index(moving_image_layer), -1)
+            viewer.layers.move(viewer.layers.index(moving_points_layer), -1)
+            moving_points_layer.mode = 'add'
+    elif moving_points_layer in viewer.layers.selection:
+        if n1 == n0:
+            # we just added enough points:
+            # estimate transform, go back to layer0
+            if n0 > ndim:
+                mat = calculate_transform(pts0, pts1, model_class=model_class)
+                moving_image_layer.affine = (
+                        reference_image_layer.affine.affine_matrix @ mat.params
+                        )
+                moving_points_layer.affine = (
+                        reference_image_layer.affine.affine_matrix @ mat.params
+                        )
+                if output is not None:
+                    np.savetxt(output, np.asarray(mat.params), delimiter=',')
+            viewer.layers.selection.active = reference_points_layer
+            reference_points_layer.mode = 'add'
+            viewer.layers.move(viewer.layers.index(reference_image_layer), -1)
+            viewer.layers.move(viewer.layers.index(reference_points_layer), -1)
+            reset_view(viewer, reference_image_layer)
+
+
+# make a bindable function to shut things down
+@magicgui
+def close_affinder(layers, callback):
+    for layer in layers:
+        layer.events.data.disconnect(callback)
+        layer.mode = 'pan_zoom'
+
+
+def _update_unique_choices(widget, choice_name):
+    """Update the selected choice in a ComboBox widget to be unique.
+
+    When `choice_name` is selected by another widget, and the choice in
+    `widget` needs to be different, this callback can be called to update the
+    choice in `widget`.
+    """
+    if not isinstance(choice_name, str):
+        # in some circumstances, widget.changed.connect passes the choice
+        # name to the callback, and in other cases it's the actual choice
+        # value. Here we coerce it to always be the name but that's an
+        # arbitrary choice.
+        choice_name = choice_name.name
+    choices = widget.choices
+    choice_names = [value.name for value in choices]
+    index = choice_names.index(choice_name)
+    value = widget.choices[index]
+    if widget.value is value:
+        next_index = (index+1) % len(choices)
+        with widget.changed.blocked():
+            widget.value = widget.choices[next_index]
+
+
+def _on_affinder_main_init(widget):
+    """Make sure that the reference and moving image are not the same."""
+    widget.reference.changed.connect(
+            lambda v: _update_unique_choices(widget.moving, v)
+            )
+    widget.moving.changed.connect(
+            lambda v: _update_unique_choices(widget.reference, v)
+            )
+    _update_unique_choices(widget.moving, widget.reference.current_choice)
+
+
+@magic_factory(
+        widget_init=_on_affinder_main_init,
+        call_button='Start',
+        layout='vertical',
+        output={'mode': 'w'},
+        viewer={'visible': False, 'label': ' '},
+        )
+def start_affinder(
+        viewer: 'napari.viewer.Viewer',
+        *,
+        reference: 'napari.layers.Layer',
+        reference_points: Optional['napari.layers.Points'] = None,
+        moving: 'napari.layers.Layer',
+        moving_points: Optional['napari.layers.Points'] = None,
+        model: AffineTransformChoices,
+        output: Optional[pathlib.Path] = None,
+        ):
+    mode = start_affinder._call_button.text  # can be "Start" or "Finish"
+
+    if mode == 'Start':
+        # focus on the reference layer
+        reset_view(viewer, reference)
+        # set points layer for each image
+        points_layers = [reference_points, moving_points]
+        # Use C0 and C1 from matplotlib color cycle
+        points_layers_to_add = [(reference, (0.122, 0.467, 0.706, 1.0)),
+                                (moving, (1.0, 0.498, 0.055, 1.0))]
+        # make points layer if it was not specified
+        for i in range(len(points_layers)):
+            if points_layers[i] is None:
+                layer, color = points_layers_to_add[i]
+                new_layer = viewer.add_points(
+                        ndim=layer.ndim,
+                        name=layer.name + '_pts',
+                        affine=layer.affine,
+                        face_color=[color],
+                        )
+                points_layers[i] = new_layer
+        pts_layer0 = points_layers[0]
+        pts_layer1 = points_layers[1]
+
+        # make a callback for points added
+        callback = next_layer_callback(
+                viewer=viewer,
+                reference_image_layer=reference,
+                reference_points_layer=pts_layer0,
+                moving_image_layer=moving,
+                moving_points_layer=pts_layer1,
+                model_class=model.value,
+                output=output,
+                )
+        pts_layer0.events.data.connect(callback)
+        pts_layer1.events.data.connect(callback)
+
+        # get the layer order started
+        for layer in [moving, pts_layer1, reference, pts_layer0]:
+            viewer.layers.move(viewer.layers.index(layer), -1)
+
+        viewer.layers.selection.active = pts_layer0
+        pts_layer0.mode = 'add'
+
+        close_affinder.layers.bind(points_layers)
+        close_affinder.callback.bind(callback)
+
+        # change the button/mode for next run
+        start_affinder._call_button.text = 'Finish'
+    else:  # we are in Finish mode
+        close_affinder()
+        start_affinder._call_button.text = 'Start'
+
+
+def calculate_transform(src, dst, model_class=AffineTransform):
+    """Calculate transformation matrix from matched coordinate pairs.
+
+    Parameters
+    ----------
+    src : ndarray
+        Matched row, column coordinates from source image.
+    dst : ndarray
+        Matched row, column coordinates from destination image.
+    model_class : scikit-image transformation class, optional.
+        By default, model=AffineTransform().
+
+    Returns
+    -------
+    transform
+        scikit-image Transformation object
+    """
+    model = model_class()
+    model.estimate(dst, src)  # we want the inverse
+    return model
