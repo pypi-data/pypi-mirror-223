47791b72cd1cbef74cf25a33741255f90446174f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 03:11:24 +0000

    Add package e2b/0.0.1/e2b-0.0.1.tar.gz

---
+++ packages/e2b/e2b-0.0.1.tar.gz/e2b-0.0.1/e2b/utils/future.py
@@ -0,0 +1,39 @@
+import asyncio
+import json
+
+from typing import TypeVar, Generic, List
+
+T = TypeVar("T")
+
+
+class DeferredFuture(Generic[T]):
+    def __init__(self):
+        self.future = asyncio.Future[T]()
+
+    def __call__(self, result: T):
+        if not self.future.done():
+            self.future.set_result(result)
+
+    def __await__(self):
+        result = yield from self.future.__await__()
+        return result
+
+    def cancel(self):
+        if not self.future.done():
+            self.future.cancel()
+
+    def reject(self, reason: Exception):
+        if not self.future.done():
+            self.future.set_exception(reason)
+
+
+def format_settled_errors(settled: List[str | Exception | None]):
+    if all(s is not Exception for s in settled if s is not None):
+        return None
+
+    errors = "errors:\n"
+    for i, s in enumerate(settled):
+        if s is Exception:
+            errors += f"\n[{i}]: {json.dumps(s.exception().__str__())}"
+
+    return errors
