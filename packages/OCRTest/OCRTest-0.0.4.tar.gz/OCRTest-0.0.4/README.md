78f87ea9ce5446b715f5ef92ca22f4ff06106f59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 14:07:52 +0000

    Add package OCRTest/0.0.4/OCRTest-0.0.4.tar.gz

---
+++ packages/OCRTest/OCRTest-0.0.4.tar.gz/OCRTest-0.0.4/README.md
@@ -0,0 +1,6 @@
+# OCRTest
+
+## Version: 0.0.1
+
+## Description
+A test package that will print "Hello World from OCR Research Lab!"
---
+++ packages/OCRTest/OCRTest-0.0.4.tar.gz/OCRTest-0.0.4/ocrtest/hello.py
@@ -0,0 +1,5 @@
+class hello_class():
+    def __init__(self) -> None:
+        pass
+    def sayhello(self):
+        print('Hello World from OCR Research Lab!')
