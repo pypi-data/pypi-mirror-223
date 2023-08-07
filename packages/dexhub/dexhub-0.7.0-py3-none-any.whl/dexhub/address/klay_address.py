88506f822333d2d2f08e2efc45898c4ae6ce0991
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 03:09:25 +0000

    Add package dexhub/0.7.0/dexhub-0.7.0-py3-none-any.whl

---
+++ packages/dexhub/dexhub-0.7.0-py3-none-any.whl/dexhub/address/klay_address.py
@@ -0,0 +1,26 @@
+from web3 import Web3
+
+class SD:
+    #dex
+    sd_route=Web3.toChecksumAddress('0x9e987E5E9aB872598f601BE4aCC5ac23F484845E')
+    sd_factory=Web3.toChecksumAddress('0x36fAE766e51f17F8218C735f58426E293498Db2B')
+
+class AddressSD:
+    #dex
+    sd_route=Web3.toChecksumAddress('0x9e987E5E9aB872598f601BE4aCC5ac23F484845E')
+    sd_factory=Web3.toChecksumAddress('0x36fAE766e51f17F8218C735f58426E293498Db2B')
+class AddressClaySwap:
+    clay_swap_route=Web3.toChecksumAddress('0xEf71750C100f7918d6Ded239Ff1CF09E81dEA92D')
+    clay_swap_factory=Web3.toChecksumAddress('0x3679c3766E70133Ee4A7eb76031E49d3d1f2B50c')
+
+class KlayToken:
+    #erc20
+    ousdt=Web3.toChecksumAddress('0xceE8FAF64bB97a73bb51E115Aa89C17FfA8dD167')
+    oeth=Web3.toChecksumAddress('0x34d21b1e550D73cee41151c77F3c73359527a396')
+    owbtc=Web3.toChecksumAddress('0x16D0e1fBD024c600Ca0380A4C5D57Ee7a2eCBf9c')
+    jewel=Web3.toChecksumAddress('0x30C103f8f5A3A732DFe2dCE1Cc9446f545527b43')
+    gold=Web3.toChecksumAddress('0xe7a1B580942148451E47b92e95aEB8d31B0acA37')
+    tear=Web3.toChecksumAddress('0x8Be0cbA3c8c8F392408364ef21dfCF714A918234')
+    wavax=Web3.toChecksumAddress('0xCd8fE44A29Db9159dB36f96570d7A4d91986f528')
+    wklay=Web3.toChecksumAddress('0x19Aac5f612f524B754CA7e7c41cbFa2E981A4432')
+    jade=Web3.toChecksumAddress('0xB3F5867E277798b50ba7A71C0b24FDcA03045eDF')
