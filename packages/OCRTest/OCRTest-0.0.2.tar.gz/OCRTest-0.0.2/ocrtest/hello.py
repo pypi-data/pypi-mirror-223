07e92965e84b1c147011368e12067c4ecf80b3de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 14:07:42 +0000

    Add package OCRTest/0.0.2/OCRTest-0.0.2.tar.gz

---
+++ packages/OCRTest/OCRTest-0.0.2.tar.gz/OCRTest-0.0.2/ocrtest/hello.py
@@ -0,0 +1,5 @@
+class hello():
+    def __init__(self) -> None:
+        pass
+    def sayhello():
+        print('Hello World from OCR Research Lab!')
---
+++ packages/OCRTest/OCRTest-0.0.2.tar.gz/OCRTest-0.0.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.2'
+DESCRIPTION = 'An OCR hello package'
+
+# Setting up
+setup(
+    name="OCRTest",
+    version=VERSION,
+    author="OCR Research Lab",
+    author_email="<vuquocminhdang@gmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
