4f1ff3d849aad3d5ddc2de5d09dd9f3311d94bd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 14:08:20 +0000

    Add package t-val/0.0.3/t_val-0.0.3.tar.gz

---
+++ packages/t-val/t_val-0.0.3.tar.gz/t_val-0.0.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='t_val',
+    version='0.0.3',
+    author='M. Vidyaa Shankar',
+    description='A Python module for Terraform validation',
+    packages=find_packages(),
+)
