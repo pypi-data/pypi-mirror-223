4a339494bfd8ffb1db33b9d03f0855e965a92277
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 03:11:23 +0000

    Add package arab-transcription/1.0.1/arab_transcription-1.0.1.tar.gz

---
+++ packages/arab-transcription/arab_transcription-1.0.1.tar.gz/arab_transcription-1.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='arab_transcription',
+    version='1.0.1',
+    packages=['.', '_types'],
+    include_package_data=True,
+    author='Linzo99',
+    author_email='sallalioune99@gmail.com',
+    description='python script for arabic text transcription',
+    url="https://github.com/Tariha/transcription",
+    install_requires=[]
+)
