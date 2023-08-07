a9fa851adc5cb07ddf37fc9451f9a32938d245e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 14:07:29 +0000

    Add package wagtail-favicon/0.3.0/wagtail_favicon-0.3.0-py2.py3-none-any.whl

---
+++ packages/wagtail-favicon/wagtail_favicon-0.3.0-py2.py3-none-any.whl/wagtail_favicon/templatetags/favicon_tags.py
@@ -0,0 +1,24 @@
+from django import template
+
+from wagtail.models import Site
+
+from wagtail_favicon.models import FaviconSettings
+
+
+register = template.Library()
+
+
+@register.inclusion_tag('tags/favicon_meta.html', takes_context=True)
+def favicon_meta(context):
+    site = Site.find_for_request(context.get('request'))
+    favicon_settings = FaviconSettings.for_site(site)
+    icon_image = favicon_settings.base_favicon_image
+    ms_image = icon_image.get_rendition('fill-144x144') if icon_image else None
+
+    return {
+        'icon_image': icon_image,
+        'theme_color': favicon_settings.app_theme_color,
+        'apple_icons': favicon_settings.apple_icons,
+        'icons': favicon_settings.icons,
+        'ms_image': ms_image,
+    }
