7cb628490e33ffff682e067bc4defba368f27b96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 04:13:52 +0000

    Add package GaussianAndBinomialDistributions202/0.1/GaussianAndBinomialDistributions202-0.1.tar.gz

---
+++ packages/GaussianAndBinomialDistributions202/GaussianAndBinomialDistributions202-0.1.tar.gz/GaussianAndBinomialDistributions202-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='GaussianAndBinomialDistributions202',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['GaussianAndBinomialDistributions202'],
+      zip_safe=False)
