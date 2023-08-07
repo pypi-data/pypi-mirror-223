0dbc24854f58c458486e931caf6a4ab20d010e38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 14:08:21 +0000

    Add package terminal-manager/0.9.4/terminal_manager-0.9.4.tar.gz

---
+++ packages/terminal-manager/terminal_manager-0.9.4.tar.gz/terminal_manager-0.9.4/src/terminal_manager/default_collections/const.py
@@ -0,0 +1,42 @@
+class ActionKey:
+    TURN_OFF = "turn_off"
+    RESTART = "restart"
+
+
+class ActionName:
+    TURN_OFF = "Turn off"
+    RESTART = "Restart"
+
+
+class SensorKey:
+    NETWORK_INTERFACE = "network_interface"
+    MAC_ADDRESS = "mac_address"
+    WAKE_ON_LAN = "wake_on_lan"
+    MACHINE_TYPE = "machine_type"
+    HOSTNAME = "hostname"
+    OS_NAME = "os_name"
+    OS_VERSION = "os_version"
+    OS_ARCHITECTURE = "os_architecture"
+    TOTAL_MEMORY = "total_memory"
+    FREE_MEMORY = "free_memory"
+    CPU_LOAD = "cpu_load"
+    FREE_DISK_SPACE = "free_disk_space"
+    TEMPERATURE = "temperature"
+    PROCESSES = "processes"
+
+
+class SensorName:
+    NETWORK_INTERFACE = "Network Interface"
+    MAC_ADDRESS = "MAC Address"
+    WAKE_ON_LAN = "Wake on LAN"
+    MACHINE_TYPE = "Machine Type"
+    HOSTNAME = "Hostname"
+    OS_NAME = "OS Name"
+    OS_VERSION = "OS Version"
+    OS_ARCHITECTURE = "OS Architecture"
+    TOTAL_MEMORY = "Total Memory"
+    FREE_MEMORY = "Free Memory"
+    CPU_LOAD = "CPU Load"
+    FREE_DISK_SPACE = "Free Disk Space"
+    TEMPERATURE = "Temperature"
+    PROCESSES = "Processes"
