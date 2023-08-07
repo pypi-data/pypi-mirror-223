c88cefeeec9992541160999aaa6e838f65f6bbfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 14:07:37 +0000

    Add package pakagesdist/1.0.1/pakagesdist-1.0.1.tar.gz

---
+++ packages/pakagesdist/pakagesdist-1.0.1.tar.gz/pakagesdist-1.0.1/setup.py
@@ -0,0 +1,13 @@
+
+
+from setuptools import setup
+def readme_file():
+
+    with open("README.rst", encoding="utf-8") as f:
+        dec = f.read()
+        return dec
+
+setup(name="pakagesdist", version="1.0.1", description="this is the first pakage that was created by myself222",
+      packages=["pakagesdist"], py_modules=["study"], author="clw", author_email="2023@qq.com",
+      url="https://pypi.test.org/simple",
+      long_description = readme_file(), license="MIT")
