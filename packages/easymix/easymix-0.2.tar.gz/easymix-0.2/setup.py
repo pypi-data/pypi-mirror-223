0125d220f754d6abe4144bc3a90039f68cff7a96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 04:13:05 +0000

    Add package easymix/0.2/easymix-0.2.tar.gz

---
+++ packages/easymix/easymix-0.2.tar.gz/easymix-0.2/setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='easymix',
+    version='0.2',
+    author='flokapi',
+    author_email='flokapi@pm.me',
+    packages=find_packages()
+)
