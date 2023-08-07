ae855118de7b803350864bc0dd31e0441c79655d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 03:10:54 +0000

    Add package phantom-types/2.1.1/phantom-types-2.1.1.tar.gz

---
+++ packages/phantom-types/phantom-types-2.1.1.tar.gz/phantom-types-2.1.1/src/phantom/__init__.py
@@ -0,0 +1,34 @@
+"""
+Use ``Phantom`` to create arbitrary phantom types using boolean predicates.
+
+.. code-block:: python
+
+    from phantom import Phantom
+
+
+    def is_big(value: int) -> bool:
+        return value > 5
+
+
+    class Big(int, Phantom, predicate=is_big):
+        ...
+
+
+    assert isinstance(10, Big)  # this passes
+"""
+from ._base import Phantom
+from ._base import PhantomBase
+from ._base import PhantomMeta
+from .bounds import get_bound_parser
+from .errors import BoundError
+from .predicates import Predicate
+
+__version__ = "2.1.1"
+__all__ = (
+    "BoundError",
+    "Phantom",
+    "PhantomBase",
+    "PhantomMeta",
+    "get_bound_parser",
+    "Predicate",
+)
