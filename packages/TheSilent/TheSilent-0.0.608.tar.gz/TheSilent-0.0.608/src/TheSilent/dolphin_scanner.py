fc573cb77d9e59cd884d60bbf9d57e469bdd568e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 04:12:47 +0000

    Add package TheSilent/0.0.608/TheSilent-0.0.608.tar.gz

---
+++ packages/TheSilent/TheSilent-0.0.608.tar.gz/TheSilent-0.0.608/src/TheSilent/dolphin_scanner.py
@@ -0,0 +1,23 @@
+import socket
+
+def dolphin_scanner(host):
+    port_list = []
+    host = host.replace("https://", "")
+    host = host.replace("http://", "")
+    try:
+        tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        tcp_socket.settimeout(15)
+        tcp_socket.connect((host,80))
+        tcp_socket.close()
+
+    except socket.gaierror:
+        return False
+
+    except OSError as error:
+        if error.errno == 113:
+            return False
+
+    except (ConnectionRefusedError, TimeoutError):
+        pass
+
+    return True
