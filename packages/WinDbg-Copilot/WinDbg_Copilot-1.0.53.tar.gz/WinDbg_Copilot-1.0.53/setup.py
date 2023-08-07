a007e05e7104115ac11bdba2771f645fc44cde2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 14:07:28 +0000

    Add package WinDbg-Copilot/1.0.53/WinDbg_Copilot-1.0.53.tar.gz

---
+++ packages/WinDbg-Copilot/WinDbg_Copilot-1.0.53.tar.gz/WinDbg_Copilot-1.0.53/setup.py
@@ -0,0 +1,11 @@
+# setup.py
+from setuptools import setup
+
+if __name__ == "__main__":
+  setup(packages = ['WinDbg_Copilot'],
+        entry_points={
+        'console_scripts': [
+            'windbg-copilot=WinDbg_Copilot.WinDbg_Copilot_Console:main',         # Assumes main() function in WinDbg_Copilot.py
+            'windbg-copilot-gui=WinDbg_Copilot.WinDbg_Copilot_GUI:main', # Assumes main() function in WinDbg_Copilot_GUI.py
+        ],
+    })
