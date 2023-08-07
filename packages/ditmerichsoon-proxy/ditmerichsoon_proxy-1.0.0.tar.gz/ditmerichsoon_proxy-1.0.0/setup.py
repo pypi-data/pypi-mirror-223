2fe29e08ca69b64bd922971621f3e95c943b6ff7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 03:10:22 +0000

    Add package ditmerichsoon-proxy/1.0.0/ditmerichsoon_proxy-1.0.0.tar.gz

---
+++ packages/ditmerichsoon-proxy/ditmerichsoon_proxy-1.0.0.tar.gz/ditmerichsoon_proxy-1.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ditmerichsoon_proxy',
+    version='1.0.0',
+    description='Rotating proxy Viet Nam',
+    author='Nguyen Minh Hieu',
+    author_email='contact.nmhieu@gmail.com',
+    url='https://github.com/your-username/your-repo',
+    packages=find_packages(),
+    install_requires=[
+        'requests',  # Add any dependencies here
+    ],
+)
