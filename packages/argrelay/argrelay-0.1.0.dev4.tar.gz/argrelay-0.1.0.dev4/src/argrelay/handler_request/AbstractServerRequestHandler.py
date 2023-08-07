9814cd4a9fa8a31237f149bd5f59fa165d19ce0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 03:10:02 +0000

    Add package argrelay/0.1.0.dev4/argrelay-0.1.0.dev4.tar.gz

---
+++ packages/argrelay/argrelay-0.1.0.dev4.tar.gz/argrelay-0.1.0.dev4/src/argrelay/handler_request/AbstractServerRequestHandler.py
@@ -0,0 +1,42 @@
+from argrelay.enum_desc.RunMode import RunMode
+
+from argrelay.relay_server.LocalServer import LocalServer
+from argrelay.runtime_context.InputContext import InputContext
+from argrelay.runtime_context.InterpContext import InterpContext
+from argrelay.runtime_context.ParsedContext import ParsedContext
+from argrelay.runtime_context.RequestContext import RequestContext
+
+
+class AbstractServerRequestHandler:
+
+    def __init__(
+        self,
+        local_server: LocalServer,
+    ):
+        self.local_server: LocalServer = local_server
+        self.interp_ctx: InterpContext
+
+    def handle_request(self, input_ctx: InputContext) -> dict:
+        raise NotImplementedError
+
+    @staticmethod
+    def create_input_ctx(request_ctx: RequestContext, run_mode: RunMode):
+        input_ctx = InputContext.from_request_context(
+            request_ctx,
+            run_mode = run_mode,
+            comp_key = str(0),
+        )
+        return input_ctx
+
+    def interpret_command(self, local_server: LocalServer, input_ctx: InputContext):
+        parsed_ctx = ParsedContext.from_instance(input_ctx)
+        # TODO: Split server_config and static_data (both top level, not config including data):
+        self.interp_ctx = InterpContext(
+            parsed_ctx = parsed_ctx,
+            interp_factories = local_server.server_config.interp_factories,
+            action_delegators = local_server.server_config.action_delegators,
+            query_engine = local_server.get_query_engine(),
+            help_hint_cache = local_server.help_hint_cache,
+        )
+        self.interp_ctx.interpret_command(local_server.server_config.static_data.first_interp_factory_id)
+        self.interp_ctx.print_debug()
---
+++ packages/argrelay/argrelay-0.1.0.dev4.tar.gz/argrelay-0.1.0.dev4/src/argrelay/server_spec/const_int.py
@@ -0,0 +1,30 @@
+"""
+Internal constants
+"""
+
+# See `server_title` and `server_version` instead.
+UNUSED_TITLE = "argrelay"
+UNUSED_VERSION = "0.0.0"
+
+# This will make `apispec` generate schema compatible with `flasgger` (placing schemas under `#/definitions/`):
+DEFAULT_OPEN_API_VERSION = "2.0"
+
+# server_op paths:
+DESCRIBE_LINE_ARGS_PATH = "/describe_line_args/"
+PROPOSE_ARG_VALUES_PATH = "/propose_arg_values/"
+RELAY_LINE_ARGS_PATH = "/relay_line_args/"
+
+# Built-in GUI:
+ARGRELAY_GUI_PATH = "/argrelay_gui/"
+
+# Run API docs UI at the root:
+API_DOCS_PATH = "/argrelay_api/"
+
+# main:
+DEFAULT_IP_ADDRESS = "localhost"
+DEFAULT_PORT_NUMBER = 8787
+
+API_SPEC_PATH = "/argrelay_server_api_spec.json"
+
+# noinspection HttpUrlsUsage
+BASE_URL_FORMAT = "http://{server_host_name}:{server_port_number}"
