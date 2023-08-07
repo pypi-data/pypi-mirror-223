71fe2c610c9af7957f02b5219845e46cfe8a38e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 03:08:53 +0000

    Add package superqt/0.5.0/superqt-0.5.0.tar.gz

---
+++ packages/superqt/superqt-0.5.0.tar.gz/superqt-0.5.0/src/superqt/__init__.py
@@ -0,0 +1,56 @@
+"""superqt is a collection of Qt components for python."""
+from typing import TYPE_CHECKING, Any
+
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "unknown"
+
+if TYPE_CHECKING:
+    from .spinbox._quantity import QQuantity
+
+from .collapsible import QCollapsible
+from .combobox import QEnumComboBox, QSearchableComboBox
+from .elidable import QElidingLabel, QElidingLineEdit
+from .selection import QSearchableListWidget, QSearchableTreeWidget
+from .sliders import (
+    QDoubleRangeSlider,
+    QDoubleSlider,
+    QLabeledDoubleRangeSlider,
+    QLabeledDoubleSlider,
+    QLabeledRangeSlider,
+    QLabeledSlider,
+    QRangeSlider,
+)
+from .spinbox import QLargeIntSpinBox
+from .utils import QMessageHandler, ensure_main_thread, ensure_object_thread
+
+__all__ = [
+    "ensure_main_thread",
+    "ensure_object_thread",
+    "QDoubleRangeSlider",
+    "QCollapsible",
+    "QDoubleSlider",
+    "QElidingLabel",
+    "QElidingLineEdit",
+    "QEnumComboBox",
+    "QLabeledDoubleRangeSlider",
+    "QLabeledDoubleSlider",
+    "QLabeledRangeSlider",
+    "QLabeledSlider",
+    "QLargeIntSpinBox",
+    "QMessageHandler",
+    "QQuantity",
+    "QRangeSlider",
+    "QSearchableComboBox",
+    "QSearchableListWidget",
+    "QSearchableTreeWidget",
+]
+
+
+def __getattr__(name: str) -> Any:
+    if name == "QQuantity":
+        from .spinbox._quantity import QQuantity
+
+        return QQuantity
+    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
