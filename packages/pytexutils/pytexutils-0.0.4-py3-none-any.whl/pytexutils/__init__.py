472c6896ab12da105c8b9a0bdf6d897f6053b4d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 03:10:12 +0000

    Add package pytexutils/0.0.4/pytexutils-0.0.4-py3-none-any.whl

---
+++ packages/pytexutils/pytexutils-0.0.4-py3-none-any.whl/pytexutils/__init__.py
@@ -0,0 +1,8 @@
+"""
+    This package contains basic functionalities to speed-up the process of creating 
+    tables, images, figures for LaTeX documents.
+    
+"""
+
+from .figures import *
+from .tables import *
