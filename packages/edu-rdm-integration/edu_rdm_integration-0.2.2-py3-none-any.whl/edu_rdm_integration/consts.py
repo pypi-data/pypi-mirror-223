e63170cf134bd4211f8ea8a3249e195a443cf28a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 03:09:43 +0000

    Add package edu-rdm-integration/0.2.2/edu_rdm_integration-0.2.2-py3-none-any.whl

---
+++ packages/edu-rdm-integration/edu_rdm_integration-0.2.2-py3-none-any.whl/edu_rdm_integration/consts.py
@@ -0,0 +1,15 @@
+REGIONAL_DATA_MART_INTEGRATION_COLLECTING_DATA = 'regional_data_mart_integration_collecting_data'
+REGIONAL_DATA_MART_INTEGRATION_EXPORTING_DATA = 'regional_data_mart_integration_exporting_data'
+
+# Формат даты. Используется для выгрузки
+DATE_FORMAT = '%Y-%m-%d'
+
+# Формат даты/времени. Для выгрузки не используется (в выгрузке ISO формат)
+DATETIME_FORMAT = '%d.%m.%Y %H:%M:%S'
+
+# Формат даты/времени. Для выгрузки
+EXPORT_DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S'
+
+LOGS_DELIMITER = '    '
+
+HASH_ALGORITHM = 'md_gost12_512'
