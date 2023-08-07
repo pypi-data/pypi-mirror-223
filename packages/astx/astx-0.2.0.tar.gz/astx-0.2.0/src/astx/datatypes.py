97256e068f617527ce34135ca915cace8ee0b93a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 14:07:32 +0000

    Add package astx/0.2.0/astx-0.2.0.tar.gz

---
+++ packages/astx/astx-0.2.0.tar.gz/astx-0.2.0/src/astx/datatypes.py
@@ -0,0 +1,120 @@
+from __future__ import annotations
+
+from astx.base import DataType, ASTKind, SourceLocation, Expr, ExprType
+from astx.operators import BinaryOp
+
+
+class DataTypeOps(DataType):
+    def __add__(self, other: DataType) -> BinaryOp:
+        return BinaryOp("+", self, other)
+
+    def __sub__(self, other: DataType) -> BinaryOp:
+        return BinaryOp("-", self, other)
+
+    def __div__(self, other: DataType) -> BinaryOp:
+        return BinaryOp("/", self, other)
+
+    def __mul__(self, other: DataType) -> BinaryOp:
+        return BinaryOp("*", self, other)
+
+    def __pow__(self, other: DataType) -> BinaryOp:
+        return BinaryOp("^", self, other)
+
+    def __mod__(self, other: DataType) -> BinaryOp:
+        return BinaryOp("%", self, other)
+
+
+class Number(DataTypeOps):
+    """Number data type expression."""
+
+
+class Integer(Number):
+    """Integer number data type expression."""
+
+
+class SignedInteger(Integer):
+    """Signed integer number data type expression."""
+
+
+class Int8(SignedInteger):
+    """Int8 data type expression."""
+
+    nbytes: int = 1
+
+
+class Int16(SignedInteger):
+    """Int16 data type expression."""
+
+    nbytes: int = 2
+
+
+class Int32(SignedInteger):
+    """Int32 data type expression."""
+
+    nbytes: int = 4
+
+
+class Int64(SignedInteger):
+    """Int64 data type expression."""
+
+    nbytes: int = 8
+
+
+class Floating(Number):
+    """AST for the literal float number."""
+
+
+class Float16(Floating):
+    """Float16 data type expression."""
+
+
+class Float32(Floating):
+    """Float32 data type expression."""
+
+
+class Float64(Floating):
+    """Float64 data type expression."""
+
+
+class Boolean(DataType):
+    """Boolean data type expression."""
+
+
+class Literal(DataTypeOps):
+    """Literal Data type."""
+
+    type_: ExprType
+    loc: SourceLocation
+
+
+class Int32Literal(Literal):
+    value: int
+
+    def __init__(
+        self, value: int, loc: SourceLocation = SourceLocation(0, 0)
+    ) -> None:
+        super().__init__(loc)
+        self.value = value
+        self.type_ = Int32
+        self.loc = loc
+
+
+class Variable(DataTypeOps):
+    """AST class for the variable usage."""
+
+    type_: ExprType
+    value: DataType
+
+    def __init__(
+        self,
+        name: str,
+        type_: ExprType,
+        value: DataType,
+        loc: SourceLocation = SourceLocation(0, 0),
+    ) -> None:
+        """Initialize the Variable instance."""
+        super().__init__(loc)
+        self.name = name
+        self.type_ = type_
+        self.kind = ASTKind.VariableKind
+        self.value: DataType = value
---
+++ packages/astx/astx-0.2.0.tar.gz/astx-0.2.0/src/astx/statements.py
@@ -0,0 +1,76 @@
+from typing import List, Tuple, Optional
+
+from astx.base import StatementType, Expr, SourceLocation, ASTKind
+from astx.blocks import Block
+
+
+class IfStmt(StatementType):
+    """AST class for `if` statement."""
+
+    condition: Expr
+    then: Block
+    else_: Optional[Block]
+
+    def __init__(
+        self,
+        condition: Expr,
+        then: Block,
+        else_: Optional[Block] = None,
+        loc: SourceLocation = SourceLocation(0, 0),
+    ) -> None:
+        """Initialize the IfStmt instance."""
+        self.loc = loc
+        self.condition = condition
+        self.then = then
+        self.else_ = else_
+        self.kind = ASTKind.IfKind
+
+
+class ForStmt(StatementType):
+    """AST class for `For` statement."""
+
+    var_name: str
+    start: Expr
+    end: Expr
+    step: Expr
+    body: Block
+
+    def __init__(
+        self,
+        var_name: str,
+        start: Expr,
+        end: Expr,
+        step: Expr,
+        body: Block,
+        loc: SourceLocation = SourceLocation(0, 0),
+    ) -> None:
+        """Initialize the ForStmt instance."""
+        self.loc = loc
+        self.var_name = var_name
+        self.start = start
+        self.end = end
+        self.step = step
+        self.body = body
+        self.kind = ASTKind.ForKind
+
+
+class VarExprAST(StatementType):
+    """AST class for variable declaration."""
+
+    var_names: List[Tuple[str, Expr]]
+    type_name: str
+    body: Block
+
+    def __init__(
+        self,
+        var_names: List[Tuple[str, Expr]],
+        type_name: str,
+        body: Block,
+        loc: SourceLocation = SourceLocation(0, 0),
+    ) -> None:
+        """Initialize the VarExprAST instance."""
+        self.loc = loc
+        self.var_names = var_names
+        self.type_name = type_name
+        self.body = body
+        self.kind = ASTKind.VarKind
