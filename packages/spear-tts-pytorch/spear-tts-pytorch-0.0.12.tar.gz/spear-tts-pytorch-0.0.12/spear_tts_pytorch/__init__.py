7cab4e0fb15f47021bfb6215f864cea6d642e21f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 03:10:01 +0000

    Add package spear-tts-pytorch/0.0.12/spear-tts-pytorch-0.0.12.tar.gz

---
+++ packages/spear-tts-pytorch/spear-tts-pytorch-0.0.12.tar.gz/spear-tts-pytorch-0.0.12/spear_tts_pytorch/__init__.py
@@ -0,0 +1,9 @@
+from spear_tts_pytorch.spear_tts_pytorch import (
+    TextToSemantic,
+    SpeechSpeechPretrainWrapper,
+    SemanticToTextWrapper,
+)
+from spear_tts_pytorch.trainer import (
+    SpeechSpeechPretrainer,
+    SemanticToTextTrainer
+)
