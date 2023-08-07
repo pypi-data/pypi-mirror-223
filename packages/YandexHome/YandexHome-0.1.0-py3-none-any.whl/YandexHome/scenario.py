f67f302594f154b350a640137fb232add0181233
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 03:11:04 +0000

    Add package YandexHome/0.1.0/YandexHome-0.1.0-py3-none-any.whl

---
+++ packages/YandexHome/YandexHome-0.1.0-py3-none-any.whl/YandexHome/scenario.py
@@ -0,0 +1,18 @@
+from aiohttp import ClientSession
+
+class YandexHomeError(BaseException): pass
+
+class Scenario:
+    def __init__(self, id: str, name: str, is_active: bool, token: str) -> None:
+        self.id = id
+        self.name = name
+        self.is_active = is_active
+        self.__token = token
+
+    async def start(self):
+        if self.is_active:
+            async with ClientSession() as session:
+                async with session.post(f'https://api.iot.yandex.net/v1.0/scenarios/{self.id}/actions', headers={'Authorization': f'Bearer {self.__token}'}) as req:
+                    json = await req.json()
+                    if json['status'] == 'error':
+                        raise YandexHomeError(json['message'])
