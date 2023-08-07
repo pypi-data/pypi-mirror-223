e6aa10445168668a87f1303245a13f79831f755c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 03:09:43 +0000

    Add package tempmail-python/2.0.0/tempmail_python-2.0.0-py3-none-any.whl

---
+++ packages/tempmail-python/tempmail_python-2.0.0-py3-none-any.whl/tempmail/__init__.py
@@ -0,0 +1,11 @@
+"""tempmail-python is a Python library for generating and managing temporary email addresses using the 1secmail service.
+
+Example usage:
+```python
+TODO
+```
+"""
+
+from .providers import OneSecMail as EMail
+
+__all__ = ('EMail',)
