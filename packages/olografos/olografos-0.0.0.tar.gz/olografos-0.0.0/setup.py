37c2eb3329a9a865f68a95ba88681b9e6e3360fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 04:15:17 +0000

    Add package olografos/0.0.0/olografos-0.0.0.tar.gz

---
+++ packages/olografos/olografos-0.0.0.tar.gz/olografos-0.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="olografos",
+    version="",
+    packages=find_packages(),
+    install_requires=[],
+    author="Yiannis Mertzanis",
+    author_email="imertz@protonmail.com",
+    description="A package to convert numbers to greek words",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/imertz/olografos-py",
+)
