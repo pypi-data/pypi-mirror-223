5d6189d66226c58d5a3149272e8bead90f27a29d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 04:13:17 +0000

    Add package sofia-redux/1.3.3/sofia_redux-1.3.3-py3-none-any.whl

---
+++ packages/sofia-redux/sofia_redux-1.3.3-py3-none-any.whl/sofia_redux/visualization/display/ui/makeUis.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+#
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+#
+
+pyuic5 simple_spec_viewer.ui -o simple_spec_viewer.py
+pyuic5 fit_result_window.ui -o fit_result_window.py
+pyuic5 cursor_location.ui -o cursor_location.py
+pyuic5 reference_data.ui -o reference_data.py
+pyuic5 textview.ui -o textview.py
+
