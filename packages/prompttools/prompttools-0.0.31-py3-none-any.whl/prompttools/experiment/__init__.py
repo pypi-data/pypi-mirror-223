afccf499a7ba09728fcbedcdf61084852503988f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 14:07:31 +0000

    Add package prompttools/0.0.31/prompttools-0.0.31-py3-none-any.whl

---
+++ packages/prompttools/prompttools-0.0.31-py3-none-any.whl/prompttools/experiment/__init__.py
@@ -0,0 +1,32 @@
+# Copyright (c) Hegel AI, Inc.
+# All rights reserved.
+#
+# This source code's license can be found in the
+# LICENSE file in the root directory of this source tree.
+
+
+from .experiments.experiment import Experiment
+from .experiments.openai_chat_experiment import OpenAIChatExperiment
+from .experiments.openai_completion_experiment import OpenAICompletionExperiment
+from .experiments.anthropic_completion_experiment import AnthropicCompletionExperiment
+from .experiments.huggingface_hub_experiment import HuggingFaceHubExperiment
+from .experiments.google_palm_experiment import GooglePaLMCompletionExperiment
+from .experiments.llama_cpp_experiment import LlamaCppExperiment
+from .experiments.chromadb_experiment import ChromaDBExperiment
+from .experiments.weaviate_experiment import WeaviateExperiment
+from .experiments.mindsdb_experiment import MindsDBExperiment
+from .experiments.langchain_experiment import SequentialChainExperiment
+
+__all__ = [
+    "AnthropicCompletionExperiment",
+    "ChromaDBExperiment",
+    "Experiment",
+    "GooglePaLMCompletionExperiment",
+    "LlamaCppExperiment",
+    "HuggingFaceHubExperiment",
+    "MindsDBExperiment",
+    "OpenAIChatExperiment",
+    "OpenAICompletionExperiment",
+    "SequentialChainExperiment",
+    "WeaviateExperiment",
+]
---
+++ packages/prompttools/prompttools-0.0.31-py3-none-any.whl/prompttools/version.py
@@ -0,0 +1,2 @@
+__version__ = '0.0.31a0'
+git_version = 'Unknown'
