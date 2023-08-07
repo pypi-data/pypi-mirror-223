3b1461a8d5af7eec4c21946e01752992e9699afd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 14:07:54 +0000

    Add package RuletaEuropea/0.1.6/RuletaEuropea-0.1.6.tar.gz

---
+++ packages/RuletaEuropea/RuletaEuropea-0.1.6.tar.gz/RuletaEuropea-0.1.6/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='RuletaEuropea',
+    version='0.1.6',
+    packages=find_packages(),
+    author='Marcos Porteiro',
+    description='Implementacion de la ruleta europea hecha en python',  
+)
