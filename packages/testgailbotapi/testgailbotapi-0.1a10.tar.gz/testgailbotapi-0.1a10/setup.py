96bda3aa7902ea88014bc5539d8779aa06dea1b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 04:14:22 +0000

    Add package testgailbotapi/0.1a10/testgailbotapi-0.1a10.tar.gz

---
+++ packages/testgailbotapi/testgailbotapi-0.1a10.tar.gz/testgailbotapi-0.1a10/setup.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Author: Jason Y. Wu
+# @Date:   2023-07-03 22:44:39
+# @Last Modified by:   Jason Y. Wu
+# @Last Modified time: 2023-07-19 19:12:54
+
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup(license_files=["LICENSE"])
