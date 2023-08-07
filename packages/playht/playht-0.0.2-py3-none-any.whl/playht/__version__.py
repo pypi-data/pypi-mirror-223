b97eaa8d8efffab00844041b45482b97a56179e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 04:12:52 +0000

    Add package playht/0.0.2/playht-0.0.2-py3-none-any.whl

---
+++ packages/playht/playht-0.0.2-py3-none-any.whl/playht/__version__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+#
+#
+# Hans Park
+# rhcproc@gmail.com
+#
+# License: MIT
+#
+
+__title__ = 'playht'
+__license__ = 'MIT'
+__version__ = '0.0.2'
+__author__ = 'Hans Park'
+__contact__ = 'rhcproc@gmail.com'
+__url__ = 'https://github.com/rhcproc/playht'
+__description__ = (
+    '')
---
+++ packages/playht/playht-0.0.2-py3-none-any.whl/playht/playht.py
@@ -0,0 +1,7 @@
+
+class PlayHT:
+    def __init__(self):
+        self._ht = {}
+
+    def print(self):
+        print('PlayHT')
