0e54ca982b1473b094bfb042cefe84a51291fdf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 03:10:22 +0000

    Add package gracy/1.26.0/gracy-1.26.0.tar.gz

---
+++ packages/gracy/gracy-1.26.0.tar.gz/gracy-1.26.0/src/gracy/_types.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+import typing as t
+from http import HTTPStatus
+
+import httpx
+
+
+class Unset:
+    """
+    The default "unset" state indicates that whatever default is set on the
+    client should be used. This is different to setting `None`, which
+    explicitly disables the parameter, possibly overriding a client default.
+    """
+
+    def __bool__(self):
+        return False
+
+
+PARSER_KEY = t.Union[HTTPStatus, int, t.Literal["default"]]
+PARSER_VALUE = t.Union[t.Type[Exception], t.Callable[[httpx.Response], t.Any], None]
+PARSER_TYPE = t.Union[t.Dict[PARSER_KEY, PARSER_VALUE], Unset, None]
+
+UNSET_VALUE: t.Final = Unset()
