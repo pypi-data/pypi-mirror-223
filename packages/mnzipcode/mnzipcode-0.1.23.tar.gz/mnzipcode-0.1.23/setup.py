ac1a39cc57fcb0ec2b5eda317ea7296e0a162ccd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 14:08:26 +0000

    Add package mnzipcode/0.1.23/mnzipcode-0.1.23.tar.gz

---
+++ packages/mnzipcode/mnzipcode-0.1.23.tar.gz/mnzipcode-0.1.23/setup.py
@@ -0,0 +1,21 @@
+import setuptools 
+
+with open('README.md') as f: 
+  readme = f.read()
+
+setuptools.setup(
+  name='mnzipcode',
+  version='0.1.23',
+  author='Bekkage',
+  author_email='bilguunsec@gmail.com',
+  url='https://github.com/bekkage/mnzipcode',
+  description='mnzipcode is a simple library for querying Mongolian zip codes.',
+  package_data={
+    'mnzipcode': ['data.json.bz2']
+  },
+  long_description_content_type= 'text/markdown',
+  long_description=readme,
+  keywords='mnzipcode zipcode mongolia mn zip code',
+  package=['mnzipcode'],
+  include_package_data=True,
+)
