cb87276ec6040053ae6f89ec556cffc73d9b87fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 14:07:33 +0000

    Add package subtraction-package/0.1/subtraction-package-0.1.tar.gz

---
+++ packages/subtraction-package/subtraction-package-0.1.tar.gz/subtraction-package-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='subtraction-package',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # List any dependencies here, if needed
+    ],
+)
