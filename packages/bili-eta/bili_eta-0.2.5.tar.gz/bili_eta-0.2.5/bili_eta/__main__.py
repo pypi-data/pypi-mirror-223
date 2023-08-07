5862c3cfe67d103d6f7d42d19a52b9e11e5e3967
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 03:10:09 +0000

    Add package bili-eta/0.2.5/bili_eta-0.2.5.tar.gz

---
+++ packages/bili-eta/bili_eta-0.2.5.tar.gz/bili_eta-0.2.5/bili_eta/__main__.py
@@ -0,0 +1,55 @@
+import click
+from os import path, getcwd,mkdir,listdir
+import dotenv
+
+env = {
+    'HOST':'127.0.0.1',
+    'PORT': '8080',
+    'SUPERUSERS': [],
+    'DynTextFont':'',
+    'DynEmojiFont':'',
+    'DynFontStyle':''
+    
+}
+
+
+@click.group()
+def main():
+    pass
+
+
+@click.command()
+def start():
+    creat_config()
+    creat_plugins_dir()
+    from bot import run
+    run()
+
+
+main.add_command(start)
+
+
+def creat_config():
+    env_file_path = path.join(getcwd(), ".env.prod")
+    if not path.exists(env_file_path):
+        for key, value in env.items():
+            dotenv.set_key(
+                env_file_path,
+                key,
+                str(value).replace(' ', ''),
+                quote_mode="never",
+            )
+    
+
+def creat_plugins_dir():
+    plugins_dir_path = path.join(getcwd(),"plugins")
+    if not path.exists(plugins_dir_path):
+        mkdir(plugins_dir_path)
+    
+
+
+if __name__ == "__main__":
+#     creat_config()
+    start()
+    # from bot import driver
+    # print(driver.config)
