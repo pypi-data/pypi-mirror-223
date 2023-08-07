74c3318608a0f9309739bf7823758201333ac97f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 04:13:10 +0000

    Add package posaydones-material-theming/0.1.1/posaydones_material_theming-0.1.1-py3-none-any.whl

---
+++ packages/posaydones-material-theming/posaydones_material_theming-0.1.1-py3-none-any.whl/posaydones_material_theming/misc/utils.py
@@ -0,0 +1,23 @@
+import os
+from posaydones_material_theming.modules.theming import get_theme_from_image
+from posaydones_material_theming.modules.templates import handle_templates
+from posaydones_material_theming.modules.wallpaper import set_wallpaper
+
+def add_slash_if_needed(s):
+    if s == "":
+        return ""
+    elif s[-1] != '/':
+        s += '/'
+    return s
+
+
+def proceed_theme(path, color_scheme_type):
+    if os.path.isdir(path):
+        files = os.listdir(path)
+        random_file = random.choice(files)
+        path = os.path.join(path, random_file)
+
+    theme = get_theme_from_image(path, color_scheme_type)
+    handle_templates(theme)
+    set_wallpaper(path, "swww")
+
