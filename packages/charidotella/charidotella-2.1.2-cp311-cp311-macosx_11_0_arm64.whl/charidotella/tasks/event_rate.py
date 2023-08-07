138f9a2e93e4bf03788d8fdc99805b5c95b87ba0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 14:07:35 +0000

    Add package charidotella/2.1.2/charidotella-2.1.2-cp311-cp311-macosx_11_0_arm64.whl

---
+++ packages/charidotella/charidotella-2.1.2-cp311-cp311-macosx_11_0_arm64.whl/charidotella/tasks/event_rate.py
@@ -0,0 +1,36 @@
+import importlib.resources
+import pathlib
+import subprocess
+import typing
+
+EXTENSION = ".svg"
+
+
+def run(
+    input: pathlib.Path,
+    output: pathlib.Path,
+    begin: int,
+    end: int,
+    parameters: dict[str, typing.Any],
+):
+    subprocess.run(
+        [
+            str(
+                importlib.resources.files("charidotella").joinpath("assets/event_rate")
+            ),
+            str(input),
+            str(output),
+            f"--begin={begin}",
+            f"--end={end}",
+            f"--long={parameters['long_tau']}",
+            f"--short={parameters['short_tau']}",
+            f"--width={parameters['width']}",
+            f"--height={parameters['height']}",
+            f"--longcolor={parameters['long_tau_color']}",
+            f"--shortcolor={parameters['short_tau_color']}",
+            f"--axiscolor={parameters['axis_color']}",
+            f"--maingridcolor={parameters['main_grid_color']}",
+            f"--secondarygridcolor={parameters['secondary_grid_color']}",
+        ],
+        check=True,
+    )
