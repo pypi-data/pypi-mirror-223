827f1d43b2366aa539b5e15f97f2c03259868e8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 03:09:39 +0000

    Add package chain-harvester/0.2.1/chain_harvester-0.2.1.tar.gz

---
+++ packages/chain-harvester/chain_harvester-0.2.1.tar.gz/chain_harvester-0.2.1/chain_harvester/networks/ethereum/mainnet.py
@@ -0,0 +1,38 @@
+import json
+import logging
+
+import requests
+
+from chain_harvester.chain import Chain
+from chain_harvester.constants import CHAINS
+
+log = logging.getLogger(__name__)
+
+
+class EthereumMainnetChain(Chain):
+    def __init__(self, rpc=None, rpc_nodes=None, api_key=None, abis_path=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        print(rpc_nodes)
+        self.chain = "ethereum"
+        self.network = "mainnet"
+        self.rpc = rpc or rpc_nodes[self.chain][self.network]
+        self.chain_id = CHAINS[self.chain][self.network]
+        self.step = 10_000
+        self.abis_path = abis_path or "abis/"
+        self.api_key = api_key
+
+    def get_abi_from_source(self, contract_address):
+        try:
+            req = requests.get(
+                "https://api.etherscan.io/api?module=contract&action=getabi&address="
+                + contract_address
+                + "&apikey="
+                + self.api_key,
+                timeout=5,
+            )
+        except requests.exceptions.Timeout:
+            log.exception("Timeout when get abi from etherscan", extra={"contract_address": contract_address})
+            raise
+        resp = json.loads(req.text)
+        abi = json.loads(resp["result"])
+        return abi
