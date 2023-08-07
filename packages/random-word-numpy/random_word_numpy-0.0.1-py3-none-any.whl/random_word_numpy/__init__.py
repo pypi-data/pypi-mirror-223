90bc6dfc25e45eb2a5596d779d62805bb9383422
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 04:15:25 +0000

    Add package random-word-numpy/0.0.1/random_word_numpy-0.0.1-py3-none-any.whl

---
+++ packages/random-word-numpy/random_word_numpy-0.0.1-py3-none-any.whl/random_word_numpy/__init__.py
@@ -0,0 +1,6 @@
+from random_word_numpy.random_word import RandomWords
+
+__all__ = [ "random_word_numpy"]
+__name__ = "random_word_numpy"
+__author__ = "Ben Schreck <ben@benschreck.com>"
+__version__ = "0.0.1"
