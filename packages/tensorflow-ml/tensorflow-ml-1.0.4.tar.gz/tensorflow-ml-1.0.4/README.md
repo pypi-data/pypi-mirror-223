a6b35b793b8443377b6a09452f1e3d080f1106c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 04:12:52 +0000

    Add package tensorflow-ml/1.0.4/tensorflow-ml-1.0.4.tar.gz

---
+++ packages/tensorflow-ml/tensorflow-ml-1.0.4.tar.gz/tensorflow-ml-1.0.4/README.md
@@ -0,0 +1,15 @@
+# tensorflow-ml
+
+TensorFlow ML - an abstract implementation of commonly used machine learning algorithms using TensorFlow
+
+## Status
+
+| Model               | Type           | Code | Test | Remarks                                               |
+| ------------------- | -------------- | ---- | ---- | ----------------------------------------------------- |
+| Linear Regression   | Regression     | Done | Pass | Need to put errors for safety checks in all functions |
+| Logistic Regression | Classification | Done | Fail | Poor accuracy ~33%                                    |
+| NB - GNB/BNB        | Classification | Done | TBC  | -                                                     |
+| DT/RF/GBT/CART      | Classification | Done | TBC  | -                                                     |
+| Lasso Regression    | Regression     | Done | TBC  | -                                                     |
+| Polynomial Reg      | Regression     | Done | TBC  | -                                                     |
+| Ridge Regression   | Regression     | Done | TBC  | -                                                     |
---
+++ packages/tensorflow-ml/tensorflow-ml-1.0.4.tar.gz/tensorflow-ml-1.0.4/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import os
+
+VERSION = '1.0.4' 
+DESCRIPTION = 'Tensorflow ML'
+LONG_DESCRIPTION = 'An abstract implementation of commonly used machine learning algorithms using TensorFlow 2.0'
+
+lib_folder = os.path.dirname(os.path.realpath(__file__))
+requirement_path = f"{lib_folder}/requirements.txt"
+install_requires = [] # Here we'll add: ["gunicorn", "docutils>=0.3", "lxml==0.5a7"]
+if os.path.isfile(requirement_path):
+    with open(requirement_path) as f:
+        install_requires = f.read().splitlines()
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="tensorflow-ml", 
+        version=VERSION,
+        author="Siddhant Pathak",
+        author_email="siddhantpathak2@gmail.com",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=install_requires, # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        
+        keywords=['python', 'tensorflow', 'ml'],
+        classifiers= [
+            "Programming Language :: Python :: 3.11",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: POSIX :: Linux",
+        ]
+)
