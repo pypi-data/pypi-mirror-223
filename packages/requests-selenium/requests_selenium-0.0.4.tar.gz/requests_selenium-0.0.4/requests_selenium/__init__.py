2fcffe23aa3daeffa8219d6d048c5baa2e9b9943
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 03:09:23 +0000

    Add package requests-selenium/0.0.4/requests_selenium-0.0.4.tar.gz

---
+++ packages/requests-selenium/requests_selenium-0.0.4.tar.gz/requests_selenium-0.0.4/requests_selenium/__init__.py
@@ -0,0 +1,18 @@
+
+# Import the main class from the selenium_utils module using an absolute import
+#from requests_selenium.selenium_utils import requests_selenium
+
+# Define the __all__ variable to control what gets imported with 'from requests_selenium import *'
+#__all__ = ['requests_selenium']
+
+# Import the main class from the selenium_utils module
+from .selenium_utils import requests_selenium
+
+# Define the __all__ variable to control what gets imported with 'from requests_selenium import *'
+__all__ = ['requests_selenium']
+
+# Make the requests_selenium class available at the package level
+requests_selenium = requests_selenium
+
+#python setup.py sdist bdist_wheel
+#twine upload dist/*
