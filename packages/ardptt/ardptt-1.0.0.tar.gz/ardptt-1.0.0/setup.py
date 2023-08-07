606d67674f087a9eae861c4111cfae74672d178b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 14:07:34 +0000

    Add package ardptt/1.0.0/ardptt-1.0.0.tar.gz

---
+++ packages/ardptt/ardptt-1.0.0.tar.gz/ardptt-1.0.0/setup.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(name='ardptt',
+      version='1.0.0',
+      description='android performance test tool',
+      author='gewanpiaoyi',
+      author_email='745718548@qq.com',
+      packages=find_packages(),
+      install_requires=['pyecharts>=2.0.3'],
+      )
