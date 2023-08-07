5ed9c13fc503f11d2d0e4899b642aae5a55c7560
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 03:09:45 +0000

    Add package rwai/0.1.0/rwai-0.1.0.tar.gz

---
+++ packages/rwai/rwai-0.1.0.tar.gz/rwai-0.1.0/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+
+name = "rwai"
+version = "0.1.0"
+description = "Фановый ИИ основаный на Ваших словах"
+long_description = """
+RWAI (Random Words AI) - это фановый искусственный интеллект, который генерирует случайные ответы 
+на основе ваших слов. Просто отправьте ему сообщение, и он вернет вам случайные слова из 
+сохраненного списка. RWAI может использоваться для забавных разговоров, создания случайных 
+фраз или просто для удивления ваших друзей.
+
+Установка:
+```py
+pip install rwai_ru
+```
+
+# Пример использования:
+```py
+from rwai_ru import rwai
+response = rwai.send("привет!")
+print(response)
+```
+"""
+author = "barlin41k"
+author_email = "sasaigrypocta@gmail.com"
+install_requires = [
+    "barladb"
+]
+packages = find_packages()
+keywords = ["rwai", "randomword", "randomai"]
+license = "MIT"
+
+setup(
+    name=name,
+    version=version,
+    description=description,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author=author,
+    author_email=author_email,
+    packages=packages,
+    install_requires=install_requires,
+    keywords=keywords,
+    license=license,
+)
