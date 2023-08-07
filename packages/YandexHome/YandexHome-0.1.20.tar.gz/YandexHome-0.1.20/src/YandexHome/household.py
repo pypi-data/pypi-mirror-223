44b40fe8635d11de1efe6990730eed3dc1f7d35d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 14:07:30 +0000

    Add package YandexHome/0.1.20/YandexHome-0.1.20.tar.gz

---
+++ packages/YandexHome/YandexHome-0.1.20.tar.gz/YandexHome-0.1.20/src/YandexHome/household.py
@@ -0,0 +1,15 @@
+class Household:
+    id: str
+    name: str
+    type: str
+
+    def __init__(self, id: str, name: str, house_type: str) -> None:
+        self.id = id
+        self.name = name
+        self.type = house_type
+
+    def __str__(self) -> str:
+        return f'<Household id={self.id} name={self.name} type={self.type}>'
+    
+    def __repr__(self) -> str:
+        return self.__str__()
