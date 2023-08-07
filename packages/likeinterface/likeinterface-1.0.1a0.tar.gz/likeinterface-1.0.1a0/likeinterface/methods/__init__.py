80e3be49390b7395a3098a67d2df0cd07a799855
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 04:13:13 +0000

    Add package likeinterface/1.0.1a0/likeinterface-1.0.1a0.tar.gz

---
+++ packages/likeinterface/likeinterface-1.0.1a0.tar.gz/likeinterface-1.0.1a0/likeinterface/methods/__init__.py
@@ -0,0 +1,16 @@
+from .auth import GetAuthorizationInformationMethod, RootAuthMethod, SignInMethod
+from .base import LikeType, Method, Request, Response
+from .like import EvaluatorMethod, RootLikeMethod
+
+__all__ = (
+    "EvaluatorMethod",
+    "GetAuthorizationInformationMethod",
+    "RootAuthMethod",
+    "LikeType",
+    "Method",
+    "Request",
+    "Response",
+    "RootAuthMethod",
+    "RootLikeMethod",
+    "SignInMethod",
+)
---
+++ packages/likeinterface/likeinterface-1.0.1a0.tar.gz/likeinterface-1.0.1a0/likeinterface/types/__init__.py
@@ -0,0 +1,5 @@
+from .auth import Authorization, User
+from .base import LikeObject, MutableLikeObject
+from .like import Hand
+
+__all__ = ("Authorization", "Hand", "LikeObject", "MutableLikeObject", "User")
