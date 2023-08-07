52bace2c4137e8b769e974160d55f9fc5486974b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 03:11:00 +0000

    Add package podmanclispawner/0.2.0/podmanclispawner-0.2.0-py3-none-any.whl

---
+++ packages/podmanclispawner/podmanclispawner-0.2.0-py3-none-any.whl/tests/conftest.py
@@ -0,0 +1,68 @@
+""" pytest config for ansiblespawner tests """
+# https://github.com/jupyterhub/yarnspawner/blob/0.4.0/yarnspawner/tests/conftest.py
+import pytest_asyncio
+
+from jupyterhub.tests.mocking import MockHub
+import os
+import socket
+import sys
+from traitlets.config import Config
+
+from podmanclispawner import PodmanCLISpawner
+
+
+# make Hub connectable by default
+MockHub.hub_ip = "0.0.0.0"
+
+
+def _get_host_default_ip():
+    """
+    IP associated with the default route
+    https://stackoverflow.com/a/28950776
+    """
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+        # doesn't even have to be reachable
+        s.connect(("10.255.255.255", 1))
+        return s.getsockname()[0]
+
+
+# https://docs.pytest.org/en/latest/example/parametrize.html#apply-indirect-on-particular-arguments
+@pytest_asyncio.fixture()
+async def app(request):
+    """
+    Mock a jupyterhub app for testing
+
+    Takes a parameter indicating the name of the directory under examples
+    """
+
+    def abspath(f):
+        return os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            "..",
+            "examples",
+            request.param,
+            f,
+        )
+
+    c = Config()
+    c.JupyterHub.spawner_class = PodmanCLISpawner
+    c.PodmanCLISpawner.remove = True
+    c.PodmanCLISpawner.start_timeout = 600
+    c.JupyterHub.hub_connect_ip = _get_host_default_ip()
+
+    mocked_app = MockHub.instance(config=c)
+
+    await mocked_app.initialize([])
+    await mocked_app.start()
+
+    try:
+        yield mocked_app
+    finally:
+        # disconnect logging during cleanup because pytest closes captured FDs
+        # prematurely
+        mocked_app.log.handlers = []
+        MockHub.clear_instance()
+        try:
+            mocked_app.stop()
+        except Exception as e:
+            print("Error stopping Hub: %s" % e, file=sys.stderr)
