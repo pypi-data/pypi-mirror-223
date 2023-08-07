d570ce162e6eb793ad2e4cd60222d64513b7864b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 03:11:04 +0000

    Add package pyspk/1.8/pyspk-1.8.tar.gz

---
+++ packages/pyspk/pyspk-1.8.tar.gz/pyspk-1.8/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    print(long_description)
+
+setuptools.setup(
+    name="pyspk",
+    version=1.8,
+    description="Python package to predict the suppression of the total matter power spectrum due to baryonic physics",
+    url="https://github.com/jemme07/pyspk",
+    author="Jaime Salcido",
+    author_email="j.salcidonegrete@ljmu.ac.uk",
+    packages=['pyspk'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["numpy", "scipy"],
+    include_package_data=True
+)
