a7e21d0fc59c739ed1e14e1117b6a25e4520daf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 14:07:18 +0000

    Add package loq0/0.0.5/loq0-0.0.5.tar.gz

---
+++ packages/loq0/loq0-0.0.5.tar.gz/loq0-0.0.5/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+install_requires = [
+    'numpy==1.25.2'
+]
+
+setuptools.setup(
+    name="loq0",
+    version="0.0.5",
+    author="seorii",
+    author_email="me@seorii.page",
+    description="League of Quoridor",
+    long_description="League of Quoridor",
+    long_description_content_type="text/markdown",
+    url="https://github.com/dastyinc/loq0",
+    packages=setuptools.find_packages('.'),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+
+)
