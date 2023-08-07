5226de119554e0e321d0a9bfdb4e3d5236eddd6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 14:07:12 +0000

    Add package pysesh/0.2.0/pysesh-0.2.0-py3-none-any.whl

---
+++ packages/pysesh/pysesh-0.2.0-py3-none-any.whl/pysesh/__init__.py
@@ -0,0 +1,47 @@
+import sys
+import pickle
+import readline
+import argparse
+from colr import color
+from rich.console import Console
+from collections import OrderedDict
+import pysesh.state.ignore
+import inquirer
+import pysesh
+import os
+
+console = Console()
+
+col = OrderedDict([
+    ('n', '#67D0A8'),
+    ('w', '#FFCC00'),
+    ('d', '#ff2e63'),
+    ('s', '#00fc03'),
+    ('l', '#F0F0F0'),
+])
+
+def cpr(text: str):
+    console.print(text)
+
+parser = argparse.ArgumentParser(description='Python Multiplex - A Session Management Utility for Python.')
+subparsers = parser.add_subparsers(dest='command')
+
+new_parser = subparsers.add_parser('new', help='Create a new session.')
+new_parser.add_argument('name', type=str, help='Name of the new session.')
+
+load_parser = subparsers.add_parser('load', help='Load an existing session.')
+load_parser.add_argument('name', type=str, help='Name of the session to load.')
+
+delete_parser = subparsers.add_parser('delete', help='Delete a session.')
+delete_parser.add_argument('name', type=str, help='Name of the session to delete.')
+
+list_parser = subparsers.add_parser('list', help='List all available sessions.')
+
+args = parser.parse_args()
+
+
+DIRECTORY = os.path.abspath(os.path.join(os.path.dirname(__file__), 'state'))
+REPOSITORY = os.path.join(DIRECTORY, 'sessions.pickle')
+
+if not os.path.exists(DIRECTORY):
+    os.makedirs(DIRECTORY)
---
+++ packages/pysesh/pysesh-0.2.0-py3-none-any.whl/pysesh/session.py
@@ -0,0 +1,76 @@
+import inquirer
+from pysesh import os, pickle, console, color, col, sys, inquirer, REPOSITORY, cpr
+
+
+def load_sessions():
+    try:
+        with open(REPOSITORY, 'rb') as f:
+            return pickle.load(f)
+    except FileNotFoundError:
+        return {}
+
+
+
+def save_sessions(sessions):
+    with open(REPOSITORY, 'wb') as f:
+        pickle.dump(sessions, f)
+
+
+def create_new_session(sessions, name):
+    if name in sessions:
+        cpr(f"\n[bold {col['d']}][-][/bold {col['d']}] Session '[bold {col['w']}]{name}[/bold {col['w']}]' already exists.\n")
+        return
+    sessions[name] = {'name': name, 'state': {}}
+    cpr(f"\n[bold {col['s']}][+][/bold {col['s']}] Session '[bold {col['w']}]{name}[/bold {col['w']}]' created successfully.\n")
+
+
+def list_sessions(sessions):
+    for session in sessions:
+        cpr(f"\n[bold {col['n']}] Available sessions:[/bold {col['n']}]\n")
+        cpr(f"[bold {col['s']}]  -[/bold {col['s']}][bold {col['w']}] {session}[/bold {col['w']}]\n")
+
+
+def delete_session(sessions, name):
+    if name not in sessions:
+        cpr(f"\n[bold {col['d']}][[/bold {col['d']}][bold {col['w']}]![/bold {col['w']}][bold {col['d']}]][/bold {col['d']}] Session '[bold {col['w']}]{name}[/bold {col['w']}]' does not exist.\n")
+        return
+    del sessions[name]
+    cpr(f"\n[bold {col['s']}][-][/bold {col['s']}] Session '[bold {col['w']}]{name}[/bold {col['w']}]' deleted successfully.\n")
+
+
+def load_session(sessions, name):
+    if name not in sessions:
+        cpr(f"\n[bold {col['d']}][[/bold {col['d']}][bold {col['w']}]![/bold {col['w']}][bold {col['d']}]][/bold {col['d']}] Session '[bold {col['w']}]{name}[/bold {col['w']}]' does not exist.\n")
+        return
+
+    current_session_state = sessions[name]['state']
+
+    sys.ps1 = ''
+    sys.ps2 = ''
+
+    try:
+        cpr(f"[bold {col['n']}]Now running in '[bold {col['w']}]{name}[/bold {col['w']}]', press Ctrl+C to save state and exit.[/bold {col['n']}]\n")
+        while True:
+            code_input = input()
+            if code_input.strip().lower() == 'exit()':
+                break
+            exec(code_input, current_session_state)
+    except KeyboardInterrupt:
+        cpr(f"\n[bold {col['s']}][+][/bold {col['s']}][bold {col['l']}] Session state for '[bold {col['w']}]{name}[/bold {col['w']}]' has been saved.[/bold {col['l']}]\n")
+    except Exception as e:
+        cpr(f"\n[bold {col['d']}][[/bold {col['d']}][bold {col['w']}]![/bold {col['w']}][bold {col['d']}]][/bold {col['d']}] Session '[bold {col['w']}]{name}[/bold {col['w']}]' state has not been saved.\n")
+
+
+
+def select_session(sessions):
+    choices = list(sessions.keys())
+    cpr('')
+    questions = [
+        inquirer.List('selected_session',
+                      message="Select a session",
+                      choices=choices,
+                      )
+    ]
+    answers = inquirer.prompt(questions)
+    return answers["selected_session"] if answers else None
+
