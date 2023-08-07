921779ed6ee32a3945671e1ff71a887986fd71f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 03:10:03 +0000

    Add package loggingx-py/0.4.0/loggingx-py-0.4.0.tar.gz

---
+++ packages/loggingx-py/loggingx-py-0.4.0.tar.gz/loggingx-py-0.4.0/loggingx/formatter.py
@@ -0,0 +1,68 @@
+import json
+import logging
+from logging import Formatter
+
+from loggingx.context import CtxRecord
+
+# https://docs.python.org/3/library/logging.html#logrecord-attributes
+_DEFAULT_KEYS = (
+    "args",
+    "asctime",
+    "created",
+    "exc_info",
+    "exc_text",
+    "filename",
+    "funcName",
+    "levelname",
+    "levelno",
+    "lineno",
+    "module",
+    "msecs",
+    "message",
+    "msg",
+    "name",
+    "pathname",
+    "process",
+    "processName",
+    "relativeCreated",
+    "stack_info",
+    "thread",
+    "threadName",
+    # CtxRecord
+    "caller",
+    "ctxFields",
+)
+
+_LEVEL_TO_LOWER_NAME = {
+    logging.CRITICAL: "fatal",
+    logging.ERROR: "error",
+    logging.WARNING: "warn",
+    logging.INFO: "info",
+    logging.DEBUG: "debug",
+    logging.NOTSET: "notset",
+}
+
+
+class JSONFormatter(Formatter):
+    def format(self, record: CtxRecord) -> str:
+        msg_dict = {
+            "time": record.created,
+            "level": _LEVEL_TO_LOWER_NAME[record.levelno],
+        }
+
+        msg_dict["caller"] = record.caller
+        msg_dict["msg"] = record.getMessage()
+        for k, v in record.ctxFields.items():
+            msg_dict[k] = v
+
+        # TODO: record.exc_info
+        # TODO: record.exc_text
+        # TODO: record.stack_info
+
+        # extra
+        for k, v in record.__dict__.items():
+            if k not in _DEFAULT_KEYS and not k.startswith("_"):
+                msg_dict[k] = v
+
+        # Set ensure_ascii to False to output the message as it is typed.
+        return json.dumps(msg_dict, ensure_ascii=False)
