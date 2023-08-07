4aa1d178359a9937c001249f10eba89e81130cd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 03:09:52 +0000

    Add package autorunpy/6.0.1/autorunpy-6.0.1-py2.py3-none-any.whl

---
+++ packages/autorunpy/autorunpy-6.0.1-py2.py3-none-any.whl/autorunpy/dl_and_ret_dirpath.py
@@ -0,0 +1,12 @@
+"""
+
+    """
+
+import sys
+
+from .auto import dl_and_ret_dirpath
+
+
+if __name__ == '__main__' :
+    conf_fp = sys.argv[1]
+    dl_and_ret_dirpath(conf_fp)
---
+++ packages/autorunpy/autorunpy-6.0.1-py2.py3-none-any.whl/autorunpy/ret_module_2_run_name.py
@@ -0,0 +1,12 @@
+"""
+
+    """
+
+import sys
+
+from .auto import ret_module_2_run_name
+
+
+if __name__ == '__main__' :
+    conf_fp = sys.argv[1]
+    ret_module_2_run_name(conf_fp)
