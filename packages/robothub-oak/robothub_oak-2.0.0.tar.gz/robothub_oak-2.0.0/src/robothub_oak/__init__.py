595122c094764e9b8bb44445d2fafeadc91b377a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 03:10:30 +0000

    Add package robothub-oak/2.0.0/robothub_oak-2.0.0.tar.gz

---
+++ packages/robothub-oak/robothub_oak-2.0.0.tar.gz/robothub_oak-2.0.0/src/robothub_oak/__init__.py
@@ -0,0 +1,18 @@
+import os
+
+from robothub_oak.application import *
+from robothub_oak.live_view import *
+from robothub_oak.utils import set_logging_level
+
+try:
+    import blobconverter
+
+    blobconverter.set_defaults(silent=True)
+except:
+    pass
+
+__version__ = '2.0.0'
+
+REPLAY_PATH = os.environ.get('RH_OAK_REPLAY_PATH', None) or os.environ.get('RH_REPLAY_PATH', None)
+
+set_logging_level('INFO')
