8ba3f835eac82b02acfe65b831db8c28d699e795
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 03:10:11 +0000

    Add package bili-eta/0.2.7/bili_eta-0.2.7.tar.gz

---
+++ packages/bili-eta/bili_eta-0.2.7.tar.gz/bili_eta-0.2.7/bili_eta/plugins/new.py
@@ -0,0 +1,12 @@
+from nonebot.plugin import PluginMetadata
+
+
+
+
+__plugin_meta__ = PluginMetadata(
+    name="示例插件",
+    description="这是一个示例插件",
+    usage="没什么用",
+    type="application",
+    extra={},
+)
