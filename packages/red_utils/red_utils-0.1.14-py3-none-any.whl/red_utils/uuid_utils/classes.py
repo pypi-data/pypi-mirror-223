e27dc32127ac3a61a1beb26aa46fc435e29bca9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 14:07:21 +0000

    Add package red_utils/0.1.14/red_utils-0.1.14-py3-none-any.whl

---
+++ packages/red_utils/red_utils-0.1.14-py3-none-any.whl/red_utils/uuid_utils/classes.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+
+@dataclass
+class UUIDLength:
+    """Simple dataclass to store UUID string lengths."""
+
+    standard: int = 36
+    hex: int = 32
