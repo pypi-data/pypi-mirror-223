052ee2c0809c6a80a418159535acddc90ec174dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 04:13:30 +0000

    Add package amalgam-lang/2.3.12/amalgam_lang-2.3.12-py3-none-any.whl

---
+++ packages/amalgam-lang/amalgam_lang-2.3.12-py3-none-any.whl/amalgam/__init__.py
@@ -0,0 +1,10 @@
+__all__ = ['api']
+
+
+"""
+The version number is automatically incremented by the pipeline
+It should not be manually changed.
+To change the major/minor number change the number in azure-pipelines.yml
+"""
+
+__version__ = "2.3.12"
