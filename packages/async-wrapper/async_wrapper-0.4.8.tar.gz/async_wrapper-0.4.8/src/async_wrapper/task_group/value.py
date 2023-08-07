c33e2a3caea7017260993ce0a07de82642e741af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 03:09:11 +0000

    Add package async-wrapper/0.4.8/async_wrapper-0.4.8.tar.gz

---
+++ packages/async-wrapper/async_wrapper-0.4.8.tar.gz/async_wrapper-0.4.8/src/async_wrapper/task_group/value.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+
+from threading import local
+from typing import Generic, TypeVar
+
+from async_wrapper.exception import PendingError
+
+ValueT_co = TypeVar("ValueT_co", covariant=True)
+Pending = local()
+
+__all__ = ["SoonValue"]
+
+
+class SoonValue(Generic[ValueT_co]):
+    """will get value soon"""
+
+    __slots__ = ("_value",)
+
+    def __init__(self) -> None:
+        self._value: ValueT_co | local = Pending
+
+    def __repr__(self) -> str:
+        status = "pending" if self._value is Pending else "done"
+        return f"<SoonValue: status={status}>"
+
+    @property
+    def value(self) -> ValueT_co:
+        """soon value"""
+        if self._value is Pending:
+            raise PendingError
+        return self._value  # type: ignore
+
+    @property
+    def is_ready(self) -> bool:
+        """value status"""
+        return self._value is not Pending
