547959e9a1e66f8edc1e001d43d77179eb0064c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 04:12:48 +0000

    Add package open-interpreter/0.0.255/open_interpreter-0.0.255-py3-none-any.whl

---
+++ packages/open-interpreter/open_interpreter-0.0.255-py3-none-any.whl/interpreter/cli.py
@@ -0,0 +1,39 @@
+import argparse
+from rich import print
+from rich.markdown import Markdown
+
+confirm_mode_message = """
+**Open Interpreter** will require approval before running code. Use `interpreter -y` to bypass this.
+
+Press `CTRL-C` to exit.
+"""
+
+
+def cli(interpreter):
+  """
+  Takes an instance of interpreter.
+  Modifies it according to command line flags, then runs chat.
+  """
+
+  parser = argparse.ArgumentParser(description='Chat with Open Interpreter.')
+  parser.add_argument('-y',
+                      '--yes',
+                      action='store_true',
+                      help='execute code without user confirmation')
+  parser.add_argument('-l',
+                      '--local',
+                      action='store_true',
+                      help='run fully local with llama-2')
+  args = parser.parse_args()
+
+  if args.yes:
+    interpreter.auto_run = True
+  else:
+    # Print message with newlines on either side (aesthetic choice)
+    print('', Markdown(confirm_mode_message), '')
+
+  if args.local:
+    interpreter.local = True
+
+  # Now run the chat method
+  interpreter.chat()
