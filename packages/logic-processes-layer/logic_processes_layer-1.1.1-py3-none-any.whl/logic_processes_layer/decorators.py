4684bbaa370c09a5980467775fb1c83a7cf9fc7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 03:10:11 +0000

    Add package logic-processes-layer/1.1.1/logic_processes_layer-1.1.1-py3-none-any.whl

---
+++ packages/logic-processes-layer/logic_processes_layer-1.1.1-py3-none-any.whl/logic_processes_layer/decorators.py
@@ -0,0 +1,34 @@
+__all__ = (
+    "make_run",
+    "run_subprocesses",
+)
+
+import typing
+from functools import wraps
+
+if typing.TYPE_CHECKING:
+    from . import context
+    from .sub_processors import BaseSubprocessor
+
+
+def run_subprocesses(
+    processor: typing.Union[typing.Callable, "BaseSubprocessor"], context_instance: "context.BaseProcessorContext"
+):
+    kwargs = {}
+    if getattr(processor, "allow_context", False):
+        processor.context = context_instance  # type: ignore
+        kwargs["context"] = context_instance
+    return processor(**kwargs)
+
+
+def make_run(run_func):
+    @wraps(run_func)
+    def run_wrapper(self, *args, **kwargs):
+        for pre_proc in self.pre_run:
+            self.results.pre_run[pre_proc] = run_subprocesses(pre_proc, self.context)
+        self.results.run = run_func(self, *args, **kwargs)
+        for post_proc in self.post_run:
+            self.results.post_run[post_proc] = run_subprocesses(post_proc, self.context)
+        return self.results.run
+
+    return run_wrapper
