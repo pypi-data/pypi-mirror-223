f03f9fcf35980c0d54fd122fe9f90487a840b1f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-08 05:11:30 +0000

    Add package dubo/0.2.6/dubo-0.2.6-py3-none-any.whl

---
+++ packages/dubo/dubo-0.2.6-py3-none-any.whl/dubo/__init__.py
@@ -0,0 +1,7 @@
+from .ask_dubo import ask, chart  # noqa
+from .config import DOCS_URL, FEEDBACK_URL
+from .__version__ import *
+
+MSG = f"Dubo | Examples: {DOCS_URL} | Discord: {FEEDBACK_URL} | Privacy policy: https://mercator.tech/privacy"
+
+print(MSG)
