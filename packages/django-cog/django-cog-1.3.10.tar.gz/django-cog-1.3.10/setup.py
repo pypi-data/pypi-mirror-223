53ddbc08514705222162b33e787a06784721f544
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-08 04:12:33 +0000

    Add package django-cog/1.3.10/django-cog-1.3.10.tar.gz

---
+++ packages/django-cog/django-cog-1.3.10.tar.gz/django-cog-1.3.10/setup.py
@@ -0,0 +1,32 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="django-cog",
+    version="1.3.10",
+    description="Django library for launching pipelines of multiple stages and parallel tasks.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/david-pettifor-nd/django_cog.git",
+    author="David W Pettifor",
+    author_email="dpettifo@nd.edu",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.11",
+        "Framework :: Django :: 2.2",
+        "Framework :: Django :: 3.2",
+        "Framework :: Django :: 4.1",
+    ],
+    packages=["django_cog"],
+    include_package_data=True,
+    install_requires=["celery==5.2.7", "django-celery-beat", "django-nested-inline>=0.4.2"]
+)
