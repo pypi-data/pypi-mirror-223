85036cee4f5de575a374c857cdb61489e29905cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-08 14:09:49 +0000

    Add package backend.ai-manager/22.9.23/backend.ai-manager-22.9.23.tar.gz

---
+++ packages/backend.ai-manager/backend.ai-manager-22.9.23.tar.gz/backend.ai-manager-22.9.23/ai/backend/manager/models/alembic/versions/0f7a4b643940_.py
@@ -0,0 +1,23 @@
+"""empty message
+
+Revision ID: 0f7a4b643940
+Revises: 7dd1d81c3204, 911023380bc9
+Create Date: 2022-03-14 06:20:12.850338
+
+"""
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "0f7a4b643940"
+down_revision = ("7dd1d81c3204", "911023380bc9")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
