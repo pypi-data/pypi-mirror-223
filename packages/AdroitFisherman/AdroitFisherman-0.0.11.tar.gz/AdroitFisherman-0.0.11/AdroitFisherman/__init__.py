3aa465b59bf50f97502bfa72f02fbac853dda6e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-08 14:09:51 +0000

    Add package AdroitFisherman/0.0.11/AdroitFisherman-0.0.11.tar.gz

---
+++ packages/AdroitFisherman/AdroitFisherman-0.0.11.tar.gz/AdroitFisherman-0.0.11/AdroitFisherman/__init__.py
@@ -0,0 +1,19 @@
+from . import SequentialList
+import os
+run_plat="dll"
+if not "Libraries" in os.listdir():
+    os.mkdir("Libraries")
+env_path=os.path.abspath(__file__).rstrip("__init__.py")
+real_path=os.path.abspath("")
+with open(f"{env_path}descriptor.txt",'r') as fe:
+    file_msg=fe.read()
+fe.close()
+data_structures=file_msg.split(",")
+data_types=["int","char","float","double","string"]
+os.chdir("./Libraries")
+for data_structure in data_structures:
+    for data_type in data_types:
+        if not f"{data_structure}_{data_type}.{run_plat}" in os.listdir():
+            print(f"copy {env_path}includes\\{data_structure}_{data_type}.{run_plat} {real_path}\\Libraries")
+            os.system(f"copy {env_path}includes\\{data_structure}_{data_type}.{run_plat} {real_path}\\Libraries")
+os.chdir("../")
