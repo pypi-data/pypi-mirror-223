26e4188b6e7d7ae938fe79bd799f644e17f9152d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-08 14:09:45 +0000

    Add package PwnAssistor/0.1/PwnAssistor-0.1.tar.gz

---
+++ packages/PwnAssistor/PwnAssistor-0.1.tar.gz/PwnAssistor-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="PwnAssistor",
+    version="0.1",
+    description="De",
+    author="V3rdant",
+    license="MIT",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
