4b67d419d628885f340fdb56bad6a1cfd7fb34bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-08 03:10:47 +0000

    Add package cougar/0.3.7/cougar-0.3.7.tar.gz

---
+++ packages/cougar/cougar-0.3.7.tar.gz/cougar-0.3.7/cougar/__init__.py
@@ -0,0 +1,13 @@
+__version__ = "0.3.7"
+
+from .rolling import (  # rolling_kurt,; rolling_skew,; rolling_std,; rolling_var,
+    rolling_argmax,
+    rolling_argmin,
+    rolling_max,
+    rolling_mean,
+    rolling_median,
+    rolling_min,
+    rolling_quantile,
+    rolling_rank,
+    rolling_sum,
+)
