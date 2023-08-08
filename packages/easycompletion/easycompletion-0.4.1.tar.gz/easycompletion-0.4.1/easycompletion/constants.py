e607f348f30d2228119fa0015946fa0e9b0a3715
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-08 14:09:15 +0000

    Add package easycompletion/0.4.1/easycompletion-0.4.1.tar.gz

---
+++ packages/easycompletion/easycompletion-0.4.1.tar.gz/easycompletion-0.4.1/easycompletion/constants.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables from .env.
+
+TEXT_MODEL = os.getenv("EASYCOMPLETION_TEXT_MODEL")
+if TEXT_MODEL == None or TEXT_MODEL == "":
+    TEXT_MODEL = "gpt-3.5-turbo-0613"
+LONG_TEXT_MODEL = os.getenv("EASYCOMPLETION_LONG_TEXT_MODEL")
+if LONG_TEXT_MODEL == None or LONG_TEXT_MODEL == "":
+    LONG_TEXT_MODEL = "gpt-3.5-turbo-16k"
+
+EASYCOMPLETION_API_KEY = os.getenv("OPENAI_API_KEY")
+if EASYCOMPLETION_API_KEY is None:
+    EASYCOMPLETION_API_KEY = os.getenv("EASYCOMPLETION_API_KEY")
+
+EASYCOMPLETION_API_ENDPOINT = os.getenv("EASYCOMPLETION_API_ENDPOINT") or "https://api.openai.com/v1"
+
+DEBUG = os.environ.get("EASYCOMPLETION_DEBUG") == "true" or os.environ.get("EASYCOMPLETION_DEBUG") == "True"
+
+DEFAULT_CHUNK_LENGTH = 4096 * 3 / 4  # 3/4ths of the context window size
