41540e55e107b036b2a640e0d032ce6416b1bdf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-08 04:14:19 +0000

    Add package jlogr/0.1.205/jlogr-0.1.205-cp39-abi3-manylinux_2_34_x86_64.whl

---
+++ packages/jlogr/jlogr-0.1.205-cp39-abi3-manylinux_2_34_x86_64.whl/jlogr/jlogr.pyi
@@ -0,0 +1,24 @@
+from ..jlogr import (
+        info as _info,
+        debug as _debug,
+        warning as _warning,
+        error as _error,
+        parse_list_of_logs as _parse_list_of_logs
+        )
+
+from typing import Optional
+
+__all__ = ["info", "debug", "warning", "error", "parse_list_of_logs"]
+
+def info(message: str) -> None:
+    ...
+
+def debug(message: str) -> None: ...
+
+def warning(message: str) -> None: ...
+
+def error(message: str) -> None: ...
+
+def parse_list_of_logs(
+        logs: list[tuple[str, str, Optional[str], Optional[str], Optional[str]]]
+        ) -> None: ...
