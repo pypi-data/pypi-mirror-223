72cbaff0e80a4e224c5c0532b7e030998166fa04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-08 04:12:21 +0000

    Add package nonebot-plugin-muteme/0.0.3b0/nonebot-plugin-muteme-0.0.3b0.tar.gz

---
+++ packages/nonebot-plugin-muteme/nonebot-plugin-muteme-0.0.3b0.tar.gz/nonebot-plugin-muteme-0.0.3b0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="nonebot-plugin-muteme",
+    version="0.0.3-beta",
+    packages=["nonebot_plugin_muteme"],
+    install_requires=["nonebot2","nonebot-adapter-onebot"],
+    description="高仿@能干辉的muteme,我禁我自己",
+    author="NightWind",
+    author_email="2125714976@qq.com",
+    url="https://github.com/XTxiaoting14332/nonebot-plugin-muteme",
+)
