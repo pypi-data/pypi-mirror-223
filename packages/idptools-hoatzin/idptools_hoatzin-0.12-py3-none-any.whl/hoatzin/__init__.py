9f98cc385d13e2d99f1fe30b9d7b76bc1a3ca44d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-08 04:12:51 +0000

    Add package idptools-hoatzin/0.12/idptools_hoatzin-0.12-py3-none-any.whl

---
+++ packages/idptools-hoatzin/idptools_hoatzin-0.12-py3-none-any.whl/hoatzin/__init__.py
@@ -0,0 +1,12 @@
+"""A Python package for DNA-based evolution of protien sequences. Holistic apprOach for SequAnces To underZtand evolutIoN."""
+
+# Add imports here
+from .evolve import *
+
+import os
+import sys
+
+# ------------------------------------------------------------
+from ._version import __version__
+
+
