5e917c9dad30fb044046f9335cf23300232a3a0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-08 03:10:31 +0000

    Add package mercury/2.3.2/mercury-2.3.2.tar.gz

---
+++ packages/mercury/mercury-2.3.2.tar.gz/mercury-2.3.2/mercury/apps/accounts/views/utils.py
@@ -0,0 +1,46 @@
+import os
+import uuid
+
+from django.template.defaultfilters import slugify
+
+
+PLAN_KEY = "plan"
+PLAN_STARTER = "starter"
+PLAN_PRO = "pro"
+PLAN_BUSINESS = "business"
+
+IDLE_TIME = {
+    PLAN_STARTER: 5 * 60, 
+    PLAN_PRO: 30 * 60,
+    PLAN_BUSINESS: 60 * 60
+}
+
+
+def is_cloud_version():
+    return os.environ.get("MERCURY_CLOUD", "0") == "1"
+
+
+def get_idle_time(owner):
+    if not is_cloud_version():
+        return 24 * 60 * 60
+    plan = owner.get(PLAN_KEY, PLAN_STARTER)
+    return IDLE_TIME.get(plan, 5 * 60)
+
+
+def get_max_run_time(owner):
+    return get_idle_time(owner)
+
+
+def some_random_slug():
+    h = uuid.uuid4().hex.replace("-", "")
+    return h[:8]
+
+
+def get_slug(slug, title):
+    new_slug = slugify(slug)
+    if new_slug == "":
+        new_slug = slugify(title)
+    if new_slug == "":
+        new_slug = some_random_slug()
+    return new_slug
+
