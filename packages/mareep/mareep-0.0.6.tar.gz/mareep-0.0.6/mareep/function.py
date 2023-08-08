0706386b8cb6d6eb46d3793ee20e8c4466ebc389
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-08 14:09:15 +0000

    Add package mareep/0.0.6/mareep-0.0.6.tar.gz

---
+++ packages/mareep/mareep-0.0.6.tar.gz/mareep-0.0.6/mareep/function.py
@@ -0,0 +1,55 @@
+from pathlib import Path
+from typing import *
+
+from typer import Argument, Option, Context
+
+from .app import app
+from .renderer import Renderer
+
+
+@app.command(
+    context_settings={"allow_extra_args": True, "ignore_unknown_options": True},
+)
+def render(
+    ctx: Context,
+    template_path: Path = Argument(..., help="Template path"),
+    output_path: Path = Argument(..., help="Output path"),
+
+    extensions: List[str] = Option(["j2", "jinja2", "jinja"], help="List of templete extensions"),
+    case_sensitive: bool = Option(True, help="Case sensitive"),
+    
+    use_env: bool = Option(False, help="Use env variables"),
+    env_prefix: str = Option("", help="Env prefix"),
+    
+    vars_path: Path = Option(None, help="Data path"),
+    section: str = Option(None, help="Vars sections"),
+):
+    cli_data = {}
+    key = None
+    val = None
+    for extra_arg in ctx.args:
+        if extra_arg.startswith("--d"):
+            if val is not None:
+                cli_data[key] = val
+
+            key = extra_arg[3:]
+            val = None
+            continue
+
+        val = f"{val} {extra_arg}" if val is not None else extra_arg
+
+    if key is not None and val is not None:
+        cli_data[key] = val
+
+    renderer = Renderer(
+        template_path=template_path,
+        extensions=extensions,
+        case_sensitive=case_sensitive,
+        use_env=use_env,
+        env_prefix=env_prefix,
+        vars_path=vars_path,
+        section=section,
+        output_path=output_path,
+    )
+
+    return renderer.render(**cli_data)
---
+++ packages/mareep/mareep-0.0.6.tar.gz/mareep-0.0.6/mareep/renderer.py
@@ -0,0 +1,86 @@
+import os
+import re
+from pathlib import Path
+from typing import *
+
+from jinja2 import Template
+from mousse import Dataclass
+from typer import progressbar
+
+from .loaders import loaders
+
+__all__ = ["Renderer"]
+
+
+class Renderer(Dataclass):
+    template_path: Path
+    extensions: List[str] = ["j2", "jinja2", "jinja"]
+
+    case_sensitive: bool = True
+
+    use_env: bool = False
+    env_prefix: str = ""
+    
+    vars_path: Path = None
+    section: str = None
+
+    output_path: Path
+
+    def render(self, **kwargs):
+        assert self.template_path.exists(), f"Invalid path: {self.template_path}"
+
+        template_files = []
+        if self.template_path.is_dir():
+            template_files = []
+            for extension in self.extensions:
+                template_files += [
+                    file for file in self.template_path.rglob(f"*.{extension}")
+                ]
+        else:
+            template_files = [self.template_path]
+
+        env = {}
+        if self.use_env:
+            for key, val in os.environ.items():
+                if not self.case_sensitive:
+                    key = key.lower()
+                
+                if key.startswith(self.env_prefix):
+                    key = key[len(self.env_prefix):]
+                    env[key] = val
+
+        if self.vars_path is not None:
+            assert self.vars_path.exists(), f"Invalid path: {self.vars_path}"
+            data_type = self.vars_path.suffix[1:]
+            assert data_type in loaders, f"Invalid data file: {self.vars_path}"
+
+            data = loaders[data_type].load(self.vars_path)
+            if self.section is not None:
+                data = data[self.section]
+            
+            if not self.case_sensitive:
+                data = {key.lower(): val for key, val in data.items()}
+
+            env.update(data)
+
+        if not self.case_sensitive:
+            kwargs = {key.lower(): val for key, val in kwargs.items()}
+
+        env.update(kwargs)
+
+        with progressbar(template_files) as progress:
+            for template_file in progress:
+                with open(template_file) as fin:
+                    template: Template = Template(fin.read())
+
+                if template_file.suffix[1:] in self.extensions:
+                    output_file: Path = self.output_path / template_file.stem
+                else:
+                    output_file: Path = self.output_path / template_file.name
+
+                output_file.parent.mkdir(parents=True, exist_ok=True)
+
+                with open(output_file, "w") as fout:
+                    content = template.render(env).strip() + "\n"
+                    content = re.sub("(?<=\n\n)\n+", "", content)
+                    fout.write(content)
