72ab9e3db6ca972a811b3f9d2c40931c48b833e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-08 05:10:54 +0000

    Add package deltares-coastalhazardstoolkit/0.2.2/deltares_coastalhazardstoolkit-0.2.2-py3-none-any.whl

---
+++ packages/deltares-coastalhazardstoolkit/deltares_coastalhazardstoolkit-0.2.2-py3-none-any.whl/cht/misc/argo.py
@@ -0,0 +1,31 @@
+from hera.workflows import Workflow, WorkflowStatus, Task
+from hera.workflows.models import WorkflowTemplateRef
+from hera.shared import GlobalConfig
+import time
+
+class Argo:
+
+    def __init__(self, host, workflow_name):
+        self.host = host
+        self.workflow_name = workflow_name
+
+    def submit_job(self, **kwargs):
+
+        GlobalConfig.namespace = "argo"
+        GlobalConfig.host = self.host
+        GlobalConfig.verify_ssl = False
+
+        wt_ref = WorkflowTemplateRef(name=self.workflow_name,
+                                     cluster_scope=False)
+
+        w = Workflow(
+            generate_name=self.workflow_name+"-",
+            workflow_template_ref=wt_ref,
+            arguments=kwargs
+        )
+
+        w.create()
+        time.sleep(1) 
+        w.wait()
+
+        return w
