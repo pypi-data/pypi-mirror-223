54b8e23a60b83bd5f091f2c5fb1f9bc86aabb10e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-08 05:11:47 +0000

    Add package astx/0.2.1/astx-0.2.1-py3-none-any.whl

---
+++ packages/astx/astx-0.2.1-py3-none-any.whl/astx/blocks.py
@@ -0,0 +1,41 @@
+from __future__ import annotations
+from typing import List
+
+from astx.base import AST, Expr, ASTKind
+
+
+class Block(AST):
+    """The AST tree."""
+
+    nodes: List[AST]
+    position: int = 0
+
+    def __init__(self):
+        self.nodes: List[Expr] = []
+        self.position: int = 0
+
+    def append(self, value: AST):
+        self.nodes.append(value)
+
+    def __iter__(self) -> Block:
+        return self
+
+    def __next__(self) -> AST:
+        if self.position >= len(self.nodes):
+            raise StopIteration()
+
+        i = self.position
+        self.position += 1
+        return self.nodes[i]
+
+
+class Module(Block):
+    """AST main expression class."""
+
+    name: str
+
+    def __init__(self, name: str = "main") -> None:
+        """Initialize the AST instance."""
+        super().__init__()
+        self.name = name
+        self.kind = ASTKind.ModuleKind
