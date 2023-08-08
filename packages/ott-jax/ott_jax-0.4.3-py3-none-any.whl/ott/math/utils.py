991acef16a608e41b144422f978010488c80fa3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-08 03:10:29 +0000

    Add package ott-jax/0.4.3/ott_jax-0.4.3-py3-none-any.whl

---
+++ packages/ott-jax/ott_jax-0.4.3-py3-none-any.whl/ott/math/utils.py
@@ -0,0 +1,155 @@
+# Copyright OTT-JAX
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import functools
+from typing import TYPE_CHECKING, Optional
+
+import jax
+import jax.numpy as jnp
+import jax.scipy as jsp
+
+if TYPE_CHECKING:
+  from ott.geometry import costs
+
+__all__ = [
+    "safe_log", "kl", "gen_kl", "js", "logsumexp", "softmin",
+    "barycentric_projection"
+]
+
+
+def safe_log(  # noqa: D103
+    x: jnp.ndarray,
+    *,
+    eps: Optional[float] = None
+) -> jnp.ndarray:
+  if eps is None:
+    eps = jnp.finfo(x.dtype).tiny
+  return jnp.where(x > 0., jnp.log(x), jnp.log(eps))
+
+
+# TODO(michalk8): add axis argument
+def kl(p: jnp.ndarray, q: jnp.ndarray) -> float:
+  """Kullback-Leibler divergence."""
+  return jnp.vdot(p, (safe_log(p) - safe_log(q)))
+
+
+def gen_kl(p: jnp.ndarray, q: jnp.ndarray) -> float:
+  """Generalized Kullback-Leibler divergence."""
+  return jnp.vdot(p, (safe_log(p) - safe_log(q))) + jnp.sum(q) - jnp.sum(p)
+
+
+# TODO(michalk8): add axis argument
+def js(p: jnp.ndarray, q: jnp.ndarray, c: float = 0.5) -> float:
+  """Jensen-Shannon divergence."""
+  return c * (kl(p, q) + kl(q, p))
+
+
+@functools.partial(jax.custom_jvp, nondiff_argnums=(1, 2, 4))
+def logsumexp(  # noqa: D103
+    mat, axis=None, keepdims=False, b=None, return_sign=False
+):
+  return jax.scipy.special.logsumexp(
+      mat, axis=axis, keepdims=keepdims, b=b, return_sign=return_sign
+  )
+
+
+@logsumexp.defjvp
+def logsumexp_jvp(axis, keepdims, return_sign, primals, tangents):
+  """Custom derivative rule for lse that does not blow up with -inf.
+
+  This logsumexp implementation uses the standard jax one in forward mode but
+  implements a custom rule to differentiate. Given the preference of jax for
+  jvp over vjp, and the fact that this is a simple linear rule, jvp is used.
+  This custom differentiation address issues when the output of lse is
+  -inf (which corresponds to the case where all inputs in a slice are -inf,
+  which happens typically when ``a`` or ``b`` weight vectors have zeros.)
+
+  Although both exp(lse) and its derivative should be 0, automatic
+  differentiation returns a NaN derivative because of a -inf - (-inf) operation
+  appearing in the definition of centered_exp below. This is corrected in the
+  implementation below.
+
+  Args:
+    axis: argument from original logsumexp
+    keepdims: argument from original logsumexp
+    return_sign: argument from original logsumexp
+    primals: mat and b, the two arguments against which we differentiate.
+    tangents: of same size as mat and b.
+
+  Returns:
+    original primal outputs + their tangent.
+  """  # noqa: D401
+  mat, b = primals
+  tan_mat, tan_b = tangents
+  lse = logsumexp(mat, axis, keepdims, b, return_sign)
+  if return_sign:
+    lse, sign = lse
+  lse = jnp.where(jnp.isfinite(lse), lse, 0.0)
+  centered_exp = jnp.exp(mat - jnp.expand_dims(lse, axis=axis))
+
+  if b is None:
+    res = jnp.sum(centered_exp * tan_mat, axis=axis, keepdims=keepdims)
+  else:
+    res = jnp.sum(b * centered_exp * tan_mat, axis=axis, keepdims=keepdims)
+    res += jnp.sum(tan_b * centered_exp, axis=axis, keepdims=keepdims)
+  if return_sign:
+    return (lse, sign), (sign * res, jnp.zeros_like(sign))
+  return lse, res
+
+
+@functools.partial(jax.custom_vjp, nondiff_argnums=(2,))
+def softmin(
+    x: jnp.ndarray, gamma: float, axis: Optional[int] = None
+) -> jnp.ndarray:
+  r"""Soft-min operator.
+
+  Args:
+    x: Input data.
+    gamma: Smoothing parameter :math:`> 0`.
+    axis: Axis or axes over which to operate. If ``None``, use flattened input.
+
+  Returns:
+    The soft minimum.
+  """
+  return -gamma * jsp.special.logsumexp(x / -gamma, axis=axis)
+
+
+@functools.partial(jax.vmap, in_axes=[0, 0, None])
+def barycentric_projection(
+    matrix: jnp.ndarray, y: jnp.ndarray, cost_fn: "costs.CostFn"
+) -> jnp.ndarray:
+  """Compute the barycentric projection of a matrix.
+
+  Args:
+    matrix: a matrix of shape (n, m)
+    y: a vector of shape (m,)
+    cost_fn: a CostFn instance.
+
+  Returns:
+    a vector of shape (n,) containing the barycentric projection of matrix.
+  """
+  return jax.vmap(
+      lambda m, y: cost_fn.barycenter(m, y)[0], in_axes=[0, None]
+  )(matrix, y)
+
+
+softmin.defvjp(
+    lambda x, gamma, axis: (softmin(x, gamma, axis), (x / -gamma, axis)),
+    lambda axis, res, g: (
+        jnp.where(
+            jnp.isinf(res[0]), 0.0,
+            jax.nn.softmax(res[0], axis=axis) *
+            (g if axis is None else jnp.expand_dims(g, axis=axis))
+        ), None
+    )
+)
---
+++ packages/ott-jax/ott_jax-0.4.3-py3-none-any.whl/ott/utils.py
@@ -0,0 +1,160 @@
+# Copyright OTT-JAX
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import dataclasses
+import functools
+import warnings
+from typing import Any, Callable, NamedTuple, Optional, Tuple
+
+import jax
+import jax.numpy as jnp
+import numpy as np
+
+__all__ = [
+    "register_pytree_node", "deprecate", "is_jax_array", "default_prng_key",
+    "default_progress_fn"
+]
+
+
+def register_pytree_node(cls: type) -> type:
+  """Register dataclasses as pytree_nodes."""
+  cls = dataclasses.dataclass()(cls)
+  flatten = lambda obj: jax.tree_flatten(dataclasses.asdict(obj))
+  unflatten = lambda d, children: cls(**d.unflatten(children))
+  jax.tree_util.register_pytree_node(cls, flatten, unflatten)
+  return cls
+
+
+def deprecate(  # noqa: D103
+    *,
+    version: Optional[str] = None,
+    alt: Optional[str] = None,
+    func: Optional[Callable[[Any], Any]] = None
+) -> Callable[[Any], Any]:
+
+  def wrapper(*args: Any, **kwargs: Any) -> Any:
+    warnings.warn(msg, category=DeprecationWarning, stacklevel=2)
+    return func(*args, **kwargs)
+
+  if func is None:
+    return lambda fn: deprecate(version=version, alt=alt, func=fn)
+
+  msg = f"`{func.__name__}` will be removed in the "
+  msg += ("next" if version is None else f"`ott-jax=={version}`") + " release."
+  if alt:
+    msg += " " + alt
+
+  return functools.wraps(func)(wrapper)
+
+
+def is_jax_array(obj: Any) -> bool:
+  """Check if an object is a Jax array."""
+  if hasattr(jax, "Array"):
+    # https://jax.readthedocs.io/en/latest/jax_array_migration.html
+    return isinstance(obj, jax.Array)
+  return isinstance(obj, jnp.DeviceArray)
+
+
+def default_prng_key(
+    rng: Optional[jax.random.PRNGKeyArray] = None,
+) -> jax.random.PRNGKeyArray:
+  """Get the default PRNG key.
+
+  Args:
+    rng: PRNG key.
+
+  Returns:
+    If ``rng = None``, returns the default PRNG key.
+    Otherwise, it returns the unmodified ``rng`` key.
+  """
+  return jax.random.PRNGKey(0) if rng is None else rng
+
+
+def default_progress_fn(
+    status: Tuple[np.ndarray, np.ndarray, np.ndarray, NamedTuple], *args: Any
+) -> None:
+  """Callback function that reports progress of
+  :class:`~ott.solvers.linear.sinkhorn.Sinkhorn` by printing to the console.
+
+  It prints the progress only when the error is computed, that is every
+  :attr:`~ott.solvers.linear.sinkhorn.Sinkhorn.inner_iterations`.
+
+  Note:
+    This function is called during solver iterations via
+    :func:`~jax.experimental.host_callback.id_tap` so the solver execution
+    remains :func:`jittable <jax.jit>`.
+
+  Args:
+    status: status consisting of:
+
+      - the current iteration number
+      - the number of inner iterations after which the error is computed
+      - the total number of iterations
+      - the current :class:`~ott.solvers.linear.sinkhorn.SinkhornState`
+
+    args: unused, see :mod:`jax.experimental.host_callback`.
+
+  Returns:
+    Nothing, just prints.
+
+  Examples:
+    If instead of printing you want to report progress using a progress bar such
+    as `tqdm <https://tqdm.github.io>`_, then simply provide a slightly modified
+    version of this callback, for instance:
+
+    .. code-block:: python
+
+      import jax
+      import numpy as np
+      from tqdm import tqdm
+
+      from ott.problems.linear import linear_problem
+      from ott.solvers.linear import sinkhorn
+
+      def progress_fn(status, *args):
+        iteration, inner_iterations, total_iter, state = status
+        iteration = int(iteration) + 1
+        inner_iterations = int(inner_iterations)
+        total_iter = int(total_iter)
+        errors = np.asarray(state.errors).ravel()
+
+        # Avoid reporting error on each iteration,
+        # because errors are only computed every `inner_iterations`.
+        if iteration % inner_iterations == 0:
+          error_idx = max(0, iteration // inner_iterations - 1)
+          error = errors[error_idx]
+
+          pbar.set_postfix_str(f"error: {error:0.6e}")
+          pbar.total = total_iter // inner_iterations
+          pbar.update()
+
+      prob = linear_problem.LinearProblem(...)
+      solver = sinkhorn.Sinkhorn(progress_fn=progress_fn)
+
+      with tqdm() as pbar:
+        out_sink = jax.jit(solver)(prob)
+  """  # noqa: D205
+  # Convert arguments.
+  iteration, inner_iterations, total_iter, state = status
+  iteration = int(iteration) + 1
+  inner_iterations = int(inner_iterations)
+  total_iter = int(total_iter)
+  errors = np.array(state.errors).ravel()
+
+  # Avoid reporting error on each iteration,
+  # because errors are only computed every `inner_iterations`.
+  if iteration % inner_iterations == 0:
+    error_idx = max(0, iteration // inner_iterations - 1)
+    error = errors[error_idx]
+
+    print(f"{iteration} / {total_iter} -- {error}")  # noqa: T201
