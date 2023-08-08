f581c14694333003d6b8609a8e0407ad3cb82a72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-08 14:09:30 +0000

    Add package kafka-schema-registry-admin/0.2.6/kafka_schema_registry_admin-0.2.6-py3-none-any.whl

---
+++ packages/kafka-schema-registry-admin/kafka_schema_registry_admin-0.2.6-py3-none-any.whl/kafka_schema_registry_admin/__init__.py
@@ -0,0 +1,8 @@
+# SPDX-License-Identifier: LGPL-3.0-only
+# Copyright 2021 John Mille <john@ews-network.net>
+
+"""Top-level package for Kafka schema registry admin."""
+
+__author__ = """JohnPreston"""
+__email__ = "john@ews-network.net"
+__version__ = "0.2.6"
