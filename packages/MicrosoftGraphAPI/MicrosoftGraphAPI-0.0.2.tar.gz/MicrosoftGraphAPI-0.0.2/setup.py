7681d54794e2d0665abd017bbd3ee59ea67aece3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-08 05:10:44 +0000

    Add package MicrosoftGraphAPI/0.0.2/MicrosoftGraphAPI-0.0.2.tar.gz

---
+++ packages/MicrosoftGraphAPI/MicrosoftGraphAPI-0.0.2.tar.gz/MicrosoftGraphAPI-0.0.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    # metadata here
+    name="MicrosoftGraphAPI",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    version="0.0.2",
+    author="Ashlin Darius Govindasamy",
+    author_email="adg@adgstudios.co.za",
+    url="https://www.adgstudios.co.za",
+    description="A Python package to interact with the Microsoft Graph API",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    # license here
+    license='MIT',
+    # modules here
+    install_requires=[
+        'requests',
+        'pandas',
+        'msal',
+    ],
+)
