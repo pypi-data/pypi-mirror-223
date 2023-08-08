dc7f40e848e81d07f50ab0c3b78a0b042bf17831
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-08 04:11:40 +0000

    Add package queue-local/0.0.3/queue-local-0.0.3.tar.gz

---
+++ packages/queue-local/queue-local-0.0.3.tar.gz/queue-local-0.0.3/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+setuptools.setup(
+     name='queue-local',
+     version='0.0.3',  # https://pypi.org/project/queue-local-python-package/
+     author="Circles",
+     author_email="info@circles.life",
+     description="PyPI Package for Circles queue Local Python",
+     long_description="This is a package for sharing common Queue function used in different repositories",
+     long_description_content_type="text/markdown",
+     url="https://github.com/circles",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: Other/Proprietary License",
+         "Operating System :: OS Independent",
+     ],
+ )
