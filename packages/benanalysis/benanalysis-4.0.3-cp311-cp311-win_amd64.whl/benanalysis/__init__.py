47561703473eab546a13b94353de88c0deaeb1cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-08 04:13:19 +0000

    Add package benanalysis/4.0.3/benanalysis-4.0.3-cp311-cp311-win_amd64.whl

---
+++ packages/benanalysis/benanalysis-4.0.3-cp311-cp311-win_amd64.whl/benanalysis/__init__.py
@@ -0,0 +1,22 @@
+
+
+""""""# start delvewheel patch
+def _delvewheel_patch_1_4_0():
+    import os
+    libs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'benanalysis.libs'))
+    if os.path.isdir(libs_dir):
+        os.add_dll_directory(libs_dir)
+
+
+_delvewheel_patch_1_4_0()
+del _delvewheel_patch_1_4_0
+# end delvewheel patch
+
+#
+# @file __init__.py
+# @author Markus Führer
+# @date 5 May 2023
+# @copyright Copyright © 2023 Bentham Instruments Ltd. All Rights Reserved.
+#
+
+from benanalysis._benpy_core import *
