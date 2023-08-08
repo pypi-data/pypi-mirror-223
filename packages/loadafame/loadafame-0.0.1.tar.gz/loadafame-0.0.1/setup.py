3d773745d0c9c083c6ee8123d896b4c202110fb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-08 15:09:47 +0000

    Add package loadafame/0.0.1/loadafame-0.0.1.tar.gz

---
+++ packages/loadafame/loadafame-0.0.1.tar.gz/loadafame-0.0.1/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="loadafame",
+    version="0.0.1",
+    author="Theodore Kaczynski",
+    url="",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+)
