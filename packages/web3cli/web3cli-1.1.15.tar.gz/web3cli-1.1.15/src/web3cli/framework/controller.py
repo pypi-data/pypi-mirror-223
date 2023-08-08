cf5866ba0140667e2d34e1d992ad93803cdd1906
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-08 03:10:38 +0000

    Add package web3cli/1.1.15/web3cli-1.1.15.tar.gz

---
+++ packages/web3cli/web3cli-1.1.15.tar.gz/web3cli-1.1.15/src/web3cli/framework/controller.py
@@ -0,0 +1,22 @@
+from cement import Controller as CementController
+
+from web3cli.framework.app import App
+
+
+class Controller(CementController):
+    """Extend this class to define new commands"""
+
+    def _default(self) -> None:
+        """Default action if no sub-command is passed."""
+        self.app.args.print_help()
+
+    def __init__(self) -> None:
+        """Make self.app attributes discoverable by IDEs. See
+        https://github.com/datafolklabs/cement/issues/599
+        for more details"""
+        super().__init__()
+        self.app = self.get_app()
+
+    def get_app(self) -> App:
+        """Get app object, with typing"""
+        return self.app
