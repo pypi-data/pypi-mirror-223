25f0b0bd9b22d968a0093196177e581efa2834a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-08 04:11:29 +0000

    Add package mykit/8.1.0b1/mykit-8.1.0b1-py3-none-any.whl

---
+++ packages/mykit/mykit-8.1.0b1-py3-none-any.whl/mykit/ghactions/eLog.py
@@ -0,0 +1,36 @@
+import subprocess as _sp
+
+from mykit.kit.color import (
+    Hex as _Hex,
+    Colored as _Colored
+)
+
+
+def _logger(level, color, msg):
+    _sp.run(['echo', "[$(date +'%H:%M:%S')] " + _Colored(level, color) + ': ' + msg])
+
+
+class eL:
+    """
+    eL (`echo` Log): A simple logger using `echo` function,
+    intended for use within GitHub Action virtual machines.
+    Inspired by `mykit.kit.pLog.pL`.
+    """
+
+    def group(name:str, /) -> None:
+        _sp.run(['echo', f'::group::{name}'])
+
+    def endgroup() -> None:
+        _sp.run(['echo', '::endgroup::'])
+
+    def debug(msg:str, /) -> None:
+        _logger('DEBUG', _Hex.WHEAT, msg)
+
+    def info(msg:str, /) -> None:
+        _logger('INFO', _Hex.BLUE_GRAY, msg)
+
+    def warning(msg:str, /) -> None:
+        _logger('WARNING', _Hex.DARK_ORANGE, msg)
+
+    def error(msg:str, /) -> None:
+        _logger('ERROR', _Hex.SCARLET, msg)
