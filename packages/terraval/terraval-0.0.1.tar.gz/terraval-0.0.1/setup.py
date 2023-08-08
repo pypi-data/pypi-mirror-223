305a7a6e168939ae29ee436fb78d0ee9bcd88f51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-08 03:10:31 +0000

    Add package terraval/0.0.1/terraval-0.0.1.tar.gz

---
+++ packages/terraval/terraval-0.0.1.tar.gz/terraval-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='terraval',
+    version='0.0.1',
+    author='M. Vidyaa Shankar',
+    description='A Python module for Terraform validation',
+    packages=find_packages(),
+)
