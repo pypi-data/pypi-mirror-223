9f4a8c969402410ce9295aa3632d0307db66d211
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-08 04:13:08 +0000

    Add package gget/0.27.9/gget-0.27.9-py3-none-any.whl

---
+++ packages/gget/gget-0.27.9-py3-none-any.whl/gget/__init__.py
@@ -0,0 +1,27 @@
+from .gget_ref import ref
+from .gget_search import search
+from .gget_info import info
+from .gget_seq import seq
+from .gget_muscle import muscle
+from .gget_blast import blast
+from .gget_blat import blat
+from .gget_enrichr import enrichr
+from .gget_archs4 import archs4
+from .gget_alphafold import alphafold
+from .gget_setup import setup
+from .gget_pdb import pdb
+from .gget_gpt import gpt
+from .gget_cellxgene import cellxgene
+
+import logging
+logging.basicConfig(
+    format="%(asctime)s %(levelname)s %(message)s",
+    level=logging.INFO,
+    datefmt="%c",
+)
+# Mute numexpr threads info
+logging.getLogger("numexpr").setLevel(logging.WARNING)
+
+__version__ = "0.27.9"
+__author__ = "Laura Luebbert"
+__email__ = "lauraluebbert@caltech.edu"
