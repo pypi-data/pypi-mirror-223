5dc2b53ca1806677c9af2155e348cb454b6ae948
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-08 04:13:51 +0000

    Add package test-adding/0.1.1/test-adding-0.1.1.tar.gz

---
+++ packages/test-adding/test-adding-0.1.1.tar.gz/test-adding-0.1.1/adding/addition.py
@@ -0,0 +1,7 @@
+def add_numbers(a, b):
+    print("Sum:", a + b)
+
+if __name__ == "__main__":
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    add_numbers(num1, num2)
