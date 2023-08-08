dae4b94c2f93b8833490d43f1f2e379317e0196d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-08 14:09:35 +0000

    Add package hrequests/0.5.1/hrequests-0.5.1-py3-none-any.whl

---
+++ packages/hrequests/hrequests-0.5.1-py3-none-any.whl/hrequests/__init__.py
@@ -0,0 +1,8 @@
+from .response import Response, ProcessResponse
+from .session import Session, TLSSession, chrome, firefox, opera
+from .reqs import *
+from .browser import BrowserSession, render
+from .parser import HTML
+from .playwright_mock import PlaywrightMock
+from .__version__ import __version__
+from .__version__ import __author__
