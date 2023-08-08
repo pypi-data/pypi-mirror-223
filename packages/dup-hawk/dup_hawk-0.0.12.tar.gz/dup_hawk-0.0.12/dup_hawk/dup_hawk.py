c22f22ffb101151c2cc2f5eb62bba7ea800c4c7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-08 14:09:12 +0000

    Add package dup-hawk/0.0.12/dup_hawk-0.0.12.tar.gz

---
+++ packages/dup-hawk/dup_hawk-0.0.12.tar.gz/dup_hawk-0.0.12/dup_hawk/dup_hawk.py
@@ -0,0 +1,178 @@
+import logging as log
+import os
+import sys
+
+# from .__version__ import __title__, __version__
+from importlib.metadata import version
+from typing import List
+
+import click
+import numpy as np
+import openai
+import pandas as pd
+from dotenv import load_dotenv
+from openai.embeddings_utils import get_embedding
+from scipy.spatial.distance import cdist
+
+from dup_hawk.github import Github
+
+TITLE = "dup_hawk"
+__version__ = version(__package__)
+
+load_dotenv()
+
+EMBEDDING_MODEL = "text-embedding-ada-002"
+SIMILARITY_THRESHOLD_DEFAULT = 0.85
+
+# We aren't exactly figuring out how many tokens each issue has, but instead sort of "dumb"
+# Checking that their are less than 1,000 words in an issue.
+PSEUDO_TOKEN_LIMIT = 1000
+
+log.basicConfig(level=log.INFO)
+
+
+@click.command()
+@click.version_option(
+    version=__version__,
+    prog_name=TITLE,
+)
+@click.option(
+    "--git-repo-url",
+    default=None,
+    required=True,
+    help="GitHub repo URL that you want to mark duplicate issues for.",
+)
+@click.option(
+    "--git-pat-token",
+    default=os.getenv("GITHUB_PAT_TOKEN"),
+    help="GitHub PAT token of the github user that will be marking the issues.",
+)
+@click.option(
+    "--openai-api-key",
+    default=os.getenv("OPENAI_API_KEY"),
+    help="OpenAI API key for generating embeddings.",
+)
+@click.option(
+    "--similarity-threshold",
+    default=SIMILARITY_THRESHOLD_DEFAULT,
+    help="The threshold for how similar issues need to be to be marked as duplicates.",
+)
+@click.option(
+    "--debug/--no-debug",
+    default=False,
+    help="Turn on debug logging.",
+)
+def dup_hawk_click(
+    git_repo_url: str,
+    git_pat_token: str,
+    openai_api_key: str,
+    similarity_threshold: float,
+    debug: bool,
+):
+    dup_hawk(git_repo_url, git_pat_token, openai_api_key, similarity_threshold, debug)
+
+
+def dup_hawk(
+    git_repo_url: str,
+    git_pat_token: str,
+    openai_api_key: str,
+    similarity_threshold: float,
+    debug: bool = False,
+):
+    if debug:
+        log.basicConfig(level=log.DEBUG)
+    openai.key = openai_api_key
+    g: Github = Github(git_pat_token, debug=debug)
+    log.info(f"Getting issues from {git_repo_url}")
+    repo_issues: List[dict] = g.get_issues(git_repo_url, state="open")
+    log.info(f"Found {len(repo_issues)} issues, converting to embeddings")
+    df, dist_df = create_embeddings_and_dfs(repo_issues, similarity_threshold)
+    log.info(f"Finding duplicates")
+    duplicates_dict = mark_duplicates_from_dfs(df, dist_df)
+    log.info(f"Tagging duplicates")
+    for github_issue_number in duplicates_dict:
+        for duplicate_number in duplicates_dict[github_issue_number]:
+            log.debug(
+                f"Tagging {duplicate_number} as a duplicate of {github_issue_number}"
+            )
+            g.tag_issue(
+                github_issue_number, git_repo_url, [f"ai-dup-{duplicate_number}"]
+            )
+    log.info(f"Done!")
+
+
+def create_embeddings_and_dfs(
+    repo_issues: List[dict],
+    similarity_threshold: float = SIMILARITY_THRESHOLD_DEFAULT,
+) -> tuple[pd.DataFrame, pd.DataFrame]:
+    """
+    Reaches out to the OpenAI API to get embeddings for each issue and then creates
+    a dataframe with the embeddings and a dataframe with the distances between each
+
+    Args:
+        repo_issues (List[dict]): A list of dicts from the GitHub API
+
+    Returns:
+        tuple[pd.DataFrame, pd.DataFrame]: _description_
+    """
+    df: pd.DataFrame = pd.DataFrame(repo_issues)
+    # Combine title and body of the issue
+    df["text"] = df["title"] + " " + df["body"]
+    df.dropna(subset=["text"], inplace=True)
+
+    # Create a mask for texts with token count exceeding the limit
+    mask = df["text"].apply(lambda x: len(x.split()) > PSEUDO_TOKEN_LIMIT)
+
+    # Print the "number" column for rows where the mask is True
+    filtered_numbers = df[mask]["number"].tolist()
+    log.info(f"Filtered out issues with too many tokens: {filtered_numbers}")
+    df = df[~mask]
+
+    # Get embeddings for each issue
+    log.info("Creating embeddings, please wait...")
+    df["embeddings"] = df["text"].apply(lambda x: get_embedding(x, EMBEDDING_MODEL))
+
+    log.info("Creating distance matrix...")
+    distances = cdist(list(df["embeddings"]), list(df["embeddings"]), metric="cosine")
+    distances[distances > 1 - similarity_threshold] = np.nan
+    dist_df = pd.DataFrame(distances)
+    dist_df.index = df.index
+    dist_df.columns = df.index
+    return df, dist_df
+
+
+def mark_duplicates_from_dfs(df: pd.DataFrame, dist_df: pd.DataFrame) -> dict:
+    """
+    returns a dict where the keys are the issue ids and the value is a list of duplicate IDs
+
+    Args:
+        df (pd.DataFrame): the dataframe with the issues
+        dist_df (pd.DataFrame): the distance dataframe, which shows how "far away" issues are from each other
+
+    Returns:
+        dict: a dict where the keys are the issue ids and the value is a list of duplicate IDs
+    """
+    duplicates_dict = {}
+
+    # Find duplicates for each issue
+    for idx, row in df.iterrows():
+        log.debug(f"Issue: {row['title']}")
+        log.debug(f"Possible duplicates:")
+        duplicates = dist_df.loc[idx].dropna().index
+        duplicates_df = df.loc[duplicates]
+        log.debug(duplicates_df)
+        # Add id and corresponding duplicates to the dictionary
+        if len(duplicates_df) > 1:
+            duplicates_dict[row["number"]] = duplicates_df["number"].tolist()
+            duplicates_dict[row["number"]].remove(row["number"])
+        # This will only be a list of duplicates
+        # else:
+        #     duplicates_dict[row["number"]] = []
+    return duplicates_dict
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        dup_hawk_click.main["--help"]
+    else:
+        dup_hawk_click()
---
+++ packages/dup-hawk/dup_hawk-0.0.12.tar.gz/dup_hawk-0.0.12/dup_hawk/github.py
@@ -0,0 +1,87 @@
+import json
+import logging as log
+from typing import List, Optional
+from urllib.parse import urlparse
+
+import requests
+
+log.basicConfig(level=log.INFO)
+
+BASE_URL = "https://api.github.com/"
+X_GITHUB_API_VERSION = "2022-11-28"
+HEADERS = {
+    "Accept": "application/vnd.github+json",
+    "Authorization": "token ",
+    "X-GitHub-Api-Version": X_GITHUB_API_VERSION,
+}
+PER_PAGE = 100
+
+
+class Github:
+    def __init__(self, token: str, debug: bool = False):
+        self.token = token
+        self.headers = HEADERS
+        self.headers["Authorization"] = f"token {self.token}"
+        self.url = BASE_URL
+        if debug:
+            log.basicConfig(level=log.DEBUG)
+
+    def get_user(self) -> dict:
+        url = self.url + "user"
+        response = requests.get(url, headers=self.headers)
+        return response.json()
+
+    def get_repo(self, repo_url: str) -> dict:
+        owner, repo = self.get_owner_and_repo(repo_url)
+        url = self.url + f"repos/{owner}/{repo}"
+        response = requests.get(url, headers=self.headers)
+        return response.json()
+
+    def get_issues(self, repo_url: str, state: Optional[str] = "open") -> List[dict]:
+        """
+        Gets a list of issues for a given repo.
+
+        Args:
+            repo_url (str): The repo to get issues from
+            state (str, optional): Can be one of [open, closed, all]. Defaults to open.
+
+        Returns:
+            dict: _description_
+        """
+        owner, repo = self.get_owner_and_repo(repo_url)
+        url = self.url + f"repos/{owner}/{repo}/issues"
+        page_index = 1
+        results = []
+        while True:
+            params = {"state": state, "per_page": PER_PAGE, "page": page_index}
+            log.info(f"Getting page {page_index} of issues for {repo_url}")
+            log.debug(f"Params: {params}")
+            log.debug(f"URL: {url}")
+            response = requests.get(url, headers=self.headers, params=params)
+            if len(response.json()) == 0:
+                break
+            results.extend(response.json())
+            page_index += 1
+        return results
+
+    def tag_issue(self, issue_number: int, repo_url: str, labels: list[str]) -> dict:
+        owner, repo = self.get_owner_and_repo(repo_url)
+        url = self.url + f"repos/{owner}/{repo}/issues/{issue_number}/labels"
+        data = {"labels": labels}
+        response = requests.post(url, headers=self.headers, data=json.dumps(data))
+        if response.status_code > 399:
+            raise Exception(response.json())
+        return response.json()
+
+    def remove_labels(self, issue_id: int, repo_url: str):
+        owner, repo = self.get_owner_and_repo(repo_url)
+        url = self.url + f"repos/{owner}/{repo}/issues/{issue_id}/labels"
+        response = requests.delete(url, headers=self.headers)
+        return response.json()
+
+    @staticmethod
+    def get_owner_and_repo(url: str) -> tuple[str, str]:
+        parsed_url = urlparse(url)
+        path = parsed_url.path
+        owner, repo = path.strip("/").split("/")[0:2]
+        return owner, repo
