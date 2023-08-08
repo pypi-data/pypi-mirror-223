2a755c5156ccf18223c7260d5996f2a4e4789d31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-08 04:13:23 +0000

    Add package je-editor/0.0.128/je_editor-0.0.128-py3-none-any.whl

---
+++ packages/je-editor/je_editor-0.0.128-py3-none-any.whl/je_editor/pyside_ui/main_ui/menu/menu_bar/check_style_menu/build_check_style_menu.py
@@ -0,0 +1,44 @@
+from PySide6.QtGui import QAction
+from PySide6.QtWidgets import QMainWindow
+from yapf.yapflib.yapf_api import FormatCode
+
+from je_editor.utils.json_format.json_process import reformat_json
+
+
+def set_check_menu(ui_we_want_to_set: QMainWindow) -> None:
+    # Yapf code check
+    ui_we_want_to_set.check_menu.check_python_action = QAction("yapf")
+    ui_we_want_to_set.check_menu.check_python_action.setShortcut("Ctrl+y")
+    ui_we_want_to_set.check_menu.check_python_action.triggered.connect(
+        lambda: check_python_code(
+            ui_we_want_to_set
+        )
+    )
+    ui_we_want_to_set.check_menu.addAction(ui_we_want_to_set.check_menu.check_python_action)
+    # Reformat JSON
+    ui_we_want_to_set.check_menu.reformat_json_action = QAction("Reformat JSON")
+    ui_we_want_to_set.check_menu.reformat_json_action.setShortcut("Ctrl+j")
+    ui_we_want_to_set.check_menu.reformat_json_action.triggered.connect(
+        lambda: reformat_json_text(
+            ui_we_want_to_set
+        )
+    )
+    ui_we_want_to_set.check_menu.addAction(ui_we_want_to_set.check_menu.reformat_json_action)
+
+
+def check_python_code(ui_we_want_to_set) -> None:
+    code_text = ui_we_want_to_set.code_edit.toPlainText()
+    ui_we_want_to_set.code_result.setPlainText("")
+    format_code = FormatCode(
+        unformatted_source=code_text,
+        verify=True,
+        style_config="google"
+    )
+    if isinstance(format_code, tuple):
+        ui_we_want_to_set.code_edit.setPlainText(format_code[0])
+
+
+def reformat_json_text(ui_we_want_to_set) -> None:
+    code_text = ui_we_want_to_set.code_edit.toPlainText()
+    ui_we_want_to_set.code_result.setPlainText("")
+    ui_we_want_to_set.code_edit.setPlainText(reformat_json(code_text))
---
+++ packages/je-editor/je_editor-0.0.128-py3-none-any.whl/je_editor/pyside_ui/main_ui/menu/menu_bar/help_menu/build_help_menu.py
@@ -0,0 +1,39 @@
+import webbrowser
+
+from PySide6.QtGui import QAction
+from PySide6.QtWidgets import QMainWindow, QMessageBox
+
+
+def set_help_menu(ui_we_want_to_set: QMainWindow) -> None:
+    ui_we_want_to_set.help_menu .help_github_action = QAction("GitHub")
+    ui_we_want_to_set.help_menu .help_github_action.triggered.connect(
+        lambda: open_web_browser("https://github.com/Integrated-Testing-Environment/je_editor")
+    )
+    ui_we_want_to_set.help_menu.addAction(ui_we_want_to_set.help_menu .help_github_action)
+
+    ui_we_want_to_set.help_menu .help_doc_action = QAction("Doc")
+    ui_we_want_to_set.help_menu .help_doc_action.triggered.connect(
+        lambda: open_web_browser("https://je-editor.readthedocs.io/en/latest/")
+    )
+    ui_we_want_to_set.help_menu.addAction(ui_we_want_to_set.help_menu .help_doc_action)
+
+    ui_we_want_to_set.help_menu .help_about_action = QAction("About")
+    ui_we_want_to_set.help_menu .help_about_action.triggered.connect(
+        show_about
+    )
+    ui_we_want_to_set.help_menu.addAction(ui_we_want_to_set.help_menu.help_about_action)
+
+
+def open_web_browser(url: str):
+    webbrowser.open(url=url)
+
+
+def show_about():
+    message_box = QMessageBox()
+    message_box.setText(
+        """
+JEditor
+Create by JE-Chen
+        """
+    )
+    message_box.exec()
