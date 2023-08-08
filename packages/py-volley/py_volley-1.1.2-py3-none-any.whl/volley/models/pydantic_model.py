fa8f92bf4eafa71c13206d1c2046963128cfaf71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-08 05:09:43 +0000

    Add package py-volley/1.1.2/py_volley-1.1.2-py3-none-any.whl

---
+++ packages/py-volley/py_volley-1.1.2-py3-none-any.whl/volley/models/pydantic_model.py
@@ -0,0 +1,62 @@
+from typing import Any, Dict, List, Type, TypeVar
+
+from pydantic import BaseModel, Extra, parse_obj_as
+
+from volley.models.base import BaseModelHandler
+
+BaseModelType = TypeVar("BaseModelType", bound=BaseModel)
+
+
+class PydanticModelHandler(BaseModelHandler):
+    """for pydantic model that offloads serialization to serializer"""
+
+    def construct(self, message: Dict[str, Any], schema: Type[BaseModelType]) -> BaseModelType:
+        """coverts a dict to a Pydantic model"""
+        return schema.parse_obj(message)
+
+    def deconstruct(self, model: BaseModelType) -> Dict[str, Any]:
+        """converts a pydantic model to a dict"""
+        return model.dict()
+
+
+class PydanticParserModelHandler(BaseModelHandler):
+    """pydantic model that handles serialization internally"""
+
+    def construct(self, message: bytes, schema: Type[BaseModelType]) -> BaseModelType:
+        """coverts bytes to a Pydantic model"""
+        return schema.parse_raw(message)
+
+    def deconstruct(self, model: BaseModelType) -> bytes:
+        """converts a pydantic model to bytes"""
+        return model.json().encode("utf-8")
+
+
+class PydanticListParser(BaseModelHandler):
+    """for pydantic model that offloads serialization to serializer"""
+
+    def construct(self, message: List[Any], schema: Type[BaseModelType]) -> List[BaseModelType]:
+        """coverts a dict to a Pydantic model"""
+        return parse_obj_as(List[schema], message)  # type: ignore
+
+    def deconstruct(self, model: List[BaseModelType]) -> List[Dict[str, Any]]:
+        """converts a pydantic model to a dict"""
+        return [m.dict() for m in model]
+
+
+class GenericMessage(BaseModel):
+    """The default data model for all profiles.
+    Serves as a flexible data model. Accepts extra attributes and provides no type validation.
+    """
+
+    class Config:
+        extra = Extra.allow
+
+
+class QueueMessage(BaseModel):
+    """message in its raw state off a queue
+    message_context: any identifier for a message on a queue.
+        used for deleting or markng a message as success after post-processing
+    """
+
+    message_context: Any
+    message: Any
