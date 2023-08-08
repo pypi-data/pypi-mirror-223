26a7f5bb5524fe592b41e1701b59c3b6203c1e5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-08 03:10:29 +0000

    Add package imio.smartweb.core/1.1.19/imio.smartweb.core-1.1.19-py3-none-any.whl

---
+++ packages/imio.smartweb.core/imio.smartweb.core-1.1.19-py3-none-any.whl/imio/smartweb/core/webcomponents/src/components/Annuaire/ContactCard/ContactCard.jsx
@@ -0,0 +1,96 @@
+import React from "react";
+
+const ContactCard = ({ contactItem }) => {
+    const title = contactItem.title && contactItem.title;
+    const category =
+        contactItem.taxonomy_contact_category && contactItem.taxonomy_contact_category[0];
+
+    const number = contactItem.number ? contactItem.number : "";
+    const street = contactItem.street ? contactItem.street : "";
+    const complement = contactItem.complement ? contactItem.complement : "";
+    const zipcode = contactItem.zipcode ? contactItem.zipcode : "";
+    const city = contactItem.city ? contactItem.city : "";
+    const country = contactItem.country ? contactItem.country : "";
+    const phones = contactItem.phones ? contactItem.phones : "";
+    const mails = contactItem.mails ? contactItem.mails : "";
+    const topics = contactItem.topics ? contactItem.topics : "";
+
+    let countryTitle = contactItem.country && contactItem.country.title
+    let itineraryLink =
+		"https://www.google.com/maps/dir/?api=1&destination=" +
+		contactItem.street +
+		"+" +
+		contactItem.number +
+		"+" +
+		contactItem.complement +
+		"+" +
+		contactItem.zipcode +
+		"+" +
+		contactItem.city +
+        "+" +
+        countryTitle
+
+	itineraryLink = itineraryLink.replaceAll('+null', '')
+    console.log(contactItem)
+    return (
+        <div className="r-list-item">
+            <div
+                className={contactItem.image_preview_scale?"r-item-img":"r-item-img r-item-img-placeholder"}
+                style={{
+                    backgroundImage: contactItem.image_preview_scale
+                        ? "url(" + contactItem.image_preview_scale + ")"
+                        : "",
+                }}
+            />
+
+            <div className="r-item-text">
+                <span className="r-item-title">{title}</span>
+                {category ? <span className="r-item-categorie">{category.title}</span> : ""}
+                <div className="r-item-all">
+                    {street ? (
+                        <div className="r-item-adresse">
+                            {number ? <span>{number + " "}</span> : ""}
+                            {street ? <span>{street + ", "}</span> : ""}
+                            {complement ? <span>{complement + ", "}</span> : ""}
+                            <br />
+                            {zipcode ? <span>{zipcode + " "}</span> : ""}
+                            {city ? <span>{city}</span> : ""}
+                            <div className="itineraty">
+                                <a href={itineraryLink} target="_blank">
+                                    Itinéraire
+                                </a>
+                            </div>
+                        </div>
+                    ) : (
+                        ""
+                    )}
+                    <div className="r-item-contact">
+                        <div className="phones">
+                            {phones
+                                ? phones.map((phone, i) => {
+                                      return <span key={i}>{phone.number}</span>;
+                                  })
+                                : ""}
+                        </div>
+                        <div className="mails">
+                            {mails
+                                ? mails.map((mail, i) => {
+                                      return <span key={i}>{mail.mail_address}</span>;
+                                  })
+                                : ""}
+                        </div>
+                        <div className="topics">
+                            {topics
+                                ? topics.map((mail, i) => {
+                                      return <span key={i}>{mail.title}</span>;
+                                  })
+                                : ""}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ContactCard;
