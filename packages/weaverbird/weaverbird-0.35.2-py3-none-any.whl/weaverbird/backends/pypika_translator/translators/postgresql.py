3367bc597fe4798587126a3e1eba2b0c7a0f1dec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-08 15:09:58 +0000

    Add package weaverbird/0.35.2/weaverbird-0.35.2-py3-none-any.whl

---
+++ packages/weaverbird/weaverbird-0.35.2-py3-none-any.whl/weaverbird/backends/pypika_translator/translators/postgresql.py
@@ -0,0 +1,56 @@
+from typing import TYPE_CHECKING
+
+from pypika import Field, functions
+from pypika.dialects import PostgreSQLQuery
+
+from weaverbird.backends.pypika_translator.dialects import SQLDialect
+from weaverbird.backends.pypika_translator.operators import FromDateOp, RegexOp, ToDateOp
+from weaverbird.backends.pypika_translator.translators.base import (
+    DataTypeMapping,
+    Self,
+    SQLTranslator,
+    StepContext,
+)
+
+if TYPE_CHECKING:
+    from pypika.queries import QueryBuilder
+
+    from weaverbird.pipeline.steps import DurationStep
+
+
+class PostgreSQLTranslator(SQLTranslator):
+    DIALECT = SQLDialect.POSTGRES
+    QUERY_CLS = PostgreSQLQuery
+    DATA_TYPE_MAPPING = DataTypeMapping(
+        boolean="BOOLEAN",
+        date="DATE",
+        float="DOUBLE PRECISION",
+        integer="INTEGER",
+        text="TEXT",
+        datetime="TIMESTAMP",
+        timestamp="TIMESTAMP",
+    )
+    SUPPORT_ROW_NUMBER = True
+    SUPPORT_SPLIT_PART = True
+    FROM_DATE_OP = FromDateOp.TO_CHAR
+    REGEXP_OP = RegexOp.SIMILAR_TO
+    TO_DATE_OP = ToDateOp.TO_TIMESTAMP
+
+    def duration(
+        self: Self,
+        *,
+        builder: "QueryBuilder",
+        prev_step_table: str,
+        columns: list[str],
+        step: "DurationStep",
+    ) -> StepContext:
+        query: "QueryBuilder" = self.QUERY_CLS.from_(prev_step_table).select(
+            *columns,
+            functions.Extract(
+                step.duration_in, Field(step.end_date_column) - Field(step.start_date_column)
+            ).as_(step.new_column_name),
+        )
+        return StepContext(query, columns + [step.new_column_name])
+
+
+SQLTranslator.register(PostgreSQLTranslator)
