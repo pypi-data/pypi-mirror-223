fbad0061a7fbe8893440e3c744d03aaa53374ecb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-08 15:09:48 +0000

    Add package hatch-ci/0.0.9/hatch_ci-0.0.9-py3-none-any.whl

---
+++ packages/hatch-ci/hatch_ci-0.0.9-py3-none-any.whl/hatch_ci/__init__.py
@@ -0,0 +1,2 @@
+__version__ = "0.0.9"
+__hash__ = "beb34ecf309be4b0ee0d57d29606022230cef46f"
---
+++ packages/hatch-ci/hatch_ci-0.0.9-py3-none-any.whl/hatch_ci/_build.py
@@ -0,0 +1,5 @@
+# autogenerate build file
+ref = 'refs/heads/master'
+run_id = 5753082134
+run_number = 14
+sha = 'beb34ecf309be4b0ee0d57d29606022230cef46f'
