a5d54cceb2a4d650a5f4cb1fd48848d28c7a2327
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-08 04:11:27 +0000

    Add package pasqal-cloud/0.3.5/pasqal_cloud-0.3.5-py3-none-any.whl

---
+++ packages/pasqal-cloud/pasqal_cloud-0.3.5-py3-none-any.whl/tests/test_device_specs.py
@@ -0,0 +1,23 @@
+import json
+from unittest.mock import patch
+from uuid import uuid4
+
+import pytest
+
+from pasqal_cloud import SDK
+from tests.test_doubles.authentication import FakeAuth0AuthenticationSuccess
+
+
+class TestDeviceSpecs:
+    @pytest.fixture(autouse=True)
+    @patch("sdk.client.Auth0TokenProvider", FakeAuth0AuthenticationSuccess)
+    def init_sdk(self, start_mock_request):
+        self.sdk = SDK(
+            username="me@test.com", password="password", project_id=str(uuid4())
+        )
+
+    def test_get_device_specs_success(self):
+        device_specs_dict = self.sdk.get_device_specs_dict()
+        assert type(device_specs_dict) == dict
+        specs = device_specs_dict["FRESNEL"]
+        json.loads(specs)
