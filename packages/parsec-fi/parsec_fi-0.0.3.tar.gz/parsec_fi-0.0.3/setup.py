e62f33e5b3b36a7f638fbe524b40e61787ed4bdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-08 14:08:59 +0000

    Add package parsec-fi/0.0.3/parsec_fi-0.0.3.tar.gz

---
+++ packages/parsec-fi/parsec_fi-0.0.3.tar.gz/parsec_fi-0.0.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='parsec_fi',
+    version='0.0.3',
+    description='Python SDK for the Parsec API',
+    author='PARSEC FINANCE INC.',
+    packages=find_packages(),
+    license='MIT'
+)
