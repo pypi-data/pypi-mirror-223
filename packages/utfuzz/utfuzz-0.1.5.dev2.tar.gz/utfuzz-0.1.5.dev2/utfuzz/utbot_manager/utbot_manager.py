23a870653328a87ad8f1f7834bc077a64b6bd20c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-08 04:13:54 +0000

    Add package utfuzz/0.1.5.dev2/utfuzz-0.1.5.dev2.tar.gz

---
+++ packages/utfuzz/utfuzz-0.1.5.dev2.tar.gz/utfuzz-0.1.5.dev2/utfuzz/utbot_manager/utbot_manager.py
@@ -0,0 +1,26 @@
+import subprocess
+
+from utfuzz.utbot_manager.utbot_loader import download_utbot
+
+
+def generate_tests(
+        java: str,
+        jar_path: str,
+        sys_paths: list[str],
+        python_path: str,
+        file_under_test: str,
+        skip_regression: bool,
+        timeout: int,
+        output_dir: str,
+):
+    command = f"{java} -jar {jar_path} generate_python {file_under_test}" \
+              f" -p {python_path} -o {output_dir} -s {' '.join(sys_paths)} --timeout {timeout * 1000}"
+    if skip_regression:
+        command += ' --do-not-generate-regression-suite'
+    try:
+        output = subprocess.check_output(command.split())
+        print(output.decode())
+    except Exception:
+        pass
+
+
