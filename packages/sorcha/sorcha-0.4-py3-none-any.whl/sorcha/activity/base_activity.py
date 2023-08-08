ab3e61d0eb153411061537f5b48d8c92b05064e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-08 04:13:02 +0000

    Add package sorcha/0.4/sorcha-0.4-py3-none-any.whl

---
+++ packages/sorcha/sorcha-0.4-py3-none-any.whl/sorcha/activity/base_activity.py
@@ -0,0 +1,51 @@
+from abc import ABC, abstractmethod
+from typing import List
+import pandas as pd
+import numpy as np
+
+
+class AbstractCometaryActivity(ABC):
+    """Abstract base class for cometary activity models"""
+
+    def __init__(self, required_column_names: List[str] = []) -> None:
+        """Abstract base class accepts a list of column names that are required
+        to be present in the Pandas dataframe passed to the ``compute`` method.
+
+        Parameters
+        ----------
+        required_column_names : list, optional
+            The list of columns in ``observations`` dataframe that are required
+            for the model calculation, by default []
+        """
+        self.required_column_names = required_column_names
+
+    @abstractmethod
+    def compute(self, df: pd.DataFrame) -> np.array:
+        """User implemented calculation based on the input provided by the
+        pandas dataframe ``df``.
+
+        Parameters
+        ----------
+        df : Pandas dataframe
+            The ``observations`` dataframe provided by ``Sorcha``.
+        """
+        raise (NotImplementedError, "Must be implemented by the subclass")
+
+    def _validate_column_names(self, df: pd.DataFrame) -> None:
+        """Private method that checks that the provided pandas dataframe contains
+           the required columns defined in ``self.required_column_names``.
+
+        Parameters
+        ----------
+        df : Pandas dataframe
+            The ``observations`` dataframe provided by ``Sorcha``.
+        """
+        for colname in self.required_column_names:
+            if colname not in df.columns:
+                raise (ValueError, f"Input dataframe is missing column %{colname}")
+
+    @staticmethod
+    @abstractmethod
+    def name_id() -> str:
+        """This method will return the unique name of the LightCurve Model"""
+        raise (NotImplementedError, "Must be implemented as a static method by the subclass")
