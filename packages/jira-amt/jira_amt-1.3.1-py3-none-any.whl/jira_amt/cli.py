01530888f5e5c5cfbe7a2bbe243a9c25aeffec3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-08 03:10:22 +0000

    Add package jira-amt/1.3.1/jira_amt-1.3.1-py3-none-any.whl

---
+++ packages/jira-amt/jira_amt-1.3.1-py3-none-any.whl/jira_amt/cli.py
@@ -0,0 +1,218 @@
+"""This module provides the Jira CLI."""
+
+import json
+from pathlib import Path
+
+import tomlkit as toml
+from tomlkit import document, table, comment as cm, nl
+from typing import Optional
+import typer
+
+from jira_amt import (
+    __app_name__, __version__, __author__, config, jira
+)
+
+app = typer.Typer()
+
+
+@app.command()
+def init() -> None:
+    """Initialize Jira CLI."""
+    typer.secho("Initializing Jira CLI ...", fg=typer.colors.GREEN)
+
+    # Create work directory
+    Path(config.WORK_DIR).mkdir(parents=True, exist_ok=True)
+
+    # Create schemas file
+    schema_list = document()
+    typer.secho("  -> Fetching schemas", fg=typer.colors.BRIGHT_GREEN)
+    schema_list.add(
+        cm(f"Jira Asset Management - Status list - v{__version__}")
+    )
+    schemas = json.loads(get_jira().get_schema().text)
+    objects = []
+    for schema in schemas['objectschemas']:
+        title = f"{schema['id']}:{schema['name']}"
+        schema_list.add(title, table())
+        objs = json.loads(get_jira().get_objecttypes(schema['id']).text)
+        for object in objs:
+            object['schema'] = schema['name']
+            objects.append(object)
+            schema_list[title].add(object['name'].lower(), object['id'])
+    with open(
+        config.WORK_DIR+"/schemas.toml",
+        mode="w",
+        encoding="utf-8"
+    ) as file:
+        toml.dump(schema_list, file)
+
+    # Create attributes file
+    typer.secho("  -> Fetching attributes", fg=typer.colors.BRIGHT_GREEN)
+    attr_list = document()
+    attr_list.add(
+        cm(f"Jira Asset Management - Status list - v{__version__}")
+    )
+    for object in objects:
+        result = json.loads(get_jira().get_attributes(object['id']).text)
+        attributes = table()
+        for attribute in result:
+            attributes.add(
+                attribute['name'].lower() + "." + str(attribute['type']),
+                attribute['id']
+            )
+        attr_list.add(
+            object['name'].lower() + "." + str(object['schema']).lower(),
+            attributes
+        )
+    with open(
+        config.WORK_DIR+"/attributes.toml",
+        mode="w",
+        encoding="utf-8"
+    ) as file:
+        toml.dump(attr_list, file)
+
+    # Create statuses file
+    typer.secho("  -> Fetching statuses", fg=typer.colors.BRIGHT_GREEN)
+    status_list = document()
+    status_list.add(
+        cm(f"Jira Asset Management - Status list - v{__version__}")
+    )
+    status_list.add(nl())
+    globals = json.loads(get_jira().get_global_statustypes().text)
+    for status in globals:
+        status_list.add(status['name'].lower(), status['id'])
+    for schema in schemas['objectschemas']:
+        locals = json.loads(
+            get_jira().get_statustypes(schema['id']).text
+        )
+        temp = table()
+        for status in locals:
+            temp.add(status['name'].lower(), status['id'])
+        status_list.add(schema['name'], temp)
+
+    with open(
+        config.WORK_DIR+"/status.toml",
+        mode="w",
+        encoding="utf-8"
+    ) as file:
+        toml.dump(status_list, file)
+
+    typer.secho("Done!", fg=typer.colors.GREEN)
+
+
+@app.command()
+def comment(
+    schema: str = typer.Argument(
+        help="Your schema name"
+    ),
+    object: str = typer.Argument(
+        help="Your object name"
+    ),
+    asset: str = typer.Argument(
+        help="Asset's name"
+    ),
+    body: str = typer.Argument(
+        help="Comment's body"
+    )
+) -> None:
+    """Set a comment for your asset."""
+    result = get_jira().add_comment(schema, object, asset, body)
+    if result:
+        typer.secho(
+            f"Comment added to asset '{asset}' successfully.",
+            fg=typer.colors.GREEN
+        )
+    else:
+        typer.secho(
+            f"Error adding comment to asset '{asset}'.",
+            fg=typer.colors.RED
+        )
+        raise typer.Exit(1)
+
+
+@app.command()
+def attr(
+    schema: str = typer.Argument(
+        help="Your schema name"
+    ),
+    object: str = typer.Argument(
+        help="Your object name"
+    ),
+    asset: str = typer.Argument(
+        help="Asset's name"
+    ),
+    name: str = typer.Argument(
+        help="Attribute name"
+    ),
+    value: str = typer.Argument(
+        help="Attribute value"
+    )
+) -> None:
+    """Update an attribute of your asset."""
+    result = get_jira().update_asset(schema, object, asset, name, value)
+    if result:
+        typer.secho(
+            f"Asset '{asset}' updated successfully.",
+            fg=typer.colors.GREEN
+        )
+    else:
+        typer.secho(
+            f"Error updating asset '{asset}'.",
+            fg=typer.colors.RED
+        )
+        raise typer.Exit(1)
+
+
+def get_jira() -> jira.JiraAssetHandler:
+    if not config.JIRA_SERVER:
+        typer.secho(
+            "JIRA_SERVER environment variable not set.",
+            fg=typer.colors.RED,
+        )
+        raise typer.Exit(1)
+
+    if not config.JIRA_PAT:
+        typer.secho(
+            "JIRA_PAT environment variable not set.",
+            fg=typer.colors.RED,
+        )
+        raise typer.Exit(1)
+
+    return jira.JiraAssetHandler(
+        config.JIRA_SERVER,
+        config.JIRA_PAT
+    )
+
+
+def _version_callback(value: bool) -> None:
+    if value:
+        typer.secho(f"{__app_name__} v{__version__}", fg=typer.colors.CYAN)
+        raise typer.Exit()
+
+
+def _author_callback(value: bool) -> None:
+    if value:
+        typer.secho(f"{__author__}", fg=typer.colors.BRIGHT_MAGENTA)
+        raise typer.Exit()
+
+
+@app.callback()
+def main(
+    version: Optional[bool] = typer.Option(
+        None,
+        "--version",
+        "-v",
+        help="Show the application's version.",
+        callback=_version_callback,
+        is_eager=True,
+    ),
+    author: Optional[bool] = typer.Option(
+        None,
+        "--author",
+        "-a",
+        help="Show the application's author information.",
+        callback=_author_callback,
+        is_eager=True,
+    )
+) -> None:
+    return
---
+++ packages/jira-amt/jira_amt-1.3.1-py3-none-any.whl/jira_amt/jira.py
@@ -0,0 +1,151 @@
+import requests
+import json
+import typer
+
+from jira_amt import config
+
+
+class JiraAssetHandler:
+    def __init__(self, server, pat):
+        self.url = server + '/rest/assets/1.0'
+        self.headers = {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + pat
+        }
+
+    # ------ Assets ------
+
+    def get_asset(self, schema, object, asset):
+        schema_id = config.getSchema(schema)
+        path = '/object/navlist/aql'
+        data = {
+            "objectTypeId": config.getObject(schema_id+":"+schema, object),
+            "resultsPerPage": 1,
+            "includeAttributes": "false",
+            "objectSchemaId": schema_id,
+            "qlQuery": f"Name = \"{asset}\""
+        }
+        return self._make_api_call("POST", path, data)
+
+    def create_asset(self, schema, object, attributes):
+        schema_id = config.getSchema(schema)
+
+        input = []
+
+        for attribute_name, value in attributes.items():
+            object_type_attribute_id = config.getAttribute(
+                object+"."+schema,
+                attribute_name
+            )
+
+            if object_type_attribute_id is not None and isinstance(value, list):
+                output_dict = {
+                    "objectTypeAttributeId": object_type_attribute_id,
+                    "objectAttributeValues": [{"value": config.getAttributeValue(
+                        object+"."+schema,
+                        attribute_name,
+                        v
+                    )} for v in value]
+                }
+
+                input.append(output_dict)
+            elif object_type_attribute_id is not None:
+                output_dict = {
+                    "objectTypeAttributeId": object_type_attribute_id,
+                    "objectAttributeValues": [{"value": config.getAttributeValue(
+                        object,
+                        attribute_name,
+                        value
+                    )}]
+                }
+
+                input.append(output_dict)
+
+        path = '/object/create'
+        data = {
+            "objectTypeId": config.getObject(schema_id+":"+schema, object),
+            "attributes": input
+        }
+        return self._make_api_call("POST", path, data)
+
+    def update_asset(self, schema, object, asset_name, attr_name, attr_value):
+        attr_value = config.getAttributeValue(
+            object+"."+schema,
+            attr_name,
+            attr_value
+        )
+        attr_name = config.getAttribute(
+            object+"."+schema,
+            attr_name
+        )
+
+        print(attr_value)
+        print(attr_name)
+
+        object = self.get_asset(schema, object, asset_name)
+        id = json.loads(object.text)['matchedFilterValues'][0]['objectId']
+
+        path = f"/object/{id}"
+        data = {
+            "objectTypeId": config.JIRA_OBJECT,
+            "attributes": [
+                {
+                    "objectTypeAttributeId": attr_name,
+                    "objectAttributeValues": [{"value": attr_value}]
+                }
+            ]
+        }
+        return self._make_api_call("PUT", path, data)
+
+    # ------ Comments ------
+
+    def add_comment(self, schema, object, asset_name, comment):
+        object = self.get_asset(schema, object, asset_name)
+        id = json.loads(object.text)['matchedFilterValues'][0]['objectId']
+
+        path = "/comment/create"
+        data = {
+            "comment": comment,
+            "objectId": f"{id}",
+            "role": 0
+        }
+
+        return self._make_api_call("POST", path, data)
+
+    # ------ Objects ------
+
+    def get_schema(self):
+        path = '/objectschema/list'
+        return self._make_api_call("GET", path, {})
+
+    def get_objecttypes(self, id):
+        path = f'/objectschema/{id}/objecttypes/flat'
+        return self._make_api_call("GET", path, {})
+
+    def get_attributes(self, objectType):
+        path = f'/objecttype/{objectType}/attributes'
+        return self._make_api_call("GET", path, {})
+
+    # ------ Status ------
+
+    def get_global_statustypes(self):
+        path = '/config/statustype'
+        return self._make_api_call("GET", path, {})
+
+    def get_statustypes(self, id):
+        path = f'/config/statustype?objectSchemaId={id}'
+        return self._make_api_call("GET", path, {})
+
+    def _make_api_call(self, method, path, data):
+        try:
+            response = requests.request(
+                method,
+                self.url + path,
+                headers=self.headers,
+                data=json.dumps(data)
+            )
+            return response
+        except Exception as e:
+            typer.secho(f"API call failed: {str(e)}", fg=typer.colors.YELLOW)
+            return None
