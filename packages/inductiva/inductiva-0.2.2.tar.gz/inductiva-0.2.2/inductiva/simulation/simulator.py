1db1dd8befbd545252990f20fcda31c9390f7d9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-08 04:12:12 +0000

    Add package inductiva/0.2.2/inductiva-0.2.2.tar.gz

---
+++ packages/inductiva/inductiva-0.2.2.tar.gz/inductiva-0.2.2/inductiva/simulation/simulator.py
@@ -0,0 +1,53 @@
+"""Base class for low-level simulators."""
+from abc import ABC, abstractmethod
+
+from typing import Optional
+from uuid import UUID
+
+from inductiva import types
+from inductiva.utils import files
+from inductiva import tasks
+
+
+class Simulator(ABC):
+    """Base simulator class."""
+
+    @property
+    @abstractmethod
+    def api_method_name(self) -> str:
+        pass
+
+    def _setup_input_dir(self, input_dir: types.Path):
+        """Setup the simulator input directory."""
+        input_dir = files.resolve_path(input_dir)
+        if not input_dir.is_dir():
+            raise ValueError(
+                f"The provided path (\"{input_dir}\") is not a directory.")
+        return input_dir
+
+    def run(
+        self,
+        input_dir: types.Path,
+        *_args,
+        resource_pool_id: Optional[UUID] = None,
+        run_async: bool = False,
+        **kwargs,
+    ) -> tasks.Task:
+        """Run the simulation.
+
+        Args:
+            input_dir: Path to the directory containing the input files.
+            _args: Unused in this method, but defined to allow for more
+                non-default arguments in method override in subclasses.
+            **kwargs: Additional keyword arguments to be passed to the
+                simulation API method.
+        """
+        input_dir = self._setup_input_dir(input_dir)
+
+        return tasks.run_simulation(
+            self.api_method_name,
+            input_dir,
+            run_async=run_async,
+            resource_pool_id=resource_pool_id,
+            **kwargs,
+        )
