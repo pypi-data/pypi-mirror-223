f58f7951500a02cfffab28a3f129a817941a6661
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-08 03:10:29 +0000

    Add package TelegramContract2vCard/1.0.0/TelegramContract2vCard-1.0.0.tar.gz

---
+++ packages/TelegramContract2vCard/TelegramContract2vCard-1.0.0.tar.gz/TelegramContract2vCard-1.0.0/README.md
@@ -0,0 +1,1 @@
+# make telegram contacts.html to 2vCard automatically
---
+++ packages/TelegramContract2vCard/TelegramContract2vCard-1.0.0.tar.gz/TelegramContract2vCard-1.0.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='TelegramContract2vCard',
+    version='1.0.0',
+    description='make telegram contacts.html or result.json to vCard automatically',
+    author='jjh4450',
+    author_email='jjh4450git@gmail.com',
+    url='https://github.com/jjh4450/TelegramContract2vCard',
+    install_requires=["beautifulsoup4"],
+    packages=find_packages(exclude=[]),
+    keywords=['telegram', 'contacts', 'vcf', 'vcard', 'html', 'json', 'parser'],
+    python_requires='>=3.6',
+    package_data={},
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
