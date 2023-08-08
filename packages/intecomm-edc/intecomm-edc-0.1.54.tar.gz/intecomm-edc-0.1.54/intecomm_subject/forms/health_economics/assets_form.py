f961f648be524262236550a7d3b9ed9223ac341a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-08 14:09:10 +0000

    Add package intecomm-edc/0.1.54/intecomm-edc-0.1.54.tar.gz

---
+++ packages/intecomm-edc/intecomm-edc-0.1.54.tar.gz/intecomm-edc-0.1.54/intecomm_subject/forms/health_economics/assets_form.py
@@ -0,0 +1,24 @@
+from django import forms
+from edc_crf.modelform_mixins import CrfSingletonModelFormMixin
+from edc_he.form_validators import HealthEconomicsAssetsFormValidator
+
+from ...models import HealthEconomicsAssets
+from ..mixins import CrfModelFormMixin
+from .modelform_mixins import HealthEconomicsModelFormMixin
+
+
+class HealthEconomicsAssetsForm(
+    CrfSingletonModelFormMixin,
+    HealthEconomicsModelFormMixin,
+    CrfModelFormMixin,
+    forms.ModelForm,
+):
+    form_validator_cls = HealthEconomicsAssetsFormValidator
+
+    def clean(self):
+        self.raise_if_singleton_exists()
+        return super().clean()
+
+    class Meta:
+        model = HealthEconomicsAssets
+        fields = "__all__"
---
+++ packages/intecomm-edc/intecomm-edc-0.1.54.tar.gz/intecomm-edc-0.1.54/intecomm_subject/forms/health_economics/property_form.py
@@ -0,0 +1,24 @@
+from django import forms
+from edc_crf.modelform_mixins import CrfSingletonModelFormMixin
+from edc_he.form_validators import HealthEconomicsPropertyFormValidator
+
+from ...models import HealthEconomicsProperty
+from ..mixins import CrfModelFormMixin
+from .modelform_mixins import HealthEconomicsModelFormMixin
+
+
+class HealthEconomicsPropertyForm(
+    CrfSingletonModelFormMixin,
+    HealthEconomicsModelFormMixin,
+    CrfModelFormMixin,
+    forms.ModelForm,
+):
+    form_validator_cls = HealthEconomicsPropertyFormValidator
+
+    def clean(self):
+        self.raise_if_singleton_exists()
+        return super().clean()
+
+    class Meta:
+        model = HealthEconomicsProperty
+        fields = "__all__"
