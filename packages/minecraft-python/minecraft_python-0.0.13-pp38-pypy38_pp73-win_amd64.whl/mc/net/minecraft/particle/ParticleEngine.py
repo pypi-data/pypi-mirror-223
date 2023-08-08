23f56e7df7d74eaf34ed293c7f095a3c860c16bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-08 15:10:27 +0000

    Add package minecraft-python/0.0.13/minecraft_python-0.0.13-pp38-pypy38_pp73-win_amd64.whl

---
+++ packages/minecraft-python/minecraft_python-0.0.13-pp38-pypy38_pp73-win_amd64.whl/mc/net/minecraft/particle/ParticleEngine.py
@@ -0,0 +1,43 @@
+from mc.net.minecraft.renderer.Tesselator import tesselator
+from pyglet import gl
+
+import math
+
+class ParticleEngine:
+    __particles = set()
+
+    def __init__(self, level, textures):
+        self._level = level
+        self.__textures = textures
+
+    def add(self, p):
+        self.__particles.add(p)
+
+    def tick(self):
+        for p in self.__particles.copy():
+            p.tick()
+            if p.removed:
+                self.__particles.remove(p)
+
+    def render(self, player, a, layer):
+        if not len(self.__particles):
+            return
+
+        gl.glEnable(gl.GL_TEXTURE_2D)
+        id_ = self.__textures.loadTexture('terrain.png', gl.GL_NEAREST)
+        gl.glBindTexture(gl.GL_TEXTURE_2D, id_)
+        xa = -math.cos(player.yRot * math.pi / 180.0)
+        za = -math.sin(player.yRot * math.pi / 180.0)
+
+        xa2 = -za * math.sin(player.xRot * math.pi / 180.0)
+        za2 = xa * math.sin(player.xRot * math.pi / 180.0)
+        ya = math.cos(player.xRot * math.pi / 180.0)
+
+        t = tesselator
+        gl.glColor4f(0.8, 0.8, 0.8, 1.0)
+        t.begin()
+        for p in self.__particles:
+            if p.isLit() ^ layer == 1:
+                p.render(t, a, xa, ya, za, xa2, za2)
+        t.end()
+        gl.glDisable(gl.GL_TEXTURE_2D)
