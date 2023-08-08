9ffa43d67573af6ce873c214e5c2ce4557a58e12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-08 04:11:50 +0000

    Add package ftmq/0.2.1/ftmq-0.2.1-py3-none-any.whl

---
+++ packages/ftmq/ftmq-0.2.1-py3-none-any.whl/ftmq/model/coverage.py
@@ -0,0 +1,80 @@
+from collections import Counter
+from typing import Any
+
+from nomenklatura.dataset.coverage import DataCoverage as NKCoverage
+from pydantic import PrivateAttr
+
+from ftmq.enums import Properties
+from ftmq.model.mixins import NKModel
+from ftmq.types import CE, CEGenerator, DateLike, Frequencies, Schemata
+
+
+class Collector:
+    schemata: Counter = None
+    countries: set[str] = None
+    start: set[DateLike] = None
+    end: set[DateLike] = None
+
+    def __init__(self):
+        self.schemata = Counter()
+        self.countries = set()
+        self.start = set()
+        self.end = set()
+
+    def collect(self, proxy: CE) -> None:
+        self.schemata[proxy.schema.name] += 1
+        self.countries.update(proxy.countries)
+        self.start.update(proxy.get(Properties.startDate, quiet=True))
+        self.start.update(proxy.get(Properties.date, quiet=True))
+        self.end.update(proxy.get(Properties.endDate, quiet=True))
+        self.end.update(proxy.get(Properties.date, quiet=True))
+
+    def export(self) -> "Coverage":
+        return Coverage(
+            start=min(self.start) if self.start else None,
+            end=max(self.end) if self.end else None,
+            schemata=dict(self.schemata),
+            countries=self.countries,
+            entities=self.schemata.total(),
+        )
+
+    def to_dict(self) -> dict[str, Any]:
+        data = self.export()
+        return data.dict()
+
+    @classmethod
+    def apply(cls, proxies: CEGenerator) -> "Coverage":
+        """
+        Generate coverage from an input stream of proxies
+        """
+        cov = cls()
+        for proxy in proxies:
+            cov.collect(proxy)
+        return cov.export()
+
+
+class Coverage(NKModel):
+    _nk_model = NKCoverage
+    _collector: Collector | None = PrivateAttr()
+
+    start: DateLike | None = None
+    end: DateLike | None = None
+    countries: list[str] | None = []
+    frequency: Frequencies | None = "unknown"
+
+    # own additions:
+    schemata: dict[Schemata, int] = None
+    entities: int = 0
+
+    def __enter__(self):
+        self._collector = Collector()
+        return self._collector
+
+    def __exit__(self, *args, **kwargs):
+        res = self._collector.export()
+        self._collector = None
+        self.start = res.start
+        self.end = res.end
+        self.schemata = res.schemata
+        self.entities = res.entities
+        self.countries = res.countries
---
+++ packages/ftmq/ftmq-0.2.1-py3-none-any.whl/ftmq/util.py
@@ -0,0 +1,27 @@
+from collections.abc import Generator
+from functools import cache
+
+from nomenklatura.dataset import DataCatalog, Dataset
+
+
+@cache
+def make_dataset(name: str) -> Dataset:
+    catalog = DataCatalog(
+        Dataset, {"datasets": [{"name": name, "title": name.title()}]}
+    )
+    return catalog.get(name)
+
+
+def parse_unknown_cli_filters(
+    filters: tuple[str],
+) -> Generator[tuple[str, str, str], None, None]:
+    filters = (f for f in filters)
+    for prop in filters:
+        prop = prop.lstrip("-")
+        if "=" in prop:  # 'country=de'
+            prop, value = prop.split("=")
+        else:  # ("country", "de"):
+            value = next(filters)
+
+        prop, *op = prop.split("__")
+        yield prop, value, op[0] if op else None
