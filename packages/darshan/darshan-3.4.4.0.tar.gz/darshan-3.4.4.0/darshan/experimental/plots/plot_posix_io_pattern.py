fd140b5a848928ed78eb2c0555fccdd5f044ea45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-08 05:11:29 +0000

    Add package darshan/3.4.4.0/darshan-3.4.4.0.tar.gz

---
+++ packages/darshan/darshan-3.4.4.0.tar.gz/darshan-3.4.4.0/darshan/experimental/plots/plot_posix_io_pattern.py
@@ -0,0 +1,173 @@
+# -*- coding: utf-8 -*-
+from collections import defaultdict
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def autolabel(ax, rects):
+    """Attach a text label above each bar in *rects*, displaying its height."""
+    for rect in rects:
+        height = rect.get_height()
+        ax.annotate(
+            '{}'.format(height),
+            xy=(rect.get_x() + rect.get_width() / 2, height),
+            xytext=(0, 3),  # 3 points vertical offset
+            textcoords="offset points",
+            ha='center',
+            va='bottom',
+            rotation=45,
+        )
+
+def gather_count_data(report, mod):
+    """
+    Collect the module counts and labels
+    for the I/O Operation Count plot.
+    """
+    # TODO: change to report.summary
+    if 'agg_ioops' in dir(report):
+        report.agg_ioops()
+    else:
+        print(
+            "Cannot create summary, agg_ioops aggregator is not "
+            "registered with the report class. Be sure to call "
+            "darshan.experimental() once before invoking this plot."
+        )
+
+    mod_data = report.summary['agg_ioops'][mod]
+
+    # Gather POSIX
+    if mod == 'POSIX':
+        labels = ['Read', 'Write', 'Open', 'Stat', 'Seek', 'Mmap', 'Fsync']
+        counts = [
+            mod_data['POSIX_READS'],
+            mod_data['POSIX_WRITES'],
+            mod_data['POSIX_OPENS'],
+            mod_data['POSIX_STATS'],
+            mod_data['POSIX_SEEKS'],
+            0, # faulty? mod_data['POSIX_MMAPS'],
+            mod_data['POSIX_FSYNCS'] + mod_data['POSIX_FDSYNCS']
+        ]
+
+    # Gather MPIIO
+    elif mod == 'MPI-IO':
+        labels = [
+            'Ind. Read', 'Ind. Write', 'Ind. Open',
+            'Col. Read', 'Col. Write', 'Col. Open', 'Sync']
+        counts = [
+            mod_data['MPIIO_INDEP_READS'],
+            mod_data['MPIIO_INDEP_WRITES'],
+            mod_data['MPIIO_INDEP_OPENS'],
+            mod_data['MPIIO_COLL_READS'],
+            mod_data['MPIIO_COLL_WRITES'],
+            mod_data['MPIIO_COLL_OPENS'],
+            mod_data['MPIIO_SYNCS'],
+        ]
+
+    # Gather Stdio
+    elif mod == 'STDIO':
+        labels = ['Read', 'Write', 'Open', 'Seek', 'Flush']
+        counts = [
+            mod_data['STDIO_READS'],
+            mod_data['STDIO_WRITES'],
+            mod_data['STDIO_OPENS'],
+            mod_data['STDIO_SEEKS'],
+            mod_data['STDIO_FLUSHES']
+        ]
+
+    elif mod == 'H5F':
+        labels = [
+            'H5D Read', 'H5D Write', 'H5D Open',
+            'H5D Flush', 'H5F Open', 'H5F Flush',
+        ]
+        counts = [
+            # set H5D counters to zero
+            0, 0, 0, 0,
+            mod_data['H5F_OPENS'],
+            mod_data['H5F_FLUSHES'],
+        ]
+
+    elif mod == 'H5D':
+        labels = [
+            'H5D Read', 'H5D Write', 'H5D Open',
+            'H5D Flush', 'H5F Open', 'H5F Flush',
+        ]
+        # H5F is not necessarily available following
+        # gh-703
+        if not "H5F" in report.summary["agg_ioops"]:
+            report.summary['agg_ioops']['H5F'] = defaultdict(lambda: 0)
+
+        counts = [
+            report.summary['agg_ioops']['H5D']['H5D_READS'],
+            report.summary['agg_ioops']['H5D']['H5D_WRITES'],
+            report.summary['agg_ioops']['H5D']['H5D_OPENS'],
+            report.summary['agg_ioops']['H5D']['H5D_FLUSHES'],
+            report.summary['agg_ioops']['H5F']['H5F_OPENS'],
+            report.summary['agg_ioops']['H5F']['H5F_FLUSHES'],
+        ]
+
+    elif mod == 'PNETCDF_FILE':
+        labels = [
+            'Var Ind Read', 'Var Ind Write', 'Var Open',
+            'Var Coll Read', 'Var Coll Write',
+            'Var NB Read', 'Var NB Write',
+            'File Open',
+            'File Sync',
+            'File Ind Waits',
+            'File Coll Waits',
+        ]
+        counts = [
+                # most of the counters will all get set in PNETCDF_VAR
+                0, 0, 0, 0, 0, 0, 0,
+                mod_data["PNETCDF_FILE_OPENS"] + mod_data["PNETCDF_FILE_CREATES"],
+                mod_data["PNETCDF_FILE_SYNCS"],
+                mod_data['PNETCDF_FILE_INDEP_WAITS'],
+                mod_data['PNETCDF_FILE_COLL_WAITS'],
+        ]
+    return labels, counts
+
+def plot_posix_io_pattern(report: darshan.DarshanReport) -> Any:
+    """
+    Generates a bar chart summary of the POSIX read/write pattern,
+    in terms of total sequential vs consecutive operations.
+
+    Parameters
+    ----------
+    report: a ``darshan.DarshanReport``.
+
+    Returns
+    -------
+    io_pattern_fig: a ``matplotlib.pyplot.figure`` object containing a
+    bar graph of the total, sequential, and consecutive counts of POSIX
+    read/write operations.
+
+    """
+
+    labels = ['read', 'write']
+
+    #total_vals = [
+    #]
+    #seq_vals = [
+    #]
+    #consec_vals = [
+    #]
+
+    fig, ax = plt.subplots()
+
+    labels, counts = gather_count_data(report=report, mod=mod)
+
+    x = np.arange(len(labels))  # the label locations
+    rects = ax.bar(x, counts)
+
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    ax.set_ylabel('Count')
+    ax.set_xticks(x)
+    ax.set_xticklabels(labels, rotation=90)
+
+    #autolabel(ax=ax, rects=rects)
+
+    plt.tight_layout()
+
+    if fig is not None:
+        plt.close()
+        return fig
---
+++ packages/darshan/darshan-3.4.4.0.tar.gz/darshan-3.4.4.0/docs/api/pydarshan/darshan.backend.rst
@@ -0,0 +1,50 @@
+darshan.backend package
+=======================
+
+.. automodule:: darshan.backend
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Submodules
+----------
+
+darshan.backend.api\_def\_c module
+----------------------------------
+
+.. automodule:: darshan.backend.api_def_c
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+darshan.backend.apmpi module
+----------------------------
+
+.. automodule:: darshan.backend.apmpi
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+darshan.backend.apss module
+---------------------------
+
+.. automodule:: darshan.backend.apss
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+darshan.backend.apxc module
+---------------------------
+
+.. automodule:: darshan.backend.apxc
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+darshan.backend.cffi\_backend module
+------------------------------------
+
+.. automodule:: darshan.backend.cffi_backend
+   :members:
+   :undoc-members:
+   :show-inheritance:
