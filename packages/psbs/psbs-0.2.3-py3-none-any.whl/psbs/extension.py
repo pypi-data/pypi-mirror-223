719b3a2974cfffdedbbdeb8fe1f402aa6d209621
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-08 04:14:26 +0000

    Add package psbs/0.2.3/psbs-0.2.3-py3-none-any.whl

---
+++ packages/psbs/psbs-0.2.3-py3-none-any.whl/psbs/extension.py
@@ -0,0 +1,52 @@
+from os.path import join, dirname, basename, isfile
+import glob
+from importlib import import_module
+from jinja2.exceptions import TemplateError
+
+
+class Extension:
+    def __init__(self, config):
+        self.methods = {}
+        self.filters = {}
+        self.post = []
+        self.config = config
+        for key, value in self.get_config().items():
+            if key not in self.config:
+                self.config[key] = None
+            if self.config[key] is None:
+                self.config[key] = value
+
+    def register(self, name, function):
+        if name not in self.methods:
+            self.methods[name] = function
+
+    def register_filter(self, name, function):
+        if name not in self.filters:
+            self.filters[name] = function
+
+    def register_post(self, function):
+        self.post.append(function)
+
+    @staticmethod
+    def get_config():
+        return {}
+
+    @classmethod
+    def get_extensions(cls):
+        for extension in glob.glob(
+            join(dirname(__file__), "extensions", "*.py")
+        ):
+            if isfile(extension) and not extension.endswith("__init__.py"):
+                import_module(f"psbs.extensions.{basename(extension)[:-3]}")
+        return cls.__subclasses__()
+
+    @classmethod
+    def get_extension_configs(cls):
+        config_dict = {}
+        for extension in cls.get_extensions():
+            if extension.get_config():
+                config_dict[extension.__name__] = extension.get_config()
+        return config_dict
+
+    class ExtensionError(TemplateError):
+        """Thrown when the extension has a problem with the template"""
