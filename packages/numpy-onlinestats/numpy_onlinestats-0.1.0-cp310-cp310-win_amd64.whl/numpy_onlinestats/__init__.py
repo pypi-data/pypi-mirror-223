0fbc4d1a6fea6b420aabbd67a916fb270db9e583
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-08 03:10:15 +0000

    Add package numpy-onlinestats/0.1.0/numpy_onlinestats-0.1.0-cp310-cp310-win_amd64.whl

---
+++ packages/numpy-onlinestats/numpy_onlinestats-0.1.0-cp310-cp310-win_amd64.whl/numpy_onlinestats/__init__.py
@@ -0,0 +1,16 @@
+
+
+""""""# start delvewheel patch
+def _delvewheel_patch_1_4_0():
+    import os
+    libs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'numpy_onlinestats.libs'))
+    if os.path.isdir(libs_dir):
+        os.add_dll_directory(libs_dir)
+
+
+_delvewheel_patch_1_4_0()
+del _delvewheel_patch_1_4_0
+# end delvewheel patch
+
+from ._numpy_onlinestats_impl import *  # noqa F403
+from ._version import __version__, __version_tuple__
