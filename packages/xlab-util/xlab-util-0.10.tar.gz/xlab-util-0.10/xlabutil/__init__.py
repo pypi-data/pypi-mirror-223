2cc60a4111f2a3a8185a7b0d4c6ccc18f1cfb6c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-08 15:10:45 +0000

    Add package xlab-util/0.10/xlab-util-0.10.tar.gz

---
+++ packages/xlab-util/xlab-util-0.10.tar.gz/xlab-util-0.10/xlabutil/__init__.py
@@ -0,0 +1,3 @@
+from .util import array_to_base64, array_to_img, img_to_array, save_base64, to_json
+from .result import ClassificationResult, DetectionResult, PoseEstimationResult, InstancePose
+from .inference import RemoteInference, InstanceSegInference, Output, PoseEstimationInference
