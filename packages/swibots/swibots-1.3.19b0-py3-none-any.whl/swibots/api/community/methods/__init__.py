a31e6d110ed969543ba7dd7e0be2d09abe1ba79f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-08 03:10:14 +0000

    Add package swibots/1.3.19b0/swibots-1.3.19b0-py3-none-any.whl

---
+++ packages/swibots/swibots-1.3.19b0-py3-none-any.whl/swibots/api/community/methods/__init__.py
@@ -0,0 +1,32 @@
+from .get_channel import GetChannel
+from .get_community import GetCommunity
+from .get_group import GetGroup
+from .permission import PermissionMethods
+from .roles import RoleMethods
+from .rolemember import RoleMemberMethods
+from .ban_user import BanUser
+from .unban_user import UnbanUser
+from .create_channel import CreateChannel
+from .update_channel import UpdateChannel
+from .restrict_user import RestrictUser
+from .get_community_member import GetCommunityMember
+from .deduct_xp import DeductXP
+from .instant_messaging import InstantMessagingMethods
+
+class CommunityMethods(
+    GetChannel,
+    GetCommunity,
+    GetCommunityMember,
+    GetGroup,
+    RoleMethods,
+    PermissionMethods,
+    RoleMemberMethods,
+    BanUser,
+    UnbanUser,
+    CreateChannel,
+    UpdateChannel,
+    RestrictUser,
+    DeductXP,
+    InstantMessagingMethods
+):
+    pass
