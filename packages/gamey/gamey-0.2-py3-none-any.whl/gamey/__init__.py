b7b1443ecdd0e9bc5875760c47d00889637c29fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-08 03:09:46 +0000

    Add package gamey/0.2/gamey-0.2-py3-none-any.whl

---
+++ packages/gamey/gamey-0.2-py3-none-any.whl/gamey/__init__.py
@@ -0,0 +1,55 @@
+import pygame as p
+
+p.init()
+
+last_window = None
+
+class Window:
+
+	def __init__(self, title="Gamie Window", width=800, height=600):
+		self.window = p.display.set_mode((width, height))
+		p.display.set_caption(title)
+
+		self.background = (255, 255, 255)
+
+		self.sprites = []
+
+		self.events = {}
+		self.add_default_events()
+
+		last_window = self
+
+	def add_event(self, event, callback):
+		self.events[str(event)] = callback
+
+	def add_default_events(self):
+		self.add_event(p.QUIT, self.close)
+	
+	def run(self):
+		self.running = True
+		while self.running:
+			for event in p.event.get():
+				if str(event.type) in self.events:
+					for callback in self.events[str(event.type)]:
+						callback(event)
+
+			self.window.fill(self.background)
+
+			for sprite in self.sprites:
+				self.window.blit(sprite.image, (sprite.x, sprite.y))
+			
+			p.display.flip()
+			p.display.update()
+
+	def close(self, event):
+		p.quit()
+		quit()
+
+class Sprite:
+
+	def __init__(self, image_path, x=0, y=0):
+		self.image = p.image.load(image_path)
+		self.x = x
+		self.y = y
+		last_window.sprites.append(self)
+
