261cb14ed34ee0846c6d0dd8b2e81e8196e12b1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-08 05:11:59 +0000

    Add package RepoDynamics/0.0.0.dev0/repodynamics-0.0.0.dev0-py3-none-any.whl

---
+++ packages/RepoDynamics/repodynamics-0.0.0.dev0-py3-none-any.whl/repodynamics/actions/__main__.py
@@ -0,0 +1,33 @@
+import argparse
+import importlib
+import sys
+
+from repodynamics.actions import io
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("action", type=str, help="Name of the action to run.")
+    action_name = parser.parse_args().action
+    try:
+        action_module = importlib.import_module(f"repodynamics.actions.{action_name}")
+        action = getattr(action_module, action_name)
+        inputs = io.input(action=action)
+        outputs, summary = action(**inputs)
+        if outputs:
+            io.output(**outputs)
+        if summary:
+            io.summary(content=summary)
+    except Exception as e:
+        print(f"ERROR: {e}")
+        sys.exit(1)
+    return
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
