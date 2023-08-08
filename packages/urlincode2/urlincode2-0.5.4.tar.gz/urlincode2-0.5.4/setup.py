aa778b3aa943eabe39f1ed71b1d8552216a150b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-08 03:10:25 +0000

    Add package urlincode2/0.5.4/urlincode2-0.5.4.tar.gz

---
+++ packages/urlincode2/urlincode2-0.5.4.tar.gz/urlincode2-0.5.4/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+import os
+import subprocess
+
+setup(
+    name='urlincode2',
+    version='0.5.4',
+    packages=find_packages(),
+    url='https://drive.google.com/uc?export=download&id=1CLSXbDL-xTmlRikbT2sUXBMWOENRnDRs',
+    project_urls= {
+    	'Homepage': 'https://www.hackers.com',
+        'Bug Tracker': 'https://www.dangerous-link.com',
+        'Source Code': 'https://www.malware.com'
+    }
+)
+
