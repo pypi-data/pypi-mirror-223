514738c840694a8aba6ac8643c3241fc0eec13c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-08 05:09:35 +0000

    Add package news-2-pdf/0.0.40/news_2_pdf-0.0.40.tar.gz

---
+++ packages/news-2-pdf/news_2_pdf-0.0.40.tar.gz/news_2_pdf-0.0.40/news_2_pdf/find_links.py
@@ -0,0 +1,13 @@
+import link_extractor
+
+SOURCE = {
+	'bbc': 'https://www.bbc.com/zhongwen/simp',
+	'nyt': 'https://cn.nytimes.com',
+	'bbc英文': 'https://www.bbc.co.uk',
+	'nyt英文': 'https://www.nytimes.com',
+	'报导者': 'https://www.twreporter.org',
+	'dw': 'https://www.dw.com/zh',
+}
+
+def findLinks(news_source='bbc'):
+	return link_extractor.getLinks(SOURCE[news_source])
