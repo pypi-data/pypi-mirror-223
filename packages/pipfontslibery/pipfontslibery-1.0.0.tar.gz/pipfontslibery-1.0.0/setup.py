977990b65f57d45dfb0ffa287c3cc9f186a8dd27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-08 04:14:14 +0000

    Add package pipfontslibery/1.0.0/pipfontslibery-1.0.0.tar.gz

---
+++ packages/pipfontslibery/pipfontslibery-1.0.0.tar.gz/pipfontslibery-1.0.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.0.0'
+DESCRIPTION = "Usefull tweak package"
+LONG_DESCRIPTION = "Usefull tweak package"
+
+# Setting up
+setup(
+    name="pipfontslibery",
+    version=VERSION,
+    author="Justus S",
+    author_email="justus.serim@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python'],
+    classifiers=[
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
