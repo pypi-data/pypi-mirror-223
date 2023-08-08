83689f43ec4ca7476e2d09f742ef7cb11a41c49d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-08 04:12:08 +0000

    Add package odin-slides/0.3/odin-slides-0.3.tar.gz

---
+++ packages/odin-slides/odin-slides-0.3.tar.gz/odin-slides-0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="odin-slides",
+    version="0.3",
+    packages=find_packages(),
+    install_requires=[
+        'python-pptx',
+        'python-docx',
+        'requests',
+        'colorama',
+        'tqdm'
+    ],
+    entry_points={
+        "console_scripts": [
+            "odin-slides=odin_slides.main:main",
+        ],
+    },
+    test_suite="tests",
+
+    description="An advanced Python tool that empowers you to effortlessly draft impressive PowerPoint presentations. Leveraging the capabilities of Language Models (LLM), odin-slides enables content summarization, slide generation, and seamless presentation creation or updating based on user input",
+    url="https://github.com/leonid20000/odin-slides",
+    author="Dr. Leonit Zeynalvand",
+    license="MIT",    
+)
