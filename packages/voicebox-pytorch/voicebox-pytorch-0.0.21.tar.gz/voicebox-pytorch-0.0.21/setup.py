b71a8117789d2b638162057110c79b7114a6f145
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 05:07:14 +0000

    Add package voicebox-pytorch/0.0.21/voicebox-pytorch-0.0.21.tar.gz

---
+++ packages/voicebox-pytorch/voicebox-pytorch-0.0.21.tar.gz/voicebox-pytorch-0.0.21/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'voicebox-pytorch',
+  packages = find_packages(exclude=[]),
+  version = '0.0.21',
+  license='MIT',
+  description = 'Voicebox - Pytorch',
+  author = 'Phil Wang',
+  author_email = 'lucidrains@gmail.com',
+  long_description_content_type = 'text/markdown',
+  url = 'https://github.com/lucidrains/voicebox-pytorch',
+  keywords = [
+    'artificial intelligence',
+    'deep learning',
+    'text to speech'
+  ],
+  install_requires=[
+    'audiolm-pytorch',
+    'naturalspeech2-pytorch>=0.0.41',
+    'beartype',
+    'einops>=0.6.1',
+    'torch>=2.0',
+    'torchdiffeq',
+    'torchode'
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
