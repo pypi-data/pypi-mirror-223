8df0ec23304d010331976983c5e40831a1b912de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 05:08:54 +0000

    Add package cvxpy/1.2.5/cvxpy-1.2.5-cp37-cp37m-manylinux_2_24_x86_64.whl

---
+++ packages/cvxpy/cvxpy-1.2.5-cp37-cp37m-manylinux_2_24_x86_64.whl/cvxpy/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM CVXPY SETUP.PY
+short_version = '1.2.5'
+version = '1.2.5'
+full_version = '1.2.5'
+git_revision = 'c733ce1'
+commit_count = '0'
+release = True
+if not release:
+    version = full_version
