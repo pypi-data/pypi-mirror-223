7c2d876be78ffddb6e5abf7f292c7eca275b0f6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 04:14:49 +0000

    Add package helen-utils/1.0.0/helen-utils-1.0.0.tar.gz

---
+++ packages/helen-utils/helen-utils-1.0.0.tar.gz/helen-utils-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='helen-utils',
+    version='1.0.0',
+    description='Helen Utils - Utils done right!',
+    author='Nelyon',
+    author_email='pauline.darrieux@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+)
