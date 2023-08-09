c87d9faf3219b49d11918c439ffd42ad694884f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 04:14:34 +0000

    Add package publishtutorial/0.0.2/publishtutorial-0.0.2-py3-none-any.whl

---
+++ packages/publishtutorial/publishtutorial-0.0.2-py3-none-any.whl/myfunc.py
@@ -0,0 +1,8 @@
+def say_hello(name=None):
+    if name is None:
+        return "Hello, World!"
+    else:
+        return f"Hello, {name}!"
+    
+def average(a:float, b:float) -> float:
+    return (a+b)/2
