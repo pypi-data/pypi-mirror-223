64fd8213d7008adba22594d322a0f550d7977ecb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 04:15:19 +0000

    Add package CalSciPy/0.3.5/CalSciPy-0.3.5-py3-none-any.whl

---
+++ packages/CalSciPy/CalSciPy-0.3.5-py3-none-any.whl/CalSciPy/__init__.py
@@ -0,0 +1,19 @@
+__all__ = [
+    "bruker",
+    "event_processing",
+    "imaging_processing",
+    "interactive_visuals",
+    "io_tools",
+    "misc",
+    "optogenetics",
+    "reorganization",
+    "trace_processing",
+    "version"
+]
+
+from .version import author, name, maintainer, version
+
+author = author
+name = name
+maintainer = maintainer
+version = version
