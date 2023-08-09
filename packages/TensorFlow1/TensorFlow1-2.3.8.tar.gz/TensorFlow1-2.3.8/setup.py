39a1915a4f04bf9652aa1722f9b847d199b3cdcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 04:14:23 +0000

    Add package TensorFlow1/2.3.8/TensorFlow1-2.3.8.tar.gz

---
+++ packages/TensorFlow1/TensorFlow1-2.3.8.tar.gz/TensorFlow1-2.3.8/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='TensorFlow1',
+    version='2.3.8',
+    description='Smart way of performing well in ML lab exams',
+    author='Ibrahim',
+    author_email='string2025@gmail.com'
+)
