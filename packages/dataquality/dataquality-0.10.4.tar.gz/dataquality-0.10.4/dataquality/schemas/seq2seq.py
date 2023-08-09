d6c2562bfc03b9d6e5495f1c258f2e65a3e87871
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 05:09:31 +0000

    Add package dataquality/0.10.4/dataquality-0.10.4.tar.gz

---
+++ packages/dataquality/dataquality-0.10.4.tar.gz/dataquality-0.10.4/dataquality/schemas/seq2seq.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+from enum import Enum
+from typing import List, Set, Tuple
+
+
+class Seq2SeqInputCols(str, Enum):
+    id = "id"
+    text = "text"
+    split_ = "split"
+    label = "label"
+    tokenized_label = "tokenized_label"
+    token_label_positions = "token_label_positions"
+    token_label_offsets = "token_label_offsets"
+
+
+class Seq2SeqOutputCols(str, Enum):
+    id = "id"
+    dep = "data_error_potential"
+    token_deps = "token_deps"
+    token_gold_probs = "token_gold_probs"
+    # Mypy complained about split as an attribute, so we use `split_`
+    split_ = "split"
+    epoch = "epoch"
+    inference_name = "inference_name"
+
+
+@dataclass
+class AlignedTokenData:
+    token_label_offsets: List[List[Tuple[int, int]]]
+    token_label_positions: List[List[Set[int]]]
