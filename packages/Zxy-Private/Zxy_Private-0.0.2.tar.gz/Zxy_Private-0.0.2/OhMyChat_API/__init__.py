9ed11c213e1ad9d674b7f2c1aeb3401f77369775
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 03:13:36 +0000

    Add package Zxy-Private/0.0.2/Zxy_Private-0.0.2.tar.gz

---
+++ packages/Zxy-Private/Zxy_Private-0.0.2.tar.gz/Zxy_Private-0.0.2/OhMyChat_API/__init__.py
@@ -0,0 +1,21 @@
+# -*- encoding: utf-8 -*-
+'''
+@Project_name    :  __init__.py
+@ProjectDescription: ..........
+@Modify Time      @Author    @Version    @Desciption
+------------      -------    --------    -----------
+2023/8/8 16:52     zxy       1.0         None
+'''
+
+
+class Test_Class():
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+
+
+if __name__ == '__main__':
+    X = Test_Class()
+    X.run()
---
+++ packages/Zxy-Private/Zxy_Private-0.0.2.tar.gz/Zxy_Private-0.0.2/setup.py
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8 -*-
+import setuptools
+
+setuptools.setup(
+    name="Zxy_Private",
+    version = "0.0.2",
+    author="AnQing",
+    description="已更新",
+    long_description_content_type="text/markdown",
+    url="https://github.com/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
