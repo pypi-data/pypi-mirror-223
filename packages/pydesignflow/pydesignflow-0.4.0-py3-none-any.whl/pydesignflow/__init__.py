35ff09390c6fd55b8861ecd6dc9680a86dcf3e43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 03:11:22 +0000

    Add package pydesignflow/0.4.0/pydesignflow-0.4.0-py3-none-any.whl

---
+++ packages/pydesignflow/pydesignflow-0.4.0-py3-none-any.whl/pydesignflow/__init__.py
@@ -0,0 +1,9 @@
+from .version import __version__
+
+from .block import Block
+from .target import TargetId
+from .task import task, action
+from .result import Result
+from .flow import Flow
+from .errors import FlowError, ResultRequired
+from . import filemgmt
