020788d4e849238de44de6f237b0f1acaaab3783
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 03:11:19 +0000

    Add package avijit1996iiti-basic-calculator/0.0.3/avijit1996iiti_basic_calculator-0.0.3.tar.gz

---
+++ packages/avijit1996iiti-basic-calculator/avijit1996iiti_basic_calculator-0.0.3.tar.gz/avijit1996iiti_basic_calculator-0.0.3/avijit1996iiti_basic_calculator/multiplication.py
@@ -0,0 +1,6 @@
+class Multiplication:
+    def multiply_two_numbers(self,num1 : float, num2 : float)->float:
+        """ 
+        This function takes two numbers as input and returns the multiplication of second one with the first one
+        """
+        return num1 * num2
---
+++ packages/avijit1996iiti-basic-calculator/avijit1996iiti_basic_calculator-0.0.3.tar.gz/avijit1996iiti_basic_calculator-0.0.3/run.py
@@ -0,0 +1,4 @@
+from avijit1996iiti_basic_calculator.addition import Addition
+
+add = Addition()
+print(add.add_two_numbers(8,9))
