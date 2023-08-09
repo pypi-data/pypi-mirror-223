9299ebd1edfc2222c123e0341ede51db3b5bd250
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 06:18:36 +0000

    Add package xesapi/1.0.0/xesapi-1.0.0.tar.gz

---
+++ packages/xesapi/xesapi-1.0.0.tar.gz/xesapi-1.0.0/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+setuptools.setup(
+    name='xesapi',
+    version='1.0.0',
+    author='bob',
+    author_email='56141537@qq.com',
+    description='XESAPI',
+    long_description_content_type="""text/markdown""",
+    url='https://www.github.com/',
+    packages=['xesapi'],
+    install_requires=["requests"],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+)
