0e9b81f95e0be6fad9a2cf36ff3e9b2738fddd1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 03:13:41 +0000

    Add package somatorio-alisson-rodrigov3/0.0.1/somatorio_alisson_rodrigov3-0.0.1.tar.gz

---
+++ packages/somatorio-alisson-rodrigov3/somatorio_alisson_rodrigov3-0.0.1.tar.gz/somatorio_alisson_rodrigov3-0.0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Selenium automation'
+LONG_DESCRIPTION = 'Selenium automation'
+
+#setting up
+setup(
+        #the name must match the folder name 'verysimplemodule'
+        name="somatorio_alisson_rodrigov3",
+        version=VERSION,
+        author="Alisson Rodrigo",
+        author_email="<alissorodrigo098@gmail.com>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=['selenium'],
+        
+        keywords=['python', 'selenium', 'automation'],
+        classifiers= [
+            "Development Status :: 1 - Planning",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+            "Operating System :: Unix",
+            "Operating System :: POSIX :: Linux",
+        ]
+)
