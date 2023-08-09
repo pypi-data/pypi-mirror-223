27ea7afe60df830c1e788dfc0ffe82c2b3935039
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 05:07:17 +0000

    Add package django-redis-metrics/2.2.0/django-redis_metrics-2.2.0.tar.gz

---
+++ packages/django-redis-metrics/django-redis_metrics-2.2.0.tar.gz/django-redis_metrics-2.2.0/redis_metrics/__init__.py
@@ -0,0 +1,9 @@
+VERSION = "2.2.0"
+__version__ = VERSION
+
+try:
+    from .utils import gauge, metric, set_metric  # NOQA
+except ImportError:  # pragma: no cover
+    pass  # pragma: no cover
+
+default_app_config = "redis_metrics.apps.RedisMetricsConfig"
