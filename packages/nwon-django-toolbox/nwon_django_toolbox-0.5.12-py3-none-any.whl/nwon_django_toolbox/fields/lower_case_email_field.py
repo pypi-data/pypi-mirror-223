b7279689d32162b79433e95e4cc5b55a3207c800
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 03:13:37 +0000

    Add package nwon-django-toolbox/0.5.12/nwon_django_toolbox-0.5.12-py3-none-any.whl

---
+++ packages/nwon-django-toolbox/nwon_django_toolbox-0.5.12-py3-none-any.whl/nwon_django_toolbox/fields/lower_case_email_field.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+class LowercaseEmailField(models.EmailField):
+    """
+    Override EmailField to convert emails to lowercase before saving.
+    """
+
+    def to_python(self, value):
+        value = super(LowercaseEmailField, self).to_python(value)
+
+        if isinstance(value, str):
+            return value.lower()
+
+        return value
+
+    def from_db_value(self, value, expression, connection):
+        if isinstance(value, str):
+            return value.lower()
+
+        return value
