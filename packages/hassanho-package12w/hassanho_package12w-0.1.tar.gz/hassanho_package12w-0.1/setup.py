8d0adc9f5bbae9e658b51112c41aabaa52a709d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 04:14:21 +0000

    Add package hassanho-package12w/0.1/hassanho_package12w-0.1.tar.gz

---
+++ packages/hassanho-package12w/hassanho_package12w-0.1.tar.gz/hassanho_package12w-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+DESCRIPTION = ''
+setup(
+    name='hassanho_package12w',
+    version='0.1',
+    author ="Hassan Alhomsi",
+    packages=find_packages(),
+    install_requires=[
+    ],
+    description =DESCRIPTION
+)
