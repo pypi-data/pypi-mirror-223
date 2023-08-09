3537159171983d8bc418563792c09cf4237016fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 04:14:24 +0000

    Add package csle-rest-api/0.2.19/csle_rest_api-0.2.19-py3-none-any.whl

---
+++ packages/csle-rest-api/csle_rest_api-0.2.19-py3-none-any.whl/csle_rest_api/util/rest_api_util.py
@@ -0,0 +1,62 @@
+from typing import Tuple, Union
+from flask import Response, jsonify
+import csle_common.constants.constants as constants
+from csle_common.dao.management.management_user import ManagementUser
+from csle_common.metastore.metastore_facade import MetastoreFacade
+import csle_rest_api.constants.constants as api_constants
+
+
+def check_if_user_is_authorized(request, requires_admin: bool = False) -> Union[None, Tuple[Response, int]]:
+    """
+    Checks if a user request is authorized
+
+    :param request: the request to check
+    :param requires_admin: boolean flag indicating whether only admins are authorized or not
+    :return: the non-authorized response or None
+    """
+    # Extract token and check if user is authorized
+    token = request.args.get(api_constants.MGMT_WEBAPP.TOKEN_QUERY_PARAM)
+    token_obj = MetastoreFacade.get_session_token_metadata(token=token)
+    if token_obj is None:
+        response = jsonify({})
+        response.headers.add(api_constants.MGMT_WEBAPP.ACCESS_CONTROL_ALLOW_ORIGIN_HEADER, "*")
+        return response, constants.HTTPS.UNAUTHORIZED_STATUS_CODE
+
+    user = None
+    if requires_admin:
+        user = MetastoreFacade.get_management_user_by_username(username=token_obj.username)
+    if token_obj is None or token_obj.expired(valid_length_hours=api_constants.SESSION_TOKENS.EXPIRE_TIME_HOURS) \
+            or (requires_admin and user is not None and not user.admin):
+        if token_obj is not None:
+            MetastoreFacade.remove_session_token(session_token=token_obj)
+        response = jsonify({})
+        response.headers.add(api_constants.MGMT_WEBAPP.ACCESS_CONTROL_ALLOW_ORIGIN_HEADER, "*")
+        return response, constants.HTTPS.UNAUTHORIZED_STATUS_CODE
+    else:
+        return None
+
+
+def check_if_user_edit_is_authorized(request, user: ManagementUser) -> Union[None, ManagementUser,
+                                                                             Tuple[Response, int]]:
+    """
+    Check if a user is authorized to edit another user
+
+    :param request: the request to extract the user from
+    :return: the non-authorized response or the user
+    """
+
+    # Extract token and check if user is authorized
+    token = request.args.get(api_constants.MGMT_WEBAPP.TOKEN_QUERY_PARAM)
+    token_obj = MetastoreFacade.get_session_token_metadata(token=token)
+    request_user = None
+    if token_obj is not None:
+        request_user = MetastoreFacade.get_management_user_by_username(username=token_obj.username)
+    if token_obj is None or token_obj.expired(valid_length_hours=api_constants.SESSION_TOKENS.EXPIRE_TIME_HOURS) \
+            or request_user is None or (not request_user.admin and request_user.username != user.username):
+        if token_obj is not None:
+            MetastoreFacade.remove_session_token(session_token=token_obj)
+        response = jsonify({})
+        response.headers.add(api_constants.MGMT_WEBAPP.ACCESS_CONTROL_ALLOW_ORIGIN_HEADER, "*")
+        return response, constants.HTTPS.UNAUTHORIZED_STATUS_CODE
+    else:
+        return request_user
