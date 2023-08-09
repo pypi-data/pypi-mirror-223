8900bfcc7ae35570dad8ec557fd24c2fb3a1c1fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 03:13:36 +0000

    Add package napari-superres/0.1.1/napari-superres-0.1.1.tar.gz

---
+++ packages/napari-superres/napari-superres-0.1.1.tar.gz/napari-superres-0.1.1/src/napari_superres/__init__.py
@@ -0,0 +1,21 @@
+
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "unknown"
+
+from ._reader import napari_get_reader
+from ._gui_creator import mssr_caller, esi_caller, sofi_caller, srrf_caller, musical_caller
+from ._writer import write_multiple, write_single_image
+
+__all__ = (
+    "napari_get_reader",
+    "write_single_image",
+    "write_multiple",
+    "mssr_caller",
+    "esi_caller",
+    "sofi_caller",
+    "srrf_caller",
+    "musical_caller",
+    "SplitChannelsWidget",
+)
