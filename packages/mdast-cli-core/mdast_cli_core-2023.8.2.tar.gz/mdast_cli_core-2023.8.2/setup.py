6668a7fa9e655e6b8f6c2e448784f2e30aaa1698
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 05:09:03 +0000

    Add package mdast-cli-core/2023.8.2/mdast_cli_core-2023.8.2.tar.gz

---
+++ packages/mdast-cli-core/mdast_cli_core-2023.8.2.tar.gz/mdast_cli_core-2023.8.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="mdast_cli_core",
+    version='2023.08.02',
+    author="Dynamic-Mobile-Security",
+    description="mDast core package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Dynamic-Mobile-Security/mdast-cli-core",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+         'requests > 2.20'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Unix",
+    ],
+)
