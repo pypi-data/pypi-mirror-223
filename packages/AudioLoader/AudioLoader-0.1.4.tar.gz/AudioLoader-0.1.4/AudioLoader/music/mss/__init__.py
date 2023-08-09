5d86e1b5e475f861d79362e2bcb8c1162b4cb21c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 03:11:26 +0000

    Add package AudioLoader/0.1.4/AudioLoader-0.1.4.tar.gz

---
+++ packages/AudioLoader/AudioLoader-0.1.4.tar.gz/AudioLoader-0.1.4/AudioLoader/music/mss/__init__.py
@@ -0,0 +1,3 @@
+from .fastmusdb import FastMUSDB
+from .musdbhq import MusdbHQ
+from .moisesdb23 import Moisesdb23
---
+++ packages/AudioLoader/AudioLoader-0.1.4.tar.gz/AudioLoader-0.1.4/setup.py
@@ -0,0 +1,39 @@
+import setuptools
+import codecs
+import os.path
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()    
+    
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")    
+    
+setuptools.setup(
+    name="AudioLoader", # Replace with your own username
+    version=get_version("AudioLoader/__init__.py"),
+    author="KinWaiCheuk",
+    author_email="u3500684@connect.hku.hk",
+    description="A collection of PyTorch audio datasets for speech and music applications",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/KinWaiCheuk/AudioLoader",
+    packages=setuptools.find_packages(),
+    include_package_data=True,    
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    pytorch='>=1.6'
+)
