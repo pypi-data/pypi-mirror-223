f2b75f5d55fc2ae2c6ff3a848859470415dade07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 03:13:37 +0000

    Add package HayderCul/0.1/HayderCul-0.1.tar.gz

---
+++ packages/HayderCul/HayderCul-0.1.tar.gz/HayderCul-0.1/setup.py
@@ -0,0 +1,8 @@
+import setuptools
+setuptools.setup(
+    name='HayderCul',
+    version='0.1',
+    author='Hayder-NamuQ',
+    description='simple library in python for test',
+    packages=setuptools.find_packages()
+)
