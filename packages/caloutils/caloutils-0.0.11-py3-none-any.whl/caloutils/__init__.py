6dacef8d9917e24eeefcfe592b8adc9908fafeb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 03:13:38 +0000

    Add package caloutils/0.0.11/caloutils-0.0.11-py3-none-any.whl

---
+++ packages/caloutils/caloutils-0.0.11-py3-none-any.whl/caloutils/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for caloutils."""
+
+__author__ = """mova"""
+__email__ = "mova@users.noreply.github.com"
+__version__ = '0.0.11'  # fmt: skip
+
+from .calorimeter import calorimeter
+
+init_calorimeter = calorimeter.init_calorimeter
