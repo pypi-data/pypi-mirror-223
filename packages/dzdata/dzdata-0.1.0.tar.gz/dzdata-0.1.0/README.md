a9c5955639cba743311a4887ee47368c6db98c66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 03:13:36 +0000

    Add package dzdata/0.1.0/dzdata-0.1.0.tar.gz

---
+++ packages/dzdata/dzdata-0.1.0.tar.gz/dzdata-0.1.0/README.md
@@ -0,0 +1,3 @@
+# datafiles
+
+A library to create data objects saved either in a directory or in a zip file.
---
+++ packages/dzdata/dzdata-0.1.0.tar.gz/dzdata-0.1.0/dzdata/__init__.py
@@ -0,0 +1,7 @@
+from .field import attribute, file_entry
+from .datafiles import DatafilesBase
+from .dataunit import DataUnitBase
+from .dataset import DatasetBase
+from .datafiles import load_datafiles
+from .datafiles import load_datafiles as load_dataset
+from .datafiles import NotADatafilesException
