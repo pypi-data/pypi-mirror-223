e2b0e3214a3ec0c356342eb178b8ee52715cf755
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 03:10:34 +0000

    Add package atas-client/0.0.5/atas_client-0.0.5.tar.gz

---
+++ packages/atas-client/atas_client-0.0.5.tar.gz/atas_client-0.0.5/src/atas_client/atas_client.py
@@ -0,0 +1,27 @@
+from .CoreATASApi import CoreATASApi
+
+
+def init(endpoints, token, launch_name, executor):
+    return CoreATASApi.init_result(endpoints, token, launch_name, executor)
+
+
+def upload_result(endpoints, token, execution_id, case_name, step_desc, step_status, case_status, executor, path,
+                  screenshot,
+                  attachment, start_time, end_time, test_plan_case_id):
+    data = {'caseName': case_name,
+            'path': path,
+            'stepStatus': step_status,
+            'stepDescription': step_desc,
+            'caseStatus': case_status,
+            'executor': executor,
+            'screen_path': screenshot,
+            'attachment_path': attachment,
+            'executionId': execution_id,
+            'startTime': start_time,
+            'endTime': end_time,
+            'testPlanCaseId': test_plan_case_id}
+    CoreATASApi.save_result(endpoints, token, data)
+
+
+def complete(endpoints, token, execution_id, case_status):
+    CoreATASApi.run_complete(endpoints, token, execution_id, case_status)
