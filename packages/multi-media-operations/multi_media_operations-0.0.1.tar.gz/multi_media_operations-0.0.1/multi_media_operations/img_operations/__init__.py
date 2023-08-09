40e80ce96d15024224a2d4adc02690184dca32ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 03:11:35 +0000

    Add package multi-media-operations/0.0.1/multi_media_operations-0.0.1.tar.gz

---
+++ packages/multi-media-operations/multi_media_operations-0.0.1.tar.gz/multi_media_operations-0.0.1/multi_media_operations/img_operations/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+@Time : 2023/8/8 17:40 
+@Author : skyoceanchen
+@TEL: 18916403796
+@项目：multi_media_operations
+@File : __init__.by
+@PRODUCT_NAME :PyCharm
+"""
+from basic_operation import *
+from img_info import *
+from mid_operation import *
+from qrcode_operation import *
