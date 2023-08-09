9aa39bc392d99cec7a841d4805f7c37e8baf8e26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 04:14:23 +0000

    Add package popgym/1.0.3/popgym-1.0.3-py3-none-any.whl

---
+++ packages/popgym/popgym-1.0.3-py3-none-any.whl/popgym/envs/labyrinth_explore.py
@@ -0,0 +1,77 @@
+from typing import Any, Dict, Optional, Tuple
+
+import gymnasium as gym
+import numpy as np
+from gymnasium.core import ActType, ObsType
+
+from popgym.core.maze import Actions, Cell, Explored, MazeEnv
+
+
+class LabyrinthExplore(MazeEnv):
+    """A maze environment where the agent receives reward for visiting
+    new grid squares
+
+    Args:
+        maze_dims: (heigh, width) of the generated mazes in blocks
+        episode_length: maximum length of an episode
+
+    Returns:
+        A gym environment
+
+    """
+
+    def __init__(self, maze_dims=(10, 10), episode_length=1024):
+        super().__init__(maze_dims, episode_length)
+        self.neg_reward_scale = -1 / self.max_episode_length
+
+    def step(self, action: ActType) -> Tuple[ObsType, float, bool, bool, dict]:
+        new_square = self.explored[tuple(self.move(action))] == Explored.NO
+        super().step(action)
+        reward = self.neg_reward_scale
+        terminated = truncated = False
+        y, x = self.position
+        if new_square:
+            reward = self.pos_reward_scale
+        if self.curr_step == self.max_episode_length - 1:
+            truncated = True
+        free_mask = self.maze.grid != Cell.OBSTACLE
+        visit_mask = self.explored == Explored.YES
+        if np.all(free_mask == visit_mask):
+            # Explored as much as possible
+            terminated = True
+
+        obs = self.get_obs(action)
+        info = {"position": (x, y)}
+
+        return obs, reward, terminated, truncated, info
+
+    def render(self):
+        print(self.tostring(start=True, end=False, agent=True, visited=True))
+
+    def reset(
+        self,
+        *,
+        seed: Optional[int] = None,
+        options: Optional[dict] = None,
+    ) -> Tuple[gym.core.ObsType, Dict[str, Any]]:
+        super().reset(seed=seed)
+        # Based on free space
+        self.pos_reward_scale = 1 / ((self.maze.grid == Cell.FREE).sum())
+        y, x = self.position
+        obs = self.get_obs(Actions.NONE)
+        return obs, {"maze": str(self.maze.grid)}
+
+
+class LabyrinthExploreEasy(LabyrinthExplore):
+    def __init__(self):
+        super().__init__(maze_dims=(10, 10), episode_length=1024)
+
+
+class LabyrinthExploreMedium(LabyrinthExplore):
+    def __init__(self):
+        super().__init__(maze_dims=(14, 14), episode_length=1024)
+
+
+class LabyrinthExploreHard(LabyrinthExplore):
+    def __init__(self):
+        super().__init__(maze_dims=(18, 18), episode_length=1024)
---
+++ packages/popgym/popgym-1.0.3-py3-none-any.whl/popgym/envs/noisy_stateless_cartpole.py
@@ -0,0 +1,48 @@
+from typing import Optional, Tuple
+
+import gymnasium as gym
+import numpy as np
+from gymnasium.core import ActType, ObsType
+
+from popgym.envs.stateless_cartpole import StatelessCartPole
+
+
+class NoisyStatelessCartPole(StatelessCartPole):
+    def __init__(self, *args, **kwargs):
+        noise_sigma = kwargs.pop("noise_sigma", 0.1)
+        super().__init__(*args, **kwargs)
+        self.noise_sigma = np.full(self.observation_space.shape, noise_sigma)
+
+    def step(self, action: ActType) -> Tuple[ObsType, float, bool, bool, dict]:
+        next_obs, reward, terminated, truncated, info = super().step(action)
+        next_obs = self.add_noise_to_obs(next_obs)
+        return next_obs, reward, terminated, truncated, info
+
+    def add_noise_to_obs(self, obs):
+        noise = self.np_random.normal(0, self.noise_sigma).astype(np.float32)
+        obs_space = self.observation_space
+        obs = np.clip(obs + noise, obs_space.low, obs_space.high)
+        return obs
+
+    def reset(
+        self, *, seed: Optional[int] = None, options: Optional[dict] = None
+    ) -> Tuple[gym.core.ObsType, dict]:
+
+        init_obs, info = super().reset(seed=seed, options=options)
+        init_obs = self.add_noise_to_obs(init_obs)
+        return init_obs, info
+
+
+class NoisyStatelessCartPoleEasy(NoisyStatelessCartPole):
+    def __init__(self):
+        super().__init__(noise_sigma=0.1)
+
+
+class NoisyStatelessCartPoleMedium(NoisyStatelessCartPole):
+    def __init__(self):
+        super().__init__(noise_sigma=0.2)
+
+
+class NoisyStatelessCartPoleHard(NoisyStatelessCartPole):
+    def __init__(self):
+        super().__init__(noise_sigma=0.3)
