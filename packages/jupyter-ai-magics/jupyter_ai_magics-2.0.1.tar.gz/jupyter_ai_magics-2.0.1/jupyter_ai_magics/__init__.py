928c3ccf665627eadd34ff085fc9f1ce55f1adaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 05:10:04 +0000

    Add package jupyter-ai-magics/2.0.1/jupyter_ai_magics-2.0.1.tar.gz

---
+++ packages/jupyter-ai-magics/jupyter_ai_magics-2.0.1.tar.gz/jupyter_ai_magics-2.0.1/jupyter_ai_magics/__init__.py
@@ -0,0 +1,33 @@
+from ._version import __version__
+
+# expose embedding model providers on the package root
+from .embedding_providers import (
+    CohereEmbeddingsProvider,
+    HfHubEmbeddingsProvider,
+    OpenAIEmbeddingsProvider,
+)
+from .exception import store_exception
+from .magics import AiMagics
+
+# expose model providers on the package root
+from .providers import (
+    AI21Provider,
+    AnthropicProvider,
+    BaseProvider,
+    BedrockProvider,
+    ChatOpenAINewProvider,
+    ChatOpenAIProvider,
+    CohereProvider,
+    HfHubProvider,
+    OpenAIProvider,
+    SmEndpointProvider,
+)
+
+
+def load_ipython_extension(ipython):
+    ipython.register_magics(AiMagics)
+    ipython.set_custom_exc((BaseException,), store_exception)
+
+
+def unload_ipython_extension(ipython):
+    ipython.set_custom_exc((BaseException,), ipython.CustomTB)
