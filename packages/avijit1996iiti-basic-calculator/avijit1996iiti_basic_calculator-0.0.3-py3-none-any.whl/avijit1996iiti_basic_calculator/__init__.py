544ec43d7f55fc5fad02073fa48c20443d52ba19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 03:11:18 +0000

    Add package avijit1996iiti-basic-calculator/0.0.3/avijit1996iiti_basic_calculator-0.0.3-py3-none-any.whl

---
+++ packages/avijit1996iiti-basic-calculator/avijit1996iiti_basic_calculator-0.0.3-py3-none-any.whl/avijit1996iiti_basic_calculator/__init__.py
@@ -0,0 +1,4 @@
+from avijit1996iiti_basic_calculator.addition import Addition
+from avijit1996iiti_basic_calculator.substraction import Substraction
+from avijit1996iiti_basic_calculator.multiplication import Multiplication
+from avijit1996iiti_basic_calculator.division import Division
---
+++ packages/avijit1996iiti-basic-calculator/avijit1996iiti_basic_calculator-0.0.3-py3-none-any.whl/avijit1996iiti_basic_calculator/calculator.py
@@ -0,0 +1,11 @@
+from addition import Addition
+from substraction import Substraction
+from multiplication import Multiplication
+from division import Division
+
+class Calculator(Addition,Substraction,Multiplication,Division):
+    """
+    This is an empty class created to inherit all required operations
+    """
+    pass
+
