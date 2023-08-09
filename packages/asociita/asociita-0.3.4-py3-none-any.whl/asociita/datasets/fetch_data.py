d4a06ab0f535e5d6f76fc9e833507e9c36eddafa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 03:11:20 +0000

    Add package asociita/0.3.4/asociita-0.3.4-py3-none-any.whl

---
+++ packages/asociita/asociita-0.3.4-py3-none-any.whl/asociita/datasets/fetch_data.py
@@ -0,0 +1,74 @@
+import logging
+import datasets
+from asociita.datasets.load_mnist import load_mnist
+from asociita.datasets.load_cifar import load_cifar
+from asociita.datasets.load_fmnist import load_fmnist
+from asociita.datasets.save_blueprint import save_blueprint
+import pickle
+import os
+
+def load_data(settings:dict) -> list[datasets.arrow_dataset.Dataset,
+                                       list[list[list[datasets.arrow_dataset.Dataset]]]]:
+    """Loads the MNIST dataset, splits it into the number of shards, pre-process selected
+    shards (subsets) and returns in a following format:
+    list[   
+        "Orchestrator Data"[
+            Dataset
+            ],   
+        "Agents Data"[
+            "Agent N"[
+                "Train Data"[
+                Dataset
+                ],
+                "Test Data"[
+                Dataset
+                ]
+            ]]]
+    Where all 'Datasets' are an instances of hugging face container datasets.arrow_dataset.Dataset
+    ---------
+    Args:
+        settings (dict) : A dictionary containing all the dataset settings.
+    Returns:
+        list[datasets.arrow_dataset.Dataset,
+                                       list[list[list[datasets.arrow_dataset.Dataset]]]]"""
+    
+    dataset_name = settings['dataset_name']
+    if dataset_name == 'mnist':
+        loaded_dataset = load_mnist(settings=settings)
+        if settings['save_dataset'] == True:
+            dataset_name = f"MNIST_{settings['shards']}_dataset"
+            path = os.path.join(settings['save_path'], dataset_name)
+            with open(path, 'wb') as file:
+                pickle.dump(loaded_dataset, file)
+        if settings['save_blueprint'] == True:
+            blueprint_name = f"MNIST_{settings['shards']}_dataset_blueprint.csv"
+            path = os.path.join(settings['save_path'], blueprint_name)
+            save_blueprint(loaded_dataset, path)
+        return loaded_dataset
+    elif dataset_name == 'cifar10':
+        loaded_dataset = load_cifar(settings=settings)
+        if settings['save_dataset'] == True:
+            dataset_name = f"CIFAR10_{settings['shards']}_dataset"
+            path = os.path.join(settings['save_path'], dataset_name)
+            with open(path, 'wb') as file:
+                pickle.dump(loaded_dataset, file)
+        if settings['save_blueprint'] == True:
+            blueprint_name = f"CIFAR10_{settings['shards']}_dataset_blueprint.csv"
+            path = os.path.join(settings['save_path'], blueprint_name)
+            save_blueprint(loaded_dataset, path)
+        return loaded_dataset
+    elif dataset_name == 'fmnist':
+        loaded_dataset = load_fmnist(settings=settings)
+        if settings['save_dataset'] == True:
+            dataset_name = f"FMNIST_{settings['shards']}_dataset"
+            path = os.path.join(settings['save_path'], dataset_name)
+            with open(path, 'wb') as file:
+                pickle.dump(loaded_dataset, file)
+        if settings['save_blueprint'] == True:
+            blueprint_name = f"FMNIST_{settings['shards']}_dataset_blueprint.csv"
+            path = os.path.join(settings['save_path'], blueprint_name)
+            save_blueprint(loaded_dataset, path)
+        return loaded_dataset
+    else:
+        logging.warning("Wrong name of the dataset. Please provide a valid name.")
+       
---
+++ packages/asociita/asociita-0.3.4-py3-none-any.whl/asociita/datasets/load_mnist.py
@@ -0,0 +1,64 @@
+import datasets
+from datasets import load_dataset
+from asociita.datasets.shard_transformation import Shard_Transformation
+from asociita.datasets.shard_splits import Shard_Splits
+from asociita.utils.showcase import save_random
+from asociita.utils.handlers import Handler
+import copy
+import pandas as pd
+import numpy as np
+
+def load_mnist(settings: dict) -> list[datasets.arrow_dataset.Dataset,
+                                       list[list[list[datasets.arrow_dataset.Dataset]]]]:
+    """Loads the MNIST dataset, splits it into the number of shards, pre-process selected
+    shards (subsets) and returns in a following format:
+    list[   
+        "Orchestrator Data"[
+            Dataset
+            ],   
+        "Agents Data"[
+            "Agent N"[
+                "Train Data"[
+                Dataset
+                ],
+                "Test Data"[
+                Dataset
+                ]
+            ]]]
+    Where all 'Datasets' are an instances of hugging face container datasets.arrow_dataset.Dataset
+    ---------
+    Args:
+        settings (dict) : A dictionary containing all the dataset settings.
+    Returns:
+        list[datasets.arrow_dataset.Dataset,
+                                       list[list[list[datasets.arrow_dataset.Dataset]]]]"""
+    
+    # Using the 'test' data as a orchestrator validaiton set.
+    orchestrator_data = load_dataset('mnist', split='test')
+    # Using the 'train' data as a dataset reserved for agents
+    dataset = load_dataset('mnist', split='train')
+
+    # Type: Homogeneous Size and Distribution (Sharding) -> Same size, similar distribution 
+    if settings['split_type'] == 'homogeneous':
+        return [orchestrator_data, Shard_Splits.homogeneous(dataset=dataset, settings=settings)]
+    
+    # Type: Heterogeneous Size, Homogeneous Distribution -> Differeny size (draws from exponential distribution), similar distribution
+    elif settings['split_type'] == 'heterogeneous_size':
+        return [orchestrator_data, Shard_Splits.heterogeneous_size(dataset=dataset, settings=settings)]
+    
+    # Type: Dominant clients are sampled first according to the pre-defined in-sample distribution. Then rest of the clients draws from 
+    # left-over data instances
+    elif settings['split_type'] == "dominant_sampling":
+        return [orchestrator_data, Shard_Splits.dominant_sampling(dataset=dataset, settings=settings)]
+
+    # Type: Dataset replication -> One dataset copied n times.
+    elif settings['split_type'] == 'replicate_same_dataset':
+        return [orchestrator_data, Shard_Splits.replicate_same_dataset(dataset=dataset, settings=settings)]
+    
+    # Type: Blocks - One dataset copied inside one block (cluster)
+    elif settings['split_type'] == 'split_in_blocks':
+        return [orchestrator_data, Shard_Splits.replicate_same_dataset(dataset=dataset, settings=settings)]
+    
+    else:
+        raise "Unable to generate the dataset. Provided split-type does not exist."
+            
