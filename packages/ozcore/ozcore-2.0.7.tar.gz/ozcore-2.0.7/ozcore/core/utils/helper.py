769dd916c6da95d2f4c8d1980b4e60492f4eb1ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 03:13:41 +0000

    Add package ozcore/2.0.7/ozcore-2.0.7.tar.gz

---
+++ packages/ozcore/ozcore-2.0.7.tar.gz/ozcore-2.0.7/ozcore/core/utils/helper.py
@@ -0,0 +1,88 @@
+"""Helper functions
+
+dirme::
+
+    core.dirme(some_class)
+    
+now_prefix::
+
+    core.now_prefix(separator='_', format='now')
+
+serialize_a_jason_field::
+
+    core.serialize_a_jason_field(some_json_content)
+
+"""
+import ast  # for safe eval of list nodes in json fields (ast.literal_eval(s))
+import datetime
+from typing import Union
+
+import numpy as np
+import pandas as pd
+from pandas.core.frame import DataFrame
+from typeguard import typechecked
+
+
+@typechecked
+def now_prefix(separator: str = "-", format: str = "now") -> str:
+    """datetime today or now as prefix
+
+    parameters:
+        separator:str, default None, a seperator string for date and time
+        format:str, default now, ::
+
+                ("now")=> "%y%m%d-%H%M%S"
+                ("today")=> "%y%m%d"
+                ("or any valid format")=> "%y%m%d-%H%M%S"
+
+    returns:
+        str
+
+    hint:
+        useful for naming files or folders
+    """
+    
+    if format == "now":
+        format = "%y%m%d" + separator + "%H%M%S"
+    elif format == "today":
+        format = "%y%m%d"
+
+    return datetime.datetime.today().strftime(format)
+
+@typechecked
+def serialize_a_json_field(val, node: Union[str, None] = None) -> Union[set, dict, list, str]:
+    """Safely eval a field with a string list or dict inherited from a json file
+        e.g. [{name:test}] => list object having dict node 'name'
+
+    parameters:
+        val: json | dict, field value passed
+        node: str, key name in the dictionary
+
+    returns:
+        * semicolon seperated values if val is a set, dict or list
+        * if node is given, returns the values in the node as semicolon separated string
+        * if val is None, returns None
+        * if fails to the operation returns back the val itself
+
+    hint:
+        useful in serializing fields in a dataframe having dict like objects
+    """
+    
+    if val == np.nan:
+        return val  # return NaN values back
+
+    try:
+        val = ast.literal_eval(
+            str(val)
+        )  # first be sure it is str then eval as dict/list object
+
+        if node:
+            if isinstance(val, dict) and (node in val.keys()):
+                val = val.get(node)  # get the node values
+
+        val = set(list(val))  # return a list
+        val = sorted(val)
+        return ";".join(val)  # return a string separated by ;
+
+    except:
+        return val  # if try is not successful, return back the value
