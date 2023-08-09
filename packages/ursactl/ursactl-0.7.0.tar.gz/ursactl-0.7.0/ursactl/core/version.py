5f88e2380a46f5e95a5aa7bf6880f79102ba36a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 06:18:32 +0000

    Add package ursactl/0.7.0/ursactl-0.7.0.tar.gz

---
+++ packages/ursactl/ursactl-0.7.0.tar.gz/ursactl-0.7.0/ursactl/core/version.py
@@ -0,0 +1,9 @@
+
+from cement.utils.version import get_version as cement_get_version
+
+VERSION = (0, 7, 0)
+PRERELEASE = ('final', 0)
+
+
+def get_version(version=VERSION):
+    return cement_get_version(version + PRERELEASE)
