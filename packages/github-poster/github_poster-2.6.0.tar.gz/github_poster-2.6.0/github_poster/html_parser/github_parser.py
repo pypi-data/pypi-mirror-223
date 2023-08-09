05da055e6ac2ef4844b6b0c4ca1aa6f50c57f449
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 03:12:42 +0000

    Add package github-poster/2.6.0/github_poster-2.6.0.tar.gz

---
+++ packages/github-poster/github_poster-2.6.0.tar.gz/github_poster-2.6.0/github_poster/html_parser/github_parser.py
@@ -0,0 +1,38 @@
+import html.parser
+
+
+class GitHubParser(html.parser.HTMLParser):
+    def __init__(self):
+        html.parser.HTMLParser.__init__(self)
+        self.recording = False
+        self.rects = []
+
+    def handle_starttag(self, tag, attrs):
+        if tag == "td":
+            self.rects.append(dict(attrs))
+            self.recording = True
+
+    def handle_data(self, data):
+        if self.recording:
+            contributions_texts = data.split("contribution")
+            print(contributions_texts)
+            if contributions_texts:
+                self.rects[-1]["data-count"] = contributions_texts[0].rstrip()
+
+    def handle_endtag(self, tag):
+        if tag == "td":
+            self.recording = False
+
+    def make_contribution_dict(self, text):
+        self.feed(text)
+        number_by_date_dict = {}
+        for r in self.rects:
+            if r.get("data-count"):
+                try:
+                    number = int(r.get("data-count"))
+                    if number:
+                        number_by_date_dict[r["data-date"]] = number
+                except Exception:
+                    # just ignore it
+                    pass
+        return number_by_date_dict
