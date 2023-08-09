c614307b53ebd7e5ac6f7279053b26fdaff2f9a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 04:14:49 +0000

    Add package declination/1.0.2/declination-1.0.2.tar.gz

---
+++ packages/declination/declination-1.0.2.tar.gz/declination-1.0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='declination',
+    version='1.0.2',
+    description='Calculates declination given a latitude and longitude.',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    package_data={
+        'declination': ['WMM.COF'],
+    },
+    author='Andrew Humphrey',
+    author_email='digiwhale.humphrey@gmail.com',
+    license='MIT',
+    url='https://github.com/DigiWhale/declination',
+    install_requires=[
+        # No third-party dependencies based on the imports you've mentioned
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Topic :: Scientific/Engineering :: GIS",
+    ],
+)
---
+++ packages/declination/declination-1.0.2.tar.gz/declination-1.0.2/tests/test_declination.py
@@ -0,0 +1,1 @@
+from declination import geomag
