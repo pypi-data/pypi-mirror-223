75152cd83814bbf29e8b7cc9a15ae8cbe050eb90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 03:12:22 +0000

    Add package aiotieba/3.6.1/aiotieba-3.6.1-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/aiotieba/aiotieba-3.6.1-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/aiotieba/api/_classdef/__init__.py
@@ -0,0 +1,25 @@
+from ...core import Account
+from .common import TypeMessage
+from .container import Containers
+from .contents import (
+    FragAt,
+    FragEmoji,
+    FragImage,
+    FragItem,
+    FragLink,
+    FragmentUnknown,
+    FragText,
+    FragTiebaPlus,
+    TypeFragAt,
+    TypeFragEmoji,
+    TypeFragImage,
+    TypeFragItem,
+    TypeFragLink,
+    TypeFragment,
+    TypeFragmentUnknown,
+    TypeFragText,
+    TypeFragTiebaPlus,
+)
+from .user import UserInfo
+from .vimage import VirtualImage
+from .vote import VoteInfo
