70950275e669ba82203473624933fde83f9b5f3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 04:15:25 +0000

    Add package pyenphase/0.18.0/pyenphase-0.18.0.tar.gz

---
+++ packages/pyenphase/pyenphase-0.18.0.tar.gz/pyenphase-0.18.0/src/pyenphase/const.py
@@ -0,0 +1,19 @@
+# System Production
+URL_PRODUCTION_INVERTERS = "/api/v1/production/inverters"
+URL_PRODUCTION_V1 = "/api/v1/production"
+URL_PRODUCTION_JSON = "/production.json"
+URL_PRODUCTION = "/production"
+
+# Battery and Enpower Status
+URL_DRY_CONTACT_STATUS = "/ivp/ensemble/dry_contacts"
+URL_DRY_CONTACT_SETTINGS = "/ivp/ensemble/dry_contact_settings"
+URL_ENSEMBLE_INVENTORY = "/ivp/ensemble/inventory"
+URL_ENCHARGE_BATTERY = "/ivp/ensemble/power"
+URL_GRID_RELAY = "/ivp/ensemble/relay"
+URL_POWER_EXPORT = "/uvp/ss/pel_settings"
+URL_TARIFF = "/admin/lib/tariff"
+
+# Generator Configuration
+URL_GEN_CONFIG = "/ivp/ss/gen_config"
+URL_GEN_MODE = "/ivp/ss/gen_mode"
+URL_GEN_SCHEDULE = "/ivp/ss/gen_schedule"
