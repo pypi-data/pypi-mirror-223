bad485a8bb544c318b19144fe0172e7b5b99941e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 03:11:59 +0000

    Add package agora-redis-client/1.1.41/agora_redis_client-1.1.41-py2.py3-none-any.whl

---
+++ packages/agora-redis-client/agora_redis_client-1.1.41-py2.py3-none-any.whl/agora_redis_client/redis_client.py
@@ -0,0 +1,44 @@
+from agora_config import config
+from agora_logging import logger
+import redis
+
+class RedisClientSingleton(redis.Redis): 
+    _instance: redis.Redis = None
+    """
+    Connects to the Redis Server from Agora Core    
+    """
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super().__new__(cls, *args, **kwargs)
+        return cls._instance
+
+    def __init__(self): 
+        logger.info("redis_client connecting")
+        self.configure()                
+        super().__init__(host=self.server, port=self.port, decode_responses=True, socket_keepalive=True)                
+        if self.ping():
+            logger.info("redis_client connected")
+
+    def configure(self):
+        self.server = config["AEA2:RedisClient:Server"]
+        if self.server == "":
+            self.server = "localhost"
+
+        self.port = config["AEA2:RedisClient:Port"]
+        if self.port == "":
+            self.port = "6379"            
+        logger.info(f"--- Server: {self.server}")
+        logger.info(f"--- Port: {self.port}")        
+      
+
+      
+    def is_connected(self):
+        try:             
+            if self.ping():                
+                return True
+        except Exception as e:
+            logger.error(
+                f"Failed to connect to {self.server}:{self.port} :{str(e)}")
+            return False
+
+redis = RedisClientSingleton()
