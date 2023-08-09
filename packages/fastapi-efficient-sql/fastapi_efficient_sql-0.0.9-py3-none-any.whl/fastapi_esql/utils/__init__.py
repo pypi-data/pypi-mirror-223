9f310aef032f9593e4cb620ca6c1d64213630395
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 04:14:25 +0000

    Add package fastapi-efficient-sql/0.0.9/fastapi_efficient_sql-0.0.9-py3-none-any.whl

---
+++ packages/fastapi-efficient-sql/fastapi_efficient_sql-0.0.9-py3-none-any.whl/fastapi_esql/utils/__init__.py
@@ -0,0 +1,4 @@
+from .cursor_handler import CursorHandler
+from .decorator import timing
+from .metaclass import Singleton
+from .sqlizer import Cases, RawSQL, SQLizer
---
+++ packages/fastapi-efficient-sql/fastapi_efficient_sql-0.0.9-py3-none-any.whl/fastapi_esql/utils/cursor_handler.py
@@ -0,0 +1,64 @@
+from logging import Logger
+from typing import Any, Dict, List, Optional
+
+from tortoise import BaseDBAsyncClient
+
+
+class CursorHandler:
+
+    @classmethod
+    async def fetch_dicts(
+        cls,
+        sql: str,
+        conn: BaseDBAsyncClient,
+        logger: Logger,
+    ) -> Optional[List[Dict[str, Any]]]:
+        try:
+            return await conn.execute_query_dict(sql)
+        except Exception as e:
+            logger.exception(f"{e} SQL=>{sql}")
+            return None
+
+    @classmethod
+    async def fetch_one(
+        cls,
+        sql: str,
+        conn: BaseDBAsyncClient,
+        logger: Logger,
+    ) -> Optional[Dict[str, Any]]:
+        try:
+            dicts = await conn.execute_query_dict(sql)
+            if dicts:
+                return dicts[0]
+            return {}
+        except Exception as e:
+            logger.exception(f"{e} SQL=>{sql}")
+            return None
+
+    @classmethod
+    async def sum_row_cnt(
+        cls,
+        sql: str,
+        conn: BaseDBAsyncClient,
+        logger: Logger,
+    ) -> Optional[int]:
+        try:
+            row_cnt, _ = await conn.execute_query(sql)
+            return row_cnt
+        except Exception as e:
+            logger.exception(f"{e} SQL=>{sql}")
+            return None
+
+    @classmethod
+    async def exec_if_ok(
+        cls,
+        sql: str,
+        conn: BaseDBAsyncClient,
+        logger: Logger,
+    ) -> bool:
+        try:
+            await conn.execute_script(sql)
+            return True
+        except Exception as e:
+            logger.exception(f"{e} SQL=>{sql}")
+            return False
