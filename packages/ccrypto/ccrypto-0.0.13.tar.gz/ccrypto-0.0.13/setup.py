77c4485d199955e491987a035d1a3279d340f02a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 03:13:32 +0000

    Add package ccrypto/0.0.13/ccrypto-0.0.13.tar.gz

---
+++ packages/ccrypto/ccrypto-0.0.13.tar.gz/ccrypto-0.0.13/setup.py
@@ -0,0 +1,36 @@
+import pathlib
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+
+VERSION = '0.0.13'
+PACKAGE_NAME = 'ccrypto'
+AUTHOR = 'Pawel Lachowicz'
+AUTHOR_EMAIL = 'pawel.lachowicz@quantatrisk.com'
+URL = 'https://github.com/quantatrisk/ccrypto'
+
+LICENSE = 'MIT'
+DESCRIPTION = 'Crypto-Finance Python library for crypto markets and data analysis by QuantAtRisk.com'
+LONG_DESCRIPTION = (HERE / "README.md").read_text()
+LONG_DESC_TYPE = "text/markdown"
+
+INSTALL_REQUIRES = [
+      'numpy',
+      'pandas',
+      'python-binance==1.0.17',
+      'matplotlib',
+      'requests'
+]
+
+setup(name=PACKAGE_NAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      long_description_content_type=LONG_DESC_TYPE,
+      author=AUTHOR,
+      license=LICENSE,
+      author_email=AUTHOR_EMAIL,
+      url=URL,
+      install_requires=INSTALL_REQUIRES,
+      packages=find_packages()
+      )
