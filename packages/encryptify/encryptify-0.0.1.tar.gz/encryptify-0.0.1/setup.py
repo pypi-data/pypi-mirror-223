4a1419d9b7d448b04d76b8d0ba958fe0804992eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 03:13:22 +0000

    Add package encryptify/0.0.1/encryptify-0.0.1.tar.gz

---
+++ packages/encryptify/encryptify-0.0.1.tar.gz/encryptify-0.0.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.13'
+DESCRIPTION = 'Encryption of data to make it encrypted as well as visual'
+LONG_DESCRIPTION = 'A package that allows to encrypt and decrypt data.'
+
+# Setting up
+setup(
+    name="encryptify",
+    version=VERSION,
+    author="Akash Nath",
+    author_email="<anath5440@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'encryption', 'decryption', 'cryptography', 'visual effect', 'binary data'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
