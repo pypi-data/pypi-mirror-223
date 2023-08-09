081a99c36cd092338c22c57cfc075eb8713ae317
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 03:13:40 +0000

    Add package yente/3.6.1/yente-3.6.1-py3-none-any.whl

---
+++ packages/yente/yente-3.6.1-py3-none-any.whl/yente/routers/util.py
@@ -0,0 +1,20 @@
+from fastapi import Path, Query
+from fastapi import HTTPException
+
+from yente.data.dataset import Dataset
+from yente.data import get_catalog
+
+
+PATH_DATASET = Path(
+    description="Data source or collection name to be queries",
+    examples=["default"],
+)
+QUERY_PREFIX = Query("", min_length=1, description="Search prefix")
+
+
+async def get_dataset(name: str) -> Dataset:
+    catalog = await get_catalog()
+    dataset = catalog.get(name)
+    if dataset is None:
+        raise HTTPException(404, detail="No such dataset.")
+    return dataset
