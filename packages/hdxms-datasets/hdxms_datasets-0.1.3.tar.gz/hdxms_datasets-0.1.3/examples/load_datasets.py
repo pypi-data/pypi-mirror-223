8378b9a7d3151681e918120f146ecb9f8fffe334
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 04:14:13 +0000

    Add package hdxms-datasets/0.1.3/hdxms_datasets-0.1.3.tar.gz

---
+++ packages/hdxms-datasets/hdxms_datasets-0.1.3.tar.gz/hdxms_datasets-0.1.3/examples/load_datasets.py
@@ -0,0 +1,26 @@
+from hdxms_datasets import DataVault
+
+# Creating a DataVault without giving a cache path name uses $home/.hdxms_datasets by default
+vault = DataVault()
+
+# Download a specific HDX dataset
+vault.fetch_dataset("20221007_1530_SecA_Krishnamurthy")
+
+# Load the dataset
+ds = vault.load_dataset("20221007_1530_SecA_Krishnamurthy")
+
+# Describe the dataset
+print(ds.describe())
+
+## Output:
+# >>> SecA_monomer:
+# >>>  FD_control: 'Total peptides: 188, timepoints: 1'
+# >>>  experiment: 'Total peptides: 1273, timepoints: 7'
+# >>>  metadata: 'Temperature: 20.0 C, pH: 7.5'
+
+# Load the FD control of the 'SecA_monomer' state .
+fd_control = ds.load_peptides("SecA_monomer", "FD_control")
+
+# States can also be referenced by their index, used here to load the peptides corresponding to
+# the experiment.
+peptides = ds.load_peptides(0, "experiment")
---
+++ packages/hdxms-datasets/hdxms_datasets-0.1.3.tar.gz/hdxms_datasets-0.1.3/examples/load_from_yaml.py
@@ -0,0 +1,18 @@
+from hdxms_datasets import DataFile, HDXDataSet
+from pathlib import Path
+import yaml
+
+from hdxms_datasets.process import parse_data_files
+
+test_pth = Path("../tests").resolve()
+data_pth = test_pth / "datasets"
+data_id = "20221007_1530_SecA_Krishnamurthy"
+
+hdx_spec = yaml.safe_load((data_pth / data_id / "hdx_spec.yaml").read_text())
+metadata = yaml.safe_load((data_pth / data_id / "metadata.yaml").read_text())
+
+#%%
+
+dataset = HDXDataSet.from_spec(hdx_spec, data_dir=data_pth / data_id, metadata=metadata)
+
+print(dataset.describe())
