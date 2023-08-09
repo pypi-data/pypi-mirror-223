1715bfe586e02d8b97c9fe593a1575dd35b92aea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 03:11:25 +0000

    Add package bollettinotest/0.0.1/bollettinotest-0.0.1-py3-none-any.whl

---
+++ packages/bollettinotest/bollettinotest-0.0.1-py3-none-any.whl/complex_library/__init__.py
@@ -0,0 +1,2 @@
+from complex_library.complex_functions import *
+from complex_library.second_script import *
---
+++ packages/bollettinotest/bollettinotest-0.0.1-py3-none-any.whl/complex_library/second_script.py
@@ -0,0 +1,4 @@
+from complex_library.complex_functions import complex_function
+
+def second_function(a):
+    return a + complex_function(a,2)
