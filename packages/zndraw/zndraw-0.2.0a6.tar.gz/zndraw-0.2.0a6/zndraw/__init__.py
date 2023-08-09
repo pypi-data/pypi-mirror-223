c41fc2ddff0567543bea3966418ad101c8bece2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 03:12:31 +0000

    Add package zndraw/0.2.0a6/zndraw-0.2.0a6.tar.gz

---
+++ packages/zndraw/zndraw-0.2.0a6.tar.gz/zndraw-0.2.0a6/zndraw/__init__.py
@@ -0,0 +1,9 @@
+import importlib.metadata
+
+from zndraw.app import app
+from zndraw.main import ZnDraw
+from zndraw.view import view
+
+__all__ = ["app", "ZnDraw", "view"]
+
+__version__ = importlib.metadata.version("zndraw")
