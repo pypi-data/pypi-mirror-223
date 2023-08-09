c5219173851d1bd31bc63bd2b0f0213e2db1adab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 06:18:31 +0000

    Add package minto/0.6.1/minto-0.6.1-py3-none-any.whl

---
+++ packages/minto/minto-0.6.1-py3-none-any.whl/minto/typing.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+from typing import Any, Dict
+
+from typing_extensions import TypeAlias
+
+# containers
+ArtifactDataType: TypeAlias = Dict[int, Dict[str, Any]]
