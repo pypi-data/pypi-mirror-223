bd54cc0508f4c13c8c2b53e02db13040b959d28d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 03:11:20 +0000

    Add package saymynamefromnhiennd/0.1/saymynamefromnhiennd-0.1.tar.gz

---
+++ packages/saymynamefromnhiennd/saymynamefromnhiennd-0.1.tar.gz/saymynamefromnhiennd-0.1/saymynamefromnhiennd/__init__.py
@@ -0,0 +1,2 @@
+def say(ur_name):
+    print("Your name is %s", ur_name)
---
+++ packages/saymynamefromnhiennd/saymynamefromnhiennd-0.1.tar.gz/saymynamefromnhiennd-0.1/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='saymynamefromnhiennd',  # How you named your package folder (MyLib)
+    packages=['saymynamefromnhiennd'],  # Chose the same as "name"
+    version='0.1',  # Start with a small number and increase it with every change you make
+    license='MIT',
+    # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+    description='SAY MY NAME PACKAGE',  # Give a short description about your library
+    author='Nhien Ngo D',  # Type in your name
+    author_email='ngodinhnhien123@gmail.com',  # Type in your E-Mail
+    url='https://github.com/nhien-ngod-otsv/saymyname',
+    # Provide either the link to your github or to your website
+    download_url='https://github.com/nhien-ngod-otsv/saymyname/archive/v0.1.tar.gz',
+    # I explain this later on
+    keywords=['SOME', 'MEANINGFULL', 'KEYWORDS'],  # Keywords that define your package best
+)
