8abbb62880cf3c11caee6324805cd584f7a57a3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 06:18:40 +0000

    Add package m-json-db/0.1.2/m_json_db-0.1.2-py3-none-any.whl

---
+++ packages/m-json-db/m_json_db-0.1.2-py3-none-any.whl/m_json_db/__init__.py
@@ -0,0 +1,22 @@
+import json
+
+class Db:
+    def __init__(self, path, *, default):
+        self.path = path
+        try:
+            with open(path, encoding="utf-8") as f:
+                self.value = json.loads(f.read())
+        except FileNotFoundError:
+            self.value = default
+
+    def __enter__(self):
+        pass
+
+    def __exit__(self, *args):
+        with open(self.path, "w", encoding="utf-8") as f:
+            f.write(json.dumps(self.value, ensure_ascii=False))
+
+    def __getattr__(self, attrname):
+        return getattr(self.value, attrname)
+
+__all__ = ["Db"]
