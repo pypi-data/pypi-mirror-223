ccabc054323e9af361fb14848c81f340dbf57866
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 05:08:11 +0000

    Add package calculaareaedu/0.0.1.1/calculaareaedu-0.0.1.1.tar.gz

---
+++ packages/calculaareaedu/calculaareaedu-0.0.1.1.tar.gz/calculaareaedu-0.0.1.1/src/calculaareaedu.py
@@ -0,0 +1,10 @@
+
+class CalculaArea():
+    def __init__(self, base, altura):
+        self._base = base
+        self._altura = altura
+
+    def calculaArea(self):
+        area = self._base * self._altura
+        return area
+
