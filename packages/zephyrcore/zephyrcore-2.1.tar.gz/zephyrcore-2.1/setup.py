806b93e0fb94e9f017e0f19be8dc48ec815d812f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 05:07:18 +0000

    Add package zephyrcore/2.1/zephyrcore-2.1.tar.gz

---
+++ packages/zephyrcore/zephyrcore-2.1.tar.gz/zephyrcore-2.1/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import setuptools
+
+setuptools.setup(name='zephyrcore',
+                 version='2.1',
+                 description='Python Distribution Utilities',
+                 author='QM Core Team',
+                 author_email='business.agility@ab-inbev.com',
+                 long_description=open('README.md').read(),
+                 long_description_content_type='text/markdown',
+                 url='https://www.python.org/sigs/distutils-sig/',
+                 packages=setuptools.find_packages(),
+                 python_requires='>=3.9',
+                 py_modules=["zephyrcore"],
+                 install_requires=['requests', 'atlassian-python-api']  # Optional
+                 )
