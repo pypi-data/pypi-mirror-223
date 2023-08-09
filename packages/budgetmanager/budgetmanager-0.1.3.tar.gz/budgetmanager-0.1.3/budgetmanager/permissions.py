60279252489b584a6dbcbec7c2f40f09cb426c36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 05:08:35 +0000

    Add package budgetmanager/0.1.3/budgetmanager-0.1.3.tar.gz

---
+++ packages/budgetmanager/budgetmanager-0.1.3.tar.gz/budgetmanager-0.1.3/budgetmanager/permissions.py
@@ -0,0 +1,6 @@
+# pylint: disable=unused-argument
+from rest_framework import permissions
+
+class IsOwner(permissions.BasePermission):
+    def has_obj_permission(self, request, view, obj):
+        return obj.user == request.user
