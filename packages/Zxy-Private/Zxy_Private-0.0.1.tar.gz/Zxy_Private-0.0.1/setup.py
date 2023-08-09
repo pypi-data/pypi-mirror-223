ea566d5e39727b9e944119beb93b483f6c3f8a25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 03:10:32 +0000

    Add package Zxy-Private/0.0.1/Zxy_Private-0.0.1.tar.gz

---
+++ packages/Zxy-Private/Zxy_Private-0.0.1.tar.gz/Zxy_Private-0.0.1/setup.py
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8 -*-
+import setuptools
+
+setuptools.setup(
+    name="Zxy_Private",
+    version = "0.0.1",
+    author="AnQing",
+    description="无聊试一下",
+    long_description_content_type="text/markdown",
+    url="https://github.com/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
