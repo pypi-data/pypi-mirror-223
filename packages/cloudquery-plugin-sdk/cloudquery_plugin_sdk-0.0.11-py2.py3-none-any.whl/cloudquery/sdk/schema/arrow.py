07dadd654bf0e4aa346ba25090fafb04eef94f88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 04:14:50 +0000

    Add package cloudquery-plugin-sdk/0.0.11/cloudquery_plugin_sdk-0.0.11-py2.py3-none-any.whl

---
+++ packages/cloudquery-plugin-sdk/cloudquery_plugin_sdk-0.0.11-py2.py3-none-any.whl/cloudquery/sdk/schema/arrow.py
@@ -0,0 +1,9 @@
+METADATA_UNIQUE = b"cq:extension:unique"
+METADATA_PRIMARY_KEY = b"cq:extension:primary_key"
+METADATA_CONSTRAINT_NAME = b"cq:extension:constraint_name"
+METADATA_INCREMENTAL = b"cq:extension:incremental"
+
+METADATA_TRUE = b"true"
+METADATA_FALSE = b"false"
+METADATA_TABLE_NAME = b"cq:table_name"
+METADATA_TABLE_DESCRIPTION = b"cq:table_description"
