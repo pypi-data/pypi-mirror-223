55165dd2d806f839d9dd5cb65c3ce125f37cfb0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 04:14:05 +0000

    Add package somatorioInteiros/0.0.4/somatorioInteiros-0.0.4.tar.gz

---
+++ packages/somatorioInteiros/somatorioInteiros-0.0.4.tar.gz/somatorioInteiros-0.0.4/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open("README.md", "r") as arq:
+    readme = arq.read()
+
+setup(name='somatorioInteiros',
+    version='0.0.4',
+    license='MIT License',
+    author='Joao Neto',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email='joaonetoprivado2001@ufpi.edu.br',
+    keywords='soma inteiros',
+    description=u'Somador de numeros inteiros em uma lista',
+    packages=['src'],
+    install_requires=[],)
