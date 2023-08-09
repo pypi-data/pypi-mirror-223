ac88dab3b7f49c12f536cc2d8b852555bb61f0ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 03:10:37 +0000

    Add package coca-websocket/0.1.1/coca_websocket-0.1.1.tar.gz

---
+++ packages/coca-websocket/coca_websocket-0.1.1.tar.gz/coca_websocket-0.1.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['coca_websocket']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'coca-websocket',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Hiroyuki Ikuno',
+    'author_email': 'sam2kaikaramegusuri@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9',
+}
+
+
+setup(**setup_kwargs)
