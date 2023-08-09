a6717bf1a62dbc7c5d4acfddb67662f2fa453684
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 03:13:12 +0000

    Add package nzpyida/0.9.4/nzpyida-0.9.4.tar.gz

---
+++ packages/nzpyida/nzpyida-0.9.4.tar.gz/nzpyida-0.9.4/nzpyida/__init__.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#-----------------------------------------------------------------------------
+# Copyright (c) 2015-2023, IBM Corp.
+# All rights reserved.
+#
+# Distributed under the terms of the BSD Simplified License.
+#
+# The full license is in the LICENSE file, distributed with this software.
+#-----------------------------------------------------------------------------
+
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+from future import standard_library
+standard_library.install_aliases()
+from .base import IdaDataBase
+from .frame import IdaDataFrame
+from .series import IdaSeries
+from .geo_frame import IdaGeoDataFrame
+from .geo_series import IdaGeoSeries
+
+
+__all__ = ['sampledata', 'aggregation', 'base', 'exceptions', 'filtering', 
+           'frame', 'indexing', 'internals', 'series', 'sql', 'statistics', 
+           'utils', 'analytics', 'geo_frame', 'geo_series']
