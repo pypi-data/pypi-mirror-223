527c3af4f19c7ce7e0228e8c3a9cfd33a52d4350
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 05:09:50 +0000

    Add package django-nested-modals/0.0.23/django-nested-modals-0.0.23.tar.gz

---
+++ packages/django-nested-modals/django-nested-modals-0.0.23.tar.gz/django-nested-modals-0.0.23/django_modals/widgets/colour_picker.py
@@ -0,0 +1,23 @@
+from django.forms.widgets import TextInput
+
+from django_modals.fields import FieldEx
+
+
+class ColourPickerFieldEx(FieldEx):
+    @staticmethod
+    def get_prepended_appended_template_name(template_pack):
+        return "django_modals/widgets/colour_picker_append.html"
+
+
+class ColourPickerWidget(TextInput):
+    template_name = 'django_modals/widgets/colour_picker.html'
+    formset_field_template = "django_modals/widgets/colour_picker_append.html"
+
+    crispy_kwargs = {'field_class': 'col-sm-12 col-md-4 col-lg-3',
+                     'appended_text': ' ',
+                     'input_size': 'input-group-sm'}
+    crispy_field_class = ColourPickerFieldEx
+
+    def get_context(self, name, value, attrs):
+        context = super().get_context(name, value, attrs)
+        return context
