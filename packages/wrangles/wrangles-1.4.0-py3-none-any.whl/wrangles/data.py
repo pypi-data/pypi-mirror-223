7923a56a35984d600cabac61a6c1b438c8ebdf51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 05:07:30 +0000

    Add package wrangles/1.4.0/wrangles-1.4.0-py3-none-any.whl

---
+++ packages/wrangles/wrangles-1.4.0-py3-none-any.whl/wrangles/data.py
@@ -0,0 +1,57 @@
+"""
+Functions for interacting with user and app data
+"""
+import requests as _requests
+from . import config as _config
+from . import auth as _auth
+
+
+class user():
+    """
+    Get user data
+    """
+
+    def models(type: str = None) -> list:
+        """
+        Get a list of the user's models
+
+        :param type: (Optional) Specify the type of models. 'classify' or 'extract'
+        :returns: List of user's model, each a dict of properties.
+        """
+        params = {}
+        if type: params['type'] = type
+        response = _requests.get(f'{_config.api_host}/user/models', params=params, headers={'Authorization': f'Bearer {_auth.get_access_token()}'})
+        results = response.json()
+        return results
+
+
+def model(id: str):
+    """
+    Get a model definition
+    :param id: model ID
+    :returns: Dict of model properties
+    """
+    params = {'id': id}
+    response = _requests.get(f'{_config.api_host}/model/metadata', params=params, headers={'Authorization': f'Bearer {_auth.get_access_token()}'})
+    results = response.json()
+    return results
+
+
+def model_data(id: str, purpose: str) -> list:
+    """
+    Get the training data for a model
+
+    :param id: Model ID
+    :param purpose: classify, extract, standardize
+    :return: Model data as a list of lists
+    """
+    params = {'model_id': id, 'type': purpose}
+    response = _requests.get(f'{_config.api_host}/model/content', params=params, headers={'Authorization': f'Bearer {_auth.get_access_token()}'})
+    if response.ok:
+        results = response.json()
+    elif response.status_code in [401, 403]:
+        raise RuntimeError('Not able to validate access, check wrangle to ensure accessibility')
+    else:
+        raise RuntimeError('Model data not found')
+
+    return results['Data']
