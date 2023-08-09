fbc972542e9c14583af1fa54dda1639ca5b8f497
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-09 03:13:36 +0000

    Add package sim-sensors/0.1/sim_sensors-0.1.tar.gz

---
+++ packages/sim-sensors/sim_sensors-0.1.tar.gz/sim_sensors-0.1/setup.py
@@ -0,0 +1,24 @@
+
+from distutils.core import setup
+setup(
+  name = 'sim_sensors',
+  packages = ['sim_sensors'],
+  version = '0.1',
+  license='MIT',
+  description = 'A collection of classes that simulate IoT sensors',
+  author = 'Ivan Šarić / Path Variable',
+  author_email = 'ivan@path-variable.com',
+  url = 'https://www.path-variable.com',
+  keywords = ['simulated', 'sensors', 'iot'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
